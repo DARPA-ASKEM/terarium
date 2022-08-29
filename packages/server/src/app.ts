82264fa760345652d5330c254f1efc95f82fbff7
bcodrington <bcodrington@uncharted.software>
2022-08-29 17:00:29 -0400

    Configure server to be able to access shared

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -3,12 +3,25 @@ import Router from 'koa-router';
 import logger from 'koa-logger';
 import json from 'koa-json';
 
+import { MyType, add } from 'shared';
+
 const app = new Koa();
 const router = new Router();
 
 router.get('/', async (ctx: Koa.ParameterizedContext, next: Koa.Next) => {
   ctx.body = {
     msg: 'TERArium'
+  };
+  await next();
+});
+
+router.get('/test', async (ctx: Koa.ParameterizedContext, next: Koa.Next) => {
+  const test: MyType = {
+    name: 'test'
+  };
+  let a = add(1, 2);
+  ctx.body = {
+    msg: test.name + ", " + a
   };
   await next();
 });
