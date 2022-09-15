d1c2c8ec4603511e91746849df2e72ffa49c03a7
Charles Coleman <bigglesandginger@gmail.com>
2022-09-15 16:34:30 -0400

    remove launch file

--- kubernetes/local/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f gateway-postgres-service.yaml -f gateway-postgres-deployment.yaml -f gateway-keycloak-realm.yaml -f gateway-keycloak-service.yaml -f gateway-keycloak-deployment.yaml -f gateway-httpd-config.yaml -f gateway-httpd-htdocs.yaml -f gateway-httpd-service.yaml -f gateway-httpd-deployment.yaml
-
-kubectl get po,svc,configMap
