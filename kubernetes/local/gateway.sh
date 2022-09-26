83ac6bdbff5fe4d68bb1666468faac850e42aedc
Charles Coleman <bigglesandginger@gmail.com>
2022-09-26 10:35:57 -0400

    Detect if running on apple silicon and not pull images

--- kubernetes/local/gateway.sh
+++ kubernetes/local/gateway.sh
@@ -2,6 +2,21 @@
 
 
 if [[ ${1} == "up" ]]; then
+
+    if [ -f `which uname` ]; then
+        if [ `uname -p` == "arm" ]; then
+            # Apple Silicon
+            APPLE_SILICON=1
+        fi
+    fi
+
+    if [ ${APPLE_SILICON:-0} -eq 0 ]; then
+        docker pull docker.uncharted.software/auth/httpd-openidc:dev-1.0.1
+        docker pull docker.uncharted.software/auth/keycloak:dev-1.0.0
+    else
+        echo "not pulling latest images as Apple Silicon httpd-openidc has not been pushed to the repo"
+    fi
+
     kubectl apply -f gateway-postgres-service.yaml -f gateway-postgres-deployment.yaml -f gateway-keycloak-realm.yaml -f gateway-keycloak-service.yaml -f gateway-keycloak-deployment.yaml -f gateway-httpd-config.yaml -f gateway-httpd-htdocs.yaml -f gateway-httpd-service.yaml -f gateway-httpd-deployment.yaml
 
     exit 0
