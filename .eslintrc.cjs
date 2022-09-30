43566cb15da92a1f18ee676eda9d66597189b666
Ahmed <amostafa@uncharted.software>
2022-09-30 17:26:39 -0400

    add more lint rules -- to be reviewed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,48 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
+	"env": {
+    "browser": true,
+    "es2021": true
+  },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/eslint-config-airbnb-with-typescript',
+		"eslint:recommended",
+		"plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+		"airbnb-base",
     'prettier' // Turns off the formatting rules from the linter since formatting is handled by prettier
   ],
   parser: 'vue-eslint-parser',
+	"parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "parser": "@typescript-eslint/parser"
+  },
+  "settings": {
+    "import/resolver": {
+      "typescript": {} // this loads <rootdir>/tsconfig.json to eslint
+    }
+  },
 	rules: {
+		"import/extensions": [
+			"error",
+			"ignorePackages",
+			{
+				"": "never",
+				"js": "never",
+				"jsx": "never",
+				"ts": "never",
+				"tsx": "never"
+			}
+	  ],
+		// to enforce using type for object type definitions, can be type or interface
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+		// others
 		'vue/multi-word-component-names': 'off',
 		'vuejs-accessibility/click-events-have-key-events': 'off',
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		"import/no-unresolved": "off",
+		"vue/no-v-html": "off"
 	}
 };
