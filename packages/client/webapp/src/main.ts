3365d117c427205bfd888b8b80443c28a6e0ea3d
Yohann Paris <github@yohannparis.com>
2022-09-30 14:02:41 -0400

    Move the stylesheet

--- packages/client/webapp/src/main.ts
+++ packages/client/webapp/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import router from './router';
-import './style.css';
 import App from './App.vue';
 
 const app = createApp(App);
