e501c64109f7d34edad144bb057dc88a1d34fc4a
mwdchang <mwdchang@gmail.com>
2022-10-05 21:46:44 -0400

    fix graph-scaffolder package spec, stub ta2-playground view

--- packages/client/webapp/src/router/index.ts
+++ packages/client/webapp/src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import ResponsiveMatrixCells from '@/components/ResponsiveMatrixCells.vue';
 import DataExplorer from '@/views/DataExplorer.vue';
+import TA2Playground from '@/views/TA2Playground.vue';
 
 const routes = [
 	{ path: '/', component: Home },
 	{ path: '/results', component: ResponsiveMatrixCells },
-	{ path: '/explorer', component: DataExplorer }
+	{ path: '/explorer', component: DataExplorer },
+	{ path: '/ta2-playground', component: TA2Playground }
 ];
 
 const router = createRouter({
---
+++ packages/client/webapp/src/views/TA2Playground.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { BasicRenderer } from 'graph-scaffolder';
+
+console.log(BasicRenderer);
+</script>
+
+<template>
+	<div>Hello world</div>
+</template>
