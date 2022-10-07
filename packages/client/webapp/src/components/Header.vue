4758f0c7ea34c41e43ed10311aaa5942bd0db250
Yohann Paris <github@yohannparis.com>
2022-10-07 16:30:47 -0400

    Add the Project name on the header

--- packages/client/webapp/src/components/Header.vue
+++ packages/client/webapp/src/components/Header.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
 import Button from '@/components/Button.vue';
 import IconLogin16 from '@carbon/icons-vue/es/login/16';
+import { useRouter } from 'vue-router';
 import { useAuthStore } from '../stores/auth';
 
 const auth = useAuthStore();
+const router = useRouter();
 
 const login = () => window.location.assign('http://localhost:8078');
+const homepage = () => router.push('/');
+
+const projectName = 'Name of the project that can be long for clarity and precision';
 </script>
 
 <template>
 	<nav>
-		<img src="@assets/images/logo.png" height="32" width="128" alt="logo" />
+		<header>
+			<img src="@assets/images/logo.png" height="32" width="128" alt="logo" />
+			<p>
+				<a @click="homepage">Projects</a>
+				<span>{{ projectName }}</span>
+			</p>
+		</header>
+
 		<Button v-if="!auth.isAuthenticated" @click="login"> Login <IconLogin16 /> </Button>
 	</nav>
 </template>
@@ -25,6 +37,24 @@ nav {
 }
 
 header {
+	align-items: center;
+	display: flex;
 	font-size: var(--un-font-xlarge);
+	gap: 2em;
+	justify-content: space-around;
+}
+
+header p > * + *::before {
+	content: '>';
+	margin: 0 1em;
+}
+
+header a {
+	text-decoration: underline;
+}
+
+header a:hover,
+header a:focus {
+	color: var(--un-color-accent-dark);
 }
 </style>
