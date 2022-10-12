55e74885adc873c486a9f3970e729efa55daebd4
Shawn Yama <syama@uncharted.software>
2022-10-12 13:38:03 -0400

    starting cards and carousel

--- packages/client/webapp/src/views/Home.vue
+++ packages/client/webapp/src/views/Home.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import Button from '@/components/Button.vue';
+import IconAddFilled32 from '@carbon/icons-vue/es/add--filled/32';
 import { uncloak } from '../utils/uncloak';
+
+const dummyProjects = Array(8).fill('New Project');
 
 async function apiCall(type = '') {
 	if (type === 'user') {
@@ -22,29 +25,63 @@ async function apiCall(type = '') {
 
 <template>
 	<section>
-		<p>Test API calls/Home placeholder</p>
-		<div>
-			<Button>Normal</Button>
-			<Button action>Action</Button>
-			<Button danger>Danger</Button>
-			<Button info>Info</Button>
+		<h2>Projects</h2>
+		<div class="project-carousel">
+			<div v-for="{ project, p } in dummyProjects" v-bind:key="p">
+				{{ project }}
+				<IconAddFilled32 />
+			</div>
 		</div>
-		<div>
-			<Button @click="apiCall('user')">User Call</Button>
-			<Button @click="apiCall('admin')">Admin Call</Button>
+		<div class="test">
+			<h3>Recent</h3>
+			<p>Test API calls/Home placeholder</p>
+			<div>
+				<Button>Normal</Button>
+				<Button action>Action</Button>
+				<Button danger>Danger</Button>
+				<Button info>Info</Button>
+			</div>
+			<div>
+				<Button @click="apiCall('user')">User Call</Button>
+				<Button @click="apiCall('admin')">Admin Call</Button>
+			</div>
 		</div>
 	</section>
 </template>
 
 <style scoped>
+h2 {
+	font-size: 2rem;
+	font-weight: 600;
+	display: block;
+}
+
 section {
-	margin: 0 auto;
-	max-width: 1280px;
-	padding: 2rem;
+	margin: 3rem auto;
+	width: 80%;
 	text-align: center;
 }
 
-div {
+.project-carousel {
+	display: flex;
+	overflow-x: scroll;
+	overflow-y: hidden;
+}
+
+.project-carousel div {
+	height: 120px;
+	width: 300px;
+	border: 1px solid var(--un-color-black-20);
+	background-color: var(--un-color-black-5);
+	border-radius: 10px;
+	margin: 0.5rem;
+}
+
+.test {
+	margin-top: 4rem;
+}
+
+.test div {
 	display: flex;
 	gap: 1em;
 	margin: 2em;
