0cdc8d86fb75983cfa3d81ab30e7b1ccc65e8599
Yohann Paris <github@yohannparis.com>
2022-10-12 12:20:54 -0400

    add test for the side panel

---
+++ packages/client/webapp/tests/component/model-sidebar-panel.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/experimental-ct-vue';
+import ModelSidebarPanel from '@/components/sidebar-panel/model-sidebar-panel.vue';
+
+test.describe('test ModelSidebarPanel component', () => {
+	test('should display the correct header', async ({ mount }) => {
+		const component = await mount(ModelSidebarPanel);
+		const header = await component.locator('header');
+
+		await expect(header).toBe('Model Space');
+	});
+
+	test('should display the button to open model from code', async ({ mount }) => {
+		const component = await mount(ModelSidebarPanel);
+		const goToTheia = await component.locator('button');
+		await expect(goToTheia).toBeVisible();
+	});
+});
