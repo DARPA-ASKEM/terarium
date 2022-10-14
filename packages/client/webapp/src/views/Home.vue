2a15bff93612646cf44252a047d45857aab7728d
Shawn Yama <syama@uncharted.software>
2022-10-14 19:33:47 -0400

    add name and action to projects

--- packages/client/webapp/src/views/Home.vue
+++ packages/client/webapp/src/views/Home.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Button from '@/components/Button.vue';
-import IconTime32 from '@carbon/icons-vue/es/time/32';
-import IconFire32 from '@carbon/icons-vue/es/fire/32';
-import IconCoronavirus32 from '@carbon/icons-vue/es/coronavirus/32';
-import IconAddFilled32 from '@carbon/icons-vue/es/add--filled/32';
+import IconList24 from '@carbon/icons-vue/es/list/24';
+// import IconTime32 from '@carbon/icons-vue/es/time/32';
+// import IconFire32 from '@carbon/icons-vue/es/fire/32';
+// import IconCoronavirus32 from '@carbon/icons-vue/es/coronavirus/32';
+// import IconAddFilled32 from '@carbon/icons-vue/es/add--filled/32';
 import { uncloak } from '../utils/uncloak';
 
-const dummyProjects = Array(8).fill({ name: 'New Project' });
+const projectCategories: string[] = ['Recents', 'Trending', 'Epidemiology'];
+const dummyProjects: string[] = Array(8).fill('Project');
 
 async function apiCall(type = '') {
 	if (type === 'user') {
@@ -29,25 +31,18 @@ async function apiCall(type = '') {
 <template>
 	<section>
 		<h2>Projects</h2>
-		<h3><IconTime32 />Recents</h3>
-		<div class="project-carousel">
-			<div v-for="{ project, p } in dummyProjects" v-bind:key="p">
-				{{ project.name }}
-				<!-- <IconAddFilled32 /> -->
-			</div>
-		</div>
-		<h3><IconFire32 />Trending</h3>
-		<div class="project-carousel">
-			<div v-for="{ project, p } in dummyProjects" v-bind:key="p">
-				{{ project }}
-				<IconAddFilled32 />
-			</div>
-		</div>
-		<h3><IconCoronavirus32 />Epidemiology</h3>
-		<div class="project-carousel">
-			<div v-for="{ project, p } in dummyProjects" v-bind:key="p">
-				{{ project }}
-				<IconAddFilled32 />
+		<div v-for="(category, c) in projectCategories" :key="c">
+			<h3><IconTime32 />{{ category }}</h3>
+			<div class="project-carousel">
+				<div class="project" v-for="(project, p) in dummyProjects" :key="p">
+					<!-- <IconAddFilled32 v-if="p === 0" /> -->
+					<div class="details">
+						<div class="name">{{ project }} {{ p }}</div>
+						<div class="actions">
+							<IconList24 />
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="test">
@@ -67,9 +62,15 @@ async function apiCall(type = '') {
 </template>
 
 <style scoped>
+section {
+	color: var(--un-color-black-80);
+	margin: 1rem auto;
+	width: 80%;
+	text-align: center;
+}
+
 h2,
 h3 {
-	color: var(--un-color-black-80);
 	font-weight: 500;
 	text-align: left;
 }
@@ -79,6 +80,7 @@ h2 {
 	font-size: 2.5rem;
 	padding: 1rem 0 1rem 0;
 	margin-bottom: 0.5rem;
+	padding-left: 1rem;
 }
 
 h3 {
@@ -94,29 +96,49 @@ h3 svg {
 	color: var(--un-color-accent);
 }
 
-section {
-	margin: 1rem auto;
-	width: 75%;
-	text-align: center;
-}
-
 .project-carousel {
 	display: flex;
 	overflow-x: scroll;
 	overflow-y: hidden;
 }
 
-.project-carousel div {
+.project-carousel .project {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
 	height: 15rem;
 	min-width: 20rem;
 	border: 1px solid var(--un-color-black-20);
 	background-color: var(--un-color-black-5);
 	border-radius: 10px;
-	margin: 0.25rem 0.5rem;
+	margin: 0.5rem;
+	transition: 0.2s;
+	text-align: left;
 }
 
-.project-carousel div svg {
-	color: var(--un-color-black-30);
+.project-carousel .project .details {
+	border-top: 1px solid var(--un-color-black-20);
+}
+
+.details .name,
+.details .actions {
+	font-weight: 500;
+	padding: 0.5rem 1rem;
+}
+
+.details .actions {
+	/* display: none; */
+	padding: 0 0 0.25rem 1rem;
+}
+
+.details .actions svg:hover {
+	color: var(--un-color-accent);
+	cursor: pointer;
+}
+
+.project-carousel .project svg {
+	color: var(--un-color-black-80);
+	margin: auto;
 }
 
 .test {
@@ -125,8 +147,8 @@ section {
 
 .test div {
 	display: flex;
-	gap: 1rem;
-	margin: 2rem;
+	gap: 1em;
+	margin: 2em;
 	justify-content: center;
 }
 </style>
