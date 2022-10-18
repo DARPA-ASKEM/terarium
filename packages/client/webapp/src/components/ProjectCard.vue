398852414e1554581ba9fc7208aae08aa031a64e
Shawn Yama <syama@uncharted.software>
2022-10-17 20:01:05 -0400

    add comment

--- packages/client/webapp/src/components/ProjectCard.vue
+++ packages/client/webapp/src/components/ProjectCard.vue
@@ -10,6 +10,7 @@ interface Props {
 	p: Number;
 }
 
+// Not sure how to get rid of these underlines - I am following these rules: https://vuejs.org/api/sfc-script-setup.html#typescript-only-features
 const props = withDefaults(defineProps<Props>(), {
 	category: '',
 	projectName: '',
