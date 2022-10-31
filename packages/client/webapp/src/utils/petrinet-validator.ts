07dd597c42a0a5571053e8ca43e19c3dcacc7a77
Shawn Yama <syama@uncharted.software>
2022-10-31 18:28:27 -0400

    just have to make sure there aren't multiple petrinet bodies

--- packages/client/webapp/src/utils/petrinet-validator.ts
+++ packages/client/webapp/src/utils/petrinet-validator.ts
@@ -1,4 +1,4 @@
-import { IGraph } from 'graph-scaffolder';
+import { IGraph, IEdge /* traverseGraph */ } from 'graph-scaffolder';
 
 interface Petrinet {
 	S: State[];
@@ -22,6 +22,11 @@ interface Output {
 	os: number;
 }
 
+const extractEdgesAttribute = (edges: IEdge[], transitionOrState: string, sourceOrTarget: string) =>
+	edges
+		.filter((edge) => edge[sourceOrTarget].charAt(0) === transitionOrState)
+		.map((node) => node[sourceOrTarget]);
+
 export const petrinetValidator = (
 	graph: IGraph,
 	petrinet: Petrinet,
@@ -30,21 +35,38 @@ export const petrinetValidator = (
 	const { edges } = graph;
 	let isValidPetrinet: boolean = true;
 
-	// Needs at least 2 edges to point to a state after one transition
-	if ((edges.length < 2 && isBoundedPetrinet) || edges.length === 0) isValidPetrinet = false;
+	// Requires at least one edge
+	if (edges.length < 1) isValidPetrinet = false;
 
-	if (isValidPetrinet && isBoundedPetrinet) {
+	if (isValidPetrinet) {
 		const transitionNodeNames: string[] = petrinet.T.map((transition) => transition.tname);
-		const transitionEdgeSources: string[] = edges
-			.filter((edge) => edge.source.charAt(0) === 't')
-			.map((node) => node.source);
+		const nodeNames: string[] = [...transitionNodeNames, ...petrinet.S.map((state) => state.sname)]; // Append state names
+		const edgeSourcesAndTargets: string[] = [
+			...edges.map((edge) => [edge.source, edge.target])
+		].flat();
 
-		// Check if every transition node is pointing at at least one state node
-		for (let i = 0; i < transitionNodeNames.length; i++) {
-			// If a transition is recognized as a source, it is pointing at another node
-			if (!transitionEdgeSources.includes(transitionNodeNames[i])) {
+		// Check that every node is at least either a source or a target
+		for (let i = 0; i < nodeNames.length; i++) {
+			if (!edgeSourcesAndTargets.includes(nodeNames[i])) {
 				isValidPetrinet = false;
 				break;
+			}
+		}
+
+		if (isValidPetrinet && isBoundedPetrinet) {
+			// Check if every transition node is bounded by state nodes
+			const transitionEdgeSources = extractEdgesAttribute(edges, 't', 'source');
+			const transitionEdgeTargets = extractEdgesAttribute(edges, 't', 'target');
+
+			for (let i = 0; i < transitionNodeNames.length; i++) {
+				// If a transition is recognized as a source and a target it is bounded
+				if (
+					!transitionEdgeSources.includes(transitionNodeNames[i]) ||
+					!transitionEdgeTargets.includes(transitionNodeNames[i])
+				) {
+					isValidPetrinet = false;
+					break;
+				}
 			}
 		}
 	}
@@ -61,14 +83,15 @@ export const petrinetValidator = (
 				isValidPetrinet = false;
 				break;
 			}
-
-			// Check if all nodes are connected somehow
 		}
 	}
 
-	console.log(graph);
-	console.table(graph.nodes);
-	console.table(graph.edges);
+	// Make sure there aren't multiple petrinet bodies - try traversal?
+	// traverseGraph(graph);
+
+	// console.log(graph);
+	// console.table(graph.nodes);
+	// console.table(graph.edges);
 
 	// console.log("states")
 	// console.table(petrinet.S);
