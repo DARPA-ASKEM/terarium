468a02a8bd2d6d99abb1a874a860cbadb1157efe
dgauldie <dgauldie@uncharted.software>
2022-11-01 12:09:31 -0400

    fix: removed mock-data-server

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/proxies/ProjectProxy.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/proxies/ProjectProxy.java
@@ -2,71 +2,45 @@ package software.uncharted.terarium.hmiserver.proxies;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import software.uncharted.terarium.hmiserver.models.Project;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @RegisterRestClient
 @Path("/data/projects")
+@Produces(MediaType.APPLICATION_JSON)
 public interface ProjectProxy {
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	List<Project> getProjects(
 		@QueryParam("sort") @DefaultValue("") String sortQuery,
 		@QueryParam("page") @DefaultValue("0") int pageIndex,
 		@QueryParam("size") @DefaultValue("100") int pageSize
 	);
 
-//	@GET
-//	@Path("/{id}")
-//	public Response getProject(
-//		@QueryParam("id") Long id
-//	) {
-//		Project entity = service.get(id);
-//
-//		if (entity == null) {
-//			throw new WebApplicationException(Response.Status.NOT_FOUND);
-//		}
-//		return Response.ok(entity).build();
-//	}
-//
-//	@POST
-//	@Transactional
-//	public Response createProject(Project newProject) {
-//		Project entity = service.add(newProject);
-//		return Response.created(URI.create("/projects/" + entity.id)).build();
-//	}
-//
-//	@PUT
-//	@Path("/{id}")
-//	@Transactional
-//	public Response updateProject(Long id, Project updatedProject) {
-//		if (service.get(id) == null) {
-//			throw new WebApplicationException(Response.Status.NOT_FOUND);
-//		}
-//
-//		updatedProject.updatedAt = Instant.now();
-//		Project entity = service.update(id, updatedProject);
-//
-//		if (entity == null) {
-//			return Response.noContent().build();
-//		}
-//		return Response.ok(entity).build();
-//	}
-//
-//	@DELETE
-//	@Path("/{id}")
-//	@Transactional
-//	public void deleteProject(Long id) {
-//		if (!service.delete(id)) {
-//			throw new WebApplicationException(Response.Status.NOT_FOUND);
-//		}
-//	}
-//
-//	@GET
-//	@Path("/count")
-//	public Long getNumProjects() {
-//		return service.count();
-//	}
+	@GET
+	@Path("/{id}")
+	Project getProject(
+		@QueryParam("id") Long id
+	);
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	Project createProject(Project newProject);
+
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	Project updateProject(Long id, Project updatedProject);
+
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	Boolean deleteProject(Long id);
+
+	@GET
+	@Path("/count")
+	@Produces(MediaType.TEXT_PLAIN)
+	Long getNumProjects();
 }
--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/resources/ProjectResource.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/resources/ProjectResource.java
@@ -4,9 +4,11 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import software.uncharted.terarium.hmiserver.models.Project;
 import software.uncharted.terarium.hmiserver.proxies.ProjectProxy;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
 import java.util.List;
 
 @Path("/api/projects")
@@ -19,69 +21,70 @@ public class ProjectResource {
 	ProjectProxy proxy;
 
 	@GET
-	@Produces("application/json")
+	@Produces(MediaType.APPLICATION_JSON)
 	@Tag(name = "Get all projects for a given user")
 	public Response getProjects(
-		@QueryParam("sort") @DefaultValue("") String sortQuery,
-		@QueryParam("page") @DefaultValue("0") int pageIndex,
-		@QueryParam("size") @DefaultValue("100") int pageSize
+		@QueryParam("sort") @DefaultValue("") final String sortQuery,
+		@QueryParam("page") @DefaultValue("0") final int pageIndex,
+		@QueryParam("size") @DefaultValue("100") final int pageSize
 	) {
-		List<Project> projects = proxy.getProjects(sortQuery, pageIndex, pageSize);
+		final List<Project> projects = proxy.getProjects(sortQuery, pageIndex, pageSize);
 		if (projects.isEmpty()) {
 			return Response.noContent().build();
 		}
 		return Response.ok(projects).build();
 	}
 
-//	@GET
-//	@Path("/{id}")
-//	public Response getProject(
-//		@QueryParam("id") Long id
-//	) {
-//		Project entity = service.get(id);
-//
-//		if (entity == null) {
-//			throw new WebApplicationException(Response.Status.NOT_FOUND);
-//		}
-//		return Response.ok(entity).build();
-//	}
-//
-//	@POST
-//	@Transactional
-//	public Response createProject(Project newProject) {
-//		Project entity = service.add(newProject);
-//		return Response.created(URI.create("/projects/" + entity.id)).build();
-//	}
-//
-//	@PUT
-//	@Path("/{id}")
-//	@Transactional
-//	public Response updateProject(Long id, Project updatedProject) {
-//		if (service.get(id) == null) {
-//			throw new WebApplicationException(Response.Status.NOT_FOUND);
-//		}
-//
-//		updatedProject.updatedAt = Instant.now();
-//		Project entity = service.update(id, updatedProject);
-//
-//		if (entity == null) {
-//			return Response.noContent().build();
-//		}
-//		return Response.ok(entity).build();
-//	}
-//
-//	@DELETE
-//	@Path("/{id}")
-//	@Transactional
-//	public void deleteProject(Long id) {
-//		if (!service.delete(id)) {
-//			throw new WebApplicationException(Response.Status.NOT_FOUND);
-//		}
-//	}
-//
-//	@GET
-//	@Path("/count")
-//	public Long getNumProjects() {
-//		return service.count();
-//	}
+	@GET
+	@Path("/{id}")
+	public Response getProject(
+		@QueryParam("id") final Long id
+	) {
+		final Project entity = proxy.getProject(id);
+
+		if (entity == null) {
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+		return Response.ok(entity).build();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createProject(final Project newProject) {
+		final Project entity = proxy.createProject(newProject);
+		return Response.created(URI.create("/projects/" + entity.id)).build();
+	}
+
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateProject(final Long id, final Project updatedProject) {
+		if (proxy.getProject(id) == null) {
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+
+		final Project entity = proxy.updateProject(id, updatedProject);
+
+		if (entity == null) {
+			return Response.noContent().build();
+		}
+		return Response.ok(entity).build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public Response deleteProject(final Long id) {
+		if (!proxy.deleteProject(id)) {
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+
+		return Response.ok().build();
+	}
+
+	@GET
+	@Path("/count")
+	public Response getNumProjects() {
+		final Long numProjects = proxy.getNumProjects();
+		return Response.ok(numProjects).build();
+	}
 }
