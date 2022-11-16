a0286066926d318e77bbe14605f25861de4030b6
David Gauldie <dgauldie@uncharted.software>
2022-11-16 13:54:06 -0500

    chore: create data service proxies in the hmi server (#227)
    Co-authored-by: Derek Vince <dvince@uncharted.software>

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/proxies/SimulationPlanProxy.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/proxies/SimulationPlanProxy.java
@@ -1,45 +1,44 @@
 package software.uncharted.terarium.hmiserver.proxies;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-import software.uncharted.terarium.hmiserver.models.Project;
+import software.uncharted.terarium.hmiserver.models.SimulationPlan;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-
 @RegisterRestClient(configKey = "data-service")
-@Path("/projects")
+@Path("/plans")
 @Produces(MediaType.APPLICATION_JSON)
-public interface ProjectProxy {
+public interface SimulationPlanProxy {
 
 	@GET
-	List<Project> getProjects();
+	List<SimulationPlan> getSimulationPlans();
 
 	@GET
 	@Path("/{id}")
-	Project getProject(
+	SimulationPlan getSimulationPlan(
 		@PathParam("id") Long id
 	);
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	Project createProject(
-		Project newProject
+	SimulationPlan createSimulationPlan(
+		SimulationPlan plan
 	);
 
 	@PUT
 	@Path("/{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	Project updateProject(
+	SimulationPlan updateSimulationPlan(
 		@PathParam("id") Long id,
-		Project updatedProject
+		SimulationPlan plan
 	);
 
 	@DELETE
 	@Path("/{id}")
 	@Produces(MediaType.TEXT_PLAIN)
-	Boolean deleteProject(
+	Boolean deleteSimulationPlan(
 		@PathParam("id") Long id
 	);
 }
