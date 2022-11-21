b63c009e67745ee61477b16c7f4f14b32dc67724
Yohann Paris <github@yohannparis.com>
2022-11-21 10:47:56 -0500

    fix: sidebar improvements (#237)

--- packages/client/hmi-client/src/components/sidebar-panel/model-sidebar-panel.vue
+++ packages/client/hmi-client/src/components/sidebar-panel/model-sidebar-panel.vue
@@ -1,13 +1,8 @@
 <template>
-	<main>
-		<header>Model Space</header>
-		<section>
-			<Button action @click="goToTheia">
-				<IconScript16 />
-				New model from code
-			</Button>
-		</section>
-	</main>
+	<Button action @click="goToTheia">
+		<IconScript16 />
+		New model from code
+	</Button>
 </template>
 
 <script setup lang="ts">
@@ -23,19 +18,3 @@ import IconScript16 from '@carbon/icons-vue/es/script/16';
 const router = useRouter();
 const goToTheia = () => router.push('/theia');
 </script>
-
-<style scoped>
-main {
-	background-color: var(--un-color-body-surface-primary);
-	display: flex;
-	flex-grow: 1;
-	flex-direction: column;
-	gap: 1rem;
-	padding: 1rem;
-}
-
-header {
-	color: var(--un-color-body-text-secondary);
-	font: var(--un-font-h6);
-}
-</style>
