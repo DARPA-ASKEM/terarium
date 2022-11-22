797ac98d9e2833c8692fe369f180c741d28e2e31
mwdchang <mwdchang@gmail.com>
2022-11-22 16:54:47 -0500

    axio interceptor to work with auth token

--- packages/client/hmi-client/src/api/api.ts
+++ packages/client/hmi-client/src/api/api.ts
@@ -1,7 +1,22 @@
 import axios from 'axios';
+import useAuthStore from '../stores/auth';
 
 const API = axios.create({
 	baseURL: '/api'
+});
+
+// Hook in bearer tokens
+// See
+// - https://medium.com/swlh/handling-access-and-refresh-tokens-using-axios-interceptors-3970b601a5da
+API.interceptors.request.use((config) => {
+	const auth = useAuthStore();
+
+	if (config.headers) {
+		config.headers.Authorization = `Bearer ${auth.token}`;
+	} else {
+		config.headers = { Authorization: `Bearer ${auth.token}` };
+	}
+	return config;
 });
 
 export default API;
