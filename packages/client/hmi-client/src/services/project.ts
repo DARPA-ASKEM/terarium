d44fbbfded3161eb8cd1eadff00166bc4fafc712
Yohann Paris <github@yohannparis.com>
2022-11-28 16:09:51 -0500

    Add some error handling on the project service

--- packages/client/hmi-client/src/services/project.ts
+++ packages/client/hmi-client/src/services/project.ts
@@ -9,9 +9,16 @@ import { Project } from '@/types/Project';
  * Get a project per id
  * @return Project|null - the appropriate project, or null if none returned by API
  */
-async function get(projectId: string): Promise<Project> {
-	const response = await API.get(`/projects/${projectId}`);
-	return response?.data ?? null;
+async function get(projectId: string): Promise<Project | null> {
+	try {
+		const response = await API.get(`/projects/${projectId}`);
+		const { status, data } = response;
+		if (status !== 200) return null;
+		return data ?? null;
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
 }
 
 export { get };
