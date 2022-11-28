982ea01bdff5de735fd21a22f7a45da8ff52876d
Ahmed <amostafa@uncharted.software>
2022-11-28 17:45:42 -0500

    remove unused code

--- packages/client/hmi-client/src/stores/resources.ts
+++ packages/client/hmi-client/src/stores/resources.ts
@@ -1,9 +1,4 @@
 import { defineStore } from 'pinia';
-import { XDDArticle } from '@/types/XDD';
-import { Model } from '@/types/Model';
-import { ResultType } from '@/types/common';
-import { getResourceID, isModel, isXDDArticle } from '@/utils/data-util';
-import { omit } from 'lodash';
 import { Project } from '@/types/Project';
 
 /**
@@ -11,21 +6,10 @@ import { Project } from '@/types/Project';
  */
 const useResourcesStore = defineStore('resources', {
 	state: () => ({
-		documents: {} as { [id: string]: XDDArticle },
-		models: {} as { [id: string]: Model },
 		xddDataset: null as string | null,
 		activeProject: null as null | Project
 	}),
 	actions: {
-		removeResource(resource: ResultType) {
-			const resId = getResourceID(resource);
-			if (isModel(resource)) {
-				this.models = omit(this.models, [resId]);
-			}
-			if (isXDDArticle(resource)) {
-				this.documents = omit(this.documents, [resId]);
-			}
-		},
 		setXDDDataset(dataset: string | null) {
 			this.xddDataset = dataset;
 		},
