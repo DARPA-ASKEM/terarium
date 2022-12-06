46cd725293a0789b38b3fc34f5dd9d02b982594b
Tom Szendrey <T.Szendrey@hotmail.com>
2022-12-06 14:49:39 -0500

    5f6d0e20a58f1dfd52184931 breaks. Need talk to XDD

--- packages/client/hmi-client/src/services/project.ts
+++ packages/client/hmi-client/src/services/project.ts
@@ -84,7 +84,7 @@ async function getRelatedArticles(aProject: Project): Promise<XDDArticle[]> {
 		// TODO: Speak with XDD Team about broken doc: 5f6d0e20a58f1dfd52184931
 		// Grab the 2nd of publication for related results because grabbing the first provides a broken doc id: 5f6d0e20a58f1dfd52184931
 		const listOfRelatedArticles = await getRelatedDocuments(
-			String(resp?.[PUBLICATIONS][0].xdd_uri),
+			String(resp?.[PUBLICATIONS][1].xdd_uri),
 			'xdd-covid-19'
 		);
 		return listOfRelatedArticles;
