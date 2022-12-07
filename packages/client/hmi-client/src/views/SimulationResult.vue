086f84ca342fe3d38d2677cd51d949c5f622b8b2
Manfred Cheung <mj3cheun@uwaterloo.ca>
2022-12-07 11:16:39 -0500

    feat: add legends to simulation results page with descriptions and make base case heatmap map to actual value

--- packages/client/hmi-client/src/views/SimulationResult.vue
+++ packages/client/hmi-client/src/views/SimulationResult.vue
@@ -80,23 +80,47 @@ function parseSimData(input) {
 		data.push(runData);
 	});
 
-	// find max diverging max/min
+	// find max diverging max/min and data parameters max/min
 	const baseRow = 0;
 	cellLabelRow[baseRow] = 'Base'; // change label of base row to base
+
 	let divergingMaxMin = 0;
+	const dataParameters = new Set();
+	const dataParametersMax: any = {};
+	const dataParametersMin: any = {};
 	data.forEach((row) =>
 		row.forEach((cell: any, j) => {
+			// diverging max/min
 			const cellBase: any = data[baseRow][j];
 			const cellBaseDiff = Math.abs(cell.Infected - cellBase.Infected);
 			divergingMaxMin = Math.max(divergingMaxMin, cellBaseDiff);
+
+			// data parameters max/min
+			Object.keys(cell).forEach((param) => {
+				if (!dataParameters.has(param)) {
+					dataParametersMin[param] = cell[param];
+					dataParametersMax[param] = cell[param];
+				} else {
+					dataParametersMin[param] = Math.min(dataParametersMin[param], cell[param]);
+					dataParametersMax[param] = Math.max(dataParametersMax[param], cell[param]);
+				}
+				dataParameters.add(param);
+			});
 		})
 	);
 
-	const fillColorFn = (datum: CellData) => {
+	const fillColorFn = (datum: CellData, parametersMin: any, parametersMax: any) => {
 		const colorExtremePos = '#4d9221';
 		const colorExtremeNeg = '#c51b7d';
 		const colorMid = '#f7f7f7';
 		const datumBase: any = data[baseRow][datum.col];
+
+		if (datum.row === baseRow) {
+			const v =
+				(datum.Infected - parametersMin.Infected) /
+				(parametersMax.Infected - parametersMin.Infected);
+			return mix('#F7F7F7', '#252525', v, 'lab');
+		}
 
 		// return midpoint color if the range of Infected is 0 to avoid divide-by-0 error
 		if (!divergingMaxMin) {
@@ -130,6 +154,8 @@ function parseSimData(input) {
 
 	return {
 		data,
+		dataParametersMax,
+		dataParametersMin,
 		cellLabelCol,
 		cellLabelRow,
 		fillColorFn,
@@ -163,31 +189,62 @@ const { data, cellLabelRow, cellLabelCol, fillColorFn, lineColorFn, barColorFn, 
 // ///////////////////////////////////////////////////////////////////////////////
 // generate legend
 
-const legendContainer = ref(null);
+const legendDiff = ref(null);
+const legendBase = ref(null);
 // FIXME: Render legend test
 onMounted(() => {
-	const svg = select(legendContainer.value as any);
-	const max = simData.divergingMaxMin;
-	const min = -simData.divergingMaxMin;
 	const legendCellW = 50;
-	let cnt = 0;
-	svg.append('text').attr('x', 10).attr('y', 12).text(String(min).slice(0, 7));
-	const colorMap = ['#c51b7d', '#f1b6da', '#f7f7f7', '#b8e186', '#4d9221'];
+
+	// diff legend
+	const svgDiff = select(legendDiff.value as any);
+	const maxDiff = simData.divergingMaxMin;
+	const minDiff = -simData.divergingMaxMin;
+	let cntDiff = 0;
+	svgDiff.append('text').attr('x', 10).attr('y', 12).text(String(minDiff).slice(0, 7));
+	const colorMapDiff = ['#c51b7d', 'mix', '#f7f7f7', 'mix', '#4d9221'];
+	colorMapDiff[1] = mix(colorMapDiff[0], colorMapDiff[2], 0.5, 'lab');
+	colorMapDiff[3] = mix(colorMapDiff[2], colorMapDiff[4], 0.5, 'lab');
 	for (let i = 0; i < 5; i++) {
-		svg
+		svgDiff
 			.append('rect')
-			.attr('x', 63 + cnt * legendCellW)
+			.attr('x', 63 + cntDiff * legendCellW)
 			.attr('y', 0)
 			.attr('width', legendCellW)
 			.attr('height', 15)
-			.style('fill', colorMap[i]);
-		cnt++;
+			.style('fill', colorMapDiff[i]);
+		cntDiff++;
 	}
-	svg
+	svgDiff
 		.append('text')
-		.attr('x', 65 + legendCellW * cnt)
+		.attr('x', 65 + legendCellW * cntDiff)
 		.attr('y', 12)
-		.text(String(max).slice(0, 6));
+		.text(String(maxDiff).slice(0, 6));
+
+	// base legend
+	const svgBase = select(legendBase.value as any);
+	const maxBase = simData.dataParametersMax.Infected;
+	const minBase = simData.dataParametersMin.Infected;
+	let cntBase = 0;
+	svgBase.append('text').attr('x', 10).attr('y', 12).text(String(minBase).slice(0, 7));
+	const colorMapBase = ['#f7f7f7', 'mix', 'mix', 'mix', '#252525'];
+	colorMapBase[1] = mix(colorMapBase[0], colorMapBase[4], 1 / 4, 'lab');
+	colorMapBase[2] = mix(colorMapBase[0], colorMapBase[4], 2 / 4, 'lab');
+	colorMapBase[3] = mix(colorMapBase[0], colorMapBase[4], 3 / 4, 'lab');
+	for (let i = 0; i < 5; i++) {
+		svgBase
+			.append('rect')
+			.attr('x', 63 + cntBase * legendCellW)
+			.attr('y', 0)
+			.attr('width', legendCellW)
+			.attr('height', 15)
+			.style('fill', colorMapBase[i]);
+		cntBase++;
+	}
+	svgBase
+		.append('text')
+		.attr('x', 65 + legendCellW * cntBase)
+		.attr('y', 12)
+		.text(String(maxBase).slice(0, 6));
 });
 
 const isDescriptionExpanded = ref(false);
@@ -196,7 +253,7 @@ const description = 'Description of scenarios';
 
 <template>
 	<section class="result-container">
-		<h3>Simulation Results</h3>
+		<h3>Simulation Results - Difference from Baseline Scenario</h3>
 
 		<div class="description" :class="{ 'is-expanded': isDescriptionExpanded }">
 			<p>{{ description }}</p>
@@ -208,7 +265,15 @@ const description = 'Description of scenarios';
 			<p>&nbsp;</p>
 		</div>
 
-		<svg ref="legendContainer" height="20px" width="400px"></svg>
+		<div class="legend-container">
+			<div class="legend-label">Difference w.r.t. Base (% Total Population Infected)</div>
+			<div class="legend-label">Infected (% Total Population)</div>
+		</div>
+		<div class="legend-container">
+			<svg ref="legendDiff" height="20px" width="370px"></svg>
+			<svg ref="legendBase" height="20px" width="370px"></svg>
+		</div>
+
 		<div class="result">
 			<ResponsiveMatrix
 				:data="data"
@@ -226,6 +291,17 @@ const description = 'Description of scenarios';
 </template>
 
 <style scoped>
+.legend-container {
+	display: flex;
+	justify-content: space-between;
+}
+
+.legend-label {
+	display: flex;
+	justify-content: center;
+	width: 370px;
+}
+
 .result-container {
 	display: flex;
 	flex-direction: column;
