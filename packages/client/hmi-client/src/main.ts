f20837b1de85b148f08a2570a2f383e8ee81c8c0
Yohann Paris <github@yohannparis.com>
2022-12-21 09:40:02 -0500

    Add PrimeVue

--- packages/client/hmi-client/src/main.ts
+++ packages/client/hmi-client/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
 import useAuthStore from './stores/auth';
 import router from './router';
 import App from './App.vue';
@@ -8,6 +9,7 @@ import './assets/css/style.css';
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue, { ripple: true });
 
 const auth = useAuthStore();
 await auth.fetchSSO();
