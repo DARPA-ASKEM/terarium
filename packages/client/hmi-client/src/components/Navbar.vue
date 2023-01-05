997c7fbdbad4525e1e7c7fb1e3898b04c7eae7f9
Edwin Lai <edwinslai@gmail.com>
2023-01-05 14:18:30 -0500

    feat: implement navbar component logout button (#431)

--- packages/client/hmi-client/src/components/Navbar.vue
+++ packages/client/hmi-client/src/components/Navbar.vue
@@ -1,16 +1,29 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import Button from 'primevue/button';
+import Menu from 'primevue/menu';
 import { useCurrentRouter } from '@/router/index';
 import { Project } from '@/types/Project';
 import useResourcesStore from '@/stores/resources';
+import useAuthStore from '@/stores/auth';
+import Dialog from 'primevue/dialog';
 
 const emit = defineEmits(['show-data-explorer']);
 const router = useRouter();
 const { isCurrentRouteHome } = useCurrentRouter();
+const auth = useAuthStore();
 const isHome = computed(() => isCurrentRouteHome.value);
-
+const userMenu = ref();
+const isLogoutConfirmationVisible = ref(false);
+const userMenuItems = ref([
+	{
+		label: 'Logout',
+		command: () => {
+			isLogoutConfirmationVisible.value = !isLogoutConfirmationVisible.value;
+		}
+	}
+]);
 const resources = useResourcesStore();
 
 const goToHomepage = () => {
@@ -22,6 +35,16 @@ const goToHomepage = () => {
 defineProps<{
 	projectName?: Project['name'];
 }>();
+
+const showUserMenu = (event) => {
+	userMenu.value.toggle(event);
+};
+
+const userInitials = computed(() =>
+	auth.name
+		?.split(' ')
+		.reduce((accumulator, currentValue) => accumulator.concat(currentValue.substring(0, 1)), '')
+);
 </script>
 
 <template>
@@ -40,7 +63,25 @@ defineProps<{
 			>
 				<i class="pi pi-search" />
 			</Button>
+			<Button
+				class="p-button p-button-icon-only p-button-rounded p-button-sm user-button"
+				@click="showUserMenu"
+			>
+				{{ userInitials }}
+			</Button>
 		</aside>
+		<Menu ref="userMenu" :model="userMenuItems" :popup="true"> </Menu>
+		<Dialog header="Logout" v-model:visible="isLogoutConfirmationVisible">
+			<span>You will be returned to the login screen.</span>
+			<template #footer>
+				<Button label="Ok" class="p-button-text" @click="auth.logout"></Button>
+				<Button
+					label="Cancel"
+					class="p-button-text"
+					@click="isLogoutConfirmationVisible = false"
+				></Button>
+			</template>
+		</Dialog>
 	</header>
 </template>
 
@@ -50,6 +91,7 @@ header {
 	background-color: var(--un-color-body-surface-primary);
 	box-shadow: var(--un-box-shadow-small);
 	display: flex;
+	justify-content: space-between;
 	gap: 2rem;
 	min-height: var(--header-height);
 	padding: 0.5rem 1rem;
@@ -77,7 +119,6 @@ p a:focus {
 
 aside {
 	display: flex;
-	margin-left: auto;
 	/* Push it to the far side */
 	gap: 1rem;
 }
@@ -89,5 +130,15 @@ aside {
 .p-button:enabled:hover,
 .p-button:enabled:focus {
 	background-color: var(--un-color-accent-light);
+}
+
+.user-button {
+	color: var(--un-color-text-secondary);
+	background-color: var(--un-color-body-surface-background);
+}
+
+.user-button:enabled:hover {
+	color: var(--un-color-text-secondary);
+	background-color: var(--un-color-body-surface-secondary);
 }
 </style>
