b22bd4c0796ef8ef54b9b1aef2b63add97551c3b
cdickson <cdickson@uncharted.software>
2023-01-18 11:22:49 -0500

    - Add an option to event type to be persisted or simply logged - Use correct REST verbs for our endpoints

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/EventType.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/EventType.java
@@ -1,5 +1,18 @@
 package software.uncharted.terarium.hmiserver.models;
 
+import lombok.Getter;
+
 public enum EventType {
-	SEARCH
+	SEARCH(true);
+
+	EventType(boolean persistent) {
+		this.persistent = persistent;
+	}
+
+	EventType() {
+		this.persistent = false;
+	}
+
+	@Getter
+	final boolean persistent;
 }
--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/resources/EventResource.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/resources/EventResource.java
@@ -2,6 +2,7 @@ package software.uncharted.terarium.hmiserver.resources;
 
 import io.quarkus.security.Authenticated;
 import io.quarkus.security.identity.SecurityIdentity;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import software.uncharted.terarium.hmiserver.entities.Event;
 import software.uncharted.terarium.hmiserver.models.EventType;
@@ -9,8 +10,8 @@ import software.uncharted.terarium.hmiserver.models.EventType;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
@@ -18,6 +19,7 @@ import javax.ws.rs.core.Response;
 @Path("/api/events")
 @Authenticated
 @Tag(name = "Events REST Endpoints")
+@Slf4j
 public class EventResource {
 	@Inject
 	private SecurityIdentity securityIdentity;
@@ -29,7 +31,7 @@ public class EventResource {
 	 * @param limit				the number of events to fetch
 	 * @return						a list of {@link Event} for the given user/project/type sorted by most to least recent
 	 */
-	@POST
+	@GET
 	public Response getEvents(@QueryParam("type") final EventType type,
 														@QueryParam("projectId") final Long projectId,
 														@QueryParam("search") final String likeValue,
@@ -50,9 +52,18 @@ public class EventResource {
 	 * @param event	the {@link Event} instance
 	 * @return			the persisted event instance
 	 */
-	@PUT
+	@POST
 	@Transactional
 	public Response putEvent(final Event event) {
+		log.info("Event | " + securityIdentity.getPrincipal().getName() + " | " + event);
+
+		// Do not save the event to the database if the type is not specified as persistent
+		if (!event.getType().isPersistent()) {
+			return Response
+				.ok(null)
+				.build();
+		}
+
 		event.setUsername(securityIdentity.getPrincipal().getName());
 		Event.persist(event);
 		return Response
