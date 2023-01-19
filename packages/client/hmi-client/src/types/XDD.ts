0cf637e9444a3e0b2d812b208a029fe0f3a70a66
Ahmed <amostafa@uncharted.software>
2023-01-19 09:59:50 -0500

    missed updated file

--- packages/client/hmi-client/src/types/XDD.ts
+++ packages/client/hmi-client/src/types/XDD.ts
@@ -60,8 +60,7 @@ export type XDDArticle = {
 
 export type PublicationAsset = {
 	id?: string;
-	xddUri: string; // this is the internal XDD id known as "docid" NOT "doi"
-	xdd_uri: string; // FIXME
+	xdd_uri: string; // FIXME: this is the internal XDD id known as "docid" NOT "doi"
 	title: string;
 };
 
