817cd3c54cb88a4482ce27228e50df3dfecd51c1
Manfred Cheung <mj3cheun@uwaterloo.ca>
2023-01-27 12:16:28 -0500

    feat: draft add model runner (#568)

--- packages/client/hmi-client/src/views/ModelEditor.vue
+++ packages/client/hmi-client/src/views/ModelEditor.vue
@@ -392,6 +392,7 @@ const runPetri = () => {
 	};
 	console.log('final', final);
 	console.log('final', JSON.stringify(final));
+	localStorage.setItem('sim-input-data', JSON.stringify(final));
 	return final;
 };
 
