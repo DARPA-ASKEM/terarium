77feb94e676e3c3eb4e62256390e25345639658e
cdickson <cdickson@uncharted.software>
2023-01-27 10:56:06 -0500

    WIP import ace

--- packages/client/hmi-client/src/views/CodeView.vue
+++ packages/client/hmi-client/src/views/CodeView.vue
@@ -1,5 +1,19 @@
 <template>
 	<div class="code-view">
-		<p>Hello world!</p>
+		<v-ace-editor v-model:value="content" lang="javascript" theme="chrome" style="height: 300px" />
 	</div>
 </template>
+<script lang="ts">
+import { VAceEditor } from 'vue3-ace-editor';
+import '@node_modules/ace-builds/src-noconflict/mode-python';
+import '@node_modules/ace-builds/src-noconflict/theme-chrome';
+import { ref } from 'vue';
+
+const content = ref(true);
+console.log(content);
+export default {
+	components: {
+		VAceEditor
+	}
+};
+</script>
