f794465f52ed247f78c8ffbd37f60b45d6f03c6d
Manfred Cheung <mj3cheun@uwaterloo.ca>
2023-01-27 14:12:21 -0500

    feat: open cart when item added to cart

--- packages/client/hmi-client/src/views/DataExplorer.vue
+++ packages/client/hmi-client/src/views/DataExplorer.vue
@@ -441,6 +441,9 @@ const toggleDataItemSelected = (dataItem: { item: ResultType; type?: string }) =
 	} else {
 		// add it to the list
 		selectedSearchItems.value = [...selectedSearchItems.value, item];
+		// open cart and close preview panel
+		previewItem.value = null;
+		isSliderResourcesOpen.value = true;
 	}
 };
 
