e88465aba32762c4bdde0d3d13dd27d51baac12b
mwdchang <mwdchang@gmail.com>
2023-01-31 11:52:42 -0500

    first pass document annotation

--- packages/client/hmi-client/src/views/DocumentView.vue
+++ packages/client/hmi-client/src/views/DocumentView.vue
@@ -2,7 +2,7 @@
 import Document from '@/components/articles/Document.vue';
 import TabContainer from '@/components/tabs/TabContainer.vue';
 import { ref, watch, computed } from 'vue';
-import { Tab } from '@/types/common';
+import { Tab, Annotation, ResourceType } from '@/types/common';
 import useResourcesStore from '@/stores/resources';
 import { Project } from '@/types/Project';
 import { RouteName } from '@/router/routes';
@@ -10,6 +10,10 @@ import { useTabStore } from '@/stores/tabs';
 import { isEmpty } from 'lodash';
 import ResourcesList from '@/components/resources/resources-list.vue';
 import SliderPanel from '@/components/widgets/slider-panel.vue';
+import Textarea from 'primevue/textarea';
+import Button from 'primevue/button';
+
+import API from '@/api/api';
 
 const props = defineProps<{
 	assetId?: string;
@@ -20,7 +24,9 @@ const resourcesStore = useResourcesStore();
 const tabStore = useTabStore();
 
 const isPanelOpen = ref(false);
+const annotations = ref<Annotation[]>([]);
 
+const annotationContent = ref<string>('');
 const newDocumentId = computed(() => props.assetId);
 const openTabs = ref<Tab[]>([]);
 const activeTabIndex = ref(0);
@@ -55,6 +61,28 @@ function setActiveTab(index: number) {
 	tabStore.setActiveTabIndex(tabContext, index);
 }
 
+const fetchAnnotations = async () => {
+	const response = await API.get('/annotations', {
+		params: {
+			artifact_type: ResourceType.XDD,
+			artifact_id: newDocumentId.value
+		}
+	});
+	annotations.value = response.data;
+};
+
+const addAnnotation = async () => {
+	const content = annotationContent.value;
+	await API.post('/annotations', {
+		content,
+		artifact_id: newDocumentId.value,
+		artifact_type: ResourceType.XDD // FIXME: need to establish artifact types
+	});
+
+	// Refresh
+	await fetchAnnotations();
+};
+
 watch(newDocumentId, (id) => {
 	if (id) {
 		const newTab = {
@@ -75,6 +103,7 @@ watch(newDocumentId, (id) => {
 		} else {
 			tabStore.setActiveTabIndex(tabContext, foundTabIndex);
 		}
+		fetchAnnotations();
 	}
 });
 
@@ -83,6 +112,11 @@ if (previousOpenTabs) {
 	openTabs.value = openTabs.value.concat(previousOpenTabs);
 	setActiveTab(tabStore.getActiveTabIndex(tabContext));
 }
+
+const formatDate = (millis: number) => {
+	const dt = new Date(millis).toLocaleDateString();
+	return dt;
+};
 </script>
 
 <template>
@@ -103,14 +137,24 @@ if (previousOpenTabs) {
 
 	<slider-panel
 		class="slider"
-		content-width="200px"
+		content-width="300px"
 		tab-width="56px"
 		direction="right"
 		header="Notes"
 		v-model:is-open="isPanelOpen"
 	>
 		<template v-slot:content>
-			<div>Future notes goes in here.</div>
+			<div v-for="(annotation, idx) of annotations" :key="idx" class="annotation-panel">
+				<p>{{ annotation.content }}</p>
+				<div class="annotation-footer">
+					<div>{{ annotation.username }}</div>
+					<div>{{ formatDate(annotation.timestampMillis) }}</div>
+				</div>
+			</div>
+			<div class="annotation-panel">
+				<Textarea v-model="annotationContent" rows="5" cols="30" aria-labelledby="annotation" />
+				<Button @click="addAnnotation()" label="Add note" />
+			</div>
 		</template>
 	</slider-panel>
 </template>
@@ -123,13 +167,24 @@ if (previousOpenTabs) {
 	gap: 1rem;
 	padding: 1rem;
 	background: var(--surface-section);
+	flex: 1;
 }
 
 .tab-container {
 	height: 100%;
+	flex: 1;
 }
 
 .slider {
 	background: var(--surface-card);
+}
+
+.annotation-panel {
+	padding: 8px;
+	font-size: var(--font-body-small);
+}
+
+.annotation-footer {
+	text-align: right;
 }
 </style>
