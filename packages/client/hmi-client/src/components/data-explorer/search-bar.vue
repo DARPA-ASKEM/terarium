8956c73b88817211c0bcb5219f1718e903b66cca
Shawn Yama <syama@uncharted.software>
2023-02-03 20:49:35 -0500

    gotta get search to work

--- packages/client/hmi-client/src/components/data-explorer/search-bar.vue
+++ packages/client/hmi-client/src/components/data-explorer/search-bar.vue
@@ -1,36 +1,30 @@
 <template>
 	<section class="search-bar-container">
 		<div class="search">
-			<AutoComplete
-				:active="searchBarRef?.containerClass[1]['p-overlay-open']"
-				placeholder="Search"
-				v-model="query"
-				:suggestions="autocompleteMenuItems"
-				@complete="execSearch"
-				@change="handleSearchEvent"
-				ref="searchBarRef"
-				scrollHeight="400px"
-				removeTokenIcon="pi pi-times"
-				loadingIcon="undefined"
-			>
-				<template #item="prop">
-					<i class="pi pi-search" />
-					<span>{{ prop.item.label }}</span>
-				</template>
-			</AutoComplete>
-			<i
-				class="pi pi-times clear-search"
-				:class="{ hidden: isClearQueryButtonHidden }"
-				style="font-size: 1rem"
-				@click="clearQuery"
-			/>
-			<!-- <span class="p-input-icon-left p-input-icon-right">
-			
-				<InputText type="text" placeholder="Search" v-model="query" @keyup.enter="execSearch"
-					@keyup.space="handleSearchEvent" class="input-text" @input="handleSearchEvent" ref="searchBarRef" />
-				<Menu ref="autocompleteMenu" :model="autocompleteMenuItems" :popup="true"> </Menu>
-			
-			</span> -->
+			<span class="p-input-icon-left p-input-icon-right">
+				<i class="pi pi-search" />
+				<AutoComplete
+					:active="
+						searchBarRef?.containerClass[1]['p-overlay-open'] && !isEmpty(autocompleteMenuItems)
+					"
+					placeholder="Search"
+					v-model="query"
+					:suggestions="autocompleteMenuItems"
+					:minLength="MIN_LENGTH"
+					@complete="execSearch"
+					@keyup.space="handleSearchEvent"
+					@change="handleSearchEvent"
+					ref="searchBarRef"
+					scrollHeight="400px"
+					removeTokenIcon="pi pi-times"
+					loadingIcon="null"
+				>
+					<template #item="prop">
+						<span><i class="pi pi-search" />{{ prop.item }}</span>
+					</template>
+				</AutoComplete>
+				<i class="pi pi-times clear-search" :class="{ hidden: !query }" @click="clearQuery" />
+			</span>
 			<!-- <i class="pi pi-history" title="Search history" /> -->
 			<!-- <i class="pi pi-image" title="Search by Example" @click="toggleSearchByExample" /> -->
 		</div>
@@ -39,28 +33,31 @@
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import { isEmpty } from 'lodash';
 import { getRelatedTerms, getAutocomplete } from '@/services/data';
-import { computed, onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import * as EventService from '@/services/event';
 import useResourcesStore from '@/stores/resources';
-// import Menu from 'primevue/menu';
-// import InputText from 'primevue/inputtext';
+import AutoComplete from 'primevue/autocomplete';
 import { EventType } from '@/types/EventType';
-
-const emit = defineEmits(['update-related-terms', 'toggle-search-by-example']);
-
-const route = useRoute();
-const resources = useResourcesStore();
 
 const props = defineProps<{
 	suggestions: boolean;
 }>();
 
-const query = ref('');
-const searchBarRef = ref();
-const autocompleteMenuItems = ref([{}]);
+const emit = defineEmits(['update-related-terms', 'toggle-search-by-example']);
 
-const isClearQueryButtonHidden = computed(() => !query.value);
+const MIN_LENGTH = 3;
+const route = useRoute();
+const resources = useResourcesStore();
+
+const query = ref<string>('');
+const searchBarRef = ref();
+const autocompleteMenuItems = ref<string[]>([]);
+
+// const toggleSearchByExample = () => {
+// 	emit('toggle-search-by-example');
+// };
 
 function clearQuery() {
 	query.value = '';
@@ -68,13 +65,10 @@ function clearQuery() {
 }
 
 const execSearch = () => {
+	console.log(query.value, autocompleteMenuItems.value);
 	emit('update-related-terms', query.value);
 	EventService.create(EventType.Search, resources.activeProject?.id, query.value);
 };
-
-// const toggleSearchByExample = () => {
-// 	emit('toggle-search-by-example');
-// };
 
 function addToQuery(term: string) {
 	query.value = query.value ? query.value.trim().concat(' ').concat(term).trim() : term;
@@ -84,54 +78,37 @@ function addToQuery(term: string) {
 }
 defineExpose({ addToQuery });
 
-function replaceSearchTerm(term) {
-	query.value = term;
-	// @ts-ignore
-	searchBarRef.value?.$el.focus();
+async function fillAutocomplete() {
+	const promise = getAutocomplete(query.value);
+	promise.then((response) => {
+		autocompleteMenuItems.value = response;
+		// @ts-ignore
+		searchBarRef.value?.$el.focus();
+	});
 }
 
-async function showAutocomplete(event) {
-	if (query.value.length >= 3) {
-		const promise = getAutocomplete(query.value);
-		promise.then((response) => {
-			autocompleteMenuItems.value = response.map((item) => ({
-				label: item,
-				icon: 'pi pi-search',
-				command: () => {
-					replaceSearchTerm(item);
-				}
-			}));
-			// @ts-ignore
-			searchBarRef.value?.$el.focus();
-		});
-		searchBarRef.value.show(event);
-	}
-}
-
-async function showSuggestions(event) {
+async function fillSuggestions() {
 	if (props.suggestions) {
 		const promise = getRelatedTerms(query.value, resources.xddDataset);
 		promise.then((response) => {
-			autocompleteMenuItems.value = response.map((item) => ({
-				label: item,
-				icon: 'pi pi-search',
-				command: () => {
-					addToQuery(item);
-				}
-			}));
+			autocompleteMenuItems.value = response;
+			// .map((item) => ({
+			// 	label: item,
+			// 	command: () => {
+			// 		addToQuery(item);
+			// 	}
+			// }));
 			// @ts-ignore
 			searchBarRef.value?.$el.focus();
 		});
-		searchBarRef.value.show(event);
 	}
 }
-
 const handleSearchEvent = (event) => {
 	const keyboardEvent = event as KeyboardEvent;
 	if (keyboardEvent.code === 'Space') {
-		showSuggestions(event);
-	} else {
-		showAutocomplete(event);
+		fillSuggestions();
+	} else if (query.value.length >= MIN_LENGTH) {
+		fillAutocomplete();
 	}
 };
 
@@ -150,34 +127,40 @@ onMounted(() => {
 	overflow: hidden;
 }
 
+.search {
+	display: flex;
+	align-items: center;
+	width: 100%;
+}
+
 .p-autocomplete {
 	border-color: var(--surface-border);
 	height: 3rem;
 	width: 100%;
 }
 
-.p-autocomplete:deep(.p-inputtext),
-.p-autocomplete:deep(.p-inputtext:hover) {
-	border-color: var(--surface-border);
+.p-autocomplete:deep(.p-inputtext) {
 	border-radius: 1.5rem;
 	width: 100%;
 }
 
 .p-autocomplete:deep(.p-inputtext:focus) {
 	box-shadow: none;
+}
+
+.p-autocomplete:deep(.p-inputtext),
+.p-autocomplete:deep(.p-inputtext:hover),
+.p-autocomplete:deep(.p-inputtext:focus) {
 	border: 1px solid var(--surface-border);
+	padding-left: 3rem;
 }
 
 .p-autocomplete[active='true']:deep(.p-inputtext) {
-	border-bottom: none;
 	border-bottom-left-radius: 0;
 	border-bottom-right-radius: 0;
-}
-
-.search {
-	display: flex;
-	align-items: center;
-	width: 100%;
+	border-bottom: none;
+	/* Required for smooth transition */
+	border-bottom-color: transparent;
 }
 
 .p-input-icon-left {
@@ -187,10 +170,23 @@ onMounted(() => {
 
 i {
 	color: var(--text-color-subdued);
+	z-index: 1;
 }
 
 .pi-search {
 	margin-right: 1rem;
+}
+
+.clear-search:hover {
+	background-color: var(--surface-hover);
+	padding: 0.5rem;
+	border-radius: 1rem;
+	top: 1rem;
+	right: 0.5rem;
+}
+
+.clear-search.hidden {
+	visibility: hidden;
 }
 
 /* 
@@ -208,32 +204,4 @@ i {
 	cursor: pointer;
 } 
 */
-
-.clear-search:hover {
-	background-color: var(--surface-hover);
-	padding: 0.5rem;
-	border-radius: 1rem;
-	top: 1rem;
-	right: 0.5rem;
-}
-
-.clear-search.hidden {
-	visibility: hidden;
-}
-
-.item {
-	background-color: var(--surface-secondary);
-	color: var(--text-color-secondary);
-	padding: 0 0.5rem 0 0.5rem;
-	margin: 0.5rem;
-}
-
-.item:enabled:hover {
-	color: var(--text-color-secondary);
-	background-color: var(--surface-hover);
-}
-
-.p-menu.autocomplete {
-	border-radius: 0 0 1.5rem 1.5rem;
-}
 </style>
