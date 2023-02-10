908a9309e1fd74ae1da455b414f42e5b5d41d449
Edwin Lai <elai@uncharted.software>
2023-02-10 11:56:40 -0500

    re-organize code

--- packages/client/hmi-client/src/views/Home.vue
+++ packages/client/hmi-client/src/views/Home.vue
@@ -6,15 +6,14 @@ import IconTime32 from '@carbon/icons-vue/es/time/32';
 import IconChevronLeft32 from '@carbon/icons-vue/es/chevron--left/32';
 import IconChevronRight32 from '@carbon/icons-vue/es/chevron--right/32';
 import IconClose32 from '@carbon/icons-vue/es/close/16';
-import { Project, ProjectAssetTypes } from '@/types/Project';
+import { Project } from '@/types/Project';
 import { XDDSearchParams } from '@/types/XDD';
 import { DocumentType } from '@/types/Document';
 import { searchXDDDocuments } from '@/services/data';
 import useResourcesStore from '@/stores/resources';
 import useQueryStore from '@/stores/query';
 import API from '@/api/api';
-import Card from 'primevue/card';
-import Button from 'primevue/button';
+import ProjectCard from '@/components/projects/ProjectCard.vue';
 
 const projects = ref<Project[]>([]);
 // Only display projects with at least one related document
@@ -120,40 +119,7 @@ const scroll = (direction: 'right' | 'left', event: PointerEvent) => {
 						:to="'/projects/' + project.id"
 						:projectId="project.id"
 					>
-						<Card>
-							<template #header>
-								<header class="project-stats">
-									<div title="Contributors"><i class="pi pi-user"></i> 1</div>
-									<div title="Models">
-										<i class="pi pi-share-alt"></i>
-										{{ project?.assets?.[ProjectAssetTypes.MODELS]?.length ?? 0 }}
-									</div>
-									<div title="Datasets">
-										<i class="pi pi-sliders-v"></i>
-										{{ project?.assets?.[ProjectAssetTypes.DATASETS]?.length ?? 0 }}
-									</div>
-									<div title="Papers">
-										<i class="pi pi-file"></i>
-										{{ project?.assets?.[ProjectAssetTypes.DOCUMENTS]?.length ?? 0 }}
-									</div>
-								</header>
-							</template>
-							<template #title>
-								<div class="project-img">
-									<img src="@assets/images/project-card.png" alt="Project image" />
-								</div>
-								<div class="project-title">{{ project.name }}</div>
-							</template>
-							<template #subtitle>
-								<div class="project-description">{{ project.description }}</div>
-							</template>
-							<template #content>
-								<div class="project-footer">
-									<span>Last modified Dec 20, 2022</span>
-									<Button icon="pi pi-ellipsis-v" class="p-button-rounded p-button-secondary" />
-								</div>
-							</template>
-						</Card>
+						<ProjectCard :project="project"></ProjectCard>
 					</router-link>
 				</li>
 			</ul>
@@ -188,62 +154,6 @@ const scroll = (direction: 'right' | 'left', event: PointerEvent) => {
 </template>
 
 <style scoped>
-.project-footer {
-	align-items: baseline;
-	display: flex;
-	justify-content: space-between;
-	color: var(--text-color-secondary);
-}
-
-.project-stats {
-	display: flex;
-	justify-content: space-between;
-	padding: 0 1rem 0 1rem;
-}
-
-.project-stats div {
-	padding-top: 1rem;
-	color: var(--text-color-secondary);
-}
-
-.project-img {
-	width: 248px;
-	height: 191px;
-	background-color: var(--surface-ground);
-	border-radius: 1rem;
-	margin-bottom: 1rem;
-	transition: opacity 0.3s ease, height 0.3s ease;
-}
-
-.p-card:hover .project-img {
-	opacity: 0;
-	height: 0px;
-}
-
-.project-title {
-	display: inline-block;
-	height: 75px;
-	width: 248px;
-	overflow: hidden;
-}
-
-.project-description {
-	opacity: 0;
-	height: 0;
-	width: 248px;
-	transition: opacity 0.3s ease, height 0.3s ease;
-}
-
-.p-card:hover .project-description {
-	opacity: 100;
-	height: 191px;
-}
-
-.p-card {
-	height: 423px;
-	width: 280px;
-}
-
 section {
 	background-color: var(--surface-secondary);
 	color: var(--text-color-secondary);
