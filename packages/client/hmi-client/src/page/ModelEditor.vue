b8205780cecc36f4f519776ab4b225a70a927cb6
Daniel Chang <mwdchang@gmail.com>
2023-02-16 09:36:14 -0500

    fix: undo temporary hackathon changes, fix grid duplicates (#726)

--- packages/client/hmi-client/src/page/ModelEditor.vue
+++ packages/client/hmi-client/src/page/ModelEditor.vue
@@ -63,7 +63,7 @@
 import * as d3 from 'd3';
 import _ from 'lodash';
 import graphScaffolder, { IGraph } from '@graph-scaffolder/index';
-import { runDagreLayout2, D3SelectionINode, D3SelectionIEdge } from '@/services/graph';
+import { runDagreLayout, D3SelectionINode, D3SelectionIEdge } from '@/services/graph';
 import { onMounted, ref, computed, watch } from 'vue';
 import { PetriNet } from '@/utils/petri-net-validator';
 import { parsePetriNet2IGraph, NodeData, EdgeData, NodeType } from '@/services/model';
@@ -804,7 +804,7 @@ onMounted(async () => {
 	renderer = new SampleRenderer({
 		el: playground,
 		useAStarRouting: false, // People get distracted with squiggly connectors - Jan 2023
-		runLayout: runDagreLayout2,
+		runLayout: runDagreLayout,
 		useStableZoomPan: true
 	});
 
