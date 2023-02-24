d167de2b06a19fbe98f7dc0bddb5758c10897ddf
mwdchang <mwdchang@gmail.com>
2023-02-23 19:15:10 -0500

    multi-edge label

--- packages/client/hmi-client/src/petrinet/petrinet-renderer.ts
+++ packages/client/hmi-client/src/petrinet/petrinet-renderer.ts
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-import graphScaffolder, { INode } from '@graph-scaffolder/index';
+import graphScaffolder, { INode, IEdge } from '@graph-scaffolder/index';
 import { D3SelectionINode, D3SelectionIEdge } from '@/services/graph';
 import { pointOnPath } from '@/utils/svg';
 import { NodeData, EdgeData, NodeType } from './petrinet-service';
@@ -75,12 +75,20 @@ export class PetrinetRenderer extends graphScaffolder.BasicRenderer<NodeData, Ed
 			.style('stroke-width', 2)
 			.attr('marker-end', 'url(#arrowhead)');
 
+		this.updateMultiEdgeLabels();
+	}
+
+	updateMultiEdgeLabels() {
+		const selection = this.chart?.selectAll('.edge') as D3SelectionIEdge<EdgeData>;
 		const multiEdges = selection.filter((d) => (d.data && d.data.numEdges > 1) as boolean);
+		multiEdges.selectAll('.multi-edge-label').remove();
+
 		multiEdges.each((_d, index, group) => {
-			const edgeSelection = d3.select<any, INode<EdgeData>>(group[index]);
+			const edgeSelection = d3.select<any, IEdge<EdgeData>>(group[index]);
 			const point = pointOnPath(edgeSelection.select('path').node() as any, 0.5);
 			edgeSelection
 				.append('text')
+				.classed('multi-edge-label', true)
 				.attr('x', point.x)
 				.attr('y', point.y)
 				.attr('stroke', null)
@@ -114,69 +122,7 @@ export class PetrinetRenderer extends graphScaffolder.BasicRenderer<NodeData, Ed
 		this.on(
 			'node-drag-move',
 			(_eventName, event /* , _selection: D3SelectionINode<NodeData> */) => {
-				if (!event.sourceEvent.shiftKey) return;
-				const pointerCoords = d3
-					.zoomTransform(svg.node() as Element)
-					.invert(d3.pointer(event, svg.node()));
-				targetData = d3.select<SVGGElement, INode<NodeData>>(event.sourceEvent.target).datum();
-				if (targetData) {
-					end.x = targetData.x + 0.5 * targetData.width;
-					end.y = targetData.y + 0.5 * targetData.height;
-				} else {
-					end.x = pointerCoords[0];
-					end.y = pointerCoords[1];
-				}
-				chart?.selectAll('.new-edge').remove();
-
-				const line = [
-					{ x: start.x, y: start.y },
-					{ x: end.x, y: end.y }
-				];
-				chart
-					?.append('path')
-					.classed('new-edge', true)
-					.attr('d', pathFn(line))
-					.attr('marker-end', 'url(#arrowhead)')
-					.style('stroke', '#000');
-			}
-		);
-
-		this.on('node-drag-end', (_eventName, event /* _selection: D3SelectionINode<NodeData> */) => {
-			chart?.selectAll('.new-edge').remove();
-			if (!event.sourceEvent.shiftKey) return;
-			if (targetData && sourceData) {
-				this.emit('add-edge', null, null, { target: targetData, source: sourceData });
-				sourceData = null;
-				targetData = null;
-			}
-		});
-	}
-
-	postRenderProcess() {
-		const chart = this.chart;
-		const svg = d3.select(this.svgEl);
-		const start: { x: number; y: number } = { x: 0, y: 0 };
-		const end: { x: number; y: number } = { x: 0, y: 0 };
-
-		let sourceData: INode<NodeData> | null = null;
-		let targetData: INode<NodeData> | null = null;
-
-		// Reset all
-		this.removeAllEvents('node-drag-start');
-		this.removeAllEvents('node-drag-move');
-		this.removeAllEvents('node-drag-end');
-
-		// (Re)create dragging listeners
-		this.on('node-drag-start', (_eventName, event, selection: D3SelectionINode<NodeData>) => {
-			if (!event.sourceEvent.shiftKey) return;
-			sourceData = selection.datum();
-			start.x = sourceData.x + 0.5 * sourceData.width;
-			start.y = sourceData.y + 0.5 * sourceData.height;
-		});
-
-		this.on(
-			'node-drag-move',
-			(_eventName, event /* , _selection: D3SelectionINode<NodeData> */) => {
+				this.updateMultiEdgeLabels();
 				if (!event.sourceEvent.shiftKey) return;
 				const pointerCoords = d3
 					.zoomTransform(svg.node() as Element)
