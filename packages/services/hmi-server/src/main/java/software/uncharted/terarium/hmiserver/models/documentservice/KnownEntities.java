8f8135a1322d89563ddad214ab5c258d20f41680
mwdchang <mwdchang@gmail.com>
2023-03-08 12:00:55 -0500

    chore: clean up

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/documentservice/KnownEntities.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/documentservice/KnownEntities.java
@@ -12,9 +12,9 @@ import java.util.Map;
 @Accessors(chain = true)
 public class KnownEntities implements Serializable {
 
-	public List<XDDUrlExtraction> urlExtractions;
+	private List<XDDUrlExtraction> urlExtractions;
 
-	public Map<String, Map<String, String>> summaries;
+	private Map<String, Map<String, String>> summaries;
 
 	@JsonbProperty("url_extractions")
 	public void setURLExtractions(List<XDDUrlExtraction> urlExtractions) {
