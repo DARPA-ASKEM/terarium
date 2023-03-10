93c11a9a4cc1b7e50a68faf7376d2c032bb35571
Edwin Lai <edwinslai@gmail.com>
2023-03-10 10:11:07 -0500

    style: various homepage stying and layout fixes (#814)

--- packages/client/hmi-client/src/components/projects/ProjectCard.vue
+++ packages/client/hmi-client/src/components/projects/ProjectCard.vue
@@ -3,13 +3,14 @@ import { IProject, ProjectAssetTypes } from '@/types/Project';
 import Card from 'primevue/card';
 import Button from 'primevue/button';
 import { formatDdMmmYyyy } from '@/utils/date';
+import Skeleton from 'primevue/skeleton';
 
-defineProps<{ project: IProject }>();
+defineProps<{ project?: IProject }>();
 </script>
 
 <template>
-	<Card>
-		<template #header>
+	<Card v-if="project">
+		<template #content>
 			<header class="project-stats">
 				<div title="Contributors"><i class="pi pi-user"></i> 1</div>
 				<div title="Models">
@@ -25,86 +26,128 @@ defineProps<{ project: IProject }>();
 					{{ project?.assets?.[ProjectAssetTypes.DOCUMENTS]?.length ?? 0 }}
 				</div>
 			</header>
-		</template>
-		<template #title>
 			<div class="project-img">
 				<img src="@assets/images/project-card.png" alt="Project image" />
 			</div>
 			<div class="project-title">{{ project.name }}</div>
-		</template>
-		<template #content>
 			<div class="project-description">{{ project.description }}</div>
-		</template>
-		<template #footer>
 			<div class="project-footer">
 				<span>Last updated {{ formatDdMmmYyyy(project.timestamp) }}</span>
 				<Button icon="pi pi-ellipsis-v" class="p-button-rounded p-button-secondary" />
 			</div>
 		</template>
 	</Card>
+	<Card v-else>
+		<template #content>
+			<header class="project-stats skeleton">
+				<Skeleton height="100%" />
+				<Skeleton height="100%" />
+				<Skeleton height="100%" />
+				<Skeleton height="100%" />
+			</header>
+			<div class="project-img skeleton">
+				<Skeleton height="100%" />
+			</div>
+			<div class="project-description skeleton">
+				<Skeleton />
+				<Skeleton />
+				<Skeleton width="60%" />
+			</div>
+			<div class="project-footer skeleton">
+				<Skeleton />
+			</div>
+		</template>
+	</Card>
 </template>
 
 <style scoped>
+.p-card {
+	width: 17rem;
+	height: 20rem;
+}
+
 .project-stats {
 	display: flex;
 	justify-content: space-between;
-	padding: 0 1rem 0 1rem;
+}
+
+.project-stats.skeleton {
+	gap: 1rem;
+	height: 17px;
 }
 
 .project-stats div {
-	padding-top: 1rem;
 	color: var(--text-color-secondary);
 }
 
 .project-img {
-	width: 248px;
-	height: 190px;
+	height: 8.75rem;
 	background-color: var(--surface-ground);
 	border-radius: 1rem;
-	margin-bottom: 1rem;
 	transition: opacity 0.3s ease, height 0.3s ease;
-}
-
-.project-img {
 	position: relative;
+	margin: 0.5rem 0 0.5rem 0;
 }
 
-.p-card:hover .project-img {
+.project-img img {
+	height: 100%;
+	width: 100%;
+}
+
+.project-img.skeleton {
+	background-color: transparent;
+}
+
+.p-card:hover .project-img:not(.skeleton) {
 	opacity: 0;
-	height: 17px;
+	height: 0;
 }
 
 .project-title {
 	display: inline-block;
-	height: 77px;
-	width: 248px;
+	height: 3.75rem;
 	overflow: hidden;
+	font-weight: var(--font-weight-semibold);
 }
 
-.project-description {
-	display: inline-block;
+.project-title.skeleton {
+	height: 100%;
+}
+
+.project-description:not(.skeleton) {
 	overflow: hidden;
 	opacity: 0;
-	height: 17px;
-	width: 248px;
+	height: 0;
 	transition: opacity 0.3s ease, height 0.3s ease;
 	color: var(--text-color-secondary);
 }
 
-.p-card:hover .project-description {
+.p-card:hover .project-description:not(.skeleton) {
 	opacity: 100;
-	height: 190px;
+	height: 8.75rem;
+}
+
+.project-description.skeleton {
+	display: flex;
+	flex-direction: column;
+	row-gap: 0.5rem;
 }
 
 .project-footer {
-	align-items: baseline;
+	height: 3rem;
+	align-items: center;
 	display: flex;
 	justify-content: space-between;
 	color: var(--text-color-secondary);
+	padding-top: 0.5rem;
 }
 
-.p-card {
-	width: 20rem;
-	cursor: pointer;
+.project-footer.skeleton {
+	align-items: flex-end;
+}
+
+.p-button.p-button-icon-only.p-button-rounded {
+	height: 3rem;
+	width: 3rem;
 }
 </style>
