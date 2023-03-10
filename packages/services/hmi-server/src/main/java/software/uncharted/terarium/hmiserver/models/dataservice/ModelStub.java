b7c38b161c80d6b52516187338219654c1c3e12e
mwdchang <mwdchang@gmail.com>
2023-03-10 16:28:15 -0500

    clean up

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/ModelStub.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/ModelStub.java
@@ -4,10 +4,6 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
 
 /**
  * This is used to mirror model-creation and model-update
