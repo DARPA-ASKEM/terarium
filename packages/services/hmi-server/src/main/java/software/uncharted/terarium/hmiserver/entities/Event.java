37b9d1629b3a1d13504b9f11d0fde79e1aa8f53c
Chris Dickson <cdickson@uncharted.software>
2023-03-10 11:01:16 -0500

    feat: generate shared models automatically (#832)

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/entities/Event.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/entities/Event.java
@@ -4,9 +4,10 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Sort;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import software.uncharted.terarium.hmiserver.annotations.TSModel;
+import software.uncharted.terarium.hmiserver.annotations.TSOptional;
 import software.uncharted.terarium.hmiserver.models.EventType;
 
 import javax.persistence.Column;
@@ -21,6 +22,7 @@ import java.util.UUID;
 
 @Entity
 @Data
+@TSModel
 @Accessors(chain = true)
 @NoArgsConstructor
 @Table(indexes = {
@@ -30,34 +32,37 @@ import java.util.UUID;
 	@Index(columnList = "type"),
 	@Index(columnList = "value")
 })
-@EqualsAndHashCode(callSuper = true)
 public class Event extends PanacheEntityBase implements Serializable {
 	@Id
+	@TSOptional
 	private String id = UUID.randomUUID().toString();
 
 	@Column(nullable = false)
+	@TSOptional
 	private Long timestampMillis = Instant.now().toEpochMilli();
 
+	@TSOptional
 	private Long projectId;
 
 	@Column(nullable = false)
+	@TSOptional
 	private String username;
 
 	@Column(nullable = false)
 	private EventType type;
 
 	@Column(length = 2047)
+	@TSOptional
 	private String value;
 
 	/**
 	 * Gets events by type and an option search string for values
-	 *
-	 * @param type      the {@link EventType}
-	 * @param projectId the optional project id
-	 * @param username  the username of the current user
-	 * @param like      optional search string for values of matching events
-	 * @param limit     the maximum number of events to return
-	 * @return a list of {@link Event} matching the input
+	 * @param type				the {@link EventType}
+	 * @param projectId		the optional project id
+	 * @param username		the username of the current user
+	 * @param like				optional search string for values of matching events
+	 * @param limit				the maximum number of events to return
+	 * @return						a list of {@link Event} matching the input
 	 */
 	public static List<Event> findAllByTypeAndProjectAndUsernameAndLikeLimit(final EventType type, final Long projectId, final String username, String like, int limit) {
 		PanacheQuery<Event> query;
