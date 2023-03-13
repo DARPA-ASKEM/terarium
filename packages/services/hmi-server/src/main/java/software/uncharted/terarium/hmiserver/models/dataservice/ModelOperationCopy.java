c7b23eba2e1e37464156912cdfbfc6a6d437e799
mwdchang <mwdchang@gmail.com>
2023-03-13 13:02:14 -0400

    remove serializable

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/ModelOperationCopy.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/ModelOperationCopy.java
@@ -3,11 +3,9 @@ package software.uncharted.terarium.hmiserver.models.dataservice;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
-import java.io.Serializable;
-
 @Data
 @Accessors(chain = true)
-public class ModelOperationCopy implements Serializable {
+public class ModelOperationCopy {
 	private Long left;
 	private String name;
 	private String description;
