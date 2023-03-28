c9d2606229b448ee740cef35d3b15353c80672f3
Manfred Cheung <mj3cheun@uwaterloo.ca>
2023-03-28 16:28:04 -0400

    Add simulation-service

---
+++ packages/client/hmi-client/src/services/models/simulation-service.ts
@@ -0,0 +1,42 @@
+import { logger } from '@/utils/logger';
+import API from '@/api/api';
+
+interface ForecastParametersType {
+	petri: string;
+	initials: { [key: string]: number };
+	tspan: [number, number];
+	params: { [key: string]: number };
+}
+
+export async function makeForecast(simulationParam: ForecastParametersType) {
+	try {
+		const resp = await API.post('simulation-service/forecast', simulationParam);
+		const output = resp.data;
+		return output;
+	} catch (err) {
+		logger.error(err);
+		return null;
+	}
+}
+
+export async function getRunStatus(runId: number) {
+	try {
+		const resp = await API.get(`simulation-service/${runId}/status`);
+		const output = resp.data;
+		return output;
+	} catch (err) {
+		logger.error(err);
+		return null;
+	}
+}
+
+export async function getRunResult(runId: number) {
+	try {
+		const resp = await API.get(`simulation-service/${runId}/result`);
+		const output = resp.data;
+		return output;
+	} catch (err) {
+		logger.error(err);
+		return null;
+	}
+}
---
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/simulationservice/SimulationParams.java
@@ -0,0 +1,17 @@
+package software.uncharted.terarium.hmiserver.models.simulationservice;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Accessors(chain = true)
+public class SimulationParams implements Serializable {
+	private String petri;
+	private Map<String, Double> initials;
+	private List<Double> tspan;
+	private Map<String, Double> params;
+}
