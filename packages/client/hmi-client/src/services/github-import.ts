83ebf772c454e9cf40ceb06c4cafafa1c22f9faa
Shawn Yama <syama@uncharted.software>
2023-03-31 12:05:08 -0400

    try catch

--- packages/client/hmi-client/src/services/github-import.ts
+++ packages/client/hmi-client/src/services/github-import.ts
@@ -1,15 +1,30 @@
 import API from '@/api/api';
+import { logger } from '@/utils/logger';
 
 export async function getGithubRepositoryContent(repoOwnerAndName: string, path: string) {
-	const response = await API.get('/code/repo_content', {
-		params: { repoOwnerAndName, path }
-	});
-	return response?.data ?? null;
+	try {
+		const response = await API.get('/code/repo_content', {
+			params: { repoOwnerAndName, path }
+		});
+		const { status, data } = response;
+		if (status !== 200) return null;
+		return data ?? null;
+	} catch (error) {
+		logger.error(error);
+		return null;
+	}
 }
 
 export async function getGithubCode(repoOwnerAndName: string, path: string) {
-	const response = await API.get('/code/repo_file_content', {
-		params: { repoOwnerAndName, path }
-	});
-	return response?.data ?? null;
+	try {
+		const response = await API.get('/code/repo_file_content', {
+			params: { repoOwnerAndName, path }
+		});
+		const { status, data } = response;
+		if (status !== 200) return null;
+		return data ?? null;
+	} catch (error) {
+		logger.error(error);
+		return null;
+	}
 }
