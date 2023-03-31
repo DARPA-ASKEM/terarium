b3c3d5bd90aa535929a2d933645b58d513479b33
Yohann Paris <github@yohannparis.com>
2023-03-31 14:14:15 -0400

    Add some data to Overview

--- packages/client/hmi-client/src/utils/date.ts
+++ packages/client/hmi-client/src/utils/date.ts
@@ -1,11 +1,20 @@
 export function formatDdMmmYyyy(timestamp) {
-	return new Date(timestamp).toLocaleDateString(undefined, {
+	return new Date(timestamp).toLocaleDateString('en-US', {
 		year: 'numeric',
 		month: 'short',
 		day: 'numeric'
 	});
 }
 
+export function formatLong(timestamp): string {
+	return new Date(timestamp).toLocaleDateString('en-US', {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	});
+}
+
 export function formatMillisToDate(millis: number) {
-	return new Date(millis).toLocaleDateString();
+	return new Date(millis).toLocaleDateString('en-US');
 }
