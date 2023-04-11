0bfece326bb9d95a885bcbd9d9a01220a5fbb439
Yohann Paris <github@yohannparis.com>
2023-04-11 16:30:29 -0400

    basic endpoint for user server sent events

---
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/resources/user/ServerSentEventResource.java
@@ -0,0 +1,33 @@
+package software.uncharted.terarium.hmiserver.resources.user;
+
+
+import com.oracle.svm.core.annotate.Inject;
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.identity.SecurityIdentity;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.resteasy.annotations.SseElementType;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api/user/")
+@Authenticated
+@Tag(name = "Server Sent Events Endpoints")
+public class ServerSentEventResource {
+
+	@Inject
+	SecurityIdentity securityIdentity;
+
+	/**
+	 * Gets all user events
+	 */
+	@GET
+	@Path("/server-sent-events")
+	@Produces(MediaType.SERVER_SENT_EVENTS)
+	@SseElementType("text/plain")
+	public UserEvents<String> stream() {
+		return newUserEvents;
+	}
+}
