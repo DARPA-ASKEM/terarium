85efd4415379ab59805b96cdd7f5b4aa3c4512bf
Shawn Yama <syama@uncharted.software>
2023-04-20 16:08:48 -0400

    init

---
+++ packages/client/hmi-client/src/components/widgets/tera-infinite-canvas.vue
@@ -0,0 +1,5 @@
+<template>
+	<slot name="toolbar" />
+	<slot name="nodes" />
+	<slot name="background" />
+</template>
---
+++ packages/client/hmi-client/src/temp/tera-simulation-workflow.vue
@@ -0,0 +1,11 @@
+<template>
+	<infinite-canvas>
+		<template #toolbar></template>
+		<template #nodes></template>
+		<template #background></template>
+	</infinite-canvas>
+</template>
+
+<script setup lang="ts">
+import InfiniteCanvas from '@/components/widgets/tera-infinite-canvas.vue';
+</script>
