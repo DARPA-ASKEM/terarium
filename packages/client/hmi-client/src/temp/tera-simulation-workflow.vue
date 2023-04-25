5fc8bf9a3acec8d51ee340a3e5e0529547201f07
mwdchang <mwdchang@gmail.com>
2023-04-25 16:07:05 -0400

    clean up

--- packages/client/hmi-client/src/temp/tera-simulation-workflow.vue
+++ packages/client/hmi-client/src/temp/tera-simulation-workflow.vue
@@ -2,7 +2,7 @@
 	<infinite-canvas>
 		<template #foreground></template>
 		<template #data>
-			<div>ssss</div>
+			<div style="font-size: 24px; padding: 10px; background: #9ef">This is a DataLayer DIV</div>
 		</template>
 	</infinite-canvas>
 </template>
