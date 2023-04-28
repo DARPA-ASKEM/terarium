ff399b21d41a4bfadfdc92784bb97c4244486d73
Shawn Yama <syama@uncharted.software>
2023-04-28 13:05:22 -0400

    project models

--- packages/client/hmi-client/src/components/workflow/tera-model-node.vue
+++ packages/client/hmi-client/src/components/workflow/tera-model-node.vue
@@ -1,6 +1,22 @@
-<template>sds</template>
+<template>
+	<Dropdown
+		class="w-full md:w-14rem"
+		v-model="selectedModel"
+		:options="models"
+		option-label="name"
+		placeholder="Select a model"
+	/>
+</template>
 
 <script setup lang="ts">
-// import Dropdown from 'primevue/dropdown';
+import { ref } from 'vue';
+import Dropdown from 'primevue/dropdown';
 // import { Operation } from '@/types/workflow';
+import { Model } from '@/types/Model';
+
+defineProps<{
+	models: Model[];
+}>();
+
+const selectedModel = ref();
 </script>
--- packages/client/hmi-client/src/temp/tera-simulation-workflow.vue
+++ packages/client/hmi-client/src/temp/tera-simulation-workflow.vue
@@ -3,14 +3,24 @@
 		<template #foreground></template>
 		<template #data>
 			<div style="font-size: 24px; padding: 10px; background: #9ef">This is a DataLayer DIV</div>
-			<tera-model-node />
+			<tera-model-node v-if="models" :models="models" />
 		</template>
 	</infinite-canvas>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import InfiniteCanvas from '@/components/widgets/tera-infinite-canvas.vue';
 import TeraModelNode from '@/components/workflow/tera-model-node.vue';
+import { IProject } from '@/types/Project';
+
+const props = defineProps<{
+	project: IProject;
+}>();
+
+const models = computed(() => props.project?.assets?.models);
+
+console.log(props.project);
 
 const lastTransform = {
 	k: 0.75,
