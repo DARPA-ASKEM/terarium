ca72db6509c0d61c2fc06f7c6be1f947762c83c5
mwdchang <mwdchang@gmail.com>
2023-04-27 23:35:35 -0400

    fix test case to use id

--- packages/client/hmi-client/tests/unit/workflow/workflow.spec.ts
+++ packages/client/hmi-client/tests/unit/workflow/workflow.spec.ts
@@ -19,9 +19,9 @@ const addOperation: Operation = {
 	// and returns an outputs to the node
 	action: (v: WorkflowPort[]) => {
 		if (v.length && v[0].type === 'number') {
-			return [{ type: 'number', value: v[0].value + v[1].value }];
+			return [{ id: '0', type: 'number', value: v[0].value + v[1].value }];
 		}
-		return [{ type: null, value: null }];
+		return [{ id: '0', type: null, value: null }];
 	}
 };
 
@@ -45,7 +45,7 @@ const addEdge = (
 	if (d) {
 		const targetNode = wf.nodes.find((n) => n.id === target);
 		if (targetNode) {
-			const targetNodePort = targetNode?.outputs.find((o) => o.id === targetPortId);
+			const targetNodePort = targetNode.inputs.find((o) => o.id === targetPortId);
 			if (targetNodePort) {
 				targetNodePort.type = d.type;
 				targetNodePort.value = d.value;
@@ -75,7 +75,9 @@ const runNode = (node: WorkflowNode): void => {
 	const operation = operationLib.get(opType);
 
 	if (!operation) return;
-	node.outputs = operation.action(node.inputs);
+	if (operation.action) {
+		node.outputs = operation.action(node.inputs);
+	}
 };
 
 const plusNode = (id: string) =>
@@ -83,8 +85,12 @@ const plusNode = (id: string) =>
 		id,
 		workflowId: '0',
 		operationType: 'add',
-		inputs: [],
-		outputs: [],
+		inputs: operationLib
+			.get('add')
+			?.inputs.map((d, i) => ({ id: `${i}`, type: d.type, value: null })),
+		outputs: operationLib
+			.get('add')
+			?.inputs.map((d, i) => ({ id: `${i}`, type: d.type, value: null })),
 		x: 0,
 		y: 0,
 		width: 0,
@@ -107,17 +113,29 @@ describe('basic tests to make sure it all works', () => {
 		workflow.nodes.push(Z);
 
 		// Pretend we are linking connections
-		X.inputs.push({ id: '1', type: 'number', value: 1 });
-		X.inputs.push({ id: '2', type: 'number', value: 2 });
-
-		Y.inputs.push({ id: '3', type: 'number', value: 3 });
-		Y.inputs.push({ id: '4', type: 'number', value: 4 });
+		let temp: WorkflowPort | undefined;
+		temp = X.inputs.find((d) => d.id === '0');
+		if (temp) {
+			temp.value = 1;
+		}
+		temp = X.inputs.find((d) => d.id === '1');
+		if (temp) {
+			temp.value = 2;
+		}
+		temp = Y.inputs.find((d) => d.id === '0');
+		if (temp) {
+			temp.value = 3;
+		}
+		temp = Y.inputs.find((d) => d.id === '1');
+		if (temp) {
+			temp.value = 4;
+		}
 
 		runNode(X); // should be 3
 		runNode(Y); // should be 7
 
-		addEdge(workflow, 'X', 0, 'Z', 0);
-		addEdge(workflow, 'Y', 0, 'Z', 1);
+		addEdge(workflow, 'X', '0', 'Z', '0');
+		addEdge(workflow, 'Y', '0', 'Z', '1');
 		runNode(Z); // should be 10
 
 		// Run
