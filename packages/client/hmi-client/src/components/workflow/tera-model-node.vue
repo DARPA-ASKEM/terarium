9288853b94e69fb13764549e18a663f60ba276b6
Shawn Yama <syama@uncharted.software>
2023-04-28 11:44:03 -0400

    r

--- packages/client/hmi-client/src/components/workflow/tera-model-node.vue
+++ packages/client/hmi-client/src/components/workflow/tera-model-node.vue
@@ -1,3 +1,6 @@
+<template>sds</template>
+
 <script setup lang="ts">
+// import Dropdown from 'primevue/dropdown';
 // import { Operation } from '@/types/workflow';
 </script>
--- packages/client/hmi-client/src/temp/tera-simulation-workflow.vue
+++ packages/client/hmi-client/src/temp/tera-simulation-workflow.vue
@@ -3,12 +3,14 @@
 		<template #foreground></template>
 		<template #data>
 			<div style="font-size: 24px; padding: 10px; background: #9ef">This is a DataLayer DIV</div>
+			<tera-model-node />
 		</template>
 	</infinite-canvas>
 </template>
 
 <script setup lang="ts">
 import InfiniteCanvas from '@/components/widgets/tera-infinite-canvas.vue';
+import TeraModelNode from '@/components/workflow/tera-model-node.vue';
 
 const lastTransform = {
 	k: 0.75,
