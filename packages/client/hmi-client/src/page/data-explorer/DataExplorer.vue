63c161c55e15e783f98bcbc3af7fa49950f7a95b
Shawn Yama <syama@uncharted.software>
2023-05-03 12:32:59 -0400

    guard search

--- packages/client/hmi-client/src/page/data-explorer/DataExplorer.vue
+++ packages/client/hmi-client/src/page/data-explorer/DataExplorer.vue
@@ -508,9 +508,15 @@ const clearItemSelected = () => {
 // };
 
 async function executeNewQuery() {
-	searchTerm.value = route.query?.q?.toString() ?? searchTerm.value;
+	const previousSearchTerm = searchTerm.value;
 
-	if (!searchTerm.value) return; // Empty string is an invalid search term
+	// If search query is not empty update the search term
+	if (!isEmpty(route.query?.q?.toString()) && route.query?.q?.toString()) {
+		searchTerm.value = route.query?.q?.toString();
+	}
+
+	// If the search term is empty or is the same as the previous term don't execute a search
+	if (isEmpty(searchTerm.value) || previousSearchTerm === searchTerm.value) return;
 
 	// search term has changed, so all search results are dirty; need re-fetch
 	disableSearchByExample();
