4560a0d8f5b291ff54fca264d94009fb0c59b595
Yohann Paris <github@yohannparis.com>
2023-05-09 14:58:59 +0200

    Update UserEvent.java

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/user/UserEvent.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/user/UserEvent.java
@@ -1,9 +1,6 @@
 package software.uncharted.terarium.hmiserver.models.user;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.oracle.svm.core.annotate.Inject;
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import io.quarkus.security.identity.SecurityIdentity;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import software.uncharted.terarium.hmiserver.models.EventType;
@@ -14,10 +11,6 @@ import java.util.UUID;
 @Data
 @Accessors(chain = true)
 public class UserEvent {
-
-	@Inject
-	SecurityIdentity securityIdentity;
-
 	private EventType type;
 	private User user;
 	private UUID id;
