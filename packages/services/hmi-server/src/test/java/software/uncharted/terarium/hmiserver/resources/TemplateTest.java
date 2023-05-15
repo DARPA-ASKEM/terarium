1aa443660d78185a8ea8f7b963551b998f9b029d
Yohann Paris <github@yohannparis.com>
2023-05-15 10:52:32 +0200

    Create TemplateTest.java

---
+++ packages/services/hmi-server/src/test/java/software/uncharted/terarium/hmiserver/resources/TemplateTest.java
@@ -0,0 +1,14 @@
+package software.uncharted.terarium.hmiserver.resources;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class TemplateTest {
+
+	@Test
+	public void testAssertionIsTrue() {
+		Assertions.assertTrue(true);
+	}
+}
