f8c589af38a8dc5f5dfdd162c4efb7ccbba499f7
mwdchang <mwdchang@gmail.com>
2023-05-16 18:34:52 -0400

    amr petrinet to igraph converter

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/petrinet/PetriNetModel.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/petrinet/PetriNetModel.java
@@ -1,5 +1,8 @@
 package software.uncharted.terarium.hmiserver.models.dataservice.petrinet;
 
+import software.uncharted.terarium.hmiserver.annotations.TSModel;
+import software.uncharted.terarium.hmiserver.annotations.TSOptional;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -10,9 +13,14 @@ import java.util.List;
 
 @Data
 @Accessors(chain = true)
+@TSModel
 public class PetriNetModel {
 	private List<PetriNetState> states;
 	private List<PetriNetTransition> transitions;
+
+	@TSOptional
 	private List<ModelParameter> parameters;
+
+	@TSOptional
 	private ModelMetadata metadata;
 }
