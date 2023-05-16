2c50d1049bad0eabc8f43b7be43f85fbe67eef80
Yohann Paris <github@yohannparis.com>
2023-05-16 13:28:18 +0200

    Update ServerSentEventResourceTest.java

--- packages/services/hmi-server/src/test/java/software/uncharted/terarium/hmiserver/resources/user/ServerSentEventResourceTest.java
+++ packages/services/hmi-server/src/test/java/software/uncharted/terarium/hmiserver/resources/user/ServerSentEventResourceTest.java
@@ -2,6 +2,9 @@ package software.uncharted.terarium.hmiserver.resources.user;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSender;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import software.uncharted.terarium.hmiserver.models.EventType;
 import software.uncharted.terarium.hmiserver.models.user.User;
@@ -11,6 +14,7 @@ import javax.inject.Inject;
 import javax.ws.rs.core.MediaType;
 
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
@@ -25,16 +29,13 @@ public class ServerSentEventResourceTest {
 	@Test
 	@TestHTTPEndpoint(ServerSentEventResource.class)
 	public void testIfUserEventAreFilteredByActiveUser() {
-		// Send a couple of message for Alice and the current user
-		userEventService.send(type, "Alice", alice);
-		userEventService.send(type, "Alice", alice);
-		userEventService.send(type, "Current");
-		userEventService.send(type, "Current");
-
-		when().get("/server-sent-events")
+		when().get("/api/server-sent-events")
 			.then()
 			.statusCode(200)
 			.contentType(MediaType.SERVER_SENT_EVENTS)
-			.body(containsString("Current"));
+			.body(not(containsString("Alice")));
+
+		userEventService.send(type, "Alice", alice);
+		userEventService.send(type, "Current");
 	}
 }
