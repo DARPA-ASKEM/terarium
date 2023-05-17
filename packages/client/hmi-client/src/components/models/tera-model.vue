c1e609c7d9c3bd5f24cb4ac6a0e3a0283068fdbc
Shawn Yama <syama@uncharted.software>
2023-05-17 08:36:50 -0400

    cell edit modal and checkboxes

--- packages/client/hmi-client/src/components/models/tera-model.vue
+++ packages/client/hmi-client/src/components/models/tera-model.vue
@@ -31,7 +31,7 @@
 				:disabled="isEditing"
 				class="p-button-sm"
 			/>
-			<Button @click="printConfig" label="Print config" class="p-button-sm" />
+			<Button @click="printConfig(9)" label="Print config" class="p-button-sm" />
 		</template>
 		<Accordion :multiple="true" :active-index="[0, 1, 2, 3, 4]">
 			<AccordionTab header="Description">
@@ -133,32 +133,29 @@
 				<template #header> Model configurations </template>
 				<DataTable
 					class="model-configuration"
+					v-model:selection="selectedModelConfig"
 					:value="modelConfiguration"
 					editMode="cell"
-					@cell-edit-complete="onCellEditComplete"
 					showGridlines
+					@cell-edit-init="onCellEditStart"
+					@cell-edit-complete="onCellEditComplete"
 				>
 					<ColumnGroup type="header">
 						<!--Style top rows-->
 						<Row>
+							<Column header="" />
 							<Column header="" />
 							<Column header="Initial conditions" :colspan="model.content.S.length" />
 							<Column header="Parameters" :colspan="model.content?.T.length" />
 							<!-- <Column header="Observables" /> -->
 						</Row>
 						<Row>
-							<Column>
-								<template #header> <Checkbox :binary="true" />Select all </template>
-							</Column>
-							<Column
-								v-for="(s, i) of model.content.S"
-								:key="i"
-								:header="s.sname"
-								@click="printConfig(i)"
-							/>
+							<Column selection-mode="multiple" headerStyle="width: 3rem" />
+							<Column header="Select all" />
+							<Column v-for="(s, i) of model.content.S" :key="i" :header="s.sname" />
 							<Column v-for="(t, i) of model.content.T" :key="i" :header="t.tname" />
 						</Row>
-						<Row>
+						<!-- <Row> Add show in workflow later
 							<Column header="Show in workflow" />
 							<Column v-for="(s, i) of model.content.S" :key="i">
 								<template #header>
@@ -170,24 +167,36 @@
 									<Checkbox :binary="true" />
 								</template>
 							</Column>
-						</Row>
+						</Row> -->
 					</ColumnGroup>
+					<Column selection-mode="multiple" headerStyle="width: 3rem" />
 					<Column field="name">
 						<template #body="{ data, field }">
-							<Checkbox :binary="true" />{{ data[field] }}
+							{{ data[field] }}
 						</template>
 						<template #editor="{ data, field }">
 							<InputText v-model="data[field]" autofocus />
 						</template>
 					</Column>
-					<Column v-for="(s, i) of model.content.S" :key="i" :field="s.sname">
+					<Column
+						v-for="(value, i) of [...model.content.S, ...model.content.T]"
+						:key="i"
+						:field="value.sname ?? value.tname"
+					>
 						<template #body="{ data, field }">
-							<div @click="printConfig(i)">{{ data[field] }}</div>
+							{{ data[field] }}
+						</template>
+						<template #editor="{ data, field }">
+							{{ data[field] }}
 						</template>
 					</Column>
-					<Column v-for="(t, i) of model.content.T" :key="i" :field="t.tname" />
 				</DataTable>
-				<Button icon="pi pi-plus" label="Add configuration" @click="addModelConfiguration" />
+				<Button
+					class="p-button-sm p-button-outlined"
+					icon="pi pi-plus"
+					label="Add configuration"
+					@click="addModelConfiguration"
+				/>
 			</AccordionTab>
 			<AccordionTab>
 				<template #header>
@@ -330,11 +339,50 @@
 				@launch-forecast="goToSimulationRunPage"
 			/>
 		</Teleport>
+		<Teleport to="body">
+			<tera-modal v-if="openValueConfig" @modal-mask-clicked="openValueConfig = false">
+				<template #header>
+					<h4>{{ cellValueToEdit.field }}</h4>
+					<span>Select a value for this configuration</span>
+				</template>
+				<template #default>
+					<TabView>
+						<TabPanel v-for="(tab, i) in fakeExtractions" :key="tab" :header="tab">
+							<div>
+								<label for="name">Name</label>
+								<InputText class="p-inputtext-sm" v-model="fakeExtractions[i]" />
+							</div>
+							<div>
+								<label for="name">Source</label>
+								<InputText class="p-inputtext-sm" />
+							</div>
+							<div>
+								<label for="name">Value</label>
+								<InputText
+									class="p-inputtext-sm"
+									v-model="cellValueToEdit.data[cellValueToEdit.field]"
+								/>
+							</div>
+						</TabPanel>
+					</TabView>
+					<Button
+						class="p-button-sm p-button-outlined"
+						icon="pi pi-plus"
+						label="Add value"
+						@click="addConfigValue"
+					/>
+				</template>
+				<template #footer>
+					<Button label="OK" @click="updateModelConfigValue" />
+					<Button class="p-button-outlined" label="Cancel" @click="openValueConfig = false" />
+				</template>
+			</tera-modal>
+		</Teleport>
 	</tera-asset>
 </template>
 
 <script setup lang="ts">
-import { remove, isEmpty, pickBy, isArray } from 'lodash';
+import { remove, isEmpty, pickBy, isArray, cloneDeep } from 'lodash';
 import { IGraph } from '@graph-scaffolder/index';
 import { watch, ref, computed, onMounted, onUnmounted, onUpdated, PropType } from 'vue';
 import { runDagreLayout } from '@/services/graph';
@@ -359,8 +407,9 @@ import { RouteName } from '@/router/routes';
 import useResourcesStore from '@/stores/resources';
 import { logger } from '@/utils/logger';
 import Button from 'primevue/button';
-import Checkbox from 'primevue/checkbox';
 import Accordion from 'primevue/accordion';
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
 import AccordionTab from 'primevue/accordiontab';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
@@ -382,6 +431,7 @@ import Toolbar from 'primevue/toolbar';
 import { FilterMatchMode } from 'primevue/api';
 import ModelParameterList from '@/components/models/tera-model-parameter-list.vue';
 import { IProject, ProjectAssetTypes } from '@/types/Project';
+import TeraModal from '@/components/widgets/tera-modal.vue';
 import TeraResizablePanel from '../widgets/tera-resizable-panel.vue';
 
 interface StringValueMap {
@@ -411,33 +461,6 @@ const props = defineProps({
 		required: false
 	}
 });
-
-const initialModelConfig = ref([{ name: 'Config 1' }]);
-
-const initialValues = ref<StringValueMap>({});
-const parameterValues = ref<StringValueMap>({});
-
-const modelConfiguration = computed(() => {
-	const newModelConfiguration: any[] = [];
-
-	for (let i = 0; i < initialModelConfig.value.length; i++) {
-		newModelConfiguration.push({
-			name: initialModelConfig.value[i].name,
-			...initialValues.value,
-			...parameterValues.value
-		});
-	}
-
-	return newModelConfiguration;
-});
-
-function printConfig(i) {
-	console.log(i, modelConfiguration.value);
-}
-
-function addModelConfiguration() {
-	initialModelConfig.value.push({ name: 'New config' });
-}
 
 const resources = useResourcesStore();
 const router = useRouter();
@@ -482,6 +505,44 @@ let renderer: PetrinetRenderer | null = null;
 let eventX = 0;
 let eventY = 0;
 
+const modelConfigNames = ref([{ name: 'Config 1' }]);
+const fakeExtractions = ref(['Resource 1', 'Resource 2', 'Resource 3']);
+
+const initialValues = ref<StringValueMap[]>([{}]);
+const parameterValues = ref<StringValueMap[]>([{}]);
+const openValueConfig = ref(false);
+const cellValueToEdit = ref({ data: {}, field: '', index: 0 });
+
+const selectedModelConfig = ref();
+
+const modelConfiguration = computed(() => {
+	const newModelConfiguration: any[] = [];
+
+	for (let i = 0; i < modelConfigNames.value.length; i++) {
+		newModelConfiguration.push({
+			name: modelConfigNames.value[i].name,
+			...initialValues.value[i],
+			...parameterValues.value[i]
+		});
+	}
+
+	return newModelConfiguration;
+});
+
+function printConfig(i = 9) {
+	console.log(i, modelConfiguration.value, model.value, selectedModelConfig.value);
+}
+
+function addModelConfiguration() {
+	modelConfigNames.value.push({ name: `Config ${modelConfigNames.value.length + 1}` });
+	initialValues.value.push(cloneDeep(initialValues.value[initialValues.value.length - 1]));
+	parameterValues.value.push(cloneDeep(parameterValues.value[parameterValues.value.length - 1]));
+}
+
+function addConfigValue() {
+	fakeExtractions.value.push(`Resource ${fakeExtractions.value.length + 1}`);
+}
+
 const onCellEditComplete = (event) => {
 	const { data, newValue, field } = event;
 
@@ -493,6 +554,27 @@ const onCellEditComplete = (event) => {
 			break;
 	}
 };
+
+const onCellEditStart = (event) => {
+	const { data, field, index } = event;
+
+	if (field !== 'name') {
+		openValueConfig.value = true;
+		cellValueToEdit.value = { data, field, index };
+	}
+};
+
+function updateModelConfigValue() {
+	const { data, field, index } = cellValueToEdit.value;
+
+	if (initialValues.value[index][field]) {
+		initialValues.value[index][field] = data[field];
+	} else if (parameterValues.value[index][field]) {
+		parameterValues.value[index][field] = data[field];
+	}
+
+	openValueConfig.value = false;
+}
 
 const updateLayout = () => {
 	if (splitterContainer.value) {
@@ -997,12 +1079,20 @@ watch(
 	() => model.value,
 	async () => {
 		if (model.value) {
+			// Reset model config on model change
+			modelConfigNames.value = [{ name: 'Config 1' }];
+			fakeExtractions.value = ['Resource 1', 'Resource 2', 'Resource 3'];
+			initialValues.value = [{}];
+			parameterValues.value = [{}];
+			openValueConfig.value = false;
+			cellValueToEdit.value = { data: {}, field: '', index: 0 };
+
 			model.value?.content.S.forEach((s) => {
-				initialValues.value[s.sname] = `${1}`;
+				initialValues.value[0][s.sname] = `${1}`;
 			});
 
 			model.value?.content.T.forEach((s) => {
-				parameterValues.value[s.tname] = `${0.0005}`;
+				parameterValues.value[0][s.tname] = `${0.0005}`;
 			});
 		}
 	}
@@ -1092,6 +1182,70 @@ section math-editor {
 .p-datatable:deep(td:hover) {
 	background-color: var(--surface-secondary);
 	cursor: pointer;
+}
+
+.p-tabview {
+	display: flex;
+	gap: 1rem;
+	margin-bottom: 1rem;
+	justify-content: space-between;
+}
+
+.p-tabview:deep(> *) {
+	width: 50vw;
+	height: 50vh;
+	overflow: auto;
+}
+
+.p-tabview:deep(.p-tabview-nav) {
+	flex-direction: column;
+}
+
+.p-tabview:deep(label) {
+	display: block;
+	font-size: var(--font-caption);
+	margin-bottom: 0.25rem;
+}
+
+.p-tabview:deep(.p-tabview-nav-container, .p-tabview-nav-content) {
+	width: 100%;
+}
+
+.p-tabview:deep(.p-tabview-panels) {
+	border-radius: var(--border-radius);
+	border: 1px solid var(--surface-border-light);
+	background-color: var(--surface-ground);
+}
+
+.p-tabview:deep(.p-tabview-panel) {
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+}
+
+.p-tabview:deep(.p-tabview-nav li) {
+	border-left: 3px solid transparent;
+}
+
+.p-tabview:deep(.p-tabview-nav .p-tabview-header:nth-last-child(n + 3)) {
+	border-bottom: 1px solid var(--surface-border-light);
+}
+
+.p-tabview:deep(.p-tabview-nav li.p-highlight) {
+	border-left: 3px solid var(--primary-color);
+	background: var(--surface-highlight);
+}
+
+.p-tabview:deep(.p-tabview-nav li.p-highlight .p-tabview-nav-link) {
+	background: none;
+}
+
+.p-tabview:deep(.p-inputtext) {
+	width: 100%;
+}
+
+.p-tabview:deep(.p-tabview-nav .p-tabview-ink-bar) {
+	display: none;
 }
 
 .tera-split-panel {
