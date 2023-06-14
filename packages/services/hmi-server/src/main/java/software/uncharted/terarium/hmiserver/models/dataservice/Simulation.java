7c36bb1cfbb13c8e6da37cbc8f3b1ab44c4228ff
Tom Szendrey <T.Szendrey@hotmail.com>
2023-06-14 15:55:47 -0400

    adding some @TSOptional

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Simulation.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Simulation.java
@@ -12,23 +12,33 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 @TSModel
 // Used as the TDS Simulations object
 public class Simulation {
-	@TSOptional
 	private String id;
 	@JsonAlias("execution_payload")
 	private Object executionPayload;
+	
 	@JsonAlias("result_files")
+	@TSOptional
 	private List<String> resultFiles;
 	private String type;
 	private String status;
+	
 	@JsonAlias("start_time")
+	@TSOptional
 	private LocalDateTime startTime;
+	
 	@JsonAlias("completed_time")
+	@TSOptional
 	private LocalDateTime completedTime;
 	private String engine;
+	
 	@JsonAlias("workflow_id")
 	private String workflowId;
+	
 	@JsonAlias("user_id")
+	@TSOptional
 	private Integer userId;
+	
 	@JsonAlias("project_id")
+	@TSOptional
 	private Integer projectId;
 }
