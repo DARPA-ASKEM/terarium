3fef77614ea64698964da3771850a9fc9074f861
mwdchang <mwdchang@gmail.com>
2023-06-14 16:04:48 -0400

    fix error

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Simulation.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Simulation.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 import software.uncharted.terarium.hmiserver.annotations.TSModel;
 import software.uncharted.terarium.hmiserver.annotations.TSOptional;
-import software.uncharted.terarium.hmiserver.models.simulationservice.SimulationParams;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,29 +16,29 @@ public class Simulation {
 	private String id;
 	@JsonAlias("execution_payload")
 	private Object executionPayload;
-	
+
 	@JsonAlias("result_files")
 	@TSOptional
 	private List<String> resultFiles;
 	private String type;
 	private String status;
-	
+
 	@JsonAlias("start_time")
 	@TSOptional
 	private LocalDateTime startTime;
-	
+
 	@JsonAlias("completed_time")
 	@TSOptional
 	private LocalDateTime completedTime;
 	private String engine;
-	
+
 	@JsonAlias("workflow_id")
 	private String workflowId;
-	
+
 	@JsonAlias("user_id")
 	@TSOptional
 	private Integer userId;
-	
+
 	@JsonAlias("project_id")
 	@TSOptional
 	private Integer projectId;
