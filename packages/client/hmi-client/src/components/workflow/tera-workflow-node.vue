5f34b1bfd351997c92e84c39ef7c23e1b911ae5d
Edwin Lai <edwinslai@gmail.com>
2023-06-20 10:48:40 -0400

    remove console logs

--- packages/client/hmi-client/src/components/workflow/tera-workflow-node.vue
+++ packages/client/hmi-client/src/components/workflow/tera-workflow-node.vue
@@ -216,8 +216,6 @@ function showNodeDrilldown() {
 function mouseoverPort(event) {
 	const el = event.target as HTMLElement;
 	const portElement = (el.firstChild as HTMLElement) ?? el;
-	console.log(el);
-	console.log(portElement);
 	const portDirection = portElement.className.split(' ')[0];
 	const nodePosition: Position = { x: props.node.x, y: props.node.y };
 	const totalOffsetX = portElement.offsetLeft + (portDirection === 'input' ? 0 : portBaseSize);
