2907c884cc4e2bda1f6b0b8c4b4d39affd97d5a7
mwdchang <mwdchang@gmail.com>
2023-07-03 10:36:52 -0400

    fix string value for model-configuration

--- packages/client/hmi-client/src/services/model-configurations.ts
+++ packages/client/hmi-client/src/services/model-configurations.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import API from '@/api/api';
 import { ModelConfiguration, Model } from '@/types/Types';
 
@@ -27,6 +28,18 @@ export const createModelConfiguration = async (
 };
 
 export const updateModelConfiguration = async (config: ModelConfiguration) => {
+	// Do a sanity pass to ensure type-safety
+	const model: Model = config.configuration as Model;
+	const parameters = model.semantics?.ode.parameters;
+	if (parameters) {
+		parameters.forEach((param) => {
+			if (param.value && typeof param.value === 'string' && _.isNumber(+param.value)) {
+				param.value = +param.value;
+				console.debug(`corerce ${param.id} ${param.value} to number`);
+			}
+		});
+	}
+
 	const response = await API.put(`/model_configurations/${config.id}`, config);
 	return response?.data ?? null;
 };
