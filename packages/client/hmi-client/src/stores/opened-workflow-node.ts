5d126f1b57de9162f0b3b897b0784ddffab9ab92
Shawn Yama <syama@uncharted.software>
2023-07-04 18:00:25 -0400

    delete some node store stuff

--- packages/client/hmi-client/src/stores/opened-workflow-node.ts
+++ packages/client/hmi-client/src/stores/opened-workflow-node.ts
@@ -1,9 +1,7 @@
 import { defineStore } from 'pinia';
 import { ProjectAssetTypes } from '@/types/Project';
-import { WorkflowNode, WorkflowPortStatus } from '@/types/workflow';
+import { WorkflowNode } from '@/types/workflow';
 import { TspanUnits, ChartConfig } from '@/types/SimulateConfig';
-import { NumericValueMap } from '@/types/common';
-import { v4 as uuidv4 } from 'uuid';
 
 // Probably will be an array later
 export const useOpenedWorkflowNodeStore = defineStore('opened-workflow-node', {
@@ -13,8 +11,6 @@ export const useOpenedWorkflowNodeStore = defineStore('opened-workflow-node', {
 		selectedOutputIndex: 0 as number,
 		// model node
 		// FIXME: won't need a store for model config values once workflow/model configs are saved
-		initialValues: null as NumericValueMap[] | null,
-		parameterValues: null as NumericValueMap[] | null,
 		node: null as WorkflowNode | null,
 		// simulate node
 		numCharts: 1,
@@ -36,18 +32,6 @@ export const useOpenedWorkflowNodeStore = defineStore('opened-workflow-node', {
 		// simulate node
 		setNode(node: WorkflowNode | null) {
 			this.node = node;
-		},
-		// FIXME: Not a good idea to update reactive variables through global storage
-		appendOutputPort(port: { type: string; label?: string; value: any }) {
-			if (this.node) {
-				this.node.outputs.push({
-					id: uuidv4(),
-					type: port.type,
-					label: port.label,
-					value: [port.value],
-					status: WorkflowPortStatus.NOT_CONNECTED
-				});
-			}
 		},
 		appendChart() {
 			this.numCharts++;
