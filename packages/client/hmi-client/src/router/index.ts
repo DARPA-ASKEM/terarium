f40685d2d9377359a80460b4435c40fa79b9d6b0
mwdchang <mwdchang@gmail.com>
2023-07-06 22:36:49 -0400

    clean up

--- packages/client/hmi-client/src/router/index.ts
+++ packages/client/hmi-client/src/router/index.ts
@@ -10,7 +10,6 @@ import ModelRunnerView from '@/temp/ModelRunner.vue';
 import ResponsivePlayground from '@/temp/ResponsivePlayground.vue';
 import TheiaView from '@/temp/theia.vue';
 import SSE from '@/temp/sse.vue';
-import WorkflowPlayground from '@/temp/workflow-playground/WorkflowPlayground.vue';
 import SimulationWorkflow from '@/components/workflow/tera-simulation-workflow.vue';
 import { RouteName } from './routes';
 
@@ -47,7 +46,6 @@ const routes = [
 	{ path: RoutePath.ResponsivePlaygroundPath, component: ResponsivePlayground },
 	{ path: RoutePath.ModelEditor, component: ModelEditorView },
 	{ path: RoutePath.ModelRunner, component: ModelRunnerView },
-	{ path: '/workflow-playground', component: WorkflowPlayground },
 	{ path: '/simulation-workflow', component: SimulationWorkflow },
 	{ path: '/sse', component: SSE }
 ];
