dff3597d311ec0b86e0d469dd17ae12c3f23b5ef
Jamie Waese <120480244+jamiewaese-uncharted@users.noreply.github.com>
2023-07-10 14:30:39 -0400

    feat: new icon in overview banner

--- packages/client/hmi-client/src/page/project/components/tera-project-overview.vue
+++ packages/client/hmi-client/src/page/project/components/tera-project-overview.vue
@@ -400,8 +400,8 @@ a {
 }
 
 .overview-banner {
-	background: url('@/assets/svg/terarium-icon-transparent.svg') no-repeat right 10% center,
-		linear-gradient(45deg, #d5e8e5 0%, #f0f4f0 100%) no-repeat;
+	background: url('@/assets/svg/terarium-icon-transparent.svg') no-repeat right 20% center,
+		linear-gradient(45deg, #8bd4af1a, #d5e8e5 100%) no-repeat;
 	background-size: 25%, 100%;
 	height: auto;
 }
