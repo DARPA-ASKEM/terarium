e40fdf8aadbc867e3d85c096b0795278cd4855b3
Derek Vince <dvince@uncharted.software>
2023-07-11 10:21:02 -0400

    feat(hmi): allow users to import documents from GitHub and disk (#1343)
    Co-authored-by: Yohann Paris <github@yohannparis.com>

--- packages/client/hmi-client/src/types/Types.ts
+++ packages/client/hmi-client/src/types/Types.ts
@@ -33,6 +33,17 @@ export interface GithubRepo {
     totalFiles: number;
 }
 
+export interface Artifact {
+    id?: string;
+    timestamp?: any;
+    username: string;
+    name: string;
+    description?: string;
+    fileNames: string[];
+    metadata?: any;
+    concepts?: Concept[];
+}
+
 export interface CsvAsset {
     csv: string[][];
     stats?: CsvColumnStats[];
@@ -172,6 +183,14 @@ export interface Links {
     html: string;
     git: string;
     self: string;
+}
+
+export interface Concept {
+    id: string;
+    curie: string;
+    type: Type;
+    status: OntologicalField;
+    object_id: string;
 }
 
 export interface ModelSemantics {
@@ -509,4 +528,19 @@ export enum ColumnType {
     Datetime = "DATETIME",
     Date = "DATE",
     Time = "TIME",
+}
+
+export enum Type {
+    Datasets = "DATASETS",
+    Extractions = "EXTRACTIONS",
+    Intermediates = "INTERMEDIATES",
+    Models = "MODELS",
+    Plans = "PLANS",
+    Publications = "PUBLICATIONS",
+    SimulationRuns = "SIMULATION_RUNS",
+}
+
+export enum OntologicalField {
+    Object = "OBJECT",
+    Unit = "UNIT",
 }
