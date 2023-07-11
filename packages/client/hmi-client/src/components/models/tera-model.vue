961323ecefee59582835099a5b402a1553ae3045
Shawn Yama <syama@uncharted.software>
2023-07-11 06:42:20 -0400

    fix: default to description view on model page (#1344)

--- packages/client/hmi-client/src/components/models/tera-model.vue
+++ packages/client/hmi-client/src/components/models/tera-model.vue
@@ -623,6 +623,7 @@ const fetchRelatedTerariumArtifacts = async () => {
 watch(
 	() => [props.assetId],
 	async () => {
+		modelView.value = ModelView.DESCRIPTION;
 		if (props.assetId !== '') {
 			model.value = await getModel(props.assetId);
 			fetchRelatedTerariumArtifacts();
