11f34146d9add5d842defe549a6c08b2e0655a22
Edwin Lai <edwinslai@gmail.com>
2023-07-13 18:10:39 -0400

    feat: stratify step 3 (#1401)
    Co-authored-by: mwdchang <mwdchang@gmail.com>

--- packages/client/hmi-client/src/components/models/tera-model-diagram.vue
+++ packages/client/hmi-client/src/components/models/tera-model-diagram.vue
@@ -253,8 +253,7 @@ const handleResize = () => {
 	updateLayout();
 };
 
-const setNewEquation = (index: number, latexEq: string, mathmlEq: string) => {
-	console.log(mathmlEq);
+const setNewEquation = (index: number, latexEq: string) => {
 	latexEquationList.value[index] = latexEq;
 };
 
