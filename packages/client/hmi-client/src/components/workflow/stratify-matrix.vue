42806c23bfa2256116d59daa5bd0bd4dcc35866c
Shawn Yama <syama@uncharted.software>
2023-07-13 08:36:28 -0400

    s

---
+++ packages/client/hmi-client/src/components/workflow/stratify-matrix.vue
@@ -0,0 +1,19 @@
+<template>
+	<vue-pivottable-ui
+		:data="[
+			{ color: 'blue', shape: 'circle' },
+			{ color: 'red', shape: 'triangle' }
+		]"
+		:rows="['color']"
+		:cols="['shape']"
+	>
+	</vue-pivottable-ui>
+</template>
+
+<script setup lang="ts">
+// import 'jquery/dist/jquery.min.js';
+// import $ from 'jquery';
+
+import { VuePivottableUi } from 'vue-pivottable';
+// import 'vue-pivottable/dist/vue-pivottable.css'
+</script>
