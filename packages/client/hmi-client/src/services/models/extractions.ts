7e434ccaf3dd4853837ce1513fd96ba2d0865aae
Yohann Paris <github@yohannparis.com>
2023-07-14 14:37:39 -0400

    Update extractions.ts

--- packages/client/hmi-client/src/services/models/extractions.ts
+++ packages/client/hmi-client/src/services/models/extractions.ts
@@ -1,20 +1,45 @@
-import API from '@/api/api';
+import API, { Poller } from '@/api/api';
 import { AxiosError } from 'axios';
 import { Model } from '@/types/Types';
 import { logger } from '@/utils/logger';
 
-// Transform a MathML list of strings to an AMR
+/**
+ * Fetch information from the extraction service via the Poller utility
+ * @param id
+ * @return {Promise<T | void>}
+ */
+async function fetchExtraction(id: string): Promise<T | void> {
+	const poller = new Poller<object>()
+		.setInterval(2000)
+		.setThreshold(90)
+		.setPollAction(async (): Promise<T | null> => {
+			const response = await API.get(`/extract/status/${id}`);
+			if (response?.status === 200 && response?.data?.status === 'finished') {
+				return response.data.result as T;
+			}
+			return null;
+		});
+	return poller.start();
+}
+
+/**
+ * Transform a MathML list of strings to an AMR
+ */
 const mathmlToAMR = async (mathml: string[], framework = 'petrinet'): Promise<Model | null> => {
 	try {
 		const response = await API.post(`/extract/mathml-to-amr?framework=${framework}`, mathml);
 		if (response && response?.status === 200) {
-			return (response?.data as Model) ?? null;
+			const { id, status } = response.data;
+			if (status === 'queue') {
+				return (await fetchExtraction(id)) as Model | null;
+			}
+			if (status === 'finished') {
+				return response.data.result as Model;
+			}
 		}
 		logger.error(
 			`MathML to AMR: Extraction-service Server did not provide a correct response [HTTP ${response?.status}]`,
-			{
-				showToast: false
-			}
+			{ showToast: false }
 		);
 	} catch (error: unknown) {
 		if ((error as AxiosError).isAxiosError) {
