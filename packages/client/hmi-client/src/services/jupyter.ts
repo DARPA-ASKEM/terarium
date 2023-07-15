ad5003de691b5862649fc404cda1b8003f6167e6
Matthew Printz <matt@jataware.com>
2023-07-14 23:55:18 -0400

    Hammer the typescript error

--- packages/client/hmi-client/src/services/jupyter.ts
+++ packages/client/hmi-client/src/services/jupyter.ts
@@ -94,6 +94,7 @@ export interface IJupyterMessageContent {
 	text?: string;
 	code?: string;
 	language?: string;
+	data?: any;
 }
 
 export interface IJupyterMessage<T extends JupyterMessageType = JupyterMessageType> {
@@ -108,7 +109,7 @@ export interface IJupyterMessage<T extends JupyterMessageType = JupyterMessageTy
 	/**
 	 * The content of the message.
 	 */
-	content: IJupyterMessageContent;
+	content: IJupyterMessageContent | any;
 	/**
 	 * The message header.
 	 */
@@ -120,7 +121,7 @@ export interface IJupyterMessage<T extends JupyterMessageType = JupyterMessageTy
 	/**
 	 * The parent message
 	 */
-	parent_header: messages.IHeader | {};
+	parent_header: messages.IHeader | any;
 }
 
 export declare type JupyterMessage = IJupyterMessage | messages.Message;
