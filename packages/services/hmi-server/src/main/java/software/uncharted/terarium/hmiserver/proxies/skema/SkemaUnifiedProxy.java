a28ce2b372c3e608490a218189c92a81da1c02a7
Yohann Paris <github@yohannparis.com>
2023-07-16 22:11:44 -0400

    Update SkemaUnifiedProxy.java

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/proxies/skema/SkemaUnifiedProxy.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/proxies/skema/SkemaUnifiedProxy.java
@@ -17,7 +17,8 @@ public interface SkemaUnifiedProxy {
 
 	/**
 	 * Converts a LaTeX equation to an AMR of different framework via TA1 Skema-unified.
-	 * @param request	the {@link CodeRequest} instance containing the LaTeX equations and model framework
+	 * @param request	the Json containing the LaTeX equations and model framework
+	 *                https://skema-unified.staging.terarium.ai/docs#/workflows/equations_to_amr_workflows_latex_equations_to_amr_post
 	 *                ie: { equations: [ "equation1", "equation2", ... ], framework: "framework" }
 	 * @return AMR Model
 	 */
