47f0dd0409b2c9735fafebe86582e5bea484a308
Al-Jami Ismail <aljami1379@gmail.com>
2023-07-26 10:35:38 -0400

    update java formatting

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/proxies/documentservice/DocumentProxy.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/proxies/documentservice/DocumentProxy.java
@@ -17,29 +17,30 @@ public interface DocumentProxy {
 	@Path("/api/v2/articles")
 	@LogRestClientTime
 	XDDResponse<DocumentsResponseOK> getDocuments(
-			@QueryParam("api_key") String apiKey,
-			@QueryParam("docid") String docid,
-			@QueryParam("doi") String doi,
-			@QueryParam("title") String title,
-			@QueryParam("term") String term,
-			@QueryParam("dataset") String dataset,
-			@QueryParam("include_score") String include_score,
-			@QueryParam("include_highlights") String include_highlights,
-			@QueryParam("inclusive") String inclusive,
-			@QueryParam("full_results") String full_results,
-			@QueryParam("max") String max,
-			@QueryParam("per_page") String per_page,
-			@QueryParam("dict") String dict,
-			@QueryParam("facets") String facets,
-			@QueryParam("min_published") String min_published,
-			@QueryParam("max_published") String max_published,
-			@QueryParam("pubname") String pubname,
-			@QueryParam("publisher") String publisher,
-			@QueryParam("additional_fields") String additional_fields,
-			@QueryParam("match") String match,
-			@QueryParam("known_entities") String known_entities,
-			@QueryParam("github_url") String github_url,
-			@QueryParam("similar_to") String similar_to);
+		@QueryParam("api_key") String apiKey,
+		@QueryParam("docid") String docid,
+		@QueryParam("doi") String doi,
+		@QueryParam("title") String title,
+		@QueryParam("term") String term,
+		@QueryParam("dataset") String dataset,
+		@QueryParam("include_score") String include_score,
+		@QueryParam("include_highlights") String include_highlights,
+		@QueryParam("inclusive") String inclusive,
+		@QueryParam("full_results") String full_results,
+		@QueryParam("max") String max,
+		@QueryParam("per_page") String per_page,
+		@QueryParam("dict") String dict,
+		@QueryParam("facets") String facets,
+		@QueryParam("min_published") String min_published,
+		@QueryParam("max_published") String max_published,
+		@QueryParam("pubname") String pubname,
+		@QueryParam("publisher") String publisher,
+		@QueryParam("additional_fields") String additional_fields,
+		@QueryParam("match") String match,
+		@QueryParam("known_entities") String known_entities,
+		@QueryParam("github_url") String github_url,
+		@QueryParam("similar_to") String similar_to
+	);
 
 	@GET
 	@Path("/sets")
@@ -52,13 +53,14 @@ public interface DocumentProxy {
 	@GET
 	@Path("/sets/{set}/doc2vec/api/similar")
 	XDDRelatedDocumentsResponse getRelatedDocuments(
-			@PathParam("set") String set,
-			@QueryParam("docid") String docid);
+		@PathParam("set") String set,
+		@QueryParam("docid") String docid
+	);
 
 	@GET
 	@Path("/sets/{set}/word2vec/api/most_similar")
 	XDDRelatedWordsResponse getRelatedWords(
-			@PathParam("set") String set,
-			@QueryParam("word") String word);
-
+		@PathParam("set") String set,
+		@QueryParam("word") String word
+	);
 }
--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/resources/documentservice/DocumentResource.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/resources/documentservice/DocumentResource.java
@@ -28,44 +28,43 @@ public class DocumentResource {
 	@ConfigProperty(name = "xdd.api_es_key")
 	Optional<String> apiKey;
 
-	// NOTE: the query parameters match the proxy version and the type
-	// XDDSearchPayload
+	// NOTE: the query parameters match the proxy version and the type XDDSearchPayload
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Tag(name = "Get all xdd documents via proxy")
 	@Path("/documents")
 	@APIResponses({
-			@APIResponse(responseCode = "500", description = "An error occurred retrieving documents"),
-			@APIResponse(responseCode = "204", description = "Request received successfully, but there are documents"),
-			@APIResponse(responseCode = "400", description = "Query must contain one of docid, doi or term") })
+		@APIResponse(responseCode = "500", description = "An error occurred retrieving documents"),
+		@APIResponse(responseCode = "204", description = "Request received successfully, but there are documents"),
+		@APIResponse(responseCode = "400", description = "Query must contain one of docid, doi or term") })
 	public Response getDocuments(
-			@QueryParam("docid") String docid,
-			@QueryParam("doi") String doi,
-			@QueryParam("title") String title,
-			@QueryParam("term") String term,
-			@QueryParam("dataset") String dataset,
-			@QueryParam("include_score") String include_score,
-			@QueryParam("include_highlights") String include_highlights,
-			@QueryParam("inclusive") String inclusive,
-			@QueryParam("full_results") String full_results,
-			@QueryParam("max") String max,
-			@QueryParam("per_page") String per_page,
-			@QueryParam("dict") String dict,
-			@QueryParam("facets") String facets,
-			@QueryParam("min_published") String min_published,
-			@QueryParam("max_published") String max_published,
-			@QueryParam("pubname") String pubname,
-			@QueryParam("publisher") String publisher,
-			@QueryParam("additional_fields") String additional_fields,
-			@QueryParam("match") String match,
-			@QueryParam("known_entities") String known_entities,
-			@QueryParam("github_url") String github_url,
-			@QueryParam("similar_to") String similar_to) {
+		@QueryParam("docid") String docid,
+		@QueryParam("doi") String doi,
+		@QueryParam("title") String title,
+		@QueryParam("term") String term,
+		@QueryParam("dataset") String dataset,
+		@QueryParam("include_score") String include_score,
+		@QueryParam("include_highlights") String include_highlights,
+		@QueryParam("inclusive") String inclusive,
+		@QueryParam("full_results") String full_results,
+		@QueryParam("max") String max,
+		@QueryParam("per_page") String per_page,
+		@QueryParam("dict") String dict,
+		@QueryParam("facets") String facets,
+		@QueryParam("min_published") String min_published,
+		@QueryParam("max_published") String max_published,
+		@QueryParam("pubname") String pubname,
+		@QueryParam("publisher") String publisher,
+		@QueryParam("additional_fields") String additional_fields,
+		@QueryParam("match") String match,
+		@QueryParam("known_entities") String known_entities,
+		@QueryParam("github_url") String github_url,
+		@QueryParam("similar_to") String similar_to
+	) {
 
 		// only go ahead with the query if at least one param is present
 		if (docid != null || doi != null || term != null || github_url != null || similar_to != null) {
-			// for a more direct search, if doi is valid, then make sure other params are
-			// null
+			// for a more direct search, if doi is valid, then make sure other params are null
 			if (docid != null || doi != null) {
 				title = null;
 				term = null;
@@ -96,15 +95,13 @@ public class DocumentResource {
 				}
 
 				XDDResponse<DocumentsResponseOK> doc = proxy.getDocuments(apiKey,
-						docid, doi, title, term, dataset, include_score, include_highlights, inclusive, full_results,
-						max, per_page, dict, facets,
-						min_published, max_published, pubname, publisher, additional_fields, match, known_entities,
-						github_url, similar_to);
+						docid, doi, title, term, dataset, include_score, include_highlights, inclusive, full_results, max, per_page, dict, facets,
+						min_published, max_published, pubname, publisher, additional_fields, match, known_entities, github_url, similar_to);
 
 				if (doc.getErrorMessage() != null) {
 					return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-							.entity(doc.getErrorMessage())
-							.build();
+						.entity(doc.getErrorMessage())
+						.build();
 				}
 
 				if (doc.getSuccess() == null || doc.getSuccess().getData().isEmpty()) {
@@ -121,5 +118,4 @@ public class DocumentResource {
 		}
 		return Response.status(Response.Status.BAD_REQUEST).build();
 	}
-
 }
