e12986aefba5810839a4730545771d8c5d889a9b
Shawn Yama <syama@uncharted.software>
2023-07-27 17:25:59 -0400

    feat: multiple chart lines (#1552)
    Co-authored-by: Daniel Chang <mwdchang@gmail.com>
    Co-authored-by: Tom Szendrey <T.Szendrey@hotmail.com>

--- packages/client/hmi-client/src/components/workflow/tera-calibrate-ensemble-ciemss.vue
+++ packages/client/hmi-client/src/components/workflow/tera-calibrate-ensemble-ciemss.vue
@@ -23,10 +23,9 @@
 			<tera-simulate-chart
 				v-for="(cfg, index) of node.state.chartConfigs"
 				:key="index"
-				:run-results="renderedRuns"
+				:run-results="runResults"
 				:chartConfig="cfg"
-				:line-color-array="lineColorArray"
-				:line-width-array="lineWidthArray"
+				has-mean-line
 				@configuration-change="chartConfigurationChange(index, $event)"
 			/>
 			<Button
@@ -177,11 +176,21 @@
 						</thead>
 						<tbody class="p-datatable-tbody">
 							<td>Steps</td>
-							<td><InputNumber v-model="timeSpan.start" /></td>
-							<td><InputNumber v-model="timeSpan.end" /></td>
-							<td><InputNumber v-model="extra.numSamples" /></td>
-							<td><InputNumber v-model="extra.numIterations" /></td>
-							<td><InputNumber v-model="extra.totalPopulation" /></td>
+							<td>
+								<InputNumber v-model="timeSpan.start" />
+							</td>
+							<td>
+								<InputNumber v-model="timeSpan.end" />
+							</td>
+							<td>
+								<InputNumber v-model="extra.numSamples" />
+							</td>
+							<td>
+								<InputNumber v-model="extra.numIterations" />
+							</td>
+							<td>
+								<InputNumber v-model="extra.totalPopulation" />
+							</td>
 						</tbody>
 					</table>
 				</AccordionTab>
@@ -263,7 +272,6 @@ const customWeights = ref<boolean>(false);
 const disabledCalibrationWeights = computed(() => true);
 const newSolutionMappingKey = ref<string>('');
 const runResults = ref<RunResults>({});
-const renderedRuns = ref<RunResults>({});
 
 // Tom TODO: Make this generic... its copy paste from node.
 const chartConfigurationChange = (index: number, config: ChartConfig) => {
@@ -276,20 +284,6 @@ const chartConfigurationChange = (index: number, config: ChartConfig) => {
 		state
 	});
 };
-
-const lineColorArray = computed(() => {
-	const output = Array(Math.max(Object.keys(runResults.value).length ?? 0 - 1, 0)).fill(
-		'#00000020'
-	);
-	output.push('#1b8073');
-	return output;
-});
-
-const lineWidthArray = computed(() => {
-	const output = Array(Math.max(Object.keys(runResults.value).length ?? 0 - 1, 0)).fill(1);
-	output.push(2);
-	return output;
-});
 
 const calculateWeights = () => {
 	if (!ensembleConfigs.value) return;
@@ -496,45 +490,6 @@ watch(
 	},
 	{ immediate: true }
 );
-
-// process run result data to create mean run line
-watch(
-	() => runResults.value,
-	(input) => {
-		const runResult: RunResults = JSON.parse(JSON.stringify(input));
-
-		// convert to array from array-like object
-		const parsedSimProbData = Object.values(runResult);
-
-		const numRuns = parsedSimProbData.length;
-		if (!numRuns) {
-			renderedRuns.value = runResult;
-			return;
-		}
-
-		const numTimestamps = (parsedSimProbData as { [key: string]: number }[][])[0].length;
-		const aggregateRun: { [key: string]: number }[] = [];
-
-		for (let timestamp = 0; timestamp < numTimestamps; timestamp++) {
-			for (let run = 0; run < numRuns; run++) {
-				if (!aggregateRun[timestamp]) {
-					aggregateRun[timestamp] = parsedSimProbData[run][timestamp];
-					Object.keys(aggregateRun[timestamp]).forEach((key) => {
-						aggregateRun[timestamp][key] = Number(aggregateRun[timestamp][key]) / numRuns;
-					});
-				} else {
-					const datum = parsedSimProbData[run][timestamp];
-					Object.keys(datum).forEach((key) => {
-						aggregateRun[timestamp][key] += datum[key] / numRuns;
-					});
-				}
-			}
-		}
-
-		renderedRuns.value = { ...runResult, [numRuns]: aggregateRun };
-	},
-	{ immediate: true, deep: true }
-);
 </script>
 
 <style scoped>
@@ -573,6 +528,7 @@ watch(
 	height: 200px;
 	/* width: 80%; */
 }
+
 .model-weights {
 	display: flex;
 }
@@ -581,6 +537,7 @@ watch(
 	display: flex;
 	margin: 1em;
 }
+
 th {
 	text-align: left;
 }
