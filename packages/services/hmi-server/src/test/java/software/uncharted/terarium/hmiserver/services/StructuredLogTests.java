841d01e82b6bcc004b57d2fa3e5bf98e30a3530d
dvince <dvince@uncharted.software>
2023-08-10 09:07:23 -0400

    Restoring tests

--- packages/services/hmi-server/src/test/java/software/uncharted/terarium/hmiserver/services/StructuredLogTests.java
+++ packages/services/hmi-server/src/test/java/software/uncharted/terarium/hmiserver/services/StructuredLogTests.java
@@ -11,17 +11,17 @@ public class StructuredLogTests {
 	@Inject
 	StructuredLog structuredLog;
 
-
+	@Test
 	public void testItThrowsWhenOddNumberOfArguments() {
 		Assertions.assertThrows(RuntimeException.class, () -> structuredLog.log(StructuredLog.Type.EVENT, "adam", "single_element"));
 	}
 
-
+	@Test
 	public void testItDoesntThrowWhenNoArguments() {
 		Assertions.assertDoesNotThrow(() -> structuredLog.log(StructuredLog.Type.EVENT, "adam"));
 	}
 
-
+	@Test
 	public void testItAllowsNullUser() {
 		Assertions.assertDoesNotThrow(() -> structuredLog.log(StructuredLog.Type.EVENT, null, "foo", "bar"));
 	}
