77e6aecc8a66bbb3a8eec724d403531311266492
dvince <dvince@uncharted.software>
2023-08-14 16:19:07 -0400

    Removing remaining AWS usages.

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/resources/dataservice/ArtifactResource.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/resources/dataservice/ArtifactResource.java
@@ -1,15 +1,22 @@
 package software.uncharted.terarium.hmiserver.resources.dataservice;
 
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-import software.amazon.awssdk.http.SdkHttpResponse;
 import software.uncharted.terarium.hmiserver.models.dataservice.Artifact;
 import software.uncharted.terarium.hmiserver.models.dataservice.dataset.PresignedURL;
 import software.uncharted.terarium.hmiserver.proxies.dataservice.ArtifactProxy;
 import software.uncharted.terarium.hmiserver.proxies.jsdelivr.JsDelivrProxy;
-import software.uncharted.terarium.hmiserver.resources.DataStorageResource;
 import software.uncharted.terarium.hmiserver.resources.SnakeCaseResource;
 
 import javax.inject.Inject;
@@ -18,15 +25,16 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+
 
 @Path("/api/artifacts")
 @Produces(MediaType.APPLICATION_JSON)
 @Tag(name = "Artifacts")
 @Slf4j
-public class ArtifactResource extends DataStorageResource implements SnakeCaseResource {
+public class ArtifactResource implements SnakeCaseResource {
 
 	@Inject
 	@RestClient
@@ -36,101 +44,103 @@ public class ArtifactResource extends DataStorageResource implements SnakeCaseRe
 	@Inject
 	JsDelivrProxy gitHubProxy;
 
-	@ConfigProperty(name = "aws.artifact_path")
-	Optional<String> artifactPath;
-
-
-
-
 
 	@GET
+	@Tag(name = "Get Artifacts from TDS Proxy")
 	public List<Artifact> getArtifacts(@DefaultValue("100") @QueryParam("page_size") Integer pageSize,
 															@DefaultValue("0") @QueryParam("page") Integer page) {
 		return artifactProxy.getArtifacts(pageSize, page);
 	}
 
 	@PUT
+	@Tag(name = "Create Artifact in TDS Proxy")
 	public Artifact createArtifact(Artifact artifact) {
 		return artifactProxy.createArtifact(convertObjectToSnakeCaseJsonNode(artifact));
 	}
 
 	@GET
 	@Path("/{id}")
+	@Tag(name = "Get specific Artifact from TDS Proxy")
 	public Artifact getArtifact(@PathParam("id") String artifactId) {
 		return artifactProxy.getArtifact(artifactId);
 	}
 
 	@PUT
 	@Path("/{id}")
+	@Tag(name = "Update Artifact in TDS Proxy")
 	public Artifact updateArtifact(@PathParam("id") String artifactId, Artifact artifact) {
 		return artifactProxy.updateArtifact(artifactId, artifact);
 	}
 
 	@DELETE
 	@Path("/{id}")
+	@Tag(name = "Delete Artifact from TDS Proxy")
 	public Response deleteArtifact(@PathParam("id") String artifactId) {
 		return artifactProxy.deleteArtifact(artifactId);
 	}
 
-	@GET
-	@Path("/{id}/download-url")
-	public PresignedURL getArtifactDownloadUrl(@PathParam("id") String artifactId, @QueryParam("filename") String filename) {
-		return artifactProxy.getArtifactDownloadUrl(artifactId, filename);
-	}
-
-	@GET
-	@Path("/{id}/upload-url")
-	public PresignedURL getArtifactUploadUrl(@PathParam("id") String artifactId, @QueryParam("filename") String filename) {
-		return artifactProxy.getArtifactUploadUrl(artifactId, filename);
-	}
 
 	@GET
 	@Path("/{id}/download-file-as-text")
+	@Tag(name = "Download Artifact file from TDS Proxy as text")
 	public Response downloadFileAsText(@PathParam("id") String artifactId, @QueryParam("filename") String filename) {
 
-		log.debug("Downloading artifact {} from project", artifactId);
+		try (CloseableHttpClient httpclient = HttpClients.custom()
+			.disableRedirectHandling()
+			.build()) {
 
-		if (artifactPath.isEmpty()) {
-			log.error("Artifact path not set");
+			final PresignedURL presignedURL = artifactProxy.getArtifactDownloadUrl(artifactId, filename);
+			final HttpGet get = new HttpGet(presignedURL.getUrl());
+			final HttpResponse response = httpclient.execute(get);
+			final String textFileAsString = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
+
+			return Response.status(Response.Status.OK).entity(textFileAsString).type(MediaType.TEXT_PLAIN).build();
+
+		} catch (Exception e) {
+			log.error("Unable to GET file as string data", e);
 			return Response
 				.status(Response.Status.INTERNAL_SERVER_ERROR)
 				.type(MediaType.APPLICATION_JSON)
 				.build();
 		}
-
-		String objectKey = String.format("%s/%s/%s", artifactPath.get(), artifactId, filename);
-
-		String fileAsString =  downloadStringFromS3(objectKey);
-
-		return Response.status(Response.Status.OK).entity(fileAsString).type(MediaType.TEXT_PLAIN).build();
 
 	}
 
 	@GET
 	@Path("/{id}/download-file")
+	@Tag(name = "Download Artifact file from TDS Proxy")
 	public Response downloadFile(@PathParam("id") String artifactId, @QueryParam("filename") String filename) {
 
 		log.debug("Downloading artifact {} from project", artifactId);
 
-		if (artifactPath.isEmpty()) {
-			log.error("Artifact path not set");
+
+		try (CloseableHttpClient httpclient = HttpClients.custom()
+				.disableRedirectHandling()
+				.build()) {
+
+			final PresignedURL presignedURL = artifactProxy.getArtifactDownloadUrl(artifactId, filename);
+			final HttpGet get = new HttpGet(presignedURL.getUrl());
+			final HttpResponse response = httpclient.execute(get);
+			if(response.getStatusLine().getStatusCode() == 200 && response.getEntity() != null) {
+				byte[] fileAsBytes = response.getEntity().getContent().readAllBytes();
+				return Response.status(Response.Status.OK).entity(fileAsBytes).type(MediaType.APPLICATION_OCTET_STREAM).build();
+			}
+			return Response.status(response.getStatusLine().getStatusCode()).build();
+
+		} catch (Exception e) {
+			log.error("Unable to GET artifact data", e);
 			return Response
-				.status(Response.Status.INTERNAL_SERVER_ERROR)
-				.type(MediaType.APPLICATION_JSON)
-				.build();
+					.status(Response.Status.INTERNAL_SERVER_ERROR)
+					.type(MediaType.APPLICATION_JSON)
+					.build();
 		}
-
-		String objectKey = String.format("%s/%s/%s", artifactPath.get(), artifactId, filename);
-
-		byte[] fileAsBytes =  downloadBytesFromS3(objectKey);
-
-		return Response.status(Response.Status.OK).entity(fileAsBytes).build();
 
 	}
 
 	@PUT
 	@Path("/{artifactId}/uploadFile")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Tag(name = "Upload Artifact file to TDS Proxy")
 	public Response uploadFile(
 		@PathParam("artifactId") final String artifactId,
 		@QueryParam("filename") final String filename,
@@ -139,35 +149,9 @@ public class ArtifactResource extends DataStorageResource implements SnakeCaseRe
 
 		log.debug("Uploading artifact {} to project", artifactId);
 
-
-		if (artifactPath.isEmpty()) {
-			log.error("Artifact path not set");
-			return Response
-				.status(Response.Status.INTERNAL_SERVER_ERROR)
-				.type(MediaType.APPLICATION_JSON)
-				.build();
-		}
-
-		String objectKey = String.format("%s/%s/%s", artifactPath.get(), artifactId, filename);
-
-		SdkHttpResponse res = uploadBytesToS3(objectKey, input.get("file").readAllBytes());
-
-		//find the status of the response
-		if (res != null && res.isSuccessful()) {
-			log.debug("Successfully uploaded file file to dataset {}", artifactId);
-			return Response
-				.status(Response.Status.OK)
-				.type(MediaType.APPLICATION_JSON)
-				.build();
-		} else {
-			log.error("Failed to upload file file to dataset {}", artifactId);
-			if(res == null  || res.statusText().isEmpty()) {
-				return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON)
-					.build();
-			}
-			return Response.status(res.statusCode(), res.statusText().get()).type(MediaType.APPLICATION_JSON)
-				.build();
-		}
+		byte[] fileAsBytes = input.get("file").readAllBytes();
+		HttpEntity fileEntity = new ByteArrayEntity(fileAsBytes, ContentType.APPLICATION_OCTET_STREAM);
+		return uploadArtifactHelper(artifactId, filename, fileEntity);
 
 
 	}
@@ -178,6 +162,7 @@ public class ArtifactResource extends DataStorageResource implements SnakeCaseRe
 	@PUT
 	@Path("/{artifactId}/uploadArtifactFromGithub")
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Tag(name = "Upload Artifact from Github to TDS Proxy")
 	public Response uploadArtifactFromGithub(
 		@PathParam("artifactId") final String artifactId,
 		@QueryParam("path") final String path,
@@ -186,40 +171,51 @@ public class ArtifactResource extends DataStorageResource implements SnakeCaseRe
 	){
 		log.debug("Uploading artifact file from github to dataset {}", artifactId);
 
-		//verify that dataSetPath and bucket are set. If not, return an error
-		if (artifactPath.isEmpty()) {
-			log.error("artifactPath information not set. Cannot upload file from github.");
+		//download file from GitHub
+		String fileString = gitHubProxy.getGithubCode(repoOwnerAndName, path);
+		HttpEntity fileEntity = new StringEntity(fileString, ContentType.TEXT_PLAIN);
+		return uploadArtifactHelper(artifactId, filename, fileEntity);
+
+	}
+
+	/**
+	 * Uploads an artifact inside the entity to TDS via a presigned URL
+	 * @param artifactId The ID of the artifact to upload to
+	 * @param fileName The name of the file to upload
+	 * @param artifactHttpEntity The entity containing the artifact to upload
+	 * @return A response containing the status of the upload
+	 */
+	private Response uploadArtifactHelper(String artifactId, String fileName, HttpEntity artifactHttpEntity){
+
+		try (CloseableHttpClient httpclient = HttpClients.custom()
+			.disableRedirectHandling()
+			.build()) {
+
+			// upload file to S3
+			final PresignedURL presignedURL = artifactProxy.getArtifactUploadUrl(artifactId, fileName);
+			final HttpPut put = new HttpPut(presignedURL.getUrl());
+			put.setEntity(artifactHttpEntity);
+			final HttpResponse response = httpclient.execute(put);
+;
+
+			return Response
+				.status(response.getStatusLine().getStatusCode())
+				.type(MediaType.APPLICATION_JSON)
+				.build();
+
+
+		} catch (Exception e) {
+			log.error("Unable to PUT artifact data", e);
 			return Response
 				.status(Response.Status.INTERNAL_SERVER_ERROR)
 				.type(MediaType.APPLICATION_JSON)
 				.build();
 		}
-
-		//download file from GitHub
-		String fileString = gitHubProxy.getGithubCode(repoOwnerAndName, path);
-
-		String objectKey = String.format("%s/%s/%s", artifactPath.get(), artifactId, filename);
-
-
-		SdkHttpResponse res = uploadStringToS3(objectKey, fileString);
-
-		//find the status of the response
-		if (res.isSuccessful()) {
-			log.debug("Successfully uploaded artifact file to dataset {}", artifactId);
-			return Response
-				.status(Response.Status.OK)
-				.type(MediaType.APPLICATION_JSON)
-				.build();
-		} else {
-			log.error("Failed to upload artifact file to dataset {}", artifactId);
-			if(res.statusText().isEmpty()) {
-				return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON)
-					.build();
-			}
-			return Response.status(res.statusCode(), res.statusText().get()).type(MediaType.APPLICATION_JSON)
-				.build();
-		}
 	}
+
+
+
+
 
 
 }
