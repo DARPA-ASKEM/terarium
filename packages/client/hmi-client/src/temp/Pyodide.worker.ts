71eb93430c48a9669a4e8c9978d671b3fb99ee89
Manfred Cheung <mj3cheun@uwaterloo.ca>
2023-08-24 10:46:41 -0400

    add skeleton for pyodide in web workers

---
+++ packages/client/hmi-client/src/temp/Pyodide.worker.ts
@@ -0,0 +1,103 @@
+import { loadPyodide } from 'pyodide';
+
+import { Actions } from '@/temp/PyodideController';
+
+const variableMap: Object = {
+	S: 1,
+	I: 2,
+	R: 'x + y',
+	x: 100,
+	y: 1000,
+	N: 'S + I + R'
+};
+
+const startLoad = Date.now();
+const pyodide = await loadPyodide({
+	indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.23.4/full'
+});
+await pyodide.loadPackage('sympy');
+pyodide.runPython('import sympy');
+pyodide.runPython(
+	'from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, convert_xor'
+);
+pyodide.runPython('from sympy.printing.latex import latex');
+pyodide.runPython('from sympy.abc import _clash1, _clash2, _clash');
+pyodide.runPython('from sympy import S');
+
+// Utility function to resolve nested subsitutions - not used
+pyodide.runPython(`
+    def recursive_sub(expr, replace):
+            for _ in range(0, len(replace) + 1):
+                    new_expr = expr.subs(replace)
+                    if new_expr == expr:
+                            return new_expr, True
+                    else:
+                            expr = new_expr
+            return new_expr, False
+`);
+
+pyodide.runPython(`
+def serialize_expr(expr):
+        eq = sympy.S(expr, locals=_clash)
+        return {
+                "latex": latex(eq),
+                "mathml": sympy.mathml(eq),
+                "str": str(eq)
+        }
+`);
+
+// Bootstrap
+const keys = Object.keys(variableMap);
+pyodide.runPython(`
+    ${keys.join(',')} = sympy.symbols('${keys.join(' ')}')
+`);
+
+const endLoad = Date.now();
+
+console.log(endLoad - startLoad);
+postMessage(true);
+
+const runParser = (expr: string) => {
+	if (!endLoad) return;
+	const output = {
+		mathml: '',
+		latex: '',
+		freeSymbols: []
+	};
+	console.log(`evalulating .... [${expr}]`);
+	if (!expr || expr.length === 0) {
+		return output;
+	}
+
+	// function to convert expression to mathml
+	let result = pyodide.runPython(`
+		eq = sympy.S("${expr}", locals=_clash)
+		sympy.mathml(eq)
+	`);
+	output.mathml = result;
+
+	result = pyodide.runPython(`
+		eq = sympy.S("${expr}", locals=_clash)
+		latex(eq)
+	`);
+	output.latex = result;
+
+	result = pyodide.runPython(`
+		eq = sympy.S("${expr}", locals=_clash)
+		list(eq.free_symbols)
+	`);
+	output.freeSymbols = result.toString();
+
+	return output;
+};
+
+onmessage = function (e) {
+	const { action, params } = e.data;
+
+	switch (action) {
+		case Actions.runParser:
+			return postMessage(runParser.apply(null, params));
+		default:
+			console.error(`${action} INVALID`);
+	}
+};
---
+++ packages/client/hmi-client/src/temp/PyodideController.ts
@@ -0,0 +1,64 @@
+export enum Actions {
+	runParser
+}
+
+export default class PyodideController {
+	private _isReady = false;
+	private _isBusy = false;
+	private worker: Promise<Worker>;
+	private taskQueue: any[] = [];
+
+	constructor() {
+		this.worker = new Promise((resolve, reject) => {
+			const worker = new Worker(new URL('./Pyodide.worker.ts', import.meta.url), {
+				type: 'module'
+			});
+			worker.onmessage = (e: any) => {
+				if (e.data) {
+					this._isReady = true;
+					resolve(worker);
+				} else {
+					console.error('Pyodide Init Error');
+					reject();
+				}
+			};
+		});
+	}
+
+	get isReady() {
+		return this._isReady;
+	}
+
+	get isBusy() {
+		return this._isBusy;
+	}
+
+	async runParser(expr) {
+		return new Promise((...promise) => {
+			this.taskQueue.push({
+				action: Actions.runParser,
+				params: [expr],
+				promise
+			});
+			this.queueTask();
+		});
+	}
+
+	private async queueTask() {
+		const worker = await this.worker;
+
+		if (this.taskQueue.length && !this._isBusy) {
+			this._isBusy = true;
+			const { action, params, promise } = this.taskQueue.pop();
+			worker.onmessage = (e) => {
+				this._isBusy = false;
+				this.queueTask();
+				promise[0](e.data);
+			};
+			worker.postMessage({
+				action,
+				params
+			});
+		}
+	}
+}
