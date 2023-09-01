7958d20702643014405ab665cc55dedd80b874c9
Daniel Chang <dchang@Daniels-MacBook-Pro.local>
2023-09-01 10:29:30 -0400

    adding header definition

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Model.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Model.java
@@ -1,6 +1,7 @@
 package software.uncharted.terarium.hmiserver.models.dataservice;
 
 import software.uncharted.terarium.hmiserver.models.dataservice.modelparts.ModelMetadata;
+import software.uncharted.terarium.hmiserver.models.dataservice.modelparts.ModelHeader;
 import software.uncharted.terarium.hmiserver.models.dataservice.modelparts.ModelSemantics;
 import software.uncharted.terarium.hmiserver.annotations.TSModel;
 import software.uncharted.terarium.hmiserver.annotations.TSOptional;
@@ -20,18 +21,28 @@ import java.util.Map;
 public class Model implements Serializable {
 	private String id;
 
+	// To be removed
+	// See: https://github.com/DARPA-ASKEM/Model-Representations/commit/f2c90f16c6c3865f71d1e727e15bc2f0b1f5ec58
+	@deprecated
 	private String name;
 
+	@deprecated
 	@JsonSetter(nulls = Nulls.SKIP)
 	private String description = "";
 
+	@deprecated
 	@TSOptional
 	private String model_version;
 
+	@deprecated
 	private String schema;
 
+	@deprecated
 	@TSOptional
 	private String schema_name;
+	// End
+
+	private ModelHeader header;
 
 	private Map<String, Object> model;
 
