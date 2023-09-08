d3905b175caca34af1e66bb8b45c7d732f4c9e55
Yohann Paris <github@yohannparis.com>
2023-09-08 15:53:27 -0400

    Update WorkflowNode.vue

--- packages/client/hmi-client/src/page/WorkflowNode.vue
+++ packages/client/hmi-client/src/page/WorkflowNode.vue
@@ -1,9 +1,45 @@
 <template>
-	<component :is="component" :node="node" />
+	<tera-model-workflow-wrapper
+		v-if="node.operationType === WorkflowOperationTypes.MODEL"
+		:node="node"
+	/>
+	<tera-stratify v-if="node.operationType === WorkflowOperationTypes.STRATIFY" :node="node" />
+	<tera-dataset-transformer
+		v-if="node.operationType === WorkflowOperationTypes.DATASET_TRANSFORMER"
+		:node="node"
+	/>
+	<tera-dataset-workflow-wrapper
+		v-if="node.operationType === WorkflowOperationTypes.DATASET"
+		:node="node"
+	/>
+	<tera-calibration-julia
+		v-if="node.operationType === WorkflowOperationTypes.CALIBRATION_JULIA"
+		:node="node"
+	/>
+	<tera-simulate-julia
+		v-if="node.operationType === WorkflowOperationTypes.SIMULATE_JULIA"
+		:node="node"
+	/>
+	<tera-calibration-ciemss
+		v-if="node.operationType === WorkflowOperationTypes.CALIBRATION_CIEMSS"
+		:node="node"
+	/>
+	<tera-calibrate-ensemble-ciemss
+		v-if="node.operationType === WorkflowOperationTypes.CALIBRATE_ENSEMBLE_CIEMSS"
+		:node="node"
+	/>
+	<tera-simulate-ciemss
+		v-if="node.operationType === WorkflowOperationTypes.SIMULATE_CIEMSS"
+		:node="node"
+	/>
+	<tera-simulate-ensemble-ciemss
+		v-if="node.operationType === WorkflowOperationTypes.SIMULATE_ENSEMBLE_CIEMSS"
+		:node="node"
+	/>
 </template>
 
 <script setup lang="ts">
-import { Component, computed, watch, ref } from 'vue';
+import { watch, ref } from 'vue';
 import { WorkflowNode, WorkflowOperationTypes } from '@/types/workflow';
 import TeraModelWorkflowWrapper from '@/components/workflow/tera-model-workflow-wrapper.vue';
 import TeraDatasetWorkflowWrapper from '@/components/workflow/tera-dataset-workflow-wrapper.vue';
@@ -20,40 +56,14 @@ import * as workflowService from '@/services/workflow';
 const props = defineProps<{ nodeId: string; workflowId: string }>();
 
 const node: WorkflowNode = ref<WorkflowNode>(null);
-const updateNode = async () => {
+
+async function updateNode() {
 	const workflow = await workflowService.getWorkflow(props.workflowId);
 	node.value = workflow.nodes.find((n) => n.id === props.nodeId);
-};
+}
 
 watch(
 	() => props.nodeId,
 	() => updateNode
 );
-
-const component: Component = computed<Component>(() => {
-	switch (node.value.operationType) {
-		case WorkflowOperationTypes.MODEL:
-			return TeraModelWorkflowWrapper;
-		case WorkflowOperationTypes.DATASET:
-			return TeraDatasetWorkflowWrapper;
-		case WorkflowOperationTypes.DATASET_TRANSFORMER:
-			return TeraDatasetTransformer;
-		case WorkflowOperationTypes.CALIBRATION_JULIA:
-			return TeraCalibrationJulia;
-		case WorkflowOperationTypes.SIMULATE_JULIA:
-			return TeraSimulateJulia;
-		case WorkflowOperationTypes.CALIBRATION_CIEMSS:
-			return TeraCalibrationCiemss;
-		case WorkflowOperationTypes.CALIBRATE_ENSEMBLE_CIEMSS:
-			return TeraCalibrateEnsembleCiemss;
-		case WorkflowOperationTypes.SIMULATE_CIEMSS:
-			return TeraSimulateCiemss;
-		case WorkflowOperationTypes.SIMULATE_ENSEMBLE_CIEMSS:
-			return TeraSimulateEnsembleCiemss;
-		case WorkflowOperationTypes.STRATIFY:
-			return TeraStratify;
-		default:
-			return null;
-	}
-});
 </script>
