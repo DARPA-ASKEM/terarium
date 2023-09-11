f35e01429fbadb98f4239ae043a808d02ed664f7
Yohann Paris <github@yohannparis.com>
2023-09-11 11:29:16 -0400

    Update floating-window.ts

--- packages/client/hmi-client/src/utils/floating-window.ts
+++ packages/client/hmi-client/src/utils/floating-window.ts
@@ -1,8 +1,21 @@
 import { logger } from '@/utils/logger';
 
-function open(url: string) {
+interface FloatingWindowOptions {
+	width?: number;
+	height?: number;
+	left?: number;
+	top?: number;
+}
+function open(url: string, options?: FloatingWindowOptions) {
 	// Define other window features as a comma-separated string (optional)
-	const features: string = 'width=600,height=600,left=200,top=200,resizable=yes,scrollbars=yes';
+	const features: string = `
+		width=${options?.width ?? 700},
+		height=${options?.height ?? 700},
+		left=${options?.left ?? 200},
+		top=${options?.top ?? 200},
+		resizable=yes,
+		scrollbars=yes
+	`;
 	const floatingWindow = window.open(url, 'FloatingWindow', features);
 	// Check if the window was blocked by a popup blocker
 	if (!floatingWindow) {
