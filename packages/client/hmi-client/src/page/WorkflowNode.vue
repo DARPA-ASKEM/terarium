4db31a1136f3186e9a800f543ca3011dd3f432cc
Yohann Paris <github@yohannparis.com>
2023-09-11 11:12:35 -0400

    Update WorkflowNode.vue

--- packages/client/hmi-client/src/page/WorkflowNode.vue
+++ packages/client/hmi-client/src/page/WorkflowNode.vue
@@ -1,26 +1,34 @@
 <template>
-	<tera-model-workflow-wrapper v-if="isNodeType(Operation.MODEL)" :node="node" />
-	<tera-stratify v-else-if="isNodeType(Operation.STRATIFY)" :node="node" />
-	<tera-dataset-transformer v-else-if="isNodeType(Operation.DATASET_TRANSFORMER)" :node="node" />
-	<tera-dataset-workflow-wrapper v-else-if="isNodeType(Operation.DATASET)" :node="node" />
-	<tera-calibration-julia v-else-if="isNodeType(Operation.CALIBRATION_JULIA)" :node="node" />
-	<tera-simulate-julia v-else-if="isNodeType(Operation.SIMULATE_JULIA)" :node="node" />
-	<tera-calibration-ciemss v-else-if="isNodeType(Operation.CALIBRATION_CIEMSS)" :node="node" />
-	<tera-calibrate-ensemble-ciemss
-		v-else-if="isNodeType(Operation.CALIBRATE_ENSEMBLE_CIEMSS)"
-		:node="node"
-	/>
-	<tera-simulate-ciemss v-else-if="isNodeType(Operation.SIMULATE_CIEMSS)" :node="node" />
-	<tera-simulate-ensemble-ciemss
-		v-else-if="isNodeType(Operation.SIMULATE_ENSEMBLE_CIEMSS)"
-		:node="node"
-	/>
+	<template v-if="node">
+		<tera-model-workflow-wrapper v-if="isNodeType(OperationType.MODEL)" :node="node" />
+		<tera-stratify v-else-if="isNodeType(OperationType.STRATIFY)" :node="node" />
+		<tera-dataset-transformer
+			v-else-if="isNodeType(OperationType.DATASET_TRANSFORMER)"
+			:node="node"
+		/>
+		<tera-dataset-workflow-wrapper v-else-if="isNodeType(OperationType.DATASET)" :node="node" />
+		<tera-calibration-julia v-else-if="isNodeType(OperationType.CALIBRATION_JULIA)" :node="node" />
+		<tera-simulate-julia v-else-if="isNodeType(OperationType.SIMULATE_JULIA)" :node="node" />
+		<tera-calibration-ciemss
+			v-else-if="isNodeType(OperationType.CALIBRATION_CIEMSS)"
+			:node="node"
+		/>
+		<tera-calibrate-ensemble-ciemss
+			v-else-if="isNodeType(OperationType.CALIBRATE_ENSEMBLE_CIEMSS)"
+			:node="node"
+		/>
+		<tera-simulate-ciemss v-else-if="isNodeType(OperationType.SIMULATE_CIEMSS)" :node="node" />
+		<tera-simulate-ensemble-ciemss
+			v-else-if="isNodeType(OperationType.SIMULATE_ENSEMBLE_CIEMSS)"
+			:node="node"
+		/>
+	</template>
 	<template v-else>{{ node }}</template>
 </template>
 
 <script setup lang="ts">
 import { watch, ref } from 'vue';
-import { WorkflowNode, WorkflowOperationTypes as Operation } from '@/types/workflow';
+import { WorkflowNode, WorkflowOperationTypes as OperationType } from '@/types/workflow';
 import TeraModelWorkflowWrapper from '@/components/workflow/tera-model-workflow-wrapper.vue';
 import TeraDatasetWorkflowWrapper from '@/components/workflow/tera-dataset-workflow-wrapper.vue';
 import TeraDatasetTransformer from '@/components/workflow/tera-dataset-transformer.vue';
@@ -37,7 +45,7 @@ const props = defineProps<{ nodeId: string; workflowId: string }>();
 
 const node = ref<WorkflowNode>();
 
-function isNodeType(type: Operation): boolean {
+function isNodeType(type: OperationType): boolean {
 	return node.value?.operationType === type;
 }
 
