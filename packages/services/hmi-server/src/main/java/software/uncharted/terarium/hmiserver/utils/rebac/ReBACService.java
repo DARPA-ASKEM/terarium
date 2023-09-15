339e42f539c7ae857f2a4586c212da23376b0221
Charles Coleman <bigglesandginger@gmail.com>
2023-09-15 13:12:52 -0400

    add initla getUsers method

--- packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/utils/rebac/ReBACService.java
+++ packages/services/hmi-server/src/main/java/software/uncharted/terarium/hmiserver/utils/rebac/ReBACService.java
@@ -15,7 +15,10 @@ import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import software.uncharted.terarium.hmiserver.models.permissions.PermissionUser;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
@@ -24,19 +27,33 @@ public class ReBACService {
 	Keycloak keycloak;
 
 	@ConfigProperty(name = "admin.keycloak.realm")
-	private String REALM_NAME;
+	String REALM_NAME;
+	@ConfigProperty(name = "admin.spicedb.shared-key")
+	String SPICEDB_PRESHARED_KEY;
+	@ConfigProperty(name = "admin.spicedb.target")
+	String SPICEDB_TARGET;
 
-
-	private BearerToken bearerToken = new BearerToken("somerandomkeyhere");
+	private BearerToken spiceDbBearerToken;
+	private ManagedChannel channel;
 	private SchemaManager schemaManager = new SchemaManager();
 
 	public static final String PUBLIC_GROUP_NAME = "Public";
 	public static final String ASKEM_ADMIN_GROUP_NAME = "ASKEM_Admins";
 
+	public ReBACService() throws UnsupportedEncodingException {
+	}
+
 	void startup(@Observes StartupEvent event) throws Exception {
-		System.out.println("Init ReBAC");
-		if( !schemaManager.doesSchemaExist(channel, bearerToken) ) {
-			schemaManager.createSchema(channel, bearerToken, Schema.schema);
+		spiceDbBearerToken = new BearerToken(SPICEDB_PRESHARED_KEY);
+		channel = ManagedChannelBuilder
+			.forTarget(SPICEDB_TARGET)
+			//.useTransportSecurity() // for TLS communication
+			.usePlaintext()
+			.build();
+
+		System.out.printf("Init ReBAC");
+		if( !schemaManager.doesSchemaExist(channel, spiceDbBearerToken) ) {
+			schemaManager.createSchema(channel, spiceDbBearerToken, Schema.schema);
 
 			UsersResource usersResource = keycloak.realm(REALM_NAME).users();
 			List<UserRepresentation> users = usersResource.list();
@@ -65,32 +82,55 @@ public class ReBACService {
 		}
 	}
 
-	private ManagedChannel channel = ManagedChannelBuilder
-		.forTarget("localhost:50051")
-		//.useTransportSecurity() // for TLS communication
-		.usePlaintext()
-		.build();
+	public List<PermissionUser> getUsers() {
+		List<PermissionUser> response = new ArrayList<>();
+		UsersResource usersResource = keycloak.realm(REALM_NAME).users();
+		List<UserRepresentation> users = usersResource.list();
+		for (UserRepresentation userRepresentation : users) {
+			if (userRepresentation.getEmail() == null || userRepresentation.getEmail().isBlank()) {
+				continue;
+			}
+			UserResource userResource = usersResource.get(userRepresentation.getId());
+			boolean hasUserRole = false;
+			for (RoleRepresentation roleRepresentation: userResource.roles().getAll().getRealmMappings()) {
+				if (roleRepresentation.getDescription().isBlank()) {
+					if (roleRepresentation.getName().equals("user")) {
+						hasUserRole = true;
+					}
+				}
+			}
+			if (hasUserRole) {
+				PermissionUser user = new PermissionUser(
+					userRepresentation.getId(),
+					userRepresentation.getFirstName(),
+					userRepresentation.getLastName(),
+					userRepresentation.getEmail());
+				response.add(user);
+			}
+		}
+		return response;
+	}
 
 	public boolean canRead(SchemaObject who, SchemaObject what) throws Exception {
 		Consistency full = Consistency.newBuilder().setFullyConsistent(true).build();
-		ReBACFunctions rebac = new ReBACFunctions(channel, bearerToken);
+		ReBACFunctions rebac = new ReBACFunctions(channel, spiceDbBearerToken);
 		return rebac.checkPermission(who, Schema.Permission.READ, what, full);
 	}
 
 	public boolean canWrite(SchemaObject who, SchemaObject what) throws Exception {
 		Consistency full = Consistency.newBuilder().setFullyConsistent(true).build();
-		ReBACFunctions rebac = new ReBACFunctions(channel, bearerToken);
+		ReBACFunctions rebac = new ReBACFunctions(channel, spiceDbBearerToken);
 		return rebac.checkPermission(who, Schema.Permission.WRITE, what, full);
 	}
 
 	public boolean canAdministrate(SchemaObject who, SchemaObject what) throws Exception {
 		Consistency full = Consistency.newBuilder().setFullyConsistent(true).build();
-		ReBACFunctions rebac = new ReBACFunctions(channel, bearerToken);
+		ReBACFunctions rebac = new ReBACFunctions(channel, spiceDbBearerToken);
 		return rebac.checkPermission(who, Schema.Permission.ADMINISTRATE, what, full);
 	}
 
 	public void createRelationship(SchemaObject who, SchemaObject what, Schema.Relationship relationship) throws Exception {
-		ReBACFunctions rebac = new ReBACFunctions(channel, bearerToken);
+		ReBACFunctions rebac = new ReBACFunctions(channel, spiceDbBearerToken);
 		rebac.createRelationship(who, relationship, what);
 	}
 
