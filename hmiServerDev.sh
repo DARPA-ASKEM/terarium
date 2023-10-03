a1423db48cb99c58d6b942d4a1993ba306e74856
Yohann Paris <github@yohannparis.com>
2023-10-03 16:22:10 -0400

    task: add better intellij support for local devlopment (#1937)

--- hmiServerDev.sh
+++ hmiServerDev.sh
@@ -1,9 +1,39 @@
+#!/usr/bin/env bash
+
+# Description: This script is used to start the server locally for development purposes.
+#              It will decrypt the secrets file, start the server, then remove the secrets file.
 
 SERVER_DIR="packages/server"
-ENCRYPTED_FILE="src/main/resources/application-secrets.properties.encrypted"
-DECRYPTED_FILE="src/main/resources/application-secrets.properties"
+ENCRYPTED_FILE="${SERVER_DIR}/src/main/resources/application-secrets.properties.encrypted"
+DECRYPTED_FILE="${SERVER_DIR}/src/main/resources/application-secrets.properties"
 VAULT_PASSWORD=""~/askem-vault-id.txt""
 
+function decrypt_secrets() {
+	echo "Decrypting local secrets vault"
+	ansible-vault decrypt --vault-password-file ${VAULT_PASSWORD} --output ${DECRYPTED_FILE} ${ENCRYPTED_FILE}
+}
+
+function encrypt_secrets() {
+	echo "Encrypting local secrets vault"
+	ansible-vault encrypt --vault-password-file ${VAULT_PASSWORD} --output ${ENCRYPTED_FILE} ${DECRYPTED_FILE}
+}
+
+function delete_secrets() {
+	echo "Deleting local secrets vault"
+	rm ${DECRYPTED_FILE}
+}
+
+function deploy_containers() {
+	echo "Deploying local containers"
+	docker compose --file containers/docker-compose-base.yml --project-name terarium up --detach --no-recreate --wait
+}
+
+function start_server() {
+	echo "Starting local server"
+	cd ${SERVER_DIR} || exit
+	gradle bootRun --args='--spring.profiles.active=default,secrets'
+	cd - || exit
+}
 
 case ${1} in
 	-h | --help)
@@ -12,6 +42,9 @@ case ${1} in
 	start)
 		COMMAND="start"
 		;;
+	start-server-ide)
+  	COMMAND="start-server-ide"
+  	;;
 	decrypt)
 		COMMAND="decrypt"
 		;;
@@ -25,29 +58,27 @@ COMMAND=${COMMAND:-"start"}
 
 case ${COMMAND} in
 	start)
-		cd ${SERVER_DIR} || exit
-		echo "Decrypting local secrets vault"
-		ansible-vault decrypt --vault-password-file ${VAULT_PASSWORD} --output ${DECRYPTED_FILE} ${ENCRYPTED_FILE}
-    echo "Deploying local containers"
-    docker compose -f ../../containers/docker-compose-base.yml up -d
-    echo "Starting local server"
-    gradle bootRun --args='--spring.profiles.active=default,secrets'
-    echo "Deleting local secrets vault"
-    rm ${DECRYPTED_FILE}
+		decrypt_secrets
+    deploy_containers
+    start_server
+    delete_secrets
     ;;
+	start-server-ide)
+  	decrypt_secrets
+  	deploy_containers
+  	;;
   decrypt)
-  	cd ${SERVER_DIR} || exit
-  	ansible-vault decrypt --vault-password-file ${VAULT_PASSWORD} --output ${DECRYPTED_FILE} ${ENCRYPTED_FILE}
+  	decrypt_secrets
   	;;
  	encrypt)
- 		cd ${SERVER_DIR} || exit
-   	ansible-vault encrypt --vault-password-file ${VAULT_PASSWORD} --output ${ENCRYPTED_FILE} ${DECRYPTED_FILE}
+ 		encrypt_secrets
    	;;
   help)
   	echo "
     	Usage:
-    			${0} start              Decrypts the secrets file, starts the application via 'gradle bootRun', then removes secrets after run
-    			${0} decrypt            Decrypt secrets (${ENCRYPTED_FILE}) to an unencrypted file (${DECRYPTED_FILE})
-    			${0} encrypt            Encrypts ${DECRYPTED_FILE} to the checked in secrets file, ${ENCRYPTED_FILE}"
+    			${0} start              		Decrypts the secrets file, starts the application via 'gradle bootRun', then removes secrets after run
+    			${0} start-server-ide  			Decrypts the secrets file, starts the application via IntelliJ, then removes secrets after run
+    			${0} decrypt            		Decrypt secrets (${ENCRYPTED_FILE}) to an unencrypted file (${DECRYPTED_FILE})
+    			${0} encrypt            		Encrypts ${DECRYPTED_FILE} to the checked in secrets file, ${ENCRYPTED_FILE}"
     ;;
 esac
