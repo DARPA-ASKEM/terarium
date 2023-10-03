80ec6920eef6adc463caa0709cdfb012dfdf142a
Yohann Paris <github@yohannparis.com>
2023-10-03 11:58:11 -0400

    Update hmiServerDev.sh

--- hmiServerDev.sh
+++ hmiServerDev.sh
@@ -4,38 +4,35 @@
 #              It will decrypt the secrets file, start the server, then remove the secrets file.
 
 SERVER_DIR="packages/server"
-ENCRYPTED_FILE="src/main/resources/application-secrets.properties.encrypted"
-DECRYPTED_FILE="src/main/resources/application-secrets.properties"
+ENCRYPTED_FILE="${SERVER_DIR}/src/main/resources/application-secrets.properties.encrypted"
+DECRYPTED_FILE="${SERVER_DIR}/src/main/resources/application-secrets.properties"
 VAULT_PASSWORD=""~/askem-vault-id.txt""
 
 function decrypt_secrets() {
 	echo "Decrypting local secrets vault"
-	cd ${SERVER_DIR} || exit
 	ansible-vault decrypt --vault-password-file ${VAULT_PASSWORD} --output ${DECRYPTED_FILE} ${ENCRYPTED_FILE}
 }
 
 function encrypt_secrets() {
 	echo "Encrypting local secrets vault"
-	cd ${SERVER_DIR} || exit
 	ansible-vault encrypt --vault-password-file ${VAULT_PASSWORD} --output ${ENCRYPTED_FILE} ${DECRYPTED_FILE}
 }
 
 function delete_secrets() {
 	echo "Deleting local secrets vault"
-	cd ${SERVER_DIR} || exit
 	rm ${DECRYPTED_FILE}
 }
 
 function deploy_containers() {
 	echo "Deploying local containers"
-	cd ${SERVER_DIR} || exit
-	docker compose -f ../../containers/docker-compose-base.yml up -d
+	docker compose --file containers/docker-compose-base.yml --project-name terarium up --detach --no-recreate --wait
 }
 
 function start_server() {
 	echo "Starting local server"
 	cd ${SERVER_DIR} || exit
 	gradle bootRun --args='--spring.profiles.active=default,secrets'
+	cd - || exit
 }
 
 case ${1} in
@@ -54,6 +51,9 @@ case ${1} in
 	encrypt)
 		COMMAND="encrypt"
 		;;
+	delete-secrets)
+		COMMAND="delete-secrets"
+		;;
 esac
 
 # Default COMMAND to start if empty
@@ -69,7 +69,6 @@ case ${COMMAND} in
 	start-server-intellij)
   	decrypt_secrets
   	deploy_containers
-  	delete_secrets
   	;;
   decrypt)
   	decrypt_secrets
@@ -77,12 +76,16 @@ case ${COMMAND} in
  	encrypt)
  		encrypt_secrets
    	;;
+  delete-secrets)
+		delete_secrets
+		;;
   help)
   	echo "
     	Usage:
     			${0} start              		Decrypts the secrets file, starts the application via 'gradle bootRun', then removes secrets after run
     			${0} start-server-intellij  Decrypts the secrets file, starts the application via IntelliJ, then removes secrets after run
     			${0} decrypt            		Decrypt secrets (${ENCRYPTED_FILE}) to an unencrypted file (${DECRYPTED_FILE})
-    			${0} encrypt            		Encrypts ${DECRYPTED_FILE} to the checked in secrets file, ${ENCRYPTED_FILE}"
+    			${0} encrypt            		Encrypts ${DECRYPTED_FILE} to the checked in secrets file, ${ENCRYPTED_FILE}
+    			${0} delete-secrets     		Deletes the decrypted secrets file (${DECRYPTED_FILE})"
     ;;
 esac
