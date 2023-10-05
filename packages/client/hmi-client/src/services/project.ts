10380cc2cfc0befabfbb6cece0cb668e514e63ec
Shawn Yama <syama@uncharted.software>
2023-10-05 17:16:11 -0400

    rm temp fix

--- packages/client/hmi-client/src/services/project.ts
+++ packages/client/hmi-client/src/services/project.ts
@@ -93,7 +93,6 @@ async function getAll(): Promise<Project[] | null> {
  * @return ProjectAssets|null - the appropriate project, or null if none returned by API
  */
 async function getAssets(projectId: string, types?: string[]): Promise<ProjectAssets | null> {
-	if (projectId === '') return null;
 	try {
 		let url = `/projects/${projectId}/assets`;
 		if (types) {
