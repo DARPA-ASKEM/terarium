7e1778f49a8d82ca9853ec29d5dfd9d2826aeca8
Tom Szendrey <T.Szendrey@hotmail.com>
2023-10-05 10:05:39 -0400

    meant for last commit

--- packages/client/hmi-client/src/workflow/ops/stratify-mira/mod.ts
+++ packages/client/hmi-client/src/workflow/ops/stratify-mira/mod.ts
@@ -1,3 +1,3 @@
-export * from './stratify-operation-mira';
+export * from './stratify-mira-operation';
 export { default as TeraStratifyMira } from './tera-stratify-mira.vue';
 export { default as TeraStratifyNodeMira } from './tera-stratify-node-mira.vue';
