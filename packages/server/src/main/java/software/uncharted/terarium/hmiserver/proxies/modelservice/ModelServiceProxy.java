9b8c4935b7d3acbcdbb8e475fec60d476bb8c721
Daniel Chang <mwdchang@gmail.com>
2023-10-05 18:39:22 -0400

    fix: return type for acset-to-latex

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/proxies/modelservice/ModelServiceProxy.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/proxies/modelservice/ModelServiceProxy.java
@@ -10,8 +10,8 @@ import software.uncharted.terarium.hmiserver.models.modelservice.StratifyRequest
 
 @FeignClient(name = "model-service", url = "${terarium.dataservice.url}", path = "/model-service")
 public interface ModelServiceProxy {
-	@PostMapping("/petri-to-latex")
-	ResponseEntity<JsonNode> petrinetToLatex(
+	@PostMapping(value = "/petri-to-latex", produces = {"text/plain", "application/*"})
+	ResponseEntity<String> petrinetToLatex(
 		@RequestBody PetriNet content
 	);
 
