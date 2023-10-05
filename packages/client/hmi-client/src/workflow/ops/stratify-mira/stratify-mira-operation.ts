4e57cfd8e6bdaeef48c7ccbcfa26a8a7dca7123c
Tom Szendrey <T.Szendrey@hotmail.com>
2023-10-05 10:03:33 -0400

    renaming operation to follow naming convs

--- packages/client/hmi-client/src/workflow/ops/stratify-mira/stratify-mira-operation.ts
+++ packages/client/hmi-client/src/workflow/ops/stratify-mira/stratify-mira-operation.ts
@@ -2,7 +2,7 @@ import { Operation, WorkflowOperationTypes } from '@/types/workflow';
 
 export const StratifyMiraOperation: Operation = {
 	name: WorkflowOperationTypes.STRATIFY_MIRA,
-	displayName: 'Stratify',
+	displayName: 'Stratify MIRA',
 	description: 'Stratify a model',
 	inputs: [{ type: 'modelConfigId', label: 'Model configuration', acceptMultiple: false }],
 	outputs: [{ type: 'model' }],
