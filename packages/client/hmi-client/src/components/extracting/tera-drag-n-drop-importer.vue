5ac328cf808688ee7ef821e8b07ddb051a8bc5c5
Edwin Lai <edwinslai@gmail.com>
2023-10-06 17:13:45 -0400

    1st draft new resource upload modal

--- packages/client/hmi-client/src/components/extracting/tera-drag-n-drop-importer.vue
+++ packages/client/hmi-client/src/components/extracting/tera-drag-n-drop-importer.vue
@@ -17,41 +17,33 @@
 			/>
 			<label for="fileInput" class="file-label">
 				<div v-if="dragOver">Release mouse button to add files to import</div>
-				<div v-else>Drop resources here or <span class="text-link">upload a file</span>.</div>
-			</label>
-			<br />
-
-			<div v-if="importFiles.length > 0">
-				<div class="preview-container mt-4" v-if="importFiles.length">
-					<div v-for="file in importFiles" :key="file.name" class="file-preview" scrolling="no">
-						<TeraDragAndDropFilePreviewer
-							:file="file"
-							:show-preview="props.showPreview"
-							:is-processing="isProcessing"
-							:progress="props.progress"
-							@remove-file="removeFile(importFiles.indexOf(file))"
-						>
-						</TeraDragAndDropFilePreviewer>
+				<div v-else class="drop-zone">
+					<div><i class="pi pi-file" style="font-size: 2.5rem" /></div>
+					<div>
+						Drop resources here or <span class="text-link">click to open a file browser</span>
 					</div>
 				</div>
-				<br />
+			</label>
+		</div>
+		<div v-if="importFiles.length > 0">
+			<div class="preview-container mt-4" v-if="importFiles.length">
+				<div v-for="file in importFiles" :key="file.name" class="file-preview" scrolling="no">
+					<TeraDragAndDropFilePreviewer
+						:file="file"
+						:is-processing="isProcessing"
+						:progress="props.progress"
+						@remove-file="removeFile(importFiles.indexOf(file))"
+					>
+					</TeraDragAndDropFilePreviewer>
+				</div>
 			</div>
-			<br />
-			<Button
-				v-if="canImport"
-				type="button"
-				class="import-button"
-				@click="processFiles(importFiles)"
-				label="Import data"
-			></Button>
 		</div>
 	</section>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref } from 'vue';
 import API from '@/api/api';
-import Button from 'primevue/button';
 import { AcceptedExtensions, AcceptedTypes } from '@/types/common';
 import TeraDragAndDropFilePreviewer from './tera-drag-n-drop-file-previewer.vue';
 
@@ -65,11 +57,6 @@ const processResponse = ref(<{ file: string; response: { text: string } }[]>[]);
 const csvDescription = ref('');
 
 const props = defineProps({
-	// show preview
-	showPreview: {
-		type: Boolean,
-		required: true
-	},
 	progress: {
 		type: Number,
 		default: undefined
@@ -156,6 +143,7 @@ const addFiles = (addedFiles: File[] | undefined) => {
 			}
 		}
 	}
+	processFiles(importFiles.value);
 };
 
 /**
@@ -217,8 +205,6 @@ async function processFiles(files) {
 	emit('import-completed', processResponse.value);
 	files.value = [];
 }
-
-const canImport = computed(() => importFiles.value.length > 0);
 </script>
 
 <style scoped>
@@ -262,7 +248,7 @@ const canImport = computed(() => importFiles.value.length > 0);
 	flex-direction: column;
 	cursor: pointer;
 	align-items: center;
-	padding-top: 2.5rem;
+	padding: 2.5rem 0 2.5rem 0;
 }
 
 .text-link {
@@ -296,5 +282,19 @@ const canImport = computed(() => importFiles.value.length > 0);
 	border-radius: 0.25rem;
 	cursor: pointer;
 	border: 1px solid var(--surface-border);
+}
+
+.drop-zone {
+	display: flex;
+	gap: 1rem;
+	align-items: center;
+}
+
+i {
+	color: var(--text-color-secondary);
+}
+
+.modal label {
+	margin: 0;
 }
 </style>
--- packages/client/hmi-client/src/page/project/components/tera-upload-resources-modal.vue
+++ packages/client/hmi-client/src/page/project/components/tera-upload-resources-modal.vue
@@ -5,64 +5,41 @@
 				<h4>Upload resources</h4>
 			</template>
 			<template #default>
-				<p class="subheader">Add resources to your project here</p>
-				<tera-drag-and-drop-importer
-					:show-preview="true"
-					:accept-types="[
-						AcceptedTypes.PDF,
-						AcceptedTypes.CSV,
-						AcceptedTypes.TXT,
-						AcceptedTypes.MD,
-						AcceptedTypes.PY,
-						AcceptedTypes.R,
-						AcceptedTypes.JL
-					]"
-					:accept-extensions="[
-						AcceptedExtensions.PDF,
-						AcceptedExtensions.CSV,
-						AcceptedExtensions.TXT,
-						AcceptedExtensions.MD,
-						AcceptedExtensions.PY,
-						AcceptedExtensions.R,
-						AcceptedExtensions.JL
-					]"
-					:import-action="processFiles"
-					:progress="progress"
-					@import-completed="importCompleted"
-				></tera-drag-and-drop-importer>
-
-				<section>
-					<Card v-for="(item, i) in results" :key="i" class="card">
-						<template #title>
-							<div class="card-img"></div>
-						</template>
-						<template #content>
-							<div class="card-content">
-								<div v-if="item.file" class="file-title">{{ item.file.name }}</div>
-								<div v-if="item.response" class="file-content">
-									<br />
-									<div>Extracted Text</div>
-									<div>{{ item.response.text }}</div>
-									<br />
-									<div v-if="item.response.images">Images Found</div>
-									<div v-for="image in item.response.images" :key="image">
-										<img :src="`data:image/jpeg;base64,${image}`" alt="" />
-									</div>
-									<br />
-									<i class="pi pi-plus"></i>
-								</div>
-							</div>
-						</template>
-					</Card>
+				<section class="main-section">
+					<section>
+						<p class="subheader">Add resources to your project here</p>
+						<tera-drag-and-drop-importer
+							:accept-types="[
+								AcceptedTypes.PDF,
+								AcceptedTypes.CSV,
+								AcceptedTypes.TXT,
+								AcceptedTypes.MD,
+								AcceptedTypes.PY,
+								AcceptedTypes.R,
+								AcceptedTypes.JL
+							]"
+							:accept-extensions="[
+								AcceptedExtensions.PDF,
+								AcceptedExtensions.CSV,
+								AcceptedExtensions.TXT,
+								AcceptedExtensions.MD,
+								AcceptedExtensions.PY,
+								AcceptedExtensions.R,
+								AcceptedExtensions.JL
+							]"
+							:import-action="processFiles"
+							:progress="progress"
+							@import-completed="importCompleted"
+						></tera-drag-and-drop-importer>
+					</section>
+					<section>
+						<p>Or upload from a URL</p>
+						<InputText></InputText>
+					</section>
 				</section>
 			</template>
 			<template #footer>
-				<Button
-					label="Upload"
-					class="p-button-primary"
-					@click="() => emit('close')"
-					:disabled="!results"
-				/>
+				<Button label="Upload" class="p-button-primary" @click="upload" />
 				<Button label="Cancel" class="p-button-secondary" @click="() => emit('close')" />
 			</template>
 		</tera-modal>
@@ -75,14 +52,14 @@ import Button from 'primevue/button';
 import { AcceptedTypes, AcceptedExtensions } from '@/types/common';
 import { uploadCodeToProject } from '@/services/code';
 import { useProjects } from '@/composables/project';
-import { Artifact, AssetType, CsvAsset, Dataset } from '@/types/Types';
+import { Artifact, AssetType, Dataset } from '@/types/Types';
 import { uploadArtifactToProject } from '@/services/artifact';
-import { extractPDF } from '@/services/knowledge';
-import { createNewDatasetFromCSV, downloadRawFile } from '@/services/dataset';
+import { createNewDatasetFromCSV } from '@/services/dataset';
 import useAuthStore from '@/stores/auth';
 import { ref } from 'vue';
-import { logger } from '@/utils/logger';
 import TeraDragAndDropImporter from '@/components/extracting/tera-drag-n-drop-importer.vue';
+import InputText from 'primevue/inputtext';
+import { useToastService } from '@/services/toast';
 
 defineProps<{
 	visible: boolean;
@@ -90,9 +67,7 @@ defineProps<{
 const emit = defineEmits(['close']);
 
 const progress = ref(0);
-const results = ref<
-	{ file: File; error: boolean; response: { text: string; images: string[] } }[] | null
->(null);
+const results = ref<{ id: string; name: string; assetType: AssetType }[] | null>(null);
 
 async function processFiles(files: File[], csvDescription: string) {
 	return files.map(async (file) => {
@@ -120,14 +95,7 @@ async function processFiles(files: File[], csvDescription: string) {
 async function processCode(file: File) {
 	// This is pdf, txt, md files
 	const newCode = await uploadCodeToProject(file, progress);
-	let newAsset;
-	if (newCode && newCode.id) {
-		newAsset = await useProjects().addAsset(AssetType.Code, newCode.id);
-	}
-	if (newAsset) {
-		return { file, error: false, response: { text: '', images: [] } };
-	}
-	return { file, error: true, response: { text: '', images: [] } };
+	return { id: newCode?.id ?? '', assetType: AssetType.Code };
 }
 
 /**
@@ -142,15 +110,7 @@ async function processArtifact(file: File) {
 		useProjects().activeProject.value?.username ?? '',
 		''
 	);
-	let newAsset;
-	if (artifact && artifact.id) {
-		newAsset = await useProjects().addAsset(AssetType.Artifacts, artifact.id);
-	}
-	if (artifact && newAsset && file.name.toLowerCase().endsWith('.pdf')) {
-		await extractPDF(artifact);
-		return { file, error: false, response: { text: '', images: [] } };
-	}
-	return { file, error: true, response: { text: '', images: [] } };
+	return { id: artifact?.id ?? '', assetType: AssetType.Artifacts };
 }
 
 /**
@@ -159,49 +119,45 @@ async function processArtifact(file: File) {
  * @param description
  */
 async function processDataset(file: File, description: string) {
-	let addedCSV: CsvAsset | null = null;
+	// let addedCSV: CsvAsset | null = null;
 	const addedDataset: Dataset | null = await createNewDatasetFromCSV(
 		progress,
 		file,
 		useAuthStore().user?.name ?? '',
 		description
 	);
-
-	let newAsset;
-	if (addedDataset?.id) {
-		newAsset = await useProjects().addAsset(AssetType.Datasets, addedDataset.id);
-		addedCSV = await downloadRawFile(addedDataset.id, file.name);
-	}
-
-	if (addedCSV !== null && newAsset) {
-		const text: string = addedCSV?.csv?.join('\r\n') ?? '';
-		const images = [];
-
-		return { file, error: false, response: { text, images } };
-	}
-	return { file, error: true, response: { text: '', images: [] } };
+	return { id: addedDataset?.id ?? '', assetType: AssetType.Datasets };
 }
 
-async function importCompleted(
-	newResults: { file: File; error: boolean; response: { text: string; images: string[] } }[] | null
-) {
-	// This is a hacky override for dealing with CSVs
-	if (
-		newResults &&
-		newResults.length === 1 &&
-		(newResults[0].file.type === AcceptedTypes.CSV ||
-			newResults[0].file.type === AcceptedTypes.TXT ||
-			newResults[0].file.type === AcceptedTypes.MD)
-	) {
-		if (newResults[0].error) {
-			logger.error('Failed to upload file. Is it too large?', { showToast: true });
-		}
-		results.value = null;
+function importCompleted(newResults: { id: string; name: string; assetType: AssetType }[] | null) {
+	results.value = newResults?.filter((r) => r.id !== '') ?? [];
+}
+
+async function upload() {
+	if (results.value) {
+		const resolved = await Promise.all(
+			results.value?.map(({ id, assetType }): Promise<any> => useProjects().addAsset(assetType, id))
+		);
 		emit('close');
-	} else {
-		results.value = newResults;
+		const resourceMsg = resolved.length > 1 ? 'resources were' : 'resource was';
+		useToastService().success(
+			'Success!',
+			`${resolved.length} ${resourceMsg} successfuly added to this project`
+		);
 	}
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.main-section {
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+}
+
+.main-section section {
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+}
+</style>
