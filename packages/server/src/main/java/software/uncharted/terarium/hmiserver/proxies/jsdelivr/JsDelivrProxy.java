222dacb5e7f43e5faf07ecc1ae128ec3a9309337
Shawn Yama <syama@uncharted.software>
2023-10-10 13:20:36 -0400

    jsdelivr.url var read properly from app.props

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/proxies/jsdelivr/JsDelivrProxy.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/proxies/jsdelivr/JsDelivrProxy.java
@@ -5,12 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-@FeignClient(name = "jsdeliver", url = "jsdelivr.url")
+@FeignClient(name = "jsdelivr", url = "${jsdelivr.url}")
 public interface JsDelivrProxy {
 	@GetMapping("/gh/{repoOwnerAndName}@latest/{path}")
 	ResponseEntity<String> getGithubCode(
-		@PathVariable("repoOwnerAndName") String repoOwnerAndName,
-		@PathVariable("path") String path
-	);
+			@PathVariable("repoOwnerAndName") String repoOwnerAndName,
+			@PathVariable("path") String path);
 }
