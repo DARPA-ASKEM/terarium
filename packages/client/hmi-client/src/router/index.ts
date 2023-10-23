32c98b4fc70cc2511dfbd1d65cfe8b5afda7c699
Yohann Paris <github@yohannparis.com>
2023-10-23 14:46:17 -0400

    Update index.ts

--- packages/client/hmi-client/src/router/index.ts
+++ packages/client/hmi-client/src/router/index.ts
@@ -60,7 +60,6 @@ const routes = [
 ];
 
 const router = createRouter({
-	// 4. Provide the history implementation to use. We are using the hash history for simplicity here.
 	history: createWebHistory(),
 	routes
 });
