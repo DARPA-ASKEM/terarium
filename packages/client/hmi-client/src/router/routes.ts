4395afa06911599877801bae64908ec49500dbdd
Yohann Paris <github@yohannparis.com>
2023-10-24 11:10:19 -0400

    task: vue-router refresh issue (#2083)
    Co-authored-by: Derek Vince <dvince@uncharted.software>

--- packages/client/hmi-client/src/router/routes.ts
+++ packages/client/hmi-client/src/router/routes.ts
@@ -1,6 +1,5 @@
 import IconAccount32 from '@carbon/icons-vue/es/account/32';
 import IconFlow32 from '@carbon/icons-vue/es/flow/32';
-import IconCode32 from '@carbon/icons-vue/es/code/32';
 import { AssetType } from '@/types/Types';
 
 export enum RouteName {
@@ -8,7 +7,6 @@ export enum RouteName {
 	Project = 'project',
 	Provenance = 'provenance',
 	DataExplorer = 'dataExplorer',
-	Code = 'code',
 	WorkflowNode = 'workflow-node'
 }
 
@@ -16,11 +14,6 @@ export const RouteMetadata: {
 	[key in RouteName]: { displayName: string; icon: any; projectAsset?: AssetType };
 } = {
 	[RouteName.Project]: { displayName: 'Project summary', icon: IconAccount32 },
-	[RouteName.Code]: {
-		displayName: 'Code',
-		icon: IconCode32,
-		projectAsset: AssetType.Code
-	},
 	[RouteName.Provenance]: { displayName: 'Provenance', icon: IconFlow32 },
 	[RouteName.Home]: { displayName: 'Home', icon: 'pi pi-home' },
 	[RouteName.DataExplorer]: { displayName: 'Explorer', icon: 'pi pi-compass' },
