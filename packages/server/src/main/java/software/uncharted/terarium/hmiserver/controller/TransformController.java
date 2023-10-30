87ebde8290348e54a2500f4bbfa45dac7eb38820
dvince <dvince@uncharted.software>
2023-10-30 13:40:25 -0400

    use builder

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/TransformController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/TransformController.java
@@ -33,7 +33,7 @@ public class TransformController {
 		// since the model service returns headers that are duplicated in the hmi-server response,
 		// we need to strip them out. This stops our nginx reverse proxy from thinking that there
 		// is an HTTP smuggling attack.
-		return new ResponseEntity<>(proxyRes.getBody(), proxyRes.getStatusCode());
+		return ResponseEntity.status(proxyRes.getStatusCode()).body(proxyRes.getBody());
 	}
 
 	@PostMapping(value = "/acset-to-latex", produces = {"text/plain", "application/*"})
