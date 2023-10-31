e0c967884b491ff146a682ac3accd98f1ad5681f
Shawn Yama <syama@uncharted.software>
2023-10-31 16:07:36 -0400

    code block

--- packages/client/hmi-client/src/workflow/ops/model-from-code/tera-model-from-code.vue
+++ packages/client/hmi-client/src/workflow/ops/model-from-code/tera-model-from-code.vue
@@ -3,11 +3,25 @@
 		<section>
 			<header>
 				<h5>Code</h5>
-				<Dropdown />
-				<Button label="Add code block" icon="pi pi-plus" />
+				<Dropdown v-model="programmingLanguages" />
+				<Button label="Add code block" icon="pi pi-plus" @click="addCodeBlock" />
 			</header>
 			<ul>
-				<li>Code block 1</li>
+				<li v-for="({ name, code, programmingLanguage }, i) in codeBlocks" :key="i">
+					<Card>
+						<header>
+							<h5>{{ name }}</h5>
+						</header>
+						<v-ace-editor
+							:value="code"
+							@init="initialize"
+							:lang="programmingLanguage"
+							theme="chrome"
+							style="height: 100%; width: 100%"
+							class="ace-editor"
+						/>
+					</Card>
+				</li>
 			</ul>
 			<footer>
 				<span><label>Model framework:</label><Dropdown /></span>
@@ -21,8 +35,45 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import Dropdown from 'primevue/dropdown';
 import Button from 'primevue/button';
+import Card from 'primevue/card';
+import { VAceEditor } from 'vue3-ace-editor';
+import { VAceEditorInstance } from 'vue3-ace-editor/types';
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/mode-julia';
+import 'ace-builds/src-noconflict/mode-r';
+import { ProgrammingLanguage } from '@/types/Types';
+
+const editor = ref<VAceEditorInstance['_editor'] | null>(null);
+const programmingLanguages = [
+	ProgrammingLanguage.Julia,
+	ProgrammingLanguage.Python,
+	ProgrammingLanguage.R
+];
+
+const codeBlock = {
+	name: 'Code block 1',
+	code: '',
+	programmingLanguage: ProgrammingLanguage.Python
+};
+
+const codeBlocks = ref([codeBlock]);
+
+/**
+ * Editor initialization function
+ * @param editorInstance	the Ace editor instance
+ */
+async function initialize(editorInstance) {
+	editor.value = editorInstance;
+	editorInstance.session.selection.on('changeSelection', onSelectedTextChange);
+	editorInstance.setShowPrintMargin(false);
+}
+
+function addCodeBlock() {
+	codeBlocks.value.push(codeBlock);
+}
 </script>
 
 <style scoped>
