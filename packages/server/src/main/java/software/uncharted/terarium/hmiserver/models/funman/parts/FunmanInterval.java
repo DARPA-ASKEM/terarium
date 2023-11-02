16941c9389b9af5729fcb5dd8345cf681b1cb1f0
Tom Szendrey <T.Szendrey@hotmail.com>
2023-11-02 11:09:45 -0400

    ub and lb are optional -default to pos and neg inf

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/funman/parts/FunmanInterval.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/funman/parts/FunmanInterval.java
@@ -3,11 +3,14 @@ package software.uncharted.terarium.hmiserver.models.funman.parts;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import software.uncharted.terarium.hmiserver.annotations.TSModel;
+import software.uncharted.terarium.hmiserver.annotations.TSOptional;
 
 @Data
 @Accessors(chain = true)
 @TSModel
 public class FunmanInterval {
+	@TSOptional
 	private Double ub; //Upper bound
+	@TSOptional
 	private Double lb;  //Lower Bound
 }
