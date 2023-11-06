7b80d7b46c4b34160e9ee5dd3a0218dc6b31666c
Charles Coleman <bigglesandginger@gmail.com>
2023-11-06 13:42:56 -0500

    test fix

--- packages/server/src/test/java/software/uncharted/terarium/hmiserver/controller/UserControllerTests.java
+++ packages/server/src/test/java/software/uncharted/terarium/hmiserver/controller/UserControllerTests.java
@@ -1,13 +1,20 @@
 package software.uncharted.terarium.hmiserver.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import software.uncharted.terarium.hmiserver.TerariumApplicationTests;
 import software.uncharted.terarium.hmiserver.configuration.MockUser;
+import software.uncharted.terarium.hmiserver.security.Roles;
+import software.uncharted.terarium.hmiserver.security.SecurityConfig;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Import(SecurityConfig.class)
 public class UserControllerTests extends TerariumApplicationTests {
 
 	@Test
@@ -17,7 +24,7 @@ public class UserControllerTests extends TerariumApplicationTests {
 	}
 
 	@Test
-	@WithUserDetails(MockUser.ADAM)
+	@WithMockUser(username="ursula",roles={"user"})
 	public void testItReturnsOkOnAuthorized() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/user/me"))
 			.andExpect(status().isOk());
