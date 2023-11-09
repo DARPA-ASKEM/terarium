ef89d5168d354c06ebaa6cc63215ead77ad8f837
Daniel Chang <mwdchang@gmail.com>
2023-11-09 16:18:06 -0500

    chore: factor out event-emitter, add unit tests (#2189)

--- packages/client/hmi-client/src/services/workflow.ts
+++ packages/client/hmi-client/src/services/workflow.ts
@@ -1,5 +1,6 @@
 import API from '@/api/api';
 import _ from 'lodash';
+import { EventEmitter } from '@/utils/emitter';
 import {
 	Operation,
 	Position,
@@ -206,53 +207,7 @@ export const getWorkflow = async (id: string) => {
 /// /////////////////////////////////////////////////////////////////////////////
 // Events bus for workflow
 /// /////////////////////////////////////////////////////////////////////////////
-type EventCallback = (args: any) => void;
-type EventName = string | symbol;
-class EventEmitter {
-	listeners: Map<EventName, Set<EventCallback>> = new Map();
-
-	on(eventName: EventName, fn: EventCallback): void {
-		if (!this.listeners.has(eventName)) {
-			this.listeners.set(eventName, new Set());
-		}
-		this.listeners.get(eventName)?.add(fn);
-	}
-
-	once(eventName: EventName, fn: EventCallback): void {
-		if (!this.listeners.has(eventName)) {
-			this.listeners.set(eventName, new Set());
-		}
-
-		const onceWrapper = (args: any) => {
-			fn(args);
-			this.off(eventName, onceWrapper);
-		};
-		this.listeners.get(eventName)?.add(onceWrapper);
-	}
-
-	off(eventName: EventName, fn: EventCallback): void {
-		const set = this.listeners.get(eventName);
-		if (set) {
-			set.delete(fn);
-		}
-	}
-
-	removeAllEvents(eventName: EventName): void {
-		if (this.listeners.has(eventName)) {
-			this.listeners.delete(eventName);
-		}
-	}
-
-	emit(eventName: EventName, args?: any): boolean {
-		const fns = this.listeners.get(eventName);
-		if (!fns) return false;
-
-		fns.forEach((f) => {
-			f(args);
-		});
-		return true;
-	}
-
+class WorkflowEventEmitter extends EventEmitter {
 	emitNodeStateChange(payload: { workflowId: string; nodeId: string; state: any }) {
 		this.emit('node-state-change', payload);
 	}
@@ -262,4 +217,4 @@ class EventEmitter {
 	}
 }
 
-export const workflowEventBus = new EventEmitter();
+export const workflowEventBus = new WorkflowEventEmitter();
