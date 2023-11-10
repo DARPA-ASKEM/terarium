eeda843e5dee8fc6afc8c03b73362aebfff10da7
David Gauldie <dgauldie@uncharted.software>
2023-11-10 09:44:12 -0500

    first pass at a migration script for TDS (#2187)

--- containers/scripts/init.sql
+++ containers/scripts/init.sql
@@ -13,3 +13,7 @@ GRANT ALL PRIVILEGES ON DATABASE terarium TO terarium_user;
 -- Create spice DB for ReBAC
 CREATE DATABASE spicedb;
 GRANT ALL PRIVILEGES ON DATABASE spicedb TO terarium_user;
+
+-- Create tds for migrating data from TDS to Terarium
+CREATE DATABASE tds;
+GRANT ALL PRIVILEGES ON DATABASE tds TO terarium_user;
