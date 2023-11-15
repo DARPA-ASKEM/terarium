292acd7d65545d11d7dab65ce25d6bd2cb6d5cf4
Charles Coleman <bigglesandginger@gmail.com>
2023-11-15 17:07:51 -0500

    revisions due to testing

--- containers/permissions/script.sh
+++ containers/permissions/script.sh
@@ -23,34 +23,38 @@ if [ -z ${SPICEDB_SHARED_KEY} ]; then exit 8; fi
 # Configure spicedb
 
 SPICEDB_SETTINGS="--endpoint ${SPICEDB_TARGET} --token ${SPICEDB_SHARED_KEY}"
-if [ ${SPICEDB_INSECURE} == "true" ]; then
+if [ "${SPICEDB_INSECURE}" = "true" ]; then
   SPICEDB_SETTINGS="${SPICEDB_SETTINGS} --insecure"
 fi
 
+# Create SpiceDB schema
+zed ${SPICEDB_SETTINGS} schema write schema 2>/dev/null
+
 # Aquire Access Token for Keycloak
-ACCESS_TOKEN=$(curl -d "client_id=${KEYCLOAK_ADMIN_CLIENT_ID}" -d "username=${KEYCLOAK_ADMIN_USERNAME}" -d "password=${KEYCLOAK_ADMIN_PASSWORD}" -d "grant_type=password" "${KEYCLOAK_URL}/realms/master/protocol/openid-connect/token" 2>/dev/null | jq -r ".access_token")
+ACCESS_TOKEN=$(curl -d "client_id=${TERARIUM_KEYCLOAK_ADMIN_CLIENT_ID}" -d "username=${TERARIUM_KEYCLOAK_ADMIN_USERNAME}" -d "password=${TERARIUM_KEYCLOAK_ADMIN_PASSWORD}" -d "grant_type=password" "${TERARIUM_KEYCLOAK_URL}/realms/master/protocol/openid-connect/token" 2>/dev/null | jq -r ".access_token")
 
 # Get groups from Keycloak
-GROUPS_JSON=$(curl -H "Authorization: Bearer $ACCESS_TOKEN" -H "Accept: application/json" https://keycloak.staging.terarium.ai/admin/realms/${KEYCLOAK_REALM}/groups)
+GROUPS_JSON=$(curl -H "Authorization: Bearer $ACCESS_TOKEN" -H "Accept: application/json" ${TERARIUM_KEYCLOAK_URL}/admin/realms/${TERARIUM_KEYCLOAK_REALM}/groups 2>/dev/null)
 
 # TODO: create missing groups
 # TODO: get users
 # TODO: add users to Public Group
 # TODO: add admin users to Admin Group
 
+# Get ids for groups
 PUBLIC_GROUP_ID=$(echo "${GROUPS_JSON}" | jq -r ".[] | select(.name | contains(\"${PUBLIC_GROUP_NAME}\")) | .id")
 ASKEM_ADMIN_GROUP_ID=$(echo "${GROUPS_JSON}" | jq -r ".[] | select(.name | contains(\"${ASKEM_ADMINS_GROUP_NAME}\")) | .id")
-
 if [ -z ${PUBLIC_GROUP_ID} ]; then
-  echo "ERROR - could not find group \"${PUBLIC_GROUP_NAME}\"'s id"
+  echo "ERROR: could not find group \"${PUBLIC_GROUP_NAME}\"'s id"
   exit 1
 fi
-
 echo "INFO: Group \"${PUBLIC_GROUP_NAME}\" has id \"${PUBLIC_GROUP_ID}\""
 
-PUBLIC_FOUND=$(zed ${SPICEDB_SETTINGS} relationship read project:${PROJECT_1_ID} | grep ${PUBLIC_GROUP_ID})
-if [[ -z ${PUBLIC_FOUND} ]]; then
-  zed ${SPICEDB_SETTINGS} relationship create project:${PROJECT_1_ID} reader group:${PUBLIC_GROUP_ID}
+# assign public readership to project 1
+PUBLIC_FOUND=$(zed ${SPICEDB_SETTINGS} relationship read project:${PROJECT_1_ID} 2>/dev/null | grep ${PUBLIC_GROUP_ID})
+if [ -z "${PUBLIC_FOUND}" ]; then
+  echo "INFO: creating Project ${PROJECT_1_ID} relationship with Group ${PUBLIC_GROUP_NAME}"
+  zed ${SPICEDB_SETTINGS} relationship create project:${PROJECT_1_ID} reader group:${PUBLIC_GROUP_ID} 2>/dev/null
 else
   echo "INFO: Group \"${PUBLIC_GROUP_NAME}\" is a reader of Project \"${PROJECT_1_ID}\""
 fi
