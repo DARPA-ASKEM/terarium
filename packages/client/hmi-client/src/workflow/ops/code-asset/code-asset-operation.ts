084954c0b1a707e1f9fa86ef77c507180792b466
Cole Blanchard <33158416+blanchco@users.noreply.github.com>
2023-11-17 16:24:38 -0500

    task: code asset workflow node (#2236)
    Co-authored-by: Cole Blanchard <cblanchard@Coles-MacBook-Pro.local>

---
+++ packages/client/hmi-client/src/workflow/ops/code-asset/code-asset-operation.ts
@@ -0,0 +1,22 @@
+import { Operation, WorkflowOperationTypes } from '@/types/workflow';
+
+export interface CodeAssetState {
+	codeAssetId: string | null;
+}
+
+export const CodeAssetOperation: Operation = {
+	name: WorkflowOperationTypes.CODE,
+	description: 'Select a code asset',
+	displayName: 'Code Asset',
+	isRunnable: true,
+	inputs: [],
+	outputs: [],
+	action: () => {},
+
+	initState: () => {
+		const init: CodeAssetState = {
+			codeAssetId: null
+		};
+		return init;
+	}
+};
---
+++ packages/client/hmi-client/src/workflow/ops/code-asset/mod.ts
@@ -0,0 +1,3 @@
+export * from './code-asset-operation';
+export { default as TeraCodeAssetWrapper } from './tera-code-asset-wrapper.vue';
+export { default as TeraCodeAssetNode } from './tera-code-asset-node.vue';
