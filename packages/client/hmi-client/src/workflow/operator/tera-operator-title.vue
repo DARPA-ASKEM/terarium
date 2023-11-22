d64358ddcd7978a7f1d46a66043d150ac218342a
Shawn Yama <syama@uncharted.software>
2023-11-22 11:21:07 -0500

    feat: operator title (#2294)

---
+++ packages/client/hmi-client/src/workflow/operator/tera-operator-title.vue
@@ -0,0 +1,17 @@
+<template>
+	<h5><slot /></h5>
+</template>
+
+<style scoped>
+h5 {
+	display: -webkit-box;
+	-webkit-box-orient: vertical;
+	text-overflow: ellipsis;
+	overflow: hidden;
+	-webkit-line-clamp: 3;
+}
+
+h5:empty {
+	display: none;
+}
+</style>
