ecf18cb71f228182a42a3514f5e02056e6df798f
Shawn Yama <syama@uncharted.software>
2023-11-24 15:45:31 -0500

    bitmask tests

---
+++ packages/client/hmi-client/tests/unit/utils/bitmask.spec.ts
@@ -0,0 +1,29 @@
+import { addHover, removeHover, isHover, addDrag, removeDrag, isDrag } from '@/utils/bitmask';
+import { describe, expect, it } from 'vitest';
+
+let interactionStatus = 0;
+
+describe('test bitmasking for different interaction in the workflow UI', () => {
+	it('add hover state', () => {
+		interactionStatus = addHover(interactionStatus);
+		expect(interactionStatus).to.eq(0x0010);
+	});
+	it('add drag state', () => {
+		interactionStatus = addDrag(interactionStatus);
+		expect(interactionStatus).to.eq(0x1010);
+	});
+	it('check if hover state is on', () => {
+		expect(isHover(interactionStatus)).toBeTruthy();
+	});
+	it('check if drag state is on', () => {
+		expect(isDrag(interactionStatus)).toBeTruthy();
+	});
+	it('remove hover state', () => {
+		interactionStatus = removeHover(interactionStatus);
+		expect(interactionStatus).to.eq(0x1000);
+	});
+	it('remove drag state', () => {
+		interactionStatus = removeDrag(interactionStatus);
+		expect(interactionStatus).to.eq(0);
+	});
+});
