25fc0743c304c68ccca8d740b88b82ed43615b94
Shawn Yama <syama@Shawns-MacBook-Pro.local>
2023-11-27 16:21:00 -0500

    volar and types/react conflict

---
+++ packages/client/hmi-client/src/workflow/operator/tera-operator-image-carousel.vue
@@ -0,0 +1,5 @@
+<template>
+	<img alt="Image" />
+</template>
+
+<script setup lang="ts"></script>
--- packages/client/hmi-client/src/workflow/ops/model-from-code/tera-model-from-code-node.vue
+++ packages/client/hmi-client/src/workflow/ops/model-from-code/tera-model-from-code-node.vue
@@ -1,5 +1,8 @@
 <template>
 	<main>model from code node</main>
+	<tera-operator-image-carousel />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TeraOperatorImageCarousel from '@/workflow/operator/tera-operator-image-carousel.vue';
+</script>
