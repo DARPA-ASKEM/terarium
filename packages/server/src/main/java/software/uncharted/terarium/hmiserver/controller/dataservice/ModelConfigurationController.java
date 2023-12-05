23f19ff9e285a989806ecbe42d686d7466e63ff9
kbirk <birk.kevin@gmail.com>
2023-12-05 09:55:11 -0500

    Port over model configs

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/ModelConfigurationController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/ModelConfigurationController.java
@@ -1,64 +1,87 @@
 package software.uncharted.terarium.hmiserver.controller.dataservice;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
 import software.uncharted.terarium.hmiserver.controller.SnakeCaseController;
+import software.uncharted.terarium.hmiserver.controller.services.ModelConfigurationService;
 import software.uncharted.terarium.hmiserver.models.dataservice.model.ModelConfiguration;
-import software.uncharted.terarium.hmiserver.proxies.dataservice.ModelConfigurationProxy;
 import software.uncharted.terarium.hmiserver.security.Roles;
-
-import java.util.List;
-
 
 @RequestMapping("/model_configurations")
 @RestController
-
+@RequiredArgsConstructor
 public class ModelConfigurationController implements SnakeCaseController {
 
-	@Autowired
-	ModelConfigurationProxy proxy;
+	final ModelConfigurationService modelConfigurationService;
+	final ObjectMapper objectMapper;
 
 	@GetMapping
 	@Secured(Roles.USER)
 	public ResponseEntity<List<ModelConfiguration>> getModelConfigurations(
-		@RequestParam(name = "page_size", defaultValue = "500") final Integer pageSize,
-		@RequestParam(name = "page", defaultValue = "0") final Integer page
-	) {
-		return ResponseEntity.ok(proxy.getAssets(pageSize, page).getBody());
+			@RequestParam(name = "page_size", defaultValue = "500") final Integer pageSize,
+			@RequestParam(name = "page", defaultValue = "0") final Integer page) throws IOException {
+		return ResponseEntity.ok(modelConfigurationService.getModelConfigurations(pageSize, page));
 	}
 
 	@PostMapping
 	@Secured(Roles.USER)
-	public ResponseEntity<JsonNode> createModelConfiguration(@RequestBody ModelConfiguration config) {
-		return ResponseEntity.ok(proxy.createAsset(convertObjectToSnakeCaseJsonNode(config)).getBody());
+	public ResponseEntity<JsonNode> createModelConfiguration(@RequestBody ModelConfiguration config)
+			throws IOException {
+		ModelConfiguration modelConfiguration = modelConfigurationService.createModelConfiguration(config);
+
+		JsonNode res = objectMapper.valueToTree(Map.of("id", modelConfiguration.getId()));
+
+		return ResponseEntity.ok(res);
 	}
 
 	@GetMapping("/{id}")
 	@Secured(Roles.USER)
 	public ResponseEntity<ModelConfiguration> getModelConfiguration(
-		@PathVariable("id") String id
-	) {
-		return ResponseEntity.ok(proxy.getAsset(id).getBody());
+			@PathVariable("id") String id) throws IOException {
+
+		return ResponseEntity.ok(modelConfigurationService.getModelConfiguration(id));
 	}
 
 	@PutMapping("/{id}")
 	@Secured(Roles.USER)
 	public ResponseEntity<JsonNode> updateModelConfiguration(
-		@PathVariable("id") String id,
-		@RequestBody ModelConfiguration config
-	) {
-		return ResponseEntity.ok(proxy.updateAsset(id, convertObjectToSnakeCaseJsonNode(config)).getBody());
+			@PathVariable("id") String id,
+			@RequestBody ModelConfiguration config) throws IOException {
+
+		ModelConfiguration modelConfiguration = modelConfigurationService.updateModelConfiguration(config.setId(id));
+
+		JsonNode res = objectMapper.valueToTree(Map.of("id", modelConfiguration.getId()));
+
+		return ResponseEntity.ok(res);
 	}
 
 	@DeleteMapping("/{id}")
 	@Secured(Roles.USER)
 	public ResponseEntity<JsonNode> deleteModelConfiguration(
-		@PathVariable("id") String id
-	) {
-		return ResponseEntity.ok(proxy.deleteAsset(id).getBody());
+			@PathVariable("id") String id) throws IOException {
+		modelConfigurationService.deleteModelConfiguration(id);
+
+		JsonNode res = objectMapper
+				.valueToTree(Map.of("message", String.format("ModelConfiguration successfully deleted: %s", id)));
+
+		return ResponseEntity.ok(res);
 	}
 }
---
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/services/ModelConfigurationService.java
@@ -0,0 +1,55 @@
+package software.uncharted.terarium.hmiserver.controller.services;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import co.elastic.clients.elasticsearch._types.FieldSort;
+import co.elastic.clients.elasticsearch._types.SortOptions;
+import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import lombok.RequiredArgsConstructor;
+import software.uncharted.terarium.hmiserver.configuration.ElasticsearchConfiguration;
+import software.uncharted.terarium.hmiserver.models.dataservice.model.ModelConfiguration;
+import software.uncharted.terarium.hmiserver.service.elasticsearch.ElasticsearchService;
+
+@Service
+@RequiredArgsConstructor
+public class ModelConfigurationService {
+
+	private final ElasticsearchService elasticService;
+	private final ElasticsearchConfiguration elasticConfig;
+
+	public List<ModelConfiguration> getModelConfigurations(Integer page, Integer pageSize)
+			throws IOException {
+
+		final SearchRequest req = new SearchRequest.Builder()
+				.index(elasticConfig.getModelConfigurationIndex())
+				.size(pageSize)
+				.sort(new SortOptions.Builder()
+						.field(new FieldSort.Builder().field("timestamp").order(SortOrder.Asc).build()).build())
+				.build();
+
+		return elasticService.search(req, ModelConfiguration.class);
+	}
+
+	public ModelConfiguration getModelConfiguration(String id) throws IOException {
+		return elasticService.get(elasticConfig.getModelConfigurationIndex(), id, ModelConfiguration.class);
+	}
+
+	public void deleteModelConfiguration(String id) throws IOException {
+		elasticService.delete(elasticConfig.getModelIndex(), id);
+	}
+
+	public ModelConfiguration createModelConfiguration(ModelConfiguration modelConfiguration) throws IOException {
+		elasticService.index(elasticConfig.getModelIndex(), modelConfiguration.getId(), modelConfiguration);
+		return modelConfiguration;
+	}
+
+	public ModelConfiguration updateModelConfiguration(ModelConfiguration modelConfiguration) throws IOException {
+		elasticService.index(elasticConfig.getModelIndex(), modelConfiguration.getId(), modelConfiguration);
+		return modelConfiguration;
+	}
+
+}
