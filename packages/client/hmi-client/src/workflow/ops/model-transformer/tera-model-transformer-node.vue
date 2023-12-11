622e88a05c85036948f6a3cdd8541ed0a76ef31e
Shawn Yama <syama@uncharted.software>
2023-12-11 11:37:45 -0500

    model buttons

--- packages/client/hmi-client/src/workflow/ops/model-transformer/tera-model-transformer-node.vue
+++ packages/client/hmi-client/src/workflow/ops/model-transformer/tera-model-transformer-node.vue
@@ -4,6 +4,11 @@
 			<tera-model-diagram :model="model" :is-editable="false" nodePreview />
 		</div>
 	</template>
+	<template v-else>
+		<tera-operator-placeholder :operation-type="node.operationType">
+			Attach a model
+		</tera-operator-placeholder>
+	</template>
 </template>
 
 <script setup lang="ts">
@@ -12,7 +17,10 @@ import { Model } from '@/types/Types';
 import { WorkflowNode } from '@/types/workflow';
 import { onMounted, ref, watch } from 'vue';
 import TeraModelDiagram from '@/components/model/petrinet/model-diagrams/tera-model-diagram.vue';
+import TeraOperatorPlaceholder from '@/workflow/operator/tera-operator-placeholder.vue';
 import { ModelTransformerState } from './model-transformer-operation';
+
+// const emit = defineEmits(['open-drilldown']);
 
 const props = defineProps<{
 	node: WorkflowNode<ModelTransformerState>;
@@ -21,6 +29,7 @@ const props = defineProps<{
 const model = ref<Model | null>(null);
 
 const fetchModel = async () => {
+	// FIXME: The state now holds a modelConfigIds - so this may have to be updated to support that
 	if (!props.node?.state?.modelId) return;
 	model.value = await getModel(props.node?.state?.modelId);
 };
@@ -29,7 +38,7 @@ onMounted(async () => {
 	await fetchModel();
 });
 watch(
-	() => props.node?.state?.modelId,
+	() => props.node?.inputs,
 	async () => {
 		await fetchModel();
 	}
--- packages/client/hmi-client/src/workflow/ops/stratify-mira/tera-stratify-node-mira.vue
+++ packages/client/hmi-client/src/workflow/ops/stratify-mira/tera-stratify-node-mira.vue
@@ -1,25 +1,21 @@
 <template>
 	<section>
-		<img src="@assets/svg/plants.svg" alt="" draggable="false" />
-		Configure in the side panel
+		<tera-operator-placeholder :operation-type="node.operationType">
+			Attach a model
+		</tera-operator-placeholder>
 	</section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { WorkflowNode } from '@/types/workflow';
+import TeraOperatorPlaceholder from '@/workflow/operator/tera-operator-placeholder.vue';
+import { StratifyOperationStateMira } from './stratify-mira-operation';
 
-<style scoped>
-section {
-	align-items: center;
-	display: flex;
-	flex-direction: column;
-	margin-bottom: 1rem;
-	gap: 0.5rem;
-	color: var(--text-color-subdued);
-	font-size: var(--font-caption);
-}
+// const emit = defineEmits(['open-drilldown']);
 
-img {
-	height: 25%;
-	width: 25%;
-}
-</style>
+defineProps<{
+	node: WorkflowNode<StratifyOperationStateMira>;
+}>();
+</script>
+
+<style scoped></style>
