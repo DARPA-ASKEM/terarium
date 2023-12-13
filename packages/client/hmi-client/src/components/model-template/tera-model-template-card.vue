75cff6afc7c04f1a2b6e8e3e0775644cc987869d
Shawn Yama <syama@uncharted.software>
2023-12-13 12:56:00 -0500

    prepare model-template canvas, sidebar and cards

---
+++ packages/client/hmi-client/src/components/model-template/tera-model-template-card.vue
@@ -0,0 +1,64 @@
+<template>
+	<section class="container">
+		<section class="card">
+			<div class="draggable"></div>
+			<main>
+				<header>Template title</header>
+				<section>Diagram</section>
+			</main>
+		</section>
+		<ul>
+			<li v-for="(variable, index) in fakeVariables" :key="index">
+				{{ variable }}
+			</li>
+		</ul>
+	</section>
+</template>
+
+<script setup lang="ts">
+const fakeVariables = ['X', 'Y', 'p'];
+</script>
+
+<style scoped>
+.container {
+	display: flex;
+	font-size: var(--font-caption);
+
+	& ul {
+		list-style: none;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		margin: 0.75rem 0;
+
+		li {
+			background-color: var(--surface-section);
+			border: 1px solid var(--surface-border-alt);
+			border-top-right-radius: var(--border-radius);
+			border-bottom-right-radius: var(--border-radius);
+			padding: 0.15rem 0.25rem;
+		}
+	}
+}
+
+.card {
+	display: flex;
+	background-color: var(--surface-section);
+	border-radius: var(--border-radius-medium);
+	outline: 1px solid var(--surface-border-alt);
+
+	& > main {
+		margin: 0.5rem;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+}
+
+.draggable {
+	width: 0.75rem;
+	border-top-left-radius: var(--border-radius-medium);
+	border-bottom-left-radius: var(--border-radius-medium);
+	background-color: var(--surface-highlight);
+}
+</style>
---
+++ packages/client/hmi-client/src/components/model-template/tera-model-template.vue
@@ -0,0 +1,82 @@
+<template>
+	<tera-infinite-canvas>
+		<template #foreground>
+			<aside>
+				<section v-if="model?.header?.schema_name">
+					<h7>Model framework</h7>
+					<h5>{{ model.header.schema_name }}<i class="pi pi-info-circle"></i></h5>
+				</section>
+				<section>
+					<h7>Model templates</h7>
+					<ul></ul>
+				</section>
+				<section class="trash">
+					<i class="pi pi-trash"></i>
+					<div>Drag items here to delete</div>
+				</section>
+			</aside>
+		</template>
+		<template #data>
+			<tera-model-template-card />
+		</template>
+	</tera-infinite-canvas>
+</template>
+
+<script setup lang="ts">
+import { Model } from '@/types/Model';
+import TeraInfiniteCanvas from '../widgets/tera-infinite-canvas.vue';
+import TeraModelTemplateCard from './tera-model-template-card.vue';
+
+defineProps<{
+	model?: Model;
+}>();
+</script>
+
+<style scoped>
+aside {
+	width: 15rem;
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	background-color: #f4f7fa;
+	border-right: 1px solid var(--surface-border-alt);
+	padding: 1rem;
+	gap: 0.5rem;
+}
+
+h5 {
+	display: flex;
+	align-items: center;
+	gap: 0.25rem;
+}
+
+h5,
+h7 {
+	font-weight: var(--font-weight);
+}
+
+h7 {
+	color: var(--text-color-subdued);
+}
+
+.pi-info-circle {
+	color: var(--text-color-subdued);
+	cursor: help;
+}
+
+.trash {
+	margin-top: auto;
+	font-size: var(--font-caption);
+	color: var(--text-color-subdued);
+	border: 1px dashed #9fa9b7;
+	border-radius: var(--border-radius);
+	background-color: #eff2f5;
+	text-align: center;
+	padding: 1rem 0;
+
+	& > .pi-trash {
+		font-size: 1.5rem;
+		margin-bottom: 0.5rem;
+	}
+}
+</style>
