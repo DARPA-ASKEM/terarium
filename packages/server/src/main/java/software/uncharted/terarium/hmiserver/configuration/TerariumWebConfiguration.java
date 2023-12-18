93fe5d59c2405e4d8b18c4b898b59ca03612c476
Charles Coleman <bigglesandginger@gmail.com>
2023-12-18 13:41:34 -0500

    Inital proof of concept

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/configuration/TerariumWebConfiguration.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/configuration/TerariumWebConfiguration.java
@@ -2,9 +2,13 @@ package software.uncharted.terarium.hmiserver.configuration;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import software.uncharted.terarium.hmiserver.controller.internal.InternalController;
 import software.uncharted.terarium.hmiserver.interceptors.OrderedHandlerInterceptor;
 
 import java.util.Comparator;
@@ -16,10 +20,18 @@ import java.util.List;
 public class TerariumWebConfiguration implements WebMvcConfigurer {
 	private final List<OrderedHandlerInterceptor> interceptorList;
 
+	@Value("${server.trustedPort:3001}")
+	private String trustedPort;
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		interceptorList.stream()
 			.sorted(Comparator.comparingInt(OrderedHandlerInterceptor::getOrder))
 			.forEach(registry::addInterceptor);
+	}
+
+	@Bean
+	public FilterRegistrationBean<TrustedEndpointsFilter> trustedEndpointsFilter() {
+		return new FilterRegistrationBean<>(new TrustedEndpointsFilter(trustedPort, InternalController.PATH));
 	}
 }
