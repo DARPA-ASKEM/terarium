dd6d8a8eb068bf630d567ab6a000d0c45dc9959f
Cole Blanchard <cblanchard@Coles-MacBook-Pro.local>
2023-12-20 16:20:13 -0500

    final final

--- packages/client/hmi-client/src/workflow/ops/document/mod.ts
+++ packages/client/hmi-client/src/workflow/ops/document/mod.ts
@@ -1,6 +1,6 @@
 import { DocumentOperation as operation } from './document-operation';
 import node from './tera-document-operation-node.vue';
-import drilldown from './tera-document-operation.vue';
+import drilldown from './tera-document-operation-drilldown.vue';
 
 const name = operation.name;
 
