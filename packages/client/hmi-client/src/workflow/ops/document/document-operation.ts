8e865144ced7b9fb00db54f42142bb20b7ee8fff
Cole Blanchard <33158416+blanchco@users.noreply.github.com>
2023-12-22 09:53:11 -0500

    task: document operator  (#2452)
    Co-authored-by: Cole Blanchard <cblanchard@Coles-MacBook-Pro.local>

---
+++ packages/client/hmi-client/src/workflow/ops/document/document-operation.ts
@@ -0,0 +1,28 @@
+import { DocumentExtraction } from '@/types/Types';
+import { Operation, AssetBlock, WorkflowOperationTypes } from '@/types/workflow';
+
+export interface DocumentOperationState {
+	documentId: string | null;
+	equations: AssetBlock<DocumentExtraction>[];
+	tables: AssetBlock<DocumentExtraction>[];
+	figures: AssetBlock<DocumentExtraction>[];
+}
+
+export const DocumentOperation: Operation = {
+	name: WorkflowOperationTypes.DOCUMENT,
+	displayName: 'Document',
+	description: 'Document',
+	inputs: [],
+	outputs: [{ type: 'documentId' }],
+	isRunnable: true,
+	action: () => {},
+	initState: () => {
+		const init: DocumentOperationState = {
+			documentId: null,
+			equations: [],
+			tables: [],
+			figures: []
+		};
+		return init;
+	}
+};
