b02151bc75b189d25e641cd5fec42b38ac66af3c
Cole Blanchard <cblanchard@Coles-MacBook-Pro.local>
2024-01-08 11:18:21 -0500

    bring out extractions section

--- packages/client/hmi-client/src/components/documents/tera-document-asset.vue
+++ packages/client/hmi-client/src/components/documents/tera-document-asset.vue
@@ -14,59 +14,7 @@
 				<tera-text-editor v-else-if="docText" :initial-text="docText" />
 			</tera-drilldown-section>
 			<tera-drilldown-section class="extractions-section">
-				<Accordion multiple :active-index="[0, 1, 2]">
-					<AccordionTab v-if="!isEmpty(clonedState.equations)">
-						<template #header>
-							<header>Equation Images</header>
-						</template>
-						<tera-asset-block
-							v-for="(equation, i) in clonedState.equations"
-							:key="i"
-							:is-included="equation.includeInProcess"
-						>
-							<template #header>
-								<h5>{{ equation.name }}</h5>
-							</template>
-							<Image id="img" :src="equation.asset.metadata?.url" :alt="''" preview />
-						</tera-asset-block>
-					</AccordionTab>
-					<AccordionTab v-if="!isEmpty(clonedState.figures)">
-						<template #header>
-							<header>Figure Images</header>
-						</template>
-						<tera-asset-block
-							v-for="(figure, i) in clonedState.figures"
-							:key="i"
-							:is-included="figure.includeInProcess"
-						>
-							<template #header>
-								<h5>{{ figure.name }}</h5>
-							</template>
-							<Image id="img" :src="figure.asset.metadata?.url" :alt="''" preview />
-						</tera-asset-block>
-					</AccordionTab>
-					<AccordionTab v-if="!isEmpty(clonedState.tables)">
-						<template #header>
-							<header>Table Images</header>
-						</template>
-						<tera-asset-block
-							v-for="(table, i) in clonedState.tables"
-							:key="i"
-							:is-included="table.includeInProcess"
-						>
-							<template #header>
-								<h5>{{ table.name }}</h5>
-							</template>
-							<Image id="img" :src="table.asset.metadata?.url" :alt="''" preview />
-						</tera-asset-block>
-					</AccordionTab>
-					<AccordionTab v-if="!isEmpty(document?.text)">
-						<template #header>
-							<header>Text</header>
-						</template>
-						<p>{{ document?.text }}</p>
-					</AccordionTab>
-				</Accordion>
+				<tera-extractions :document="document" :state="clonedState" @update="onUpdateAsset" />
 			</tera-drilldown-section>
 		</div>
 	</tera-asset>
@@ -74,9 +22,6 @@
 
 <script setup lang="ts">
 import { ref, watch } from 'vue';
-import { isEmpty } from 'lodash';
-import Accordion from 'primevue/accordion';
-import AccordionTab from 'primevue/accordiontab';
 import TeraPdfEmbed from '@/components/widgets/tera-pdf-embed.vue';
 import { DocumentAsset, DocumentExtraction, ExtractionAssetType } from '@/types/Types';
 import {
@@ -84,14 +29,13 @@ import {
 	getDocumentAsset,
 	getDocumentFileAsText
 } from '@/services/document-assets';
-import Image from 'primevue/image';
 import TeraDrilldownSection from '@/components/drilldown/tera-drilldown-section.vue';
 import { AssetBlock } from '@/types/workflow';
 import { DocumentOperationState } from '@/workflow/ops/document/document-operation';
 import { FeatureConfig } from '@/types/common';
-import TeraAssetBlock from '../widgets/tera-asset-block.vue';
 import TeraAsset from '../asset/tera-asset.vue';
 import TeraTextEditor from './tera-text-editor.vue';
+import TeraExtractions from './tera-extractions.vue';
 
 const props = defineProps<{
 	assetId: string;
@@ -111,6 +55,10 @@ const clonedState = ref<DocumentOperationState>({
 });
 
 const emit = defineEmits(['close-preview', 'asset-loaded']);
+
+function onUpdateAsset(state: DocumentOperationState) {
+	clonedState.value = state;
+}
 
 watch(
 	() => props.assetId,
