021704853192f1ebfabde7d6770c8cbcddc68680
Yohann Paris <github@yohannparis.com>
2024-01-12 13:10:53 -0500

    remove getAssets

--- packages/client/hmi-client/src/services/project.ts
+++ packages/client/hmi-client/src/services/project.ts
@@ -8,19 +8,13 @@ import DatasetIcon from '@/assets/svg/icons/dataset.svg?component';
 import { Component } from 'vue';
 import * as EventService from '@/services/event';
 import {
-	Artifact,
 	AssetType,
-	Code,
-	Dataset,
-	DocumentAsset,
 	EventType,
 	ExternalPublication,
-	Model,
 	PermissionRelationships,
 	Project,
 	ProjectAsset
 } from '@/types/Types';
-import { Workflow } from '@/types/workflow';
 
 /**
  * Create a project
@@ -102,46 +96,6 @@ async function getAll(): Promise<Project[] | null> {
 }
 
 /**
- * Get project assets for a given project per id
- * @param projectId project id to get assets for
- * @param types optional list of types. If none are given we assume you want it all
- * @return ProjectAssets|null - the appropriate project, or null if none returned by API
- */
-async function getAssets(projectId: string, types?: string[]): Promise<ProjectAssets | null> {
-	try {
-		let url = `/projects/${projectId}/assets`;
-		if (types) {
-			types.forEach((type, index) => {
-				// add URL with format: ...?types=A&types=B&types=C
-				url += `${index === 0 ? '?' : '&'}types=${type}`;
-			});
-		} else {
-			Object.values(AssetType).forEach((type, index) => {
-				url += `${index === 0 ? '?' : '&'}types=${type}`;
-			});
-		}
-		const response = await API.get(url);
-		if (response.status !== 200) return null;
-
-		// FIXME: this is a hack to get around the fact that the backend returns list names in lower case and we need them in upper case for ProjectAssets
-		const data: ProjectAssets = {
-			[AssetType.Publication]: response.data?.publication ?? ([] as ExternalPublication[]),
-			[AssetType.Model]: response.data?.model ?? ([] as Model[]),
-			[AssetType.Dataset]: response.data?.dataset ?? ([] as Dataset[]),
-			[AssetType.Code]: response.data?.code ?? ([] as Code[]),
-			[AssetType.Artifact]: response.data?.artifact ?? ([] as Artifact[]),
-			[AssetType.Workflow]: response.data?.workflow ?? ([] as Workflow[]),
-			[AssetType.Document]: response.data?.document ?? ([] as DocumentAsset[])
-		};
-
-		return data ?? null;
-	} catch (error) {
-		logger.error(error);
-		return null;
-	}
-}
-
-/**
  * Get projects publication assets for a given project per id
  * @param projectId project id to get assets for
  * @return ExternalPublication[] the documents assets for the project
@@ -208,26 +162,13 @@ async function deleteAsset(
 /**
  * Get a project per id
  * @param projectId - Project['id']
- * @param containingAssetsInformation - boolean - Add the assets information during the same call
  * @return Project|null - the appropriate project, or null if none returned by API
  */
-async function get(
-	projectId: Project['id'],
-	containingAssetsInformation: boolean = false
-): Promise<Project | null> {
+async function get(projectId: Project['id']): Promise<Project | null> {
 	try {
 		const { status, data } = await API.get(`/projects/${projectId}`);
 		if (status !== 200) return null;
-		const project = data as Project;
-
-		if (project && containingAssetsInformation) {
-			const assets = await getAssets(projectId);
-			if (assets) {
-				project.assets = assets;
-			}
-		}
-
-		return project ?? null;
+		return (data as Project) ?? null;
 	} catch (error) {
 		logger.error(error);
 		return null;
@@ -325,7 +266,6 @@ export {
 	getAll,
 	addAsset,
 	deleteAsset,
-	getAssets,
 	getAssetIcon,
 	getPublicationAssets,
 	getPermissions,
