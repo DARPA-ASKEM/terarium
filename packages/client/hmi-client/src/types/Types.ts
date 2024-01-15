b1f4a991351c9c729116bc5811ca0e92fef1dba2
David Gauldie <dgauldie@uncharted.software>
2024-01-15 17:01:21 -0500

    generated types

--- packages/client/hmi-client/src/types/Types.ts
+++ packages/client/hmi-client/src/types/Types.ts
@@ -390,7 +390,7 @@ export interface ProjectAsset {
     id: string;
     assetId: string;
     assetType: AssetType;
-    assetName?: string;
+    assetName: string;
     externalRef?: string;
     createdOn?: Date;
     updatedOn?: Date;
