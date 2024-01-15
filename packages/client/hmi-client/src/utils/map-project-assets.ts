f6b886d6bd508e54fd165b4a589e6131a1c1af0a
Yohann Paris <github@yohannparis.com>
2024-01-15 15:04:05 -0500

    short date format

--- packages/client/hmi-client/src/utils/map-project-assets.ts
+++ packages/client/hmi-client/src/utils/map-project-assets.ts
@@ -1,6 +1,7 @@
 import { AssetItem } from '@/types/common';
 import { useProjects } from '@/composables/project';
 import { isProjectAssetTypes } from '@/types/Project';
+import { formatShort } from '@/utils/date';
 
 type ProjectAssetItems = Map<string, Set<AssetItem>>;
 
@@ -17,7 +18,9 @@ export const generateProjectAssetsMap = (searchAsset: string): ProjectAssetItems
 			(asset) =>
 				({
 					assetId: asset.id.toString(),
-					assetName: asset.assetName ?? asset.assetId,
+					assetName:
+						asset.assetName ??
+						`${asset.assetId.substring(0, 3)} - ${formatShort(asset?.updatedOn)}`,
 					pageType: asset.assetType
 				}) as AssetItem
 		)
@@ -29,7 +32,7 @@ export const generateProjectAssetsMap = (searchAsset: string): ProjectAssetItems
 		});
 
 	Object.entries(Object.groupBy(cleanAssets, (asset) => asset.pageType)).forEach(
-		([type, assetList]) => assetItemsMap.set(type, new Set(assetList))
+		([type, assetList]) => assetItemsMap.set(type, new Set(assetList as AssetItem[]))
 	);
 
 	return assetItemsMap;
