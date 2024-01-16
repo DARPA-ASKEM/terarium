5f3255e59cfd7b69fdc37f90e1404869e2bc78d2
Al-Jami Ismail <aljami1379@gmail.com>
2024-01-16 13:04:21 -0500

    Model config matrices functional implementation (#2515)

--- packages/client/hmi-client/src/components/model/petrinet/model-configurations/tera-stratified-matrix.vue
+++ packages/client/hmi-client/src/components/model/petrinet/model-configurations/tera-stratified-matrix.vue
@@ -5,15 +5,15 @@
 	>
 		<div class="p-datatable-wrapper">
 			<table class="p-datatable-table p-datatable-scrollable-table editable-cells-table">
-				<thead v-if="matrix[0].length > 1" class="p-datatable-thead">
+				<thead v-if="matrix[0].length > 0" class="p-datatable-thead">
 					<tr>
-						<th v-if="matrix.length > 1" class="choose-criteria">&nbsp;</th>
+						<th v-if="matrix.length > 0" class="choose-criteria">&nbsp;</th>
 						<th v-for="(row, rowIdx) in matrix[0]" :key="rowIdx">{{ row.colCriteria }}</th>
 					</tr>
 				</thead>
 				<tbody class="p-datatable-tbody">
 					<tr v-for="(row, rowIdx) in matrix" :key="rowIdx">
-						<td v-if="matrix.length > 1" class="p-frozen-column">
+						<td v-if="matrix.length > 0" class="p-frozen-column">
 							<template v-if="stratifiedMatrixType === StratifiedMatrix.Initials">
 								{{ Object.values(row[0].rowCriteria).join(' / ') }}
 							</template>
@@ -233,6 +233,10 @@ function configureMatrix() {
 		}
 	}
 }
+
+watch([() => props.id, () => props.modelConfiguration.configuration], () => {
+	configureMatrix();
+});
 
 onMounted(() => {
 	configureMatrix();
