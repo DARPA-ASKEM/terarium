cb72db8415e7ef8a92a39334c66d171b6bf620bd
dvince <dvince@uncharted.software>
2024-01-18 13:27:29 -0500

    fix(hmi): 405 error when posting provenance
    Fixing a 405 error when posting to connected nodes. There were a few problems here. The first being that the URL was incorrect. But then the second issue was another front-end type that was out of sync with the back end, so I've blown that away and made the back end type a tsmodel to use.

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/provenance/ProvenanceSearchResult.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/provenance/ProvenanceSearchResult.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import software.uncharted.terarium.hmiserver.annotations.TSModel;
 
 @Data
 @Accessors(chain = true)
+@TSModel
 public class ProvenanceSearchResult implements Serializable {
 
 	private List<ProvenanceNode> nodes;
