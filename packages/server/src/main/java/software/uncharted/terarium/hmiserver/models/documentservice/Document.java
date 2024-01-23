dd83fdb3bca207dfbbd4228300b1008e1b1e0088
Yohann Paris <github@yohannparis.com>
2024-01-23 16:29:48 -0500

    Update Document.java

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/documentservice/Document.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/documentservice/Document.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @NoArgsConstructor
 public class Document implements Serializable {
 
-	@JsonAlias("gdd_id")
+	@JsonAlias("_gddid")
 	private String gddId;
 
 	private String title;
