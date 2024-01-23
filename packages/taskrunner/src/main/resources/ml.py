9463878a053eb04e7039de4b42a73f9509679573
kbirk <birk.kevin@gmail.com>
2024-01-23 13:13:53 -0500

    Update test file.

--- packages/taskrunner/src/main/resources/ml.py
+++ packages/taskrunner/src/main/resources/ml.py
@@ -1,32 +1,32 @@
 import argparse
+import json
 import time
-import sys
 
-# Create the parser
-parser = argparse.ArgumentParser(description='Advanced Blockchain ML Task')
+def read_input(input_pipe: str):
+	line = ""
+	with open(input_pipe, 'rb') as f:
+		line = f.readline().strip()
+	return json.loads(line.decode())
 
-# Add the arguments
-parser.add_argument('--input_pipe', type=str, required=True, help='The name of the input pipe')
-parser.add_argument('--output_pipe', type=str, required=True, help='The name of the output pipe')
 
-# Parse the arguments
-args = parser.parse_args()
+def write_output(output_pipe: str, output: dict):
+	bs = (json.dumps(output) + "\n").encode()
+	with open(output_pipe, 'wb') as f_out:
+		f_out.write(bs)
 
-# Define the names of your pipes
-input_pipe = args.input_pipe
-output_pipe = args.output_pipe
+def main():
+	parser = argparse.ArgumentParser(description='Advanced Blockchain ML Task')
+	parser.add_argument('--input_pipe', type=str, required=True, help='The name of the input pipe')
+	parser.add_argument('--output_pipe', type=str, required=True, help='The name of the output pipe')
+	args = parser.parse_args()
 
-# Define the names of your pipes
-input_pipe = sys.argv[1]
-output_pipe = sys.argv[2]
+	input = read_input(args.input_pipe)
 
-line = ""
-with open(input_pipe, 'rb') as f:
-	line = f.readline().strip()
+	for i in range(5):
+		print("ML Task: {}".format(i))
+		time.sleep(1)
 
-print(line.decode())
+	write_output(args.output_pipe, {"result": "ok"})
 
-time.sleep(5)
-
-with open(output_pipe, 'wb') as f_out:
-	f_out.write(line)
+if __name__ == "__main__":
+	main()
