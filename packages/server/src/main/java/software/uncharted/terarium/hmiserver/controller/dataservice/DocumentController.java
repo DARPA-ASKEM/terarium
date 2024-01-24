ae892768ece420a5b162ddef4905dad293500c50
dvince <dvince@uncharted.software>
2024-01-24 09:31:30 -0500

    PUT should not return a body. Should just return status code

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/DocumentController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/DocumentController.java
@@ -276,7 +276,7 @@ public class DocumentController {
 	 * @param fileEntity The entity containing the file to upload
 	 * @return A response containing the status of the upload
 	 */
-	private ResponseEntity<ResponseStatus> uploadDocumentHelper(final UUID documentId, final String fileName,
+	private ResponseEntity<Void> uploadDocumentHelper(final UUID documentId, final String fileName,
 			final HttpEntity fileEntity) {
 		try (final CloseableHttpClient httpclient = HttpClients.custom()
 				.disableRedirectHandling()
@@ -300,7 +300,8 @@ public class DocumentController {
 
 				documentAssetService.updateDocumentAsset(document.get());
 			}
-			return ResponseEntity.ok(new ResponseStatus(response.getStatusLine().getStatusCode()));
+
+			return ResponseEntity.status(response.getStatusLine().getStatusCode()).build();
 
 		} catch (final IOException e) {
 			final String error = "Unable to upload document";
@@ -320,7 +321,7 @@ public class DocumentController {
 			@ApiResponse(responseCode = "200", description = "Uploaded the document.", content = @Content(mediaType = "application/json", schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ResponseStatus.class))),
 			@ApiResponse(responseCode = "500", description = "There was an issue uploading the document", content = @Content)
 	})
-	public ResponseEntity<ResponseStatus> uploadDocument(
+	public ResponseEntity<Void> uploadDocument(
 			@PathVariable("id") final UUID id,
 			@RequestParam("filename") final String filename,
 			@RequestPart("file") final MultipartFile file) {
