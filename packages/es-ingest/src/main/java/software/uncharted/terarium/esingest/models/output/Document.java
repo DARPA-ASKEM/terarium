73eac2b1bad72d93a768cb1de573054aa0aac2bc
kbirk <birk.kevin@gmail.com>
2024-02-02 16:33:04 -0500

    Fix issue with updates overwriting missing fields

--- packages/es-ingest/src/main/java/software/uncharted/terarium/esingest/models/output/Document.java
+++ packages/es-ingest/src/main/java/software/uncharted/terarium/esingest/models/output/Document.java
@@ -5,11 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
+@JsonInclude(Include.NON_NULL)
 public class Document<EmbeddingType> implements IOutputDocument<EmbeddingType>, Serializable {
 
 	private UUID id;
