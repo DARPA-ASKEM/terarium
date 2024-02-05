8c39a708d9ec71473af4a1ad96f164aa32cf6893
Daniel Chang <mwdchang@gmail.com>
2024-02-05 11:27:23 -0500

    chore: decapodes data structures (#2653)

---
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/multiphysics/ContextHeader.java
@@ -0,0 +1,19 @@
+package software.uncharted.terarium.hmiserver.models.dataservice.multiphysics;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import software.uncharted.terarium.hmiserver.annotations.TSModel;
+import java.io.Serializable;
+
+@Data
+@Accessors(chain = true)
+@TSModel
+public class ContextHeader implements Serializable {
+	private String id;
+	private String description;
+	private String name;
+
+	@JsonAlias("parent_model")
+	private String parentModel;
+}
