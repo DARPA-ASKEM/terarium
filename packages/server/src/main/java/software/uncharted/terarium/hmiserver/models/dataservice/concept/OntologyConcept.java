db12e470963939b7ab6a717573500ddb3c5c7d19
David Gauldie <dgauldie@uncharted.software>
2024-02-12 13:56:08 -0500

    updating mappings

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/concept/OntologyConcept.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/concept/OntologyConcept.java
@@ -2,45 +2,40 @@ package software.uncharted.terarium.hmiserver.models.dataservice.concept;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import software.uncharted.terarium.hmiserver.annotations.TSModel;
-import software.uncharted.terarium.hmiserver.annotations.TSOptional;
 import software.uncharted.terarium.hmiserver.models.dataservice.TaggableType;
-import software.uncharted.terarium.hmiserver.models.dataservice.TerariumAsset;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.sql.Timestamp;
-import java.util.UUID;
 
-@EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
 @Accessors(chain = true)
 @TSModel
-public class OntologyConcept extends TerariumAsset {
+public class OntologyConcept implements Serializable {
 
 	@Serial
 	private static final long serialVersionUID = 5877193452400957711L;
 
-
+	@Id
 	private String curie;
 
 	@ManyToOne
 	@JoinColumn(name = "activeConceptId", nullable = true)
 	@JsonBackReference
+	@JsonAlias("active_concept")
 	private ActiveConcept activeConcept;
 
 	private TaggableType type;
 
 	@JsonAlias("object_id")
-	private UUID objectId;
+	private String objectId;
 
 	private OntologicalField status;
 
