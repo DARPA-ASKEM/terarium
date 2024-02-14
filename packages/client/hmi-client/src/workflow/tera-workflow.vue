cfa5c00a5fa796086e7e5d90375e16c81c173cec
Daniel Chang <mwdchang@gmail.com>
2024-02-14 17:30:40 -0500

    ensure workflow save on unload

--- packages/client/hmi-client/src/workflow/tera-workflow.vue
+++ packages/client/hmi-client/src/workflow/tera-workflow.vue
@@ -750,6 +750,12 @@ const pathFn = d3
 // Get around typescript complaints
 const drawPath = (v: any) => pathFn(v) as string;
 
+const unloadCheck = () => {
+	if (workflowDirty) {
+		workflowService.updateWorkflow(wf.value);
+	}
+};
+
 watch(
 	() => [props.assetId],
 	async () => {
@@ -768,6 +774,7 @@ watch(
 
 onMounted(() => {
 	document.addEventListener('mousemove', mouseUpdate);
+	window.addEventListener('beforeunload', unloadCheck);
 	saveTimer = setInterval(() => {
 		if (workflowDirty) {
 			workflowService.updateWorkflow(wf.value);
@@ -783,6 +790,7 @@ onUnmounted(() => {
 		clearInterval(saveTimer);
 	}
 	document.removeEventListener('mousemove', mouseUpdate);
+	window.removeEventListener('beforeunload', unloadCheck);
 });
 
 function cleanUpLayout() {
