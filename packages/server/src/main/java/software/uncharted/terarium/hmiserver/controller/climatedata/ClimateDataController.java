a615313b96b6fdedcd65cc96633b6a56689052eb
Charles Coleman <bigglesandginger@gmail.com>
2024-02-14 09:29:38 -0500

    use UUID for dataset ids

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/climatedata/ClimateDataController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/climatedata/ClimateDataController.java
@@ -11,6 +11,8 @@ import software.uncharted.terarium.hmiserver.proxies.climatedata.ClimateDataProx
 import software.uncharted.terarium.hmiserver.proxies.funman.FunmanProxy;
 import software.uncharted.terarium.hmiserver.security.Roles;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/climatedata/queries")
 public class ClimateDataController {
@@ -20,49 +22,49 @@ public class ClimateDataController {
 
 	@GetMapping("/status/{uuid}")
 	@Secured(Roles.USER)
-	public ResponseEntity<JsonNode> status(@PathVariable String uuid) {
-		ResponseEntity<JsonNode> response = climateDataProxy.status(uuid);
+	public ResponseEntity<JsonNode> status(@PathVariable UUID uuid) {
+		ResponseEntity<JsonNode> response = climateDataProxy.status(uuid.toString());
 
 		return ResponseEntity.ok(response.getBody());
 	}
 
 	@GetMapping("/search-esgf")
 	@Secured(Roles.USER)
-	public ResponseEntity<JsonNode> searchEsgf(@RequestParam("query-id") String queryId) {
-		ResponseEntity<JsonNode> response = climateDataProxy.searchEsgf(queryId);
+	public ResponseEntity<JsonNode> searchEsgf(@RequestParam("query") String query) {
+		ResponseEntity<JsonNode> response = climateDataProxy.searchEsgf(query);
 
 		return ResponseEntity.ok(response.getBody());
 	}
 
 	@GetMapping("/preview-esgf/{datasetId}")
 	@Secured(Roles.USER)
-	public ResponseEntity<JsonNode> previewEsgf(@PathVariable String datasetId,
+	public ResponseEntity<JsonNode> previewEsgf(@PathVariable UUID datasetId,
 																							@RequestParam(value = "variable-id", required = false) final String variableId,
 																							@RequestParam(value = "timestamps", required = false) final String timestamps,
 																							@RequestParam(value = "time-index", required = false) final String timeIndex
 	) {
-		ResponseEntity<JsonNode> response = climateDataProxy.previewEsgf(datasetId, variableId, timestamps, timeIndex);
+		ResponseEntity<JsonNode> response = climateDataProxy.previewEsgf(datasetId.toString(), variableId, timestamps, timeIndex);
 
 		return ResponseEntity.ok(response.getBody());
 	}
 
 	@GetMapping("/subset-esgf/{datasetId}")
 	@Secured(Roles.USER)
-	public ResponseEntity<JsonNode> subsetEsgf(@PathVariable String datasetId,
-																						 @RequestParam(value = "parent-dataset-id", required = false) final String parentDatasetId,
+	public ResponseEntity<JsonNode> subsetEsgf(@PathVariable UUID datasetId,
+																						 @RequestParam(value = "parent-dataset-id", required = false) final UUID parentDatasetId,
 																						 @RequestParam(value = "timestamps", required = false) final String timestamps,
 																						 @RequestParam(value = "envelope", required = false) final String envelope,
 																						 @RequestParam(value = "thin-factor", required = false) final String thinFactor
 	) {
-		ResponseEntity<JsonNode> response = climateDataProxy.subsetEsgf(datasetId, parentDatasetId, timestamps, envelope, thinFactor);
+		ResponseEntity<JsonNode> response = climateDataProxy.subsetEsgf(datasetId.toString(), parentDatasetId.toString(), timestamps, envelope, thinFactor);
 
 		return ResponseEntity.ok(response.getBody());
 	}
 
 	@GetMapping("/fetch-esgf/{datasetId}")
 	@Secured(Roles.USER)
-	public ResponseEntity<JsonNode> fetchEsgf(@PathVariable String datasetId) {
-		ResponseEntity<JsonNode> response = climateDataProxy.fetchEsgf(datasetId);
+	public ResponseEntity<JsonNode> fetchEsgf(@PathVariable UUID datasetId) {
+		ResponseEntity<JsonNode> response = climateDataProxy.fetchEsgf(datasetId.toString());
 
 		return ResponseEntity.ok(response.getBody());
 	}
