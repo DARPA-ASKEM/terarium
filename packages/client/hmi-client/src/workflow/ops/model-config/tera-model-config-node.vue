aaf8fa95245ad3f4f485e73cf7f4452af8676051
Daniel Chang <mwdchang@gmail.com>
2024-02-20 13:59:13 -0500

    fix model-config ES errors

--- packages/client/hmi-client/src/workflow/ops/model-config/tera-model-config-node.vue
+++ packages/client/hmi-client/src/workflow/ops/model-config/tera-model-config-node.vue
@@ -1,11 +1,6 @@
 <template>
 	<section>
 		<template v-if="!isEmpty(node.state)">
-			<ul>
-				<li v-for="config of modelConfigs" :key="config.id">
-					{{ config.name }}
-				</li>
-			</ul>
 			<Button label="Open" @click="emit('open-drilldown')" severity="secondary" outlined />
 		</template>
 		<template v-else>
@@ -18,39 +13,13 @@
 
 <script setup lang="ts">
 import { isEmpty } from 'lodash';
-import { watch, ref } from 'vue';
 import { WorkflowNode } from '@/types/workflow';
-import { ModelConfiguration } from '@/types/Types';
-import { getModelConfigurations } from '@/services/model';
 import Button from 'primevue/button';
 import TeraOperatorPlaceholder from '@/components/operator/tera-operator-placeholder.vue';
 import { ModelConfigOperationState } from './model-config-operation';
 
-const props = defineProps<{
+defineProps<{
 	node: WorkflowNode<ModelConfigOperationState>;
 }>();
-const emit = defineEmits(['append-output', 'open-drilldown']);
-
-const modelConfigs = ref<ModelConfiguration[]>([]);
-
-const refresh = async (modelId: string) => {
-	modelConfigs.value = await getModelConfigurations(modelId);
-	modelConfigs.value.forEach((config) => {
-		emit('append-output', {
-			type: 'modelConfigId',
-			label: config.name,
-			value: [config.id]
-		});
-	});
-};
-
-watch(
-	() => [props.node.inputs[0].value],
-	async () => {
-		if (props.node.inputs[0].value) {
-			const modelId = props.node.inputs[0].value[0];
-			await refresh(modelId);
-		}
-	}
-);
+const emit = defineEmits(['open-drilldown']);
 </script>
