769d665c9e397b21f369df8297a204f2cb320c0e
Shawn Yama <syama@uncharted.software>
2024-02-26 12:23:34 -0500

    prepare node and drilldown

---
+++ packages/client/hmi-client/src/workflow/ops/model-comparison/tera-model-comparison.vue
@@ -0,0 +1,29 @@
+<template>
+	<tera-drilldown :title="node.displayName" @on-close-clicked="emit('close')">
+		<div :tabName="Tabs.Wizard">
+			<tera-drilldown-section> </tera-drilldown-section>
+		</div>
+		<div :tabName="Tabs.Notebook"></div>
+		<template #preview>
+			<tera-drilldown-preview>sdsd</tera-drilldown-preview>
+		</template>
+	</tera-drilldown>
+</template>
+
+<script setup lang="ts">
+import TeraDrilldown from '@/components/drilldown/tera-drilldown.vue';
+import TeraDrilldownPreview from '@/components/drilldown/tera-drilldown-preview.vue';
+import TeraDrilldownSection from '@/components/drilldown/tera-drilldown-section.vue';
+import { WorkflowNode } from '@/types/workflow';
+import { ModelComparisonOperationState } from './model-comparison-operation';
+
+defineProps<{
+	node: WorkflowNode<ModelComparisonOperationState>;
+}>();
+const emit = defineEmits(['append-output-port', 'update-state', 'close']);
+
+enum Tabs {
+	Wizard = 'Wizard',
+	Notebook = 'Notebook'
+}
+</script>
