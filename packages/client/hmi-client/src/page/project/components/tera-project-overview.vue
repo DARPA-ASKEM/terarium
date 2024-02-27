fad54d6f5c929e8ad9df6912e1fcba7c893cd853
Jamie Waese <120480244+jamiewaese-uncharted@users.noreply.github.com>
2024-02-27 16:56:56 -0500

    just a few more text edits...

--- packages/client/hmi-client/src/page/project/components/tera-project-overview.vue
+++ packages/client/hmi-client/src/page/project/components/tera-project-overview.vue
@@ -37,16 +37,16 @@ const isRenamingProject = ref(false);
 const editorContent = ref(`
     <div>
         <h2>Hey there! </h2>
-        <p>This is your project overview page. Use this space to describe your project or as a scratch pad to organize your thoughts . Not sure where to start? Here are some things you can try:</p>
+        <p>This is your project overview page. Use this space however you like. Not sure where to start? Here are some things you can try:</p>
 		<br>
         <ul>
             <li><strong>Upload stuff:</strong> Upload documents, models, code or datasets with the green button in the bottom left corner.</li>
-            <li><strong>Explore and add:</strong> Use the Explorer to find resources that you can add to your project.</li>
+            <li><strong>Explore and add:</strong> Use the project selector in the top nav to switch to the Explorer where you can find documents, models and datasets that you can add to your project.</li>
             <li><strong>Build a model:</strong> Create a model that fits just what you need.</li>
             <li><strong>Create a workflow:</strong> Connect resources with operators so you can focus on the science and not the plumbing.</li>
         </ul>
 		<br>
-        <p>Feel free to erase this text and use this space however you like.</p>
+        <p>Feel free to erase this text and make it your own.</p>
     </div>
     `);
 </script>
@@ -99,6 +99,7 @@ a {
 .editor-container:deep(.ql-editor) {
 	height: calc(100vh - 240px);
 	font-family: var(--font-family);
+	font-size: var(--font-size);
 }
 /* Editor toolbar formatting */
 :deep(.p-editor-container .p-editor-toolbar) {
