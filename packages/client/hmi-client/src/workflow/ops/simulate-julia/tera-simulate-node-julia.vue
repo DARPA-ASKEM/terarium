32463b721782dddf81d187b216fc39f56b5db3e3
Daniel Chang <mwdchang@gmail.com>
2024-02-27 12:54:19 -0500

    sim preview

--- packages/client/hmi-client/src/workflow/ops/simulate-julia/tera-simulate-node-julia.vue
+++ packages/client/hmi-client/src/workflow/ops/simulate-julia/tera-simulate-node-julia.vue
@@ -1,5 +1,12 @@
 <template>
 	<main>
+		<tera-simulate-chart
+			v-if="hasData"
+			:run-results="{ [selectedRunId]: runResults[selectedRunId] }"
+			:chartConfig="{ selectedRun: selectedRunId, selectedVariable: ['S'] }"
+			:size="{ width: 180, height: 120 }"
+		/>
+
 		<Button
 			v-if="areInputsFilled"
 			label="Edit"
@@ -14,10 +21,14 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import Button from 'primevue/button';
 import TeraOperatorPlaceholder from '@/components/operator/tera-operator-placeholder.vue';
+import TeraSimulateChart from '@/workflow/tera-simulate-chart.vue';
 import { WorkflowNode } from '@/types/workflow';
+import { RunResults } from '@/types/SimulateConfig';
+import { getRunResult } from '@/services/models/simulation-service';
+import { csvParse } from 'd3';
 import { SimulateJuliaOperationState } from './simulate-julia-operation';
 
 const props = defineProps<{
@@ -25,8 +36,28 @@ const props = defineProps<{
 }>();
 
 const emit = defineEmits(['open-drilldown']);
+const runResults = ref<RunResults>({});
+const hasData = ref(false);
 
 const areInputsFilled = computed(() => props.node.inputs[0].value);
+
+const selectedOutputId = ref<string>(props.node.active as string);
+const selectedRunId = computed(
+	() => props.node.outputs.find((o) => o.id === selectedOutputId.value)?.value?.[0]
+);
+
+watch(
+	() => selectedRunId.value,
+	async () => {
+		if (!selectedRunId.value) return;
+
+		const resultCsv = await getRunResult(selectedRunId.value, 'result.csv');
+		const csvData = csvParse(resultCsv);
+		runResults.value[selectedRunId.value] = csvData as any;
+		hasData.value = true;
+	},
+	{ immediate: true }
+);
 </script>
 
 <style scoped></style>
