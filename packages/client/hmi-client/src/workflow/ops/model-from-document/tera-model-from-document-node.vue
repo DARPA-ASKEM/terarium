4023e73c5093598efe2d9d135e544fc3b0e8e107
Yohann Paris <github@yohannparis.com>
2024-02-27 14:55:51 -0600

    Display model diagram/equations on node (#2793)
    Co-authored-by: Shawn Yama <syama@uncharted.software>

--- packages/client/hmi-client/src/workflow/ops/model-from-document/tera-model-from-document-node.vue
+++ packages/client/hmi-client/src/workflow/ops/model-from-document/tera-model-from-document-node.vue
@@ -1,24 +1,86 @@
 <template>
 	<main>
-		<tera-carousel>
-			<img v-for="(image, index) in images" :src="image" alt="alt" :key="index" />
-		</tera-carousel>
+		<template v-if="model">
+			<SelectButton
+				class="p-button-xsm"
+				:model-value="view"
+				:options="viewOptions"
+				@change="handleChange"
+			/>
+			<div class="container">
+				<tera-model-diagram
+					v-if="view === View.Diagram"
+					:model="model"
+					:is-editable="false"
+					is-preview
+				/>
+				<tera-model-equation
+					v-else-if="view === View.Equation"
+					:model="model"
+					:is-editable="false"
+				/>
+			</div>
+		</template>
+		<tera-operator-placeholder
+			v-else
+			:operation-type="WorkflowOperationTypes.MODEL_FROM_DOCUMENT"
+		/>
 		<Button @click="emit('open-drilldown')" label="Edit" severity="secondary" outlined />
 	</main>
 </template>
 
 <script setup lang="ts">
-import TeraCarousel from '@/components/widgets/tera-carousel.vue';
-import terarium from '@/assets/svg/terarium-icon.svg';
 import Button from 'primevue/button';
+import { WorkflowNode, WorkflowOperationTypes } from '@/types/workflow';
+import TeraOperatorPlaceholder from '@/components/operator/tera-operator-placeholder.vue';
+import TeraModelEquation from '@/components/model/petrinet/tera-model-equation.vue';
+import TeraModelDiagram from '@/components/model/petrinet/model-diagrams/tera-model-diagram.vue';
+import SelectButton from 'primevue/selectbutton';
+import { ref, onUpdated, onMounted } from 'vue';
+import { ModelOperationState } from '@/workflow/ops/model/model-operation';
+import { getModel } from '@/services/model';
+import { Model } from '@/types/Types';
+
+const props = defineProps<{
+	node: WorkflowNode<ModelOperationState>;
+}>();
 
 const emit = defineEmits(['open-drilldown']);
 
-const images = [
-	new URL('@/assets/svg/terarium-icon.svg', import.meta.url).href,
-	new URL('@/assets/svg/uncharted-logo-official.svg', import.meta.url).href,
-	new URL('@/assets/svg/terarium-logo.svg', import.meta.url).href,
-	terarium,
-	'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
-];
+enum View {
+	Diagram = 'Diagram',
+	Equation = 'Equation'
+}
+const view = ref(View.Diagram);
+const viewOptions = ref([View.Diagram, View.Equation]);
+const handleChange = (event) => {
+	if (event.value) {
+		view.value = event.value;
+	}
+};
+
+const model = ref(null as Model | null);
+
+// Update the model preview when the active output changes
+const activeOutputId = ref(props.node?.active ?? props.node?.outputs?.[0].id);
+const activeModelId = ref(
+	props.node?.outputs?.find((output) => output.id === activeOutputId.value)?.state?.modelId
+);
+const updateModel = async () => {
+	if (activeModelId.value) {
+		model.value = await getModel(activeModelId.value);
+	}
+};
+onMounted(updateModel);
+onUpdated(updateModel);
 </script>
+
+<style scoped>
+.p-selectbutton {
+	width: 100%;
+}
+
+.p-selectbutton:deep(.p-button) {
+	flex-grow: 1;
+}
+</style>
