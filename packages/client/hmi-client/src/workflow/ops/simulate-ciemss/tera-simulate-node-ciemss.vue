3276b439aa8e7cab32efe426d525d1995dbab098
Daniel Chang <mwdchang@gmail.com>
2024-03-07 13:38:17 -0500

    reorg ciemss simulate

--- packages/client/hmi-client/src/workflow/ops/simulate-ciemss/tera-simulate-node-ciemss.vue
+++ packages/client/hmi-client/src/workflow/ops/simulate-ciemss/tera-simulate-node-ciemss.vue
@@ -25,20 +25,23 @@
 </template>
 
 <script setup lang="ts">
+import _ from 'lodash';
 import { computed, ref, watch } from 'vue';
 import Button from 'primevue/button';
 import TeraOperatorPlaceholder from '@/components/operator/tera-operator-placeholder.vue';
 import TeraSimulateChart from '@/workflow/tera-simulate-chart.vue';
 import { WorkflowNode } from '@/types/workflow';
 import { RunResults } from '@/types/SimulateConfig';
-import { getRunResultCiemss } from '@/services/models/simulation-service';
-import { SimulateCiemssOperationState } from './simulate-ciemss-operation';
+import { getRunResultCiemss, pollAction } from '@/services/models/simulation-service';
+import { Poller, PollerState } from '@/api/api';
+import { logger } from '@/utils/logger';
+import { SimulateCiemssOperationState, SimulateCiemssOperation } from './simulate-ciemss-operation';
 
 const props = defineProps<{
 	node: WorkflowNode<SimulateCiemssOperationState>;
 }>();
 
-const emit = defineEmits(['open-drilldown']);
+const emit = defineEmits(['open-drilldown', 'update-state', 'append-output']);
 const runResults = ref<{ [runId: string]: RunResults }>({});
 
 const selectedOutputId = ref<string>(props.node.active as string);
@@ -49,6 +52,71 @@ const selectedRunId = computed(
 const areInputsFilled = computed(() => props.node.inputs[0].value);
 
 const hasData = ref(false);
+
+const poller = new Poller();
+const pollResult = async (runId: string) => {
+	poller
+		.setInterval(3000)
+		.setThreshold(300)
+		.setPollAction(async () => pollAction(runId));
+	const pollerResults = await poller.start();
+
+	if (pollerResults.state === PollerState.Cancelled) {
+		return pollerResults;
+	}
+	if (pollerResults.state !== PollerState.Done || !pollerResults.data) {
+		// throw if there are any failed runs for now
+		logger.error(`Simulation: ${runId} has failed`, {
+			toastTitle: 'Error - Pyciemss'
+		});
+		throw Error('Failed Runs');
+	}
+	return pollerResults;
+};
+
+const addChart = () => {
+	const state = _.cloneDeep(props.node.state);
+	state.chartConfigs.push([]);
+
+	emit('update-state', state);
+};
+
+const processResult = (runId: string) => {
+	const state = _.cloneDeep(props.node.state);
+	if (state.chartConfigs.length === 0) {
+		addChart();
+	}
+
+	emit('append-output', {
+		type: SimulateCiemssOperation.outputs[0].type,
+		label: `Output - ${props.node.outputs.length + 1}`,
+		value: runId,
+		state: {
+			currentTimespan: state.currentTimespan,
+			numSamples: state.numSamples,
+			method: state.method,
+			inProgressSimulationId: state.inProgressSimulationId
+		},
+		isSelected: false
+	});
+};
+
+watch(
+	() => props.node.state.inProgressSimulationId,
+	async (id) => {
+		if (!id || id === '') return;
+
+		const response = await pollResult(id);
+		if (response.state === PollerState.Done) {
+			processResult(id);
+		}
+
+		const state = _.cloneDeep(props.node.state);
+		state.inProgressSimulationId = '';
+		emit('update-state', state);
+	},
+	{ immediate: true }
+);
 
 watch(
 	() => selectedRunId.value,
