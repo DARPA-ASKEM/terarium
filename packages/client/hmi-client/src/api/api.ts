a4d97dd84ec250050af70dbcbd0c46a46c136d4e
dvince <dvince@uncharted.software>
2024-03-08 11:37:25 -0500

    feat [hmi-client]: improve error logging from the back end to the client
    cleanup
    
    Issue #2947

--- packages/client/hmi-client/src/api/api.ts
+++ packages/client/hmi-client/src/api/api.ts
@@ -41,8 +41,8 @@ API.interceptors.response.use(
 			if (error?.response?.data) {
 				const responseError: ServerError = error.response.data as ServerError;
 
-				// check to see if the 'message' property is set. If it is, set message to that value.  
-				// If not, check the 'trace' property and extract the error from that. 
+				// check to see if the 'message' property is set. If it is, set message to that value.
+				// If not, check the 'trace' property and extract the error from that.
 				// It will be the substring between the first set of quotations marks.
 				if (responseError.message) {
 					message = responseError.message;
