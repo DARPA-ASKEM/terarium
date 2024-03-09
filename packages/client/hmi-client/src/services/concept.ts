b153dd4a0d50860ea8f9acedf6f974ab335ed9e5
Yohann Paris <github@yohannparis.com>
2024-03-09 12:47:30 -0500

    Simplify concept call

--- packages/client/hmi-client/src/services/concept.ts
+++ packages/client/hmi-client/src/services/concept.ts
@@ -55,14 +55,9 @@ async function getFacets(type: AssetType, curies?: ClauseValue[]): Promise<Conce
  * Get DKG entities, either single ones or multiple at a time
  */
 async function getCuriesEntities(curies: Array<string>): Promise<Array<DKG> | null> {
-	try {
-		const response = await API.get(`/mira/currie/${curies.toString()}`);
-		if (response?.status !== 200) return null;
-		return response?.data ?? null;
-	} catch (error) {
-		logger.error(error, { showToast: false });
-		return null;
-	}
+	const response = await API.get(`/mira/currie/${curies.toString()}`);
+	if (response?.status === 200 && response?.data) return response.data;
+	return null;
 }
 
 async function searchCuriesEntities(query: string): Promise<Array<DKG>> {
