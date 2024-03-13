ee7ded71cead3dcc19d84c2228d2619bc5d9c973
Charles Coleman <bigglesandginger@gmail.com>
2024-03-13 15:53:21 -0400

    store preview image in S3

---
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/climateData/ClimateDataResultPng.java
@@ -0,0 +1,8 @@
+package software.uncharted.terarium.hmiserver.models.climateData;
+
+import lombok.Data;
+
+@Data
+public class ClimateDataResultPng {
+    private String png;
+}
--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/climatedata/ClimateDataService.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/climatedata/ClimateDataService.java
@@ -7,11 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import software.uncharted.terarium.hmiserver.configuration.Config;
 import software.uncharted.terarium.hmiserver.models.climateData.ClimateDataPreviewTask;
 import software.uncharted.terarium.hmiserver.models.climateData.ClimateDataResponse;
+import software.uncharted.terarium.hmiserver.models.climateData.ClimateDataResultPng;
 import software.uncharted.terarium.hmiserver.proxies.climatedata.ClimateDataProxy;
 import software.uncharted.terarium.hmiserver.repository.climateData.ClimateDataPreviewTaskRepository;
+import software.uncharted.terarium.hmiserver.service.s3.S3ClientService;
 
+import java.util.Base64;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,9 +23,11 @@ import java.util.List;
 @Slf4j
 public class ClimateDataService {
 
-		final ObjectMapper objectMapper;
-		final ClimateDataProxy climateDataProxy;
-		final ClimateDataPreviewTaskRepository climateDataPreviewTaskRepository;
+    final ObjectMapper objectMapper;
+    final ClimateDataProxy climateDataProxy;
+    final ClimateDataPreviewTaskRepository climateDataPreviewTaskRepository;
+    final S3ClientService s3ClientService;
+    final Config config;
 
     @Scheduled(fixedRate = 1000 * 60 * 2L) // every 2 minutes
     public void checkJobStatusTask() {
@@ -32,6 +38,18 @@ public class ClimateDataService {
             final ClimateDataResponse climateDataResponse = objectMapper.convertValue(response.getBody(), ClimateDataResponse.class);
             if (climateDataResponse.getResult().getJobResult() != null) {
                 // TODO: store result
+                final ClimateDataResultPng png = objectMapper.convertValue(climateDataResponse.getResult().getJobResult(), ClimateDataResultPng.class);
+                int index = png.getPng().indexOf(',');
+                if (index > -1 && index + 1 < png.getPng().length()) {
+                    String pngBase64 = png.getPng().substring(index+1);
+                    byte[] pngBytes = Base64.getDecoder().decode(pngBase64);
+
+                    final String bucket = config.getFileStorageS3BucketName();
+                    final String key = String.join("/dataset", previewTask.getEsgfId());;
+
+                    s3ClientService.getS3Service().putObject(bucket, key, pngBytes);
+                }
+
                 climateDataPreviewTaskRepository.delete(previewTask);
             }
             if (climateDataResponse.getResult().getJobError() != null) {
