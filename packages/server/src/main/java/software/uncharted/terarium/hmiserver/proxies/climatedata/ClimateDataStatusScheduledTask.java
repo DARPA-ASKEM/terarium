139d79fd925a51f7dfd1b650d7fa7b19ca0f8b1e
Charles Coleman <bigglesandginger@gmail.com>
2024-03-13 08:12:59 -0400

    store ClimateData id in database for status checking

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/proxies/climatedata/ClimateDataStatusScheduledTask.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/proxies/climatedata/ClimateDataStatusScheduledTask.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import software.uncharted.terarium.hmiserver.models.climateData.ClimateDataTask;
+import software.uncharted.terarium.hmiserver.repository.climateData.ClimateDataTaskRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,21 +18,23 @@ import java.util.List;
 public class ClimateDataStatusScheduledTask {
 
     private final ClimateDataProxy climateDataProxy;
-
-    private final List<String> jobIds = new ArrayList<>();
+    private final ClimateDataTaskRepository climateDataTaskRepository;
 
     @Scheduled(fixedRate = 1000 * 60 * 2L) // every 2 minutes
     public void checkJobStatusTask() {
+        List<ClimateDataTask> climateDataTasks = climateDataTaskRepository.findAll();
         log.info("TODO: Get jobs from database");
 
-        for (String id : jobIds) {
-            final ResponseEntity<JsonNode> response = climateDataProxy.status(id);
-            log.info("TODO: if job complete write response to database as a data asset");
+        for (ClimateDataTask climateDataTask : climateDataTasks) {
+            final ResponseEntity<JsonNode> response = climateDataProxy.status(climateDataTask.getClimateDataId());
+            log.info("TODO: if job complete write response to database as a data asset, then delete task");
         }
     }
 
     public void checkJob(String id) {
         log.info("TODO: write id to database");
-        jobIds.add(id);
+        ClimateDataTask climateDataTask = new ClimateDataTask();
+        climateDataTask.setClimateDataId(id);
+        climateDataTaskRepository.save(climateDataTask);
     }
 }
