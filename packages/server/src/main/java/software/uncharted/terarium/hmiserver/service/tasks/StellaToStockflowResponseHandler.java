765e2d9c257a052880e02f50b7172f3e8e75b93e
David Gauldie <dgauldie@uncharted.software>
2024-03-14 15:07:01 -0400

    fixed response handler to set the TerariumAsset.name

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/StellaToStockflowResponseHandler.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/StellaToStockflowResponseHandler.java
@@ -1,12 +1,10 @@
 package software.uncharted.terarium.hmiserver.service.tasks;
 
-import org.springframework.stereotype.Component;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import software.uncharted.terarium.hmiserver.controller.mira.MiraController.ConversionAdditionalProperties;
 import software.uncharted.terarium.hmiserver.models.dataservice.model.Model;
 import software.uncharted.terarium.hmiserver.models.task.TaskResponse;
@@ -39,6 +37,7 @@ public class StellaToStockflowResponseHandler extends TaskResponseHandler {
 			final ConversionAdditionalProperties props = resp
 					.getAdditionalProperties(ConversionAdditionalProperties.class);
 			// override the default stockflow name / description
+			model.setName(props.getFileName());
 			model.getHeader().setName(props.getFileName());
 			model.getHeader().setDescription(props.getFileName());
 			model = modelService.createAsset(model);
