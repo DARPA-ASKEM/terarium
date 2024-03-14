831f1d576f99aef4c065cb70be399c8f1dc479da
Yohann Paris <github@yohannparis.com>
2024-03-14 16:55:00 -0400

    Update MiraController.java

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/mira/MiraController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/mira/MiraController.java
@@ -116,7 +116,7 @@ public class MiraController {
 			return ResponseEntity.ok().body(mmtInfo);
 		} catch (final Exception e) {
 			final String error = "Unable to dispatch task request";
-			log.error("Unable to dispatch task request {}: {}", error, e.getMessage());
+			log.error("Unable to convert Model to MIRA model template. \n{}: {}", error, e.getMessage());
 			throw new ResponseStatusException(
 				org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR,
 				"Unable to convert Model to MIRA model template.");
