62b8b142b36b6c7120fa1aa55d8a50c6ecd56d52
Shawn Yama <syama@uncharted.software>
2024-03-15 15:44:29 -0400

    drilldown ui

--- packages/client/hmi-client/src/workflow/ops/subset-data/tera-subset-data.vue
+++ packages/client/hmi-client/src/workflow/ops/subset-data/tera-subset-data.vue
@@ -2,14 +2,57 @@
 	<tera-drilldown :title="node.displayName" @on-close-clicked="emit('close')">
 		<div :tabName="SubsetDataTabs.Wizard">
 			<tera-drilldown-section>
-				<InputText placeholder="What do you want to do?" />
+				<!-- <InputText placeholder="What do you want to do?" /> -->
 				<h3>Select geo-boundaries</h3>
-				<SelectButton :model-value="selectedBoundaryType" :options="boundaryOptions"></SelectButton>
+				<span>
+					<label>Latitude</label>
+					<InputText v-model="latitudeStart" placeholder="Start" />
+					<InputText v-model="latitudeEnd" placeholder="End" />
+				</span>
+				<span>
+					<label>Longitude</label>
+					<InputText v-model="latitudeStart" placeholder="Start" />
+					<InputText v-model="latitudeEnd" placeholder="End" />
+				</span>
+				<code>
+					selectSpatialDomain(['{{ latitudeStart }}', '{{ latitudeEnd }}', '{{ longitudeStart }}',
+					'{{ longitudeEnd }}'])
+				</code>
+				<span>
+					<Checkbox v-model="isSpatialSkipping" binary />
+					<label>Spatial skipping</label>
+				</span>
+				<div class="number-entry">
+					<label class="float-label">Keep every nth datapoint</label>
+					<InputNumber v-model="spatialSkipping" :disabled="!isSpatialSkipping" />
+				</div>
+				<h3>Select temporal slice</h3>
+				<span>
+					<div>
+						<label class="float-label">From</label>
+						<Calender v-model="fromDate" showIcon />
+					</div>
+					<div>
+						<label class="float-label">To</label>
+						<Calender v-model="toDate" showIcon />
+					</div>
+				</span>
+				<span>
+					<Checkbox v-model="isTimeSkipping" binary />
+					<label>Time skipping</label>
+				</span>
+				<div class="number-entry">
+					<label class="float-label">Keep every nth time slice</label>
+					<InputNumber v-model="timeSkipping" :disabled="!isTimeSkipping" />
+				</div>
+				<template #footer>
+					<Button label="Run" icon="pi pi-play" outlined severity="secondary" />
+				</template>
 			</tera-drilldown-section>
 		</div>
 		<div :tabName="SubsetDataTabs.Notebook"></div>
 		<template #preview>
-			<tera-drilldown-preview>sdsd</tera-drilldown-preview>
+			<tera-drilldown-preview>sdsd<template #footer> </template></tera-drilldown-preview>
 		</template>
 	</tera-drilldown>
 </template>
@@ -21,7 +64,10 @@ import TeraDrilldownPreview from '@/components/drilldown/tera-drilldown-preview.
 import TeraDrilldownSection from '@/components/drilldown/tera-drilldown-section.vue';
 import { WorkflowNode } from '@/types/workflow';
 import InputText from 'primevue/inputtext';
-import SelectButton from 'primevue/selectbutton';
+import InputNumber from 'primevue/inputnumber';
+import Button from 'primevue/button';
+import Calender from 'primevue/calendar';
+import Checkbox from 'primevue/checkbox';
 import { SubsetDataOperationState } from './subset-data-operation';
 
 // const props =
@@ -36,11 +82,48 @@ enum SubsetDataTabs {
 	Notebook = 'Notebook'
 }
 
-enum Boundaries {
-	Area = 'Area',
-	SinglePoint = 'Single point'
+const latitudeStart = ref('');
+const latitudeEnd = ref('');
+const longitudeStart = ref('');
+const longitudeEnd = ref('');
+
+const isSpatialSkipping = ref(true);
+const spatialSkipping = ref(0);
+
+const isTimeSkipping = ref(true);
+const timeSkipping = ref(0);
+
+const fromDate = ref('');
+const toDate = ref('');
+</script>
+
+<style scoped>
+span {
+	display: flex;
+	align-items: center;
+	gap: var(--gap);
+
+	& > label {
+		width: 8rem;
+	}
 }
 
-const selectedBoundaryType = ref(Boundaries.Area);
-const boundaryOptions = ref([Boundaries.Area, Boundaries.SinglePoint]);
-</script>
+:deep(main) {
+	gap: var(--gap);
+}
+
+code {
+	background-color: var(--surface-ground);
+	border-radius: var(--border-radius);
+	border: 1px solid var(--surface-border);
+	padding: var(--gap-small);
+}
+
+.float-label {
+	font-size: var(--font-caption);
+}
+
+.number-entry {
+	width: 14rem;
+}
+</style>
