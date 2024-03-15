d5dfb62bedcc444765f9b8ac39b0bdd8367074f5
Shawn Yama <syama@uncharted.software>
2024-03-15 13:07:28 -0400

    rm log

--- packages/client/hmi-client/src/workflow/ops/model-comparison/tera-model-comparison.vue
+++ packages/client/hmi-client/src/workflow/ops/model-comparison/tera-model-comparison.vue
@@ -320,7 +320,6 @@ async function buildJupyterContext() {
 }
 
 onMounted(async () => {
-	console.log(props.node.state.comparisonImageIds);
 	if (!isEmpty(props.node.state.comparisonImageIds)) {
 		isLoadingStructuralComparisons.value = true;
 		structuralComparisons.value = await getImages(props.node.state.comparisonImageIds);
