83d9d64dee1edcb6a3b8ad9ab3ec53aac2d6edb0
Jaehwan Ryu <jryu@uncharted.software>
2024-03-18 16:45:51 -0400

    Moved default project overview context text to the backend

--- packages/client/hmi-client/src/components/home/tera-project-overview-editor.vue
+++ packages/client/hmi-client/src/components/home/tera-project-overview-editor.vue
@@ -15,24 +15,10 @@ import { useProjects } from '@/composables/project';
 import { Project } from '@/types/Types';
 
 const AUTO_SAVE_DELAY = 3000;
-const DEFAULT_EDITOR_CONTENT = `
-	<div>
-			<h2>Hey there! </h2>
-			<p>This is your project overview page. Use this space however you like. Not sure where to start? Here are some things you can try:</p>
-	<br>
-			<ul>
-					<li><strong>Upload stuff:</strong> Upload documents, models, code or datasets with the green button in the bottom left corner.</li>
-					<li><strong>Explore and add:</strong> Use the project selector in the top nav to switch to the Explorer where you can find documents, models and datasets that you can add to your project.</li>
-					<li><strong>Build a model:</strong> Create a model that fits just what you need.</li>
-					<li><strong>Create a workflow:</strong> Connect resources with operators so you can focus on the science and not the plumbing.</li>
-			</ul>
-	<br>
-			<p>Feel free to erase this text and make it your own.</p>
-	</div>
-`;
+
 const props = defineProps<{ project: Project | null }>();
 const editorContent = ref('');
-const lastSavedContent = computed(() => props.project?.overviewContent ?? DEFAULT_EDITOR_CONTENT);
+const lastSavedContent = computed(() => props.project?.overviewContent ?? '');
 const hasEditPermission = computed(() =>
 	['creator', 'writer'].includes(props.project?.userPermission ?? '')
 );
