6f714997454ba833544f533b033b579e1a5c530e
Charles Coleman <bigglesandginger@gmail.com>
2024-03-20 10:17:58 -0400

    init

---
+++ packages/client/hmi-client/src/workflow/ops/regridding/regridding-operation.ts
@@ -0,0 +1,23 @@
+import { Operation, WorkflowOperationTypes } from '@/types/workflow';
+
+export interface DatasetTransformerState {
+	datasetId: string | null;
+	notebookSessionId?: string;
+}
+
+export const RegriddingOperation: Operation = {
+	name: WorkflowOperationTypes.REGRIDDING,
+	description: 'Select a dataset',
+	displayName: 'Transform dataset',
+	isRunnable: true,
+	inputs: [{ type: 'datasetId|simulationId', label: 'Dataset or Simulation' }],
+	outputs: [],
+	action: () => {},
+
+	initState: () => {
+		const init: DatasetTransformerState = {
+			datasetId: null
+		};
+		return init;
+	}
+};
