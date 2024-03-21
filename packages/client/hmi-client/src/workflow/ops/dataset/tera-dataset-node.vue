56a1b904d5700d5699548a02448d400c8339ab24
Yohann Paris <github@yohannparis.com>
2024-03-21 16:14:29 -0400

    Make sure it is not blocking adding an output.

--- packages/client/hmi-client/src/workflow/ops/dataset/tera-dataset-node.vue
+++ packages/client/hmi-client/src/workflow/ops/dataset/tera-dataset-node.vue
@@ -95,22 +95,10 @@ const columnSelectTooltip = 'Select columns to display';
 
 async function getDatasetById(id: string) {
 	dataset.value = await getDataset(id);
-	if (!dataset.value) return;
-
-	if (dataset.value?.id && dataset.value?.fileNames) {
-		const filenames = dataset.value.fileNames;
-		if (filenames.length > 0 && filenames[0].endsWith('.csv')) {
-			downloadRawFile(id, filename).then((res) => {
-				rawContent.value = res;
-				selectedColumns = ref(csvHeaders?.value);
-			});
-		} else {
-			console.debug('No CSV file found in dataset: ', dataset.value.name);
-		}
-	}
+	if (!dataset.value || dataset.value?.id) return;
 
 	// Once a dataset is selected the output is assigned here, if there is already an output do not reassign
-	if (dataset?.value?.id && isEmpty(props.node.outputs)) {
+	if (isEmpty(props.node.outputs)) {
 		emit('update-state', {
 			datasetId: dataset.value.id
 		});
@@ -120,6 +108,19 @@ async function getDatasetById(id: string) {
 			label: dataset.value.name,
 			value: [dataset.value.id]
 		});
+	}
+
+	// Fetch the CSV file from the dataset for preview purposes
+	if (dataset.value?.fileNames) {
+		const filenames = dataset.value.fileNames;
+		if (filenames.length > 0 && filenames[0].endsWith('.csv')) {
+			downloadRawFile(id, filename).then((res) => {
+				rawContent.value = res;
+				selectedColumns = ref(csvHeaders?.value);
+			});
+		} else {
+			console.debug('No CSV file found in dataset: ', dataset.value.name);
+		}
 	}
 }
 
