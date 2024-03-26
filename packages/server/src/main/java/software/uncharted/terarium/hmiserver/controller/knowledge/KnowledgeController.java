ef0b7462f64589f38b725a03536429522c008b33
dvince <dvince@uncharted.software>
2024-03-26 15:12:16 -0400

    fix[hmi-server]: Better error handling
    Updating error handling

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/knowledge/KnowledgeController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/knowledge/KnowledgeController.java
@@ -13,8 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpEntity;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -107,6 +105,8 @@ public class KnowledgeController {
 		} catch (final FeignException e) {
 			final String error = "Skema Unified Service did not return any AMR based on the provided Equations";
 			log.error(error, e);
+			if(e.status()<100)
+				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, error + ": " + e.getMessage());
 			throw new ResponseStatusException(HttpStatus.valueOf(e.status()), error + ": " + e.getMessage());
 		} catch (final Exception e) {
 			final String error = "Unable to reach Skema Unified Service";
@@ -166,19 +166,35 @@ public class KnowledgeController {
 	@PostMapping("/base64-equations-to-model")
 	@Secured(Roles.USER)
 	public ResponseEntity<Model> base64EquationsToAMR(@RequestBody final JsonNode req) {
+		try{
 		return ResponseEntity
 				.ok(skemaUnifiedProxy
 						.base64EquationsToAMR(req)
 						.getBody());
+		} catch (final FeignException e) {
+			final String error = "Error with Skema Unified Service while converting base64 equations to AMR";
+			log.error(error, e);
+			if(e.status()<100)
+				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, error + ": " + e.getMessage());
+			throw new ResponseStatusException(HttpStatus.valueOf(e.status()), error + ": " + e.getMessage());
+		}
 	}
 
 	@PostMapping("/base64-equations-to-latex")
 	@Secured(Roles.USER)
 	public ResponseEntity<String> base64EquationsToLatex(@RequestBody final JsonNode req) {
-		return ResponseEntity
+		try {
+			return ResponseEntity
 				.ok(skemaUnifiedProxy
-						.base64EquationsToLatex(req)
-						.getBody());
+					.base64EquationsToLatex(req)
+					.getBody());
+		} catch (final FeignException e) {
+			final String error = "Error with Skema Unified Service while converting base64 equations to Latex";
+			log.error(error, e);
+			if(e.status()<100)
+				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, error + ": " + e.getMessage());
+			throw new ResponseStatusException(HttpStatus.valueOf(e.status()), error + ": " + e.getMessage());
+		}
 	}
 
 	/**
@@ -271,7 +287,7 @@ public class KnowledgeController {
 				final String error = "SKEMA was unable to create a model with the code provided";
 				log.error(error, e);
 				throw new ResponseStatusException(
-						org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY,
+						e.status()< 100? org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY : HttpStatus.valueOf(e.status()),
 						error + ": " + e.getMessage());
 			} catch (final Exception e) {
 				log.error("Unable to get code to amr", e);
@@ -335,6 +351,7 @@ public class KnowledgeController {
 	@Operation(summary = "Create a model from code blocks")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Return the extraction job for code to amr", content = @Content(mediaType = "application/json", schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ExtractionResponse.class))),
+			@ApiResponse(responseCode = "400", description = "Invalid input - code file may be missing name", content = @Content),
 			@ApiResponse(responseCode = "500", description = "Error running code blocks to model", content = @Content)
 	})
 	@PostMapping(value = "/code-blocks-to-model", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -342,8 +359,7 @@ public class KnowledgeController {
 	public ResponseEntity<Model> codeBlocksToModel(@RequestPart final Code code,
 			@RequestPart("file") final MultipartFile input) throws IOException {
 
-		try (final CloseableHttpClient httpClient = HttpClients.custom()
-				.build()) {
+		try {
 			// 1. create code asset from code blocks
 			final Code createdCode = codeService.createAsset(code);
 
@@ -351,6 +367,12 @@ public class KnowledgeController {
 			final byte[] fileAsBytes = input.getBytes();
 			final HttpEntity fileEntity = new ByteArrayEntity(fileAsBytes, ContentType.APPLICATION_OCTET_STREAM);
 			final String filename = input.getOriginalFilename();
+
+			if(filename == null) {
+				throw new ResponseStatusException(
+						HttpStatus.BAD_REQUEST,
+						"File name is required");
+			}
 
 			codeService.uploadFile(code.getId(), filename, fileEntity, ContentType.TEXT_PLAIN);
 
@@ -413,7 +435,17 @@ public class KnowledgeController {
 			final StringMultipartFile codeFile = new StringMultipartFile("", "code.txt",
 					"application/text");
 
-			final ResponseEntity<JsonNode> resp = mitProxy.modelCard(MIT_OPENAI_API_KEY, textFile, codeFile);
+			final ResponseEntity<JsonNode> resp;
+			try {
+				resp = mitProxy.modelCard(MIT_OPENAI_API_KEY, textFile, codeFile);
+			} catch (final FeignException e) {
+				final String error = "Unable to get model card";
+				log.error(error, e);
+				throw new ResponseStatusException(
+						e.status()<100? HttpStatus.INTERNAL_SERVER_ERROR : HttpStatus.valueOf(e.status()),
+						error + ": " + e.getMessage());
+			}
+
 			if (!resp.getStatusCode().is2xxSuccessful()) {
 				throw new ResponseStatusException(
 						resp.getStatusCode(),
@@ -552,6 +584,12 @@ public class KnowledgeController {
 
 			return ResponseEntity.ok(datasetService.updateAsset(dataset).orElseThrow());
 
+		} catch (final FeignException e) {
+			final String error = "Unable to get profile dataset";
+			log.error(error, e);
+			throw new ResponseStatusException(
+					e.status()<100? org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR : HttpStatus.valueOf(e.status()),
+					error + ": " + e.getMessage());
 		} catch (final Exception e) {
 			final String error = "Unable to get profile dataset";
 			log.error(error, e);
@@ -582,7 +620,16 @@ public class KnowledgeController {
 					extractionsString, "extractions.json",
 					"application/json");
 
-			final ResponseEntity<JsonNode> res = skemaUnifiedProxy.linkAMRFile(amrFile, extractionFile);
+			final ResponseEntity<JsonNode> res;
+			try{
+				res = skemaUnifiedProxy.linkAMRFile(amrFile, extractionFile);
+			} catch (final FeignException e) {
+				final String error = "Unable to link AMR file";
+				log.error(error, e);
+				throw new ResponseStatusException(
+						e.status()<100? HttpStatus.INTERNAL_SERVER_ERROR : HttpStatus.valueOf(e.status()),
+						error + ": " + e.getMessage());
+			}
 			if (!res.getStatusCode().is2xxSuccessful()) {
 				throw new ResponseStatusException(
 						res.getStatusCode(),
@@ -638,7 +685,7 @@ public class KnowledgeController {
 	 * Document Extractions
 	 *
 	 * @param documentId (String): The ID of the document to profile
-	 * @return
+	 * @return the profiled document
 	 */
 	@PostMapping("/pdf-extractions")
 	@Secured(Roles.USER)
