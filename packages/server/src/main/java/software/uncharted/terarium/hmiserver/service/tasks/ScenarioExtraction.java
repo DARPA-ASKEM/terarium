01d23f6cd0f3e9dfe548a193a03efe5eef34518d
Yohann Paris <github@yohannparis.com>
2024-03-28 14:40:59 -0400

    Yohannparis/display gollm extractions (#3210)

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/ScenarioExtraction.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/ScenarioExtraction.java
@@ -7,20 +7,23 @@ import software.uncharted.terarium.hmiserver.models.dataservice.modelparts.seman
 import software.uncharted.terarium.hmiserver.utils.GreekDictionary;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class ScenarioExtraction {
 	public static List<ModelParameter> getModelParameters(JsonNode condition, Model modelCopy) {
 		final List<ModelParameter> modelParameters = modelCopy.getParameters();
 		modelParameters.forEach((parameter) -> {
 			final String parameterId = parameter.getId();
-			final JsonNode conditionParameters = condition.get("parameters");
-			conditionParameters.forEach((conditionParameter) -> {
-				// Get the parameter value from the condition
-				final String id = conditionParameter.get("id").asText();
+			condition.get("values").forEach((conditionParameter) -> {
+				if (conditionParameter.get("type").asText().equals("parameter")) {
+					// Get the parameter value from the condition
+					final String id = conditionParameter.get("id").asText();
 
-				// Test against the id of the parameter in greek alphabet or english
-				if (parameterId.equals(id) || parameterId.equals(GreekDictionary.englishToGreek(id))) {
-					parameter.setValue(conditionParameter.get("value").doubleValue());
+					// Test against the id of the parameter in greek alphabet or english
+					if (parameterId.equals(id) || parameterId.equals(GreekDictionary.englishToGreek(id))) {
+						parameter.setValue(conditionParameter.get("value").doubleValue());
+					}
 				}
 			});
 		});
@@ -31,14 +34,15 @@ public class ScenarioExtraction {
 		final List<Initial> modelInitials = modelCopy.getInitials();
 		modelInitials.forEach((initial) -> {
 			final String target = initial.getTarget();
-			final JsonNode conditionInitials = condition.get("initials");
-			conditionInitials.forEach((conditionInitial) -> {
-				// Get the initial value from the condition
-				final String id = conditionInitial.get("id").asText();
+			condition.get("values").forEach((conditionInitial) -> {
+				if (conditionInitial.get("type").asText().equals("initial")) {
+					// Get the initial value from the condition
+					final String id = conditionInitial.get("id").asText();
 
-				// Test against the id of the initial in greek alphabet or english
-				if (target.equals(id) || target.equals(GreekDictionary.englishToGreek(id))) {
-					initial.setExpression(String.valueOf(conditionInitial.get("value").doubleValue()));
+					// Test against the id of the initial in greek alphabet or english
+					if (target.equals(id) || target.equals(GreekDictionary.englishToGreek(id))) {
+						initial.setExpression(String.valueOf(conditionInitial.get("value").doubleValue()));
+					}
 				}
 			});
 		});
