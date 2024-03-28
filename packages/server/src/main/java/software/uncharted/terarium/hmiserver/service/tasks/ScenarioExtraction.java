06162adf4b59479f828e55bef3a82ec38c2220c5
Yohann Paris <github@yohannparis.com>
2024-03-28 14:35:12 -0400

    Update ScenarioExtraction.java

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/ScenarioExtraction.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/ScenarioExtraction.java
@@ -15,16 +15,15 @@ public class ScenarioExtraction {
 		final List<ModelParameter> modelParameters = modelCopy.getParameters();
 		modelParameters.forEach((parameter) -> {
 			final String parameterId = parameter.getId();
-			final List<JsonNode> conditionParameters = StreamSupport.stream(condition.spliterator(), false)
-				.filter(node -> node.get("type").asText().equals("parameter"))
-				.toList();
-			condition.forEach((conditionParameter) -> {
-				// Get the parameter value from the condition
-				final String id = conditionParameter.get("id").asText();
+			condition.get("values").forEach((conditionParameter) -> {
+				if (conditionParameter.get("type").asText().equals("parameter")) {
+					// Get the parameter value from the condition
+					final String id = conditionParameter.get("id").asText();
 
-				// Test against the id of the parameter in greek alphabet or english
-				if (parameterId.equals(id) || parameterId.equals(GreekDictionary.englishToGreek(id))) {
-					parameter.setValue(conditionParameter.get("value").doubleValue());
+					// Test against the id of the parameter in greek alphabet or english
+					if (parameterId.equals(id) || parameterId.equals(GreekDictionary.englishToGreek(id))) {
+						parameter.setValue(conditionParameter.get("value").doubleValue());
+					}
 				}
 			});
 		});
@@ -35,16 +34,15 @@ public class ScenarioExtraction {
 		final List<Initial> modelInitials = modelCopy.getInitials();
 		modelInitials.forEach((initial) -> {
 			final String target = initial.getTarget();
-			final List<JsonNode> conditionInitials = StreamSupport.stream(condition.spliterator(), false)
-				.filter(node -> node.get("type").asText().equals("initial"))
-				.toList();
-			conditionInitials.forEach((conditionInitial) -> {
-				// Get the initial value from the condition
-				final String id = conditionInitial.get("id").asText();
+			condition.get("values").forEach((conditionInitial) -> {
+				if (conditionInitial.get("type").asText().equals("initial")) {
+					// Get the initial value from the condition
+					final String id = conditionInitial.get("id").asText();
 
-				// Test against the id of the initial in greek alphabet or english
-				if (target.equals(id) || target.equals(GreekDictionary.englishToGreek(id))) {
-					initial.setExpression(String.valueOf(conditionInitial.get("value").doubleValue()));
+					// Test against the id of the initial in greek alphabet or english
+					if (target.equals(id) || target.equals(GreekDictionary.englishToGreek(id))) {
+						initial.setExpression(String.valueOf(conditionInitial.get("value").doubleValue()));
+					}
 				}
 			});
 		});
