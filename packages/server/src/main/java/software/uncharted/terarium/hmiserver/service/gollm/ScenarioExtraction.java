062d9744301c239ed1ff08e40fbfe999f267e1f3
David Gauldie <dgauldie@uncharted.software>
2024-03-29 10:56:19 -0400

    add null check

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/gollm/ScenarioExtraction.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/gollm/ScenarioExtraction.java
@@ -29,7 +29,7 @@ import java.util.List;
  */
 public class ScenarioExtraction {
 	// Replace initial values in the model with the values from the condition
-	public static void replaceInitial(Initial initial, JsonNode conditionInitial) {
+	public static void replaceInitial(final Initial initial, final JsonNode conditionInitial) {
 		final String id = conditionInitial.get("id").asText();
 		final String target = initial.getTarget();
 		if (target.equals(id) || target.equals(GreekDictionary.englishToGreek(id))) {
@@ -39,15 +39,17 @@ public class ScenarioExtraction {
 	}
 
 	// Replace parameter values in the model with the values from the condition
-	public static void replaceParameter(ModelParameter parameter, JsonNode conditionParameter) {
+	public static void replaceParameter(final ModelParameter parameter, final JsonNode conditionParameter) {
 		final String id = conditionParameter.get("id").asText();
 		if (parameter.getId().equals(id) || parameter.getId().equals(GreekDictionary.englishToGreek(id))) {
-			final double value = conditionParameter.get("value").doubleValue();
-			parameter.setValue(value);
+			if (conditionParameter.has("value")) {
+				final double value = conditionParameter.get("value").doubleValue();
+				parameter.setValue(value);
+			}
 		}
 	}
 
-	public static List<ModelParameter> getModelParameters(JsonNode condition, Model modelCopy) {
+	public static List<ModelParameter> getModelParameters(final JsonNode condition, final Model modelCopy) {
 		final List<ModelParameter> modelParameters = modelCopy.getParameters();
 		modelParameters.forEach((parameter) -> {
 			condition.forEach((conditionParameter) -> {
@@ -61,7 +63,7 @@ public class ScenarioExtraction {
 		return modelParameters;
 	}
 
-	public static List<Initial> getModelInitials(JsonNode condition, Model modelCopy) {
+	public static List<Initial> getModelInitials(final JsonNode condition, final Model modelCopy) {
 		final List<Initial> modelInitials = modelCopy.getInitials();
 		modelInitials.forEach((initial) -> {
 			final String target = initial.getTarget();
