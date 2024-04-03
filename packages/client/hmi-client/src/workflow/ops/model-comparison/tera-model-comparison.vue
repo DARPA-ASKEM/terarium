e7345d96983f5ddfc038fdc9f9c0c669e8f340eb
Shawn Yama <syama@uncharted.software>
2024-04-03 16:10:03 -0400

    refactor: operator annotation in drilldown component instead of within every operator (#3245)

--- packages/client/hmi-client/src/workflow/ops/model-comparison/tera-model-comparison.vue
+++ packages/client/hmi-client/src/workflow/ops/model-comparison/tera-model-comparison.vue
@@ -1,11 +1,9 @@
 <template>
-	<tera-drilldown :title="node.displayName" @on-close-clicked="emit('close')">
-		<template #header-actions>
-			<tera-operator-annotation
-				:state="node.state"
-				@update-state="(state: any) => emit('update-state', state)"
-			/>
-		</template>
+	<tera-drilldown
+		:node="node"
+		@on-close-clicked="emit('close')"
+		@update-state="(state: any) => emit('update-state', state)"
+	>
 		<div :tabName="Tabs.Wizard">
 			<tera-drilldown-section class="ml-4 mr-4 pt-3">
 				<!-- LLM generated overview -->
@@ -156,7 +154,7 @@ import Button from 'primevue/button';
 import { computed, onMounted, onUnmounted, ref } from 'vue';
 import { VAceEditor } from 'vue3-ace-editor';
 import { VAceEditorInstance } from 'vue3-ace-editor/types';
-import TeraOperatorAnnotation from '@/components/operator/tera-operator-annotation.vue';
+
 import TeraNotebookJupyterInput from '@/components/llm/tera-notebook-jupyter-input.vue';
 import Image from 'primevue/image';
 import TeraProgressSpinner from '@/components/widgets/tera-progress-spinner.vue';
--- packages/client/hmi-client/src/workflow/ops/subset-data/tera-subset-data.vue
+++ packages/client/hmi-client/src/workflow/ops/subset-data/tera-subset-data.vue
@@ -1,5 +1,9 @@
 <template>
-	<tera-drilldown :title="node.displayName" @on-close-clicked="emit('close')">
+	<tera-drilldown
+		:node="node"
+		@on-close-clicked="emit('close')"
+		@update-state="(state: any) => emit('update-state', state)"
+	>
 		<div :tabName="SubsetDataTabs.Wizard" class="ml-4 mr-2 pt-3">
 			<tera-drilldown-section>
 				<!-- Geo boundaries -->
