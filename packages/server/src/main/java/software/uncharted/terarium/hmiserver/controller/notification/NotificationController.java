a79e40993b1068fff271d06531e961f3c80fddb4
kbirk <birk.kevin@gmail.com>
2024-04-15 14:05:48 -0400

    Remove client event stuff from notifications

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/notification/NotificationController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/notification/NotificationController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -20,10 +19,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import software.uncharted.terarium.hmiserver.annotations.IgnoreRequestLogging;
 import software.uncharted.terarium.hmiserver.models.notification.NotificationGroup;
 import software.uncharted.terarium.hmiserver.security.Roles;
-import software.uncharted.terarium.hmiserver.service.ClientEventService;
 import software.uncharted.terarium.hmiserver.service.CurrentUserService;
 import software.uncharted.terarium.hmiserver.service.notification.NotificationService;
 
@@ -33,7 +30,6 @@ import software.uncharted.terarium.hmiserver.service.notification.NotificationSe
 @RequiredArgsConstructor
 public class NotificationController {
 
-	private final ClientEventService clientEventService;
 	private final CurrentUserService currentUserService;
 	private final NotificationService notificationService;
 
@@ -53,12 +49,6 @@ public class NotificationController {
 		final String userId = currentUserService.get().getId().toString();
 
 		return ResponseEntity.ok(notificationService.getNotificationGroupsCreatedSince(userId, since));
-	}
-
-	@GetMapping
-	@IgnoreRequestLogging
-	public SseEmitter subscribe() {
-		return clientEventService.connect(currentUserService.get());
 	}
 
 }
