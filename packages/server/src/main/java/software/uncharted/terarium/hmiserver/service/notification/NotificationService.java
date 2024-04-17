f3c6149dbda0fc0eea55e89982bf02bbbb77d2b3
kbirk <kbirk@users.noreply.github.com>
2024-04-17 19:41:26 +0000

    chore: lint server codebase

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/notification/NotificationService.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/notification/NotificationService.java
@@ -5,11 +5,9 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
-import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import software.uncharted.terarium.hmiserver.models.User;
 import software.uncharted.terarium.hmiserver.models.notification.NotificationEvent;
 import software.uncharted.terarium.hmiserver.models.notification.NotificationGroup;
@@ -51,7 +49,8 @@ public class NotificationService {
 
 	public NotificationEvent createNotificationEvent(final UUID groupId, final NotificationEvent notificationEvent) {
 
-		final NotificationGroup group = notificationGroupRepository.findById(groupId).orElseThrow();
+		final NotificationGroup group =
+				notificationGroupRepository.findById(groupId).orElseThrow();
 
 		notificationEvent.setNotificationGroup(group);
 
