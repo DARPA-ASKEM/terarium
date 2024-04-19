6b3d5a98592046938f71012d66fe0f71b321dc68
kbirk <birk.kevin@gmail.com>
2024-04-19 10:35:11 -0400

    Add some more logging and bandaid solution

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/AMRToMMTResponseHandler.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/AMRToMMTResponseHandler.java
@@ -1,11 +1,9 @@
 package software.uncharted.terarium.hmiserver.service.tasks;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
+import org.springframework.stereotype.Component;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import software.uncharted.terarium.hmiserver.models.task.TaskResponse;
 
 @Component
 @RequiredArgsConstructor
@@ -13,27 +11,8 @@ import software.uncharted.terarium.hmiserver.models.task.TaskResponse;
 public class AMRToMMTResponseHandler extends TaskResponseHandler {
 	public static final String NAME = "mira_task:amr_to_mmt";
 
-	private final ObjectMapper objectMapper;
-
 	@Override
 	public String getName() {
 		return NAME;
-	}
-
-	@Data
-	public static class Response {
-		String response;
-	}
-
-	@Override
-	public TaskResponse onSuccess(final TaskResponse resp) {
-		try {
-			final Response modelResp = objectMapper.readValue(resp.getOutput(), Response.class);
-			resp.setOutput(modelResp.getResponse().getBytes());
-		} catch (final Exception e) {
-			log.error("Failed to create model", e);
-			throw new RuntimeException(e);
-		}
-		return resp;
 	}
 }
--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/TaskService.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/TaskService.java
@@ -1,10 +1,5 @@
 package software.uncharted.terarium.hmiserver.service.tasks;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,11 +9,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.apache.catalina.connector.ClientAbortException;
 import org.redisson.api.RLock;
 import org.redisson.api.RMapCache;
@@ -38,6 +29,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import software.uncharted.terarium.hmiserver.configuration.Config;
 import software.uncharted.terarium.hmiserver.models.notification.NotificationEvent;
 import software.uncharted.terarium.hmiserver.models.notification.NotificationGroup;
@@ -287,22 +290,7 @@ public class TaskService {
 	// This is an anonymous queue, every instance the hmi-server will receive a
 	// message. Any operation that must occur on _every_ instance of the hmi-server
 	// should be triggered here.
-	@RabbitListener(
-			bindings =
-					@QueueBinding(
-							value =
-									@org.springframework.amqp.rabbit.annotation.Queue(
-											autoDelete = "true",
-											exclusive = "false",
-											durable = "${terarium.taskrunner.durable-queues}"),
-							exchange =
-									@Exchange(
-											value = "${terarium.taskrunner.response-broadcast-exchange}",
-											durable = "${terarium.taskrunner.durable-queues}",
-											autoDelete = "false",
-											type = ExchangeTypes.DIRECT),
-							key = ""),
-			concurrency = "1")
+	@RabbitListener(bindings = @QueueBinding(value = @org.springframework.amqp.rabbit.annotation.Queue(autoDelete = "true", exclusive = "false", durable = "${terarium.taskrunner.durable-queues}"), exchange = @Exchange(value = "${terarium.taskrunner.response-broadcast-exchange}", durable = "${terarium.taskrunner.durable-queues}", autoDelete = "false", type = ExchangeTypes.DIRECT), key = ""), concurrency = "1")
 	private void onTaskResponseAllInstanceReceive(final Message message) {
 		try {
 			final TaskResponse resp = decodeMessage(message, TaskResponse.class);
@@ -310,8 +298,9 @@ public class TaskService {
 				return;
 			}
 
+			log.info("Received response status {} for task {}", resp.getStatus(), resp.getId());
 			if (resp.getOutput() != null) {
-				log.info("Received response {} for task {}", new String(resp.getOutput()), resp.getId());
+				log.info("Received response output {} for task {}", new String(resp.getOutput()), resp.getId());
 			}
 
 			rLock.lock(REDIS_LOCK_LEASE_SECONDS, TimeUnit.SECONDS);
@@ -375,29 +364,15 @@ public class TaskService {
 	// This is a shared queue, messages will round robin between every instance of
 	// the hmi-server. Any operation that must occur once and only once should be
 	// triggered here.
-	@RabbitListener(
-			bindings =
-					@QueueBinding(
-							value =
-									@org.springframework.amqp.rabbit.annotation.Queue(
-											value = "${terarium.taskrunner.response-queue}",
-											autoDelete = "false",
-											exclusive = "false",
-											durable = "${terarium.taskrunner.durable-queues}"),
-							exchange =
-									@Exchange(
-											value = "${terarium.taskrunner.response-exchange}",
-											durable = "${terarium.taskrunner.durable-queues}",
-											autoDelete = "false",
-											type = ExchangeTypes.DIRECT),
-							key = ""),
-			concurrency = "1")
+	@RabbitListener(bindings = @QueueBinding(value = @org.springframework.amqp.rabbit.annotation.Queue(value = "${terarium.taskrunner.response-queue}", autoDelete = "false", exclusive = "false", durable = "${terarium.taskrunner.durable-queues}"), exchange = @Exchange(value = "${terarium.taskrunner.response-exchange}", durable = "${terarium.taskrunner.durable-queues}", autoDelete = "false", type = ExchangeTypes.DIRECT), key = ""), concurrency = "1")
 	private void onTaskResponseOneInstanceReceives(final Message message) {
 		try {
 			TaskResponse resp = decodeMessage(message, TaskResponse.class);
 			if (resp == null) {
 				return;
 			}
+
+			log.info("Received response status {} for task {}", resp.getStatus(), resp.getId());
 
 			try {
 				// execute the handler
@@ -618,6 +593,16 @@ public class TaskService {
 			log.info("Future completed for task: {}", future.getId());
 			return resp;
 		} catch (final TimeoutException e) {
+
+			// if we time out, something has probably gone wrong, lets remove it from the
+			// SHA lookup
+			rLock.lock(REDIS_LOCK_LEASE_SECONDS, TimeUnit.SECONDS);
+			try {
+				// check if there is an id associated with the hash of the request already
+				taskIdCache.remove(req.getSHA256());
+			} finally {
+				rLock.unlock();
+			}
 			throw new TimeoutException(
 					"Task " + future.getId().toString() + " did not complete within " + timeoutSeconds + " seconds");
 		}
