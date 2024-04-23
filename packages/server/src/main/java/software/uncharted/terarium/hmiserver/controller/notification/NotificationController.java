cf7164f536a307c7cf916ab957793149c659413b
Jaehwan Ryu <jryu@uncharted.software>
2024-04-23 10:28:11 -0400

    Updated error response

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/notification/NotificationController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/notification/NotificationController.java
@@ -76,7 +76,7 @@ public class NotificationController {
 	@ApiResponses(
 			value = {
 				@ApiResponse(responseCode = "200", description = "Acknowledged all events in notification group"),
-				@ApiResponse(responseCode = "500", description = "There was an issue fetching the notifications")
+				@ApiResponse(responseCode = "500", description = "There was an issue acknowledging the notification group")
 			})
 	public ResponseEntity<Void> acknowledgeNotificationGroup(@PathVariable("groupId") final UUID groupId) {
 
