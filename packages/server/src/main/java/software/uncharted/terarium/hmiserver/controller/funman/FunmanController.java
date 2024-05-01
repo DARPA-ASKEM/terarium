9e70ed27c9e1021682a0ea75d1fe22f5a1995363
Daniel Chang <mwdchang@gmail.com>
2024-05-01 12:23:00 -0700

    UI plumbing to hook into funman taskrunner (#3478)
    Co-authored-by: mwdchang <mwdchang@users.noreply.github.com>

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/funman/FunmanController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/funman/FunmanController.java
@@ -2,7 +2,6 @@ package software.uncharted.terarium.hmiserver.controller.funman;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import feign.FeignException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -18,10 +17,8 @@ import org.springframework.web.server.ResponseStatusException;
 import software.uncharted.terarium.hmiserver.models.dataservice.simulation.ProgressState;
 import software.uncharted.terarium.hmiserver.models.dataservice.simulation.Simulation;
 import software.uncharted.terarium.hmiserver.models.dataservice.simulation.SimulationType;
-import software.uncharted.terarium.hmiserver.models.funman.FunmanPostQueriesRequest;
 import software.uncharted.terarium.hmiserver.models.task.TaskRequest;
 import software.uncharted.terarium.hmiserver.models.task.TaskRequest.TaskType;
-import software.uncharted.terarium.hmiserver.proxies.funman.FunmanProxy;
 import software.uncharted.terarium.hmiserver.security.Roles;
 import software.uncharted.terarium.hmiserver.service.CurrentUserService;
 import software.uncharted.terarium.hmiserver.service.data.SimulationService;
@@ -47,7 +44,7 @@ public class FunmanController {
 		taskService.addResponseHandler(validateModelConfigHandler);
 	}
 
-	@PostMapping("/test")
+	@PostMapping
 	@Secured(Roles.USER)
 	@Operation(summary = "Dispatch a model configuration validation task")
 	@ApiResponses(
@@ -98,63 +95,6 @@ public class FunmanController {
 			e.printStackTrace();
 			final String error = "Unable to dispatch task request";
 			throw new ResponseStatusException(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, error);
-		}
-	}
-
-	// The methods below are depreacated
-	private final FunmanProxy funmanProxy;
-
-	@GetMapping("/{queryId}/halt")
-	@Secured(Roles.USER)
-	@ApiResponses(
-			value = {
-				@ApiResponse(responseCode = "200", description = "Query halted"),
-			})
-	public ResponseEntity<JsonNode> halt(@PathVariable final String queryId) {
-		try {
-			final ResponseEntity<JsonNode> response = funmanProxy.halt(queryId);
-			return ResponseEntity.ok(response.getBody());
-		} catch (final FeignException e) {
-			final String error = "Error halting";
-			final int status = e.status() >= 400 ? e.status() : 500;
-			log.error(error, e);
-			throw new ResponseStatusException(org.springframework.http.HttpStatus.valueOf(status), error);
-		}
-	}
-
-	@GetMapping("/{queryId}")
-	@Secured(Roles.USER)
-	@ApiResponses(
-			value = {
-				@ApiResponse(responseCode = "200", description = "Query found"),
-			})
-	public ResponseEntity<JsonNode> getQueries(@PathVariable final String queryId) {
-		try {
-			final ResponseEntity<JsonNode> response = funmanProxy.getQueries(queryId);
-			return ResponseEntity.ok(response.getBody());
-		} catch (final FeignException e) {
-			final String error = "Error getting query";
-			final int status = e.status() >= 400 ? e.status() : 500;
-			log.error(error, e);
-			throw new ResponseStatusException(org.springframework.http.HttpStatus.valueOf(status), error);
-		}
-	}
-
-	@PostMapping
-	@Secured(Roles.USER)
-	@ApiResponses(
-			value = {
-				@ApiResponse(responseCode = "200", description = "Query posted"),
-			})
-	public ResponseEntity<JsonNode> postQueries(@RequestBody final FunmanPostQueriesRequest requestBody) {
-		try {
-			final ResponseEntity<JsonNode> response = funmanProxy.postQueries(requestBody);
-			return ResponseEntity.ok(response.getBody());
-		} catch (final FeignException e) {
-			final String error = "Error posting query";
-			final int status = e.status() >= 400 ? e.status() : 500;
-			log.error(error, e);
-			throw new ResponseStatusException(org.springframework.http.HttpStatus.valueOf(status), error);
 		}
 	}
 }
