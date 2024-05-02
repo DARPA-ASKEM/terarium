2be3c9fb9900a793df5746a6b8317e06b4deb9e7
dvince <dvince@uncharted.software>
2024-05-02 10:45:01 -0400

    WIP

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Grounding.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Grounding.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import lombok.Data;
 import lombok.experimental.Accessors;
-import software.uncharted.terarium.hmiserver.annotations.TSModel;
+import org.hibernate.annotations.JdbcTypeCode;import org.hibernate.type.SqlTypes;import software.uncharted.terarium.hmiserver.annotations.TSModel;
 import software.uncharted.terarium.hmiserver.annotations.TSOptional;
 
 /** Represents a grounding document from TDS */
@@ -19,9 +19,11 @@ public class Grounding implements Serializable {
 	private static final long serialVersionUID = 302308407252037615L;
 
 	/** Ontological identifier per DKG */
+	@JdbcTypeCode(SqlTypes.JSON)
 	private List<Identifier> identifiers;
 
 	/** (Optional) Additional context that informs the grounding */
 	@TSOptional
+	@JdbcTypeCode(SqlTypes.JSON)
 	private Map<String, Object> context;
 }
