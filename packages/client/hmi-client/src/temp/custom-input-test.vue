ac95cb9ecad3b9f996ee2b8088fbe6f67d77e2a3
Daniel Chang <mwdchang@gmail.com>
2024-05-03 09:40:13 -0400

    demo

--- packages/client/hmi-client/src/temp/custom-input-test.vue
+++ packages/client/hmi-client/src/temp/custom-input-test.vue
@@ -24,17 +24,33 @@
 			<label>Number input</label>
 			<tera-input-field label="Label" v-model="numberstring" type="number" />
 		</div>
+
+		<p>whacky tests, counter = {{ whackyCounter }}</p>
+		<div v-for="(n, idx) in whackyComputed.listing" :key="n" class="w-2">
+			<tera-input-field :model-value="n" @update:model-value="(v) => update2(v, idx)" />
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import teraInputField from '@/components/widgets/tera-input-field.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const teststring = ref<string>('');
 const numberstring = ref<string>('');
 const errorstring = ref<string>('Error message error message');
 const errormessage = ref<string>('Error message error message');
+
+const whackyVars = ref(['first', 'second']);
+const whackyCounter = ref(0);
+const update2 = (v: string, idx: number) => {
+	whackyVars.value[idx] = v;
+};
+
+const whackyComputed = computed(() => {
+	whackyCounter.value++;
+	return { listing: [whackyVars.value[0], whackyVars.value[1]] };
+});
 
 function onUpdate(value: string) {
 	if (value) {
