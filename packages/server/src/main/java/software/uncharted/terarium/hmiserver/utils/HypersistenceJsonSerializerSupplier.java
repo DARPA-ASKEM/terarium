a4761e3b025a80a50986b72defcae058dd719427
kbirk <birk.kevin@gmail.com>
2024-05-08 17:53:19 -0400

    Fix clone logic again

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/utils/HypersistenceJsonSerializerSupplier.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/utils/HypersistenceJsonSerializerSupplier.java
@@ -1,46 +1,57 @@
 package software.uncharted.terarium.hmiserver.utils;
 
+import java.util.Collection;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+
 import io.hypersistence.utils.hibernate.type.util.JsonSerializer;
 import io.hypersistence.utils.hibernate.type.util.JsonSerializerSupplier;
 import io.hypersistence.utils.hibernate.type.util.ObjectMapperWrapper;
-import java.util.Collection;
-import java.util.Map;
 
 /**
  * This overrides the default JsonSerializer used in hypersistence.
  *
- * <p>The default JsonSerializer will clone `Serializable` objects by _not_ using jackson to serialize and deserialize
+ * <p>
+ * The default JsonSerializer will clone `Serializable` objects by _not_ using
+ * jackson to serialize and deserialize
  * the object.
  *
- * <p>This causes unwanted behavior for classes that we specifically override to behave a particular way with jackson.
+ * <p>
+ * This causes unwanted behavior for classes that we specifically override to
+ * behave a particular way with jackson.
  * Ex. classes that extend SupportsAdditionalProperties.
  *
- * <p>This code is coped from
+ * <p>
+ * This code is coped from
  * https://github.com/vladmihalcea/hypersistence-utils/blob/master/hypersistence-utils-hibernate-62/src/main/java/io/hypersistence/utils/hibernate/type/util/ObjectMapperJsonSerializer.java
- * and modified to omit the specialized Serializable behavior.
+ * and modified to omit the specialized Serializable behavior and make a more
+ * robust `jsonClone` method
  */
 public class HypersistenceJsonSerializerSupplier implements JsonSerializerSupplier {
 
 	public static class HypersistenceJsonSerializer implements JsonSerializer {
-		private final ObjectMapperWrapper objectMapperWrapper = new ObjectMapperWrapper(new ObjectMapper());
+
+		private final ObjectMapper objectMapper = new ObjectMapper();
+		private final ObjectMapperWrapper objectMapperWrapper = new ObjectMapperWrapper(objectMapper);
 
 		@Override
+		@SuppressWarnings("unchecked")
 		public <T> T clone(final T object) {
 			if (object instanceof String) {
 				return object;
 			} else if (object instanceof Collection) {
-				final Object firstElement = findFirstNonNullElement((Collection) object);
+				final Object firstElement = findFirstNonNullElement((Collection<?>) object);
 				if (firstElement != null) {
 					final JavaType type = TypeFactory.defaultInstance()
 							.constructParametricType(object.getClass(), firstElement.getClass());
 					return objectMapperWrapper.fromBytes(objectMapperWrapper.toBytes(object), type);
 				}
 			} else if (object instanceof Map) {
-				final Map.Entry firstEntry = this.findFirstNonNullEntry((Map) object);
+				final Map.Entry<?, ?> firstEntry = this.findFirstNonNullEntry((Map<?, ?>) object);
 				if (firstEntry != null) {
 					final Object key = firstEntry.getKey();
 					final Object value = firstEntry.getValue();
@@ -55,7 +66,7 @@ public class HypersistenceJsonSerializerSupplier implements JsonSerializerSuppli
 			return jsonClone(object);
 		}
 
-		private Object findFirstNonNullElement(final Collection collection) {
+		private Object findFirstNonNullElement(final Collection<?> collection) {
 			for (final java.lang.Object element : collection) {
 				if (element != null) {
 					return element;
@@ -64,8 +75,8 @@ public class HypersistenceJsonSerializerSupplier implements JsonSerializerSuppli
 			return null;
 		}
 
-		private Map.Entry findFirstNonNullEntry(final Map<?, ?> map) {
-			for (final Map.Entry entry : map.entrySet()) {
+		private Map.Entry<?, ?> findFirstNonNullEntry(final Map<?, ?> map) {
+			for (final Map.Entry<?, ?> entry : map.entrySet()) {
 				if (entry.getKey() != null && entry.getValue() != null) {
 					return entry;
 				}
@@ -73,10 +84,13 @@ public class HypersistenceJsonSerializerSupplier implements JsonSerializerSuppli
 			return null;
 		}
 
+		@SuppressWarnings("unchecked")
 		private <T> T jsonClone(final T object) {
-			final JavaType type =
-					TypeFactory.defaultInstance().constructParametricType(object.getClass(), object.getClass());
-			return objectMapperWrapper.fromBytes(objectMapperWrapper.toBytes(object), type);
+			try {
+				return (T) objectMapper.readValue(objectMapper.writeValueAsBytes(object), object.getClass());
+			} catch (final Exception e) {
+				throw new RuntimeException("Failed to clone object", e);
+			}
 		}
 	}
 
