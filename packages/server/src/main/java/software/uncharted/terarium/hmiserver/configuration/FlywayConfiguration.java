85fc0fbc544c81ee8a5f3f98873bcf0094c7d912
kbirk <birk.kevin@gmail.com>
2024-05-09 10:25:01 -0400

    Fix flyway

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/configuration/FlywayConfiguration.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/configuration/FlywayConfiguration.java
@@ -1,29 +1,32 @@
 package software.uncharted.terarium.hmiserver.configuration;
 
-import java.beans.BeanProperty;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sql.DataSource;
-import lombok.RequiredArgsConstructor;
+
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationInfo;
+import org.flywaydb.core.api.MigrationVersion;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.core.io.Resource;
+
+import lombok.RequiredArgsConstructor;
 
 /**
- * This configuration class ensures that hibernate runs BEFORE flyway. This is to preserve the schema generation
- * provided by Hibernate, but still get the benefits of having a proper version migration system in Flyway
+ * This configuration class ensures that hibernate runs BEFORE flyway. This is
+ * to preserve the schema generation
+ * provided by Hibernate, but still get the benefits of having a proper version
+ * migration system in Flyway
  */
 @Configuration
 @RequiredArgsConstructor
@@ -34,33 +37,6 @@ public class FlywayConfiguration {
 	@Value("${flyway.table:flyway_schema_history}")
 	private String flywayTableName;
 
-	@Value("classpath:${spring.flyway.locations:db/migration}/*.sql")
-	Resource[] migrations;
-
-	/**
-	 * Set the baseline version to be the latest script. In the case where flyway is not initialized, we assume
-	 * hibernate correctly sets up the database
-	 */
-	@BeanProperty
-	FlywayConfigurationCustomizer customizeBaselineVersion() {
-		return configuration -> configuration.baselineVersion(getBaselineVersion());
-	}
-
-	/** Load properties */
-	@Bean
-	@ConfigurationProperties(prefix = "spring.flyway")
-	public FlywayProperties flywayProperties() {
-		return new FlywayProperties();
-	}
-
-	/** Configure empty beans if disabled. */
-	@Bean
-	@ConditionalOnProperty(name = "spring.flyway.enabled", havingValue = "false")
-	public Flyway flyway() {
-		// Create a Flyway instance with default settings
-		return Flyway.configure().load();
-	}
-
 	/**
 	 * Do nothing on initialization
 	 *
@@ -69,13 +45,24 @@ public class FlywayConfiguration {
 	 */
 	@Bean
 	FlywayMigrationInitializer flywayInitializer(final Flyway flyway) {
-		return new FlywayMigrationInitializer(flyway, (f) -> {});
+		return new FlywayMigrationInitializer(flyway, (f) -> {
+		});
 	}
 
-	static class FlywayVoid {}
+	static class FlywayVoid {
+	}
+
+	private String removeJavaMigrationPackage(final String str) {
+		final int index = str.indexOf('V');
+		if (index != -1) {
+			return str.substring(index);
+		}
+		return str;
+	}
 
 	/**
-	 * Once the entityManagerFactory (aka, Hibernate) has been created it's safe to run migrations
+	 * Once the entityManagerFactory (aka, Hibernate) has been created it's safe to
+	 * run migrations
 	 *
 	 * @param flyway
 	 * @param flywayProperties
@@ -84,17 +71,30 @@ public class FlywayConfiguration {
 	@Bean
 	@DependsOn("entityManagerFactory")
 	FlywayVoid delayedFlywayInitializer(final Flyway flyway, final FlywayProperties flywayProperties) {
-		if (flywayProperties.isEnabled()) {
-			if (!isFlywayInitialized()) {
-				flyway.baseline();
+
+		Flyway flywayWithCorrectBaseline = flyway;
+		if (flywayProperties.isEnabled() && !isFlywayInitialized()) {
+			// get all migration names
+			final List<String> migrations = new ArrayList<>();
+			for (final MigrationInfo migration : flyway.info().all()) {
+				migrations.add(removeJavaMigrationPackage(migration.getScript()));
 			}
-			flyway.migrate();
+			// generate baseline version
+			final String baselineVersion = getBaselineVersion(migrations);
+
+			// re-create the flyway object with the correct baseline
+			final FluentConfiguration config = Flyway.configure().configuration(flyway.getConfiguration())
+					.baselineVersion(MigrationVersion.fromVersion(baselineVersion));
+			flywayWithCorrectBaseline = new Flyway(config);
+			flywayWithCorrectBaseline.baseline();
 		}
+		flywayWithCorrectBaseline.migrate();
 		return new FlywayVoid();
 	}
 
 	/**
-	 * Tests if flyway is initialized by checking if the schema is present in the database
+	 * Tests if flyway is initialized by checking if the schema is present in the
+	 * database
 	 *
 	 * @return true if flyway is initialized, false otherwise
 	 */
@@ -113,14 +113,14 @@ public class FlywayConfiguration {
 	 *
 	 * @return the largest (latest) version migration number as a string
 	 */
-	private String getBaselineVersion() {
-		return Arrays.stream(migrations)
-				.map(Resource::getFilename)
-				.filter(Objects::nonNull)
+	private String getBaselineVersion(final List<String> migrations) {
+		final String baseline = migrations.stream()
 				.map(filename -> filename.split("__")[0].substring(1))
 				.map(Integer::parseInt)
 				.max(Integer::compareTo)
 				.orElseThrow()
 				.toString();
+
+		return baseline;
 	}
 }
