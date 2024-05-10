0101cf6435ff020c744b1199fb7f5d1145c23699
kbirk <kbirk@users.noreply.github.com>
2024-05-10 19:41:59 +0000

    chore: lint server codebase

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/SnakeCaseRequestControllerAdvice.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/SnakeCaseRequestControllerAdvice.java
@@ -1,11 +1,16 @@
 package software.uncharted.terarium.hmiserver.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import jakarta.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpHeaders;
@@ -13,14 +18,6 @@ import org.springframework.http.HttpInputMessage;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import software.uncharted.terarium.hmiserver.annotations.AMRPropertyNamingStrategy;
 
 @RestControllerAdvice
@@ -36,10 +33,14 @@ public class SnakeCaseRequestControllerAdvice extends RequestBodyAdviceAdapter {
 
 	@PostConstruct
 	public void init() {
-		camelcaseMapper = camelcaseMapper.copy().setPropertyNamingStrategy(
-				new AMRPropertyNamingStrategy(new PropertyNamingStrategies.LowerCamelCaseStrategy()));
-		snakecaseMapper = snakecaseMapper.copy().setPropertyNamingStrategy(
-				new AMRPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy()));
+		camelcaseMapper = camelcaseMapper
+				.copy()
+				.setPropertyNamingStrategy(
+						new AMRPropertyNamingStrategy(new PropertyNamingStrategies.LowerCamelCaseStrategy()));
+		snakecaseMapper = snakecaseMapper
+				.copy()
+				.setPropertyNamingStrategy(
+						new AMRPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy()));
 	}
 
 	@Override
