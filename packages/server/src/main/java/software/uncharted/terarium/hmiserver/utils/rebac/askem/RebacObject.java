edfd12e475eefa759238a89e410b082f76863793
Charles Coleman <bigglesandginger@gmail.com>
2024-05-22 16:15:07 -0400

    optional id for projects

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/utils/rebac/askem/RebacObject.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/utils/rebac/askem/RebacObject.java
@@ -2,11 +2,21 @@ package software.uncharted.terarium.hmiserver.utils.rebac.askem;
 
 import software.uncharted.terarium.hmiserver.utils.rebac.SchemaObject;
 
+import java.util.UUID;
+
 public abstract class RebacObject {
 	private String id;
 
 	public RebacObject(String id) {
 		this.id = id;
+	}
+
+	public RebacObject(UUID id) {
+		if (id == null) {
+			this.id = "";
+		} else {
+			this.id = id.toString();
+		}
 	}
 
 	public String getId() {
