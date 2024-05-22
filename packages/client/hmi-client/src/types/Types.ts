93210936f5211d29578c5c06082ad79e5eae9f8b
Derek Vince <dvince@uncharted.software>
2024-05-22 16:08:05 -0400

    3626 task migrate model to postgres from es (#3676)
    Co-authored-by: dvince2 <dvince2@users.noreply.github.com>

--- packages/client/hmi-client/src/types/Types.ts
+++ packages/client/hmi-client/src/types/Types.ts
@@ -219,6 +219,7 @@ export interface Model extends TerariumAssetThatSupportsAdditionalProperties {
     properties?: any;
     semantics?: ModelSemantics;
     metadata?: ModelMetadata;
+    project?: Project;
 }
 
 export interface ModelConfiguration extends TerariumAssetThatSupportsAdditionalProperties {
@@ -409,11 +410,6 @@ export interface Project extends TerariumAsset {
      * @deprecated
      */
     projectAssets: ProjectAsset[];
-    codeAssets: Code[];
-    datasetAssets: Dataset[];
-    workflowAssets: Workflow[];
-    artifactAssets: Artifact[];
-    documentAssets: DocumentAsset[];
     metadata?: { [index: string]: string };
     publicProject?: boolean;
     userPermission?: string;
@@ -939,13 +935,6 @@ export interface PetriNetTransition {
     properties: PetriNetTransitionProperties;
 }
 
-export interface Workflow extends TerariumAsset {
-    transform: Transform;
-    nodes: WorkflowNode[];
-    edges: WorkflowEdge[];
-    project?: Project;
-}
-
 export interface ProvenanceNode {
     id: string;
     type: ProvenanceType;
@@ -1067,24 +1056,6 @@ export interface PetriNetTransitionProperties {
     name: string;
     description: string;
     grounding?: ModelGrounding;
-}
-
-export interface Transform {
-    x: number;
-    y: number;
-    k: number;
-}
-
-export interface WorkflowNode {
-    id: string;
-    workflowId: string;
-}
-
-export interface WorkflowEdge {
-    id: string;
-    workflowId: string;
-    source: string;
-    target: string;
 }
 
 export interface XDDFacetBucket {
--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/TerariumAssetThatSupportsAdditionalProperties.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/TerariumAssetThatSupportsAdditionalProperties.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 import software.uncharted.terarium.hmiserver.annotations.TSIgnore;
 
+// TODO "Clonable" should be moved back to Terarium Asset and all other classes need to be changed to use that
 public class TerariumAssetThatSupportsAdditionalProperties extends TerariumAsset {
 
 	@TSIgnore
@@ -24,5 +25,18 @@ public class TerariumAssetThatSupportsAdditionalProperties extends TerariumAsset
 	public void setAdditionalProperty(final String name, final Object value) {
 		final ObjectMapper mapper = new ObjectMapper();
 		additionalProperties.put(name, mapper.valueToTree(value));
+	}
+
+	public TerariumAssetThatSupportsAdditionalProperties cloneSuperFields(
+			final TerariumAssetThatSupportsAdditionalProperties asset) {
+
+		super.cloneSuperFields(asset);
+		if (additionalProperties != null) {
+			asset.additionalProperties = new HashMap<>();
+			for (final Map.Entry<String, JsonNode> entry : additionalProperties.entrySet()) {
+				asset.additionalProperties.put(entry.getKey(), entry.getValue().deepCopy());
+			}
+		}
+		return asset;
 	}
 }
