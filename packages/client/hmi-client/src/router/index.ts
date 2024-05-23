c52f905d1cdf51bd33f395fc1f4a9b653b723fce
Daniel Chang <mwdchang@gmail.com>
2024-05-23 14:11:19 -0400

    Revert "test"
    This reverts commit d6d4272e19a0432dc5adf0fa331e564ee7f96909.

--- packages/client/hmi-client/src/router/index.ts
+++ packages/client/hmi-client/src/router/index.ts
@@ -17,7 +17,6 @@ import PyodideTest from '@/temp/PyodideTest.vue';
 import JupyterTest from '@/temp/JupyterTest.vue';
 import ModelTemplateTest from '@/temp/model-template-test.vue';
 import CustomInputTest from '@/temp/custom-input-test.vue';
-import ClipboardTest from '@/temp/Clipboard.vue';
 import { RouteName } from './routes';
 
 export enum RoutePath {
@@ -70,8 +69,7 @@ const routes = [
 	{ path: '/pyodide-test', component: PyodideTest },
 	{ path: '/jupyter-test', component: JupyterTest },
 	{ path: '/model-template-test', component: ModelTemplateTest },
-	{ path: '/custom-input-test', component: CustomInputTest },
-	{ path: '/clipboard', component: ClipboardTest }
+	{ path: '/custom-input-test', component: CustomInputTest }
 ];
 
 const router = createRouter({
