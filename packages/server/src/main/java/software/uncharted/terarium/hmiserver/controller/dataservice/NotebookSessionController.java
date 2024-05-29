5265ead680cbdf6c9a827326953a16bb29241dd0
Derek Vince <dvince@uncharted.software>
2024-05-29 09:53:51 -0400

    3717 task migrate notebooksession from es to pg (#3722)
    Co-authored-by: dvince2 <dvince2@users.noreply.github.com>

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/NotebookSessionController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/NotebookSessionController.java
@@ -1,6 +1,5 @@
 package software.uncharted.terarium.hmiserver.controller.dataservice;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -41,7 +40,7 @@ import software.uncharted.terarium.hmiserver.utils.rebac.Schema;
 public class NotebookSessionController {
 
 	final NotebookSessionService sessionService;
-	final ObjectMapper objectMapper;
+
 	private final ProjectService projectService;
 	private final CurrentUserService currentUserService;
 
@@ -74,11 +73,11 @@ public class NotebookSessionController {
 			})
 	ResponseEntity<List<NotebookSession>> getNotebookSessions(
 			@RequestParam(name = "page-size", defaultValue = "100") final Integer pageSize,
-			@RequestParam(name = "page", defaultValue = "0") final Integer page) {
+			@RequestParam(name = "page", defaultValue = "1") final Integer page) {
 
 		try {
 			return ResponseEntity.ok(sessionService.getPublicNotTemporaryAssets(pageSize, page));
-		} catch (final IOException e) {
+		} catch (final Exception e) {
 			final String error = "Unable to get sessions";
 			log.error(error, e);
 			throw new ResponseStatusException(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, error);
@@ -88,7 +87,7 @@ public class NotebookSessionController {
 	/**
 	 * Create session and return its ID
 	 *
-	 * @param request session to create and projectId
+	 * @param session session to create and projectId
 	 * @return new ID for session
 	 */
 	@PostMapping
@@ -162,7 +161,7 @@ public class NotebookSessionController {
 			final Optional<NotebookSession> session = sessionService.getAsset(id, permission);
 			return session.map(ResponseEntity::ok)
 					.orElseGet(() -> ResponseEntity.notFound().build());
-		} catch (final IOException e) {
+		} catch (final Exception e) {
 			final String error = "Unable to get notebook session";
 			log.error(error, e);
 			throw new ResponseStatusException(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, error);
@@ -173,7 +172,7 @@ public class NotebookSessionController {
 	 * Update an session
 	 *
 	 * @param id id of session to update
-	 * @param request session to update with and projectId
+	 * @param session session to update with and projectId
 	 * @return ID of updated session
 	 */
 	@PutMapping("/{id}")
@@ -248,7 +247,7 @@ public class NotebookSessionController {
 				return ResponseEntity.notFound().build();
 			}
 			return ResponseEntity.status(HttpStatus.OK).body(session.get().clone());
-		} catch (final IOException e) {
+		} catch (final Exception e) {
 			final String error = "Unable to clone notebook session";
 			log.error(error, e);
 			throw new ResponseStatusException(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, error);
--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/notebooksession/NotebookSession.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/notebooksession/NotebookSession.java
@@ -1,10 +1,14 @@
 package software.uncharted.terarium.hmiserver.models.dataservice.notebooksession;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import java.io.Serial;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.Type;
 import software.uncharted.terarium.hmiserver.annotations.TSModel;
 import software.uncharted.terarium.hmiserver.models.TerariumAsset;
 
@@ -12,11 +16,17 @@ import software.uncharted.terarium.hmiserver.models.TerariumAsset;
 @Data
 @Accessors(chain = true)
 @TSModel
+@Entity
 public class NotebookSession extends TerariumAsset {
 
 	@Serial
 	private static final long serialVersionUID = 9176019416379347233L;
 
+	// TODO: https://github.com/DARPA-ASKEM/terarium/issues/3719
+	// private UUID workflowId;
+
+	@Type(JsonType.class)
+	@Column(columnDefinition = "json")
 	private JsonNode data;
 
 	public NotebookSession clone() {
