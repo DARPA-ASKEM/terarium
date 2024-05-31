68255df78cc625db5133f5c32617103de2f60cab
David Gauldie <dgauldie@uncharted.software>
2024-05-31 15:04:51 -0400

    update secrets

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/utils/rebac/askem/RebacUser.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/utils/rebac/askem/RebacUser.java
@@ -1,52 +1,54 @@
 package software.uncharted.terarium.hmiserver.utils.rebac.askem;
 
-import java.util.List;
-import java.util.UUID;
 import software.uncharted.terarium.hmiserver.models.permissions.PermissionGroup;
 import software.uncharted.terarium.hmiserver.utils.rebac.ReBACService;
 import software.uncharted.terarium.hmiserver.utils.rebac.RelationsipAlreadyExistsException.RelationshipAlreadyExistsException;
 import software.uncharted.terarium.hmiserver.utils.rebac.Schema;
 import software.uncharted.terarium.hmiserver.utils.rebac.SchemaObject;
 
+import java.util.List;
+import java.util.UUID;
+
 public class RebacUser extends RebacObject {
-	private ReBACService reBACService;
+	private final ReBACService reBACService;
 
-	private boolean serviceUser;
+	private final boolean serviceUser;
 
-	public RebacUser(String id, ReBACService reBACService) {
+	public RebacUser(final String id, final ReBACService reBACService) {
 		super(id);
 		this.reBACService = reBACService;
 
-		serviceUser = reBACService.isServiceUser(id);
+		serviceUser = ReBACService.isServiceUser(id);
 	}
 
+	@Override
 	public SchemaObject getSchemaObject() {
 		return new SchemaObject(Schema.Type.USER, getId());
 	}
 
-	public boolean can(RebacObject rebacObject, Schema.Permission permission) throws Exception {
+	public boolean can(final RebacObject rebacObject, final Schema.Permission permission) throws Exception {
 		if (serviceUser) return true;
 		if (rebacObject.getId().isEmpty()) return false;
 		return reBACService.can(getSchemaObject(), permission, rebacObject.getSchemaObject());
 	}
 
-	public boolean isMemberOf(RebacGroup rebacGroup) throws Exception {
+	public boolean isMemberOf(final RebacGroup rebacGroup) throws Exception {
 		return reBACService.isMemberOf(getSchemaObject(), rebacGroup.getSchemaObject());
 	}
 
-	public void createCreatorRelationship(RebacObject rebacObject)
+	public void createCreatorRelationship(final RebacObject rebacObject)
 			throws Exception, RelationshipAlreadyExistsException {
 		reBACService.createRelationship(getSchemaObject(), rebacObject.getSchemaObject(), Schema.Relationship.CREATOR);
 	}
 
-	public PermissionGroup createGroup(String name) throws Exception, RelationshipAlreadyExistsException {
-		PermissionGroup group = reBACService.createGroup(name);
+	public PermissionGroup createGroup(final String name) throws Exception, RelationshipAlreadyExistsException {
+		final PermissionGroup group = reBACService.createGroup(name);
 		reBACService.createRelationship(
 				getSchemaObject(), new SchemaObject(Schema.Type.GROUP, group.getId()), Schema.Relationship.CREATOR);
 		return group;
 	}
 
-	public String getPermissionFor(RebacProject rebacProject) throws Exception {
+	public String getPermissionFor(final RebacProject rebacProject) throws Exception {
 		if (reBACService.isCreator(getSchemaObject(), rebacProject.getSchemaObject())) {
 			return Schema.Relationship.CREATOR.toString();
 		} else if (can(rebacProject, Schema.Permission.ADMINISTRATE)) {
