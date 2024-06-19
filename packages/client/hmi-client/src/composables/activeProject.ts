d523456699e859741c46bac06d7357998b862e02
Yohann Paris <github@yohannparis.com>
2024-06-19 16:07:14 -0400

    Save activeProjectId in localStorage

--- packages/client/hmi-client/src/composables/activeProject.ts
+++ packages/client/hmi-client/src/composables/activeProject.ts
@@ -1,5 +1,17 @@
-import { computed, shallowRef } from 'vue';
+import { computed, shallowRef, watch } from 'vue';
 import type { Project } from '@/types/Types';
 
 export const activeProject = shallowRef<Project | null>(null);
-export const activeProjectId = computed<string>(() => activeProject.value?.id ?? '');
+export const activeProjectId = computed<string>(
+	() => activeProject.value?.id ?? localStorage.getItem('activeProjectId') ?? ''
+);
+
+watch(
+	activeProjectId,
+	async (projectId) => {
+		if (projectId !== activeProjectId.value || localStorage.getItem('activeProjectId') == null) {
+			localStorage.setItem('activeProjectId', projectId);
+		}
+	},
+	{ immediate: true }
+);
