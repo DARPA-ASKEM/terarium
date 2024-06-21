57d83db0ac3c6d71d3787769ddbf7aa8be5890e8
Daniel Chang <mwdchang@gmail.com>
2024-06-21 12:24:49 -0400

    make more realistic test

--- packages/client/hmi-client/src/temp/Vegalite.vue
+++ packages/client/hmi-client/src/temp/Vegalite.vue
@@ -1,17 +1,20 @@
 <template>
-	<div style="padding: 2rem">
+	<div style="padding: 2rem; display: flex; flex-direction: row">
 		<vega-chart
 			:interval-selection-signal-names="['brush']"
 			:visualization-spec="spec"
 			@chart-click="handleChartClick($event)"
 			@update-interval-selection="handleIntervalSelect"
 		/>
+		<vega-chart :visualization-spec="spec2" />
 	</div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import VegaChart from './VegaChart.vue';
+
+const rand = (v: number) => Math.round(Math.random() * v);
 
 const handleChartClick = (event: any) => {
 	console.log('!!', event);
@@ -21,6 +24,84 @@ const handleIntervalSelect = (name: any, valueRange: any) => {
 	console.log(name, valueRange);
 };
 
+const numPoints = 10;
+const numSamples = 20;
+const valueRange = 20;
+const trueValues: any[] = [];
+const dataChart1: any[] = [];
+const dataChart2: any[] = [];
+
+for (let j = 0; j < numPoints; j++) {
+	trueValues.push(rand(valueRange));
+}
+
+for (let i = 0; i < numSamples; i++) {
+	let error = 0;
+	for (let j = 0; j < numPoints; j++) {
+		const v = rand(valueRange);
+		dataChart2.push({ sample: i, timestep: j, value: v });
+		error += Math.abs(trueValues[j] - v);
+	}
+	dataChart1.push({ sample: i, error: error });
+}
+
+const spec = ref<any>({
+	$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+	data: { values: dataChart1 },
+	transform: [
+		{
+			aggregate: [{ op: 'count', field: '*', as: 'count' }],
+			groupby: ['error']
+		},
+		{ calculate: 'random()', as: 'jitter' }
+	],
+	vconcat: [
+		{
+			mark: 'area',
+			encoding: {
+				x: {
+					field: 'error',
+					type: 'quantitative'
+				},
+				y: {
+					field: 'count',
+					type: 'quantitative'
+				}
+			}
+		},
+		{
+			mark: 'point',
+			encoding: {
+				data: { value: dataChart1 },
+				color: { value: '#f80' },
+				opacity: { value: 0.8 },
+				size: { value: 15 },
+				x: { field: 'error', type: 'quantitative', title: '' },
+				y: { field: 'jitter', type: 'quantitative', title: '' }
+			},
+			params: [{ name: 'brush', select: 'interval' }]
+		}
+	]
+});
+
+const spec2 = ref<any>({
+	$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+	description: 'Stock prices of 5 Tech Companies over Time.',
+	// data: { url: 'https://vega.github.io/vega-lite/data/stocks.csv' },
+	data: { values: dataChart2 },
+	mark: 'line',
+	encoding: {
+		x: { field: 'timestep', type: 'quantitative' },
+		y: { field: 'value', type: 'quantitative' },
+		color: {
+			field: 'sample',
+			type: 'nominal',
+			legend: null
+		}
+	}
+});
+
+/*
 const spec = ref<any>({
 	$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
 	data: { url: 'https://vega.github.io/editor/data/unemployment-across-industries.json' },
@@ -75,4 +156,5 @@ const spec = ref<any>({
 		}
 	]
 });
+*/
 </script>
