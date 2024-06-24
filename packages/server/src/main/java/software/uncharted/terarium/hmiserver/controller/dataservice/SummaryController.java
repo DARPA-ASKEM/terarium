7c21daac8369b16f1df262c312de9063ad5b5bf2
Derek Vince <dvince@uncharted.software>
2024-06-24 11:17:22 -0400

    Update packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/SummaryController.java
    Co-authored-by: Yohann Paris <github@yohannparis.com>

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/SummaryController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/SummaryController.java
@@ -58,7 +58,7 @@ public class SummaryController {
 			value = {
 				@ApiResponse(
 						responseCode = "200",
-						description = "Summary found.",
+						description = "Summaries found.",
 						content =
 								@Content(
 										mediaType = MediaType.APPLICATION_JSON_VALUE,
