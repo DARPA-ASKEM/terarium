14e2eee47c66d428c4cdf68d1f16c5c2889ca00d
Cole Blanchard <cblanchard@Cole-Blanchards-MacBook-Pro.local>
2024-06-25 17:13:31 -0400

    create default config on node connection

--- packages/client/hmi-client/src/components/workflow/ops/model-config/tera-model-config-node.vue
+++ packages/client/hmi-client/src/components/workflow/ops/model-config/tera-model-config-node.vue
@@ -12,11 +12,13 @@
 </template>
 
 <script setup lang="ts">
-import { cloneDeep } from 'lodash';
+import { cloneDeep, isEmpty } from 'lodash';
 import { computed, watch } from 'vue';
 import { WorkflowNode, WorkflowPortStatus } from '@/types/workflow';
 import Button from 'primevue/button';
 import TeraOperatorPlaceholder from '@/components/operator/tera-operator-placeholder.vue';
+import { getModel, getModelConfigurations } from '@/services/model';
+import { postAsConfiguredModel } from '@/services/model-configurations';
 import { ModelConfigOperationState } from './model-config-operation';
 
 const props = defineProps<{
@@ -30,7 +32,7 @@ const isModelInputConnected = computed(() => modelInput?.status === WorkflowPort
 // Update the node with the new input ports
 watch(
 	() => props.node.inputs,
-	() => {
+	async () => {
 		const inputs = props.node.inputs;
 		const documentInputs = inputs.filter((input) => input.type === 'documentId');
 		const datasetInputs = inputs.filter((input) => input.type === 'datasetId');
@@ -47,6 +49,17 @@ watch(
 				initialSemanticList: []
 			};
 			emit('update-state', state);
+		} else {
+			// Check if it has a model configuration
+			const modelId = modelInputs[0].value[0];
+			const modelConfigurations = await getModelConfigurations(modelId);
+			if (isEmpty(modelConfigurations)) {
+				// Create a model configuration if it does not exist
+				const model = await getModel(modelId);
+				if (model) {
+					postAsConfiguredModel(model);
+				}
+			}
 		}
 
 		// If all document inputs are connected, add a new document input port
