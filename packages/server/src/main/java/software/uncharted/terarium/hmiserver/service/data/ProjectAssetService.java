8a3ba8ac8725ed7b1a27a3359bb7cab040db9043
kbirk <birk.kevin@gmail.com>
2024-07-03 10:37:14 -0400

    When project public flag is toggled, update all underlying assets.

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/data/ProjectAssetService.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/data/ProjectAssetService.java
@@ -1,16 +1,19 @@
 package software.uncharted.terarium.hmiserver.service.data;
 
-import io.micrometer.observation.annotation.Observed;
-import jakarta.validation.constraints.NotNull;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import io.micrometer.observation.annotation.Observed;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import software.uncharted.terarium.hmiserver.models.TerariumAsset;
 import software.uncharted.terarium.hmiserver.models.dataservice.AssetType;
 import software.uncharted.terarium.hmiserver.models.dataservice.model.Model;
@@ -26,14 +29,16 @@ import software.uncharted.terarium.hmiserver.utils.rebac.Schema;
 public class ProjectAssetService {
 
 	final ProjectAssetRepository projectAssetRepository;
+	final TerariumAssetServices terariumAssetServices;
 
 	final ReBACService reBACService;
 
 	/**
-	 * Find all active assets for a project. Active assets are defined as those that are not deleted and not temporary.
+	 * Find all active assets for a project. Active assets are defined as those that
+	 * are not deleted and not temporary.
 	 *
 	 * @param projectId The project ID
-	 * @param types The types of assets to find
+	 * @param types     The types of assets to find
 	 * @return The list of active assets for the project
 	 */
 	@Observed(name = "function_profile")
@@ -51,8 +56,8 @@ public class ProjectAssetService {
 			@NotNull final AssetType type,
 			@NotNull final UUID originalAssetId,
 			final Schema.Permission hasWritePermission) {
-		final ProjectAsset asset =
-				projectAssetRepository.findByProjectIdAndAssetIdAndAssetType(projectId, originalAssetId, type);
+		final ProjectAsset asset = projectAssetRepository.findByProjectIdAndAssetIdAndAssetType(projectId,
+				originalAssetId, type);
 		if (asset == null) {
 			return false;
 		}
@@ -153,5 +158,29 @@ public class ProjectAssetService {
 			}
 		}
 		return null;
+	}
+
+	@Observed(name = "function_profile")
+	public void togglePublicForAssets(final UUID projectId, final boolean isPublic,
+			final Schema.Permission hasWritePermission) throws IOException {
+
+		final List<ProjectAsset> projectAssets = projectAssetRepository
+				.findAllByProjectIdAndDeletedOnIsNullAndTemporaryFalse(projectId);
+
+		for (final ProjectAsset projectAsset : projectAssets) {
+
+			final ITerariumAssetService<? extends TerariumAsset> terariumAssetService = terariumAssetServices
+					.getServiceByType(projectAsset.getAssetType());
+
+			final Optional<? extends TerariumAsset> asset = terariumAssetService.getAsset(projectAsset.getAssetId(),
+					hasWritePermission);
+
+			if (asset.isPresent()) {
+				asset.get().setPublicAsset(isPublic);
+
+				terariumAssetServices.updateAsset((TerariumAsset) asset.get(), projectAsset.getAssetType(),
+						hasWritePermission);
+			}
+		}
 	}
 }
--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/data/TerariumAssetServices.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/data/TerariumAssetServices.java
@@ -1,10 +1,22 @@
 package software.uncharted.terarium.hmiserver.service.data;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
 import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import software.uncharted.terarium.hmiserver.models.TerariumAsset;
+import software.uncharted.terarium.hmiserver.models.dataservice.Artifact;
 import software.uncharted.terarium.hmiserver.models.dataservice.AssetType;
+import software.uncharted.terarium.hmiserver.models.dataservice.code.Code;
+import software.uncharted.terarium.hmiserver.models.dataservice.dataset.Dataset;
+import software.uncharted.terarium.hmiserver.models.dataservice.document.DocumentAsset;
+import software.uncharted.terarium.hmiserver.models.dataservice.model.Model;
+import software.uncharted.terarium.hmiserver.models.dataservice.model.configurations.ModelConfiguration;
+import software.uncharted.terarium.hmiserver.models.dataservice.workflow.Workflow;
+import software.uncharted.terarium.hmiserver.utils.rebac.Schema.Permission;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +48,29 @@ public class TerariumAssetServices {
 			case WORKFLOW -> workflowService;
 			default -> throw new IllegalArgumentException("Invalid asset type: " + type);
 		};
+	}
+
+	public Optional<? extends TerariumAsset> updateAsset(final TerariumAsset asset, final AssetType type,
+			final Permission permission) throws IOException {
+
+		switch (type) {
+			case ARTIFACT:
+				return artifactService.updateAsset((Artifact) asset, permission);
+			case CODE:
+				return codeService.updateAsset((Code) asset, permission);
+			case DATASET:
+				return datasetService.updateAsset((Dataset) asset, permission);
+			case DOCUMENT:
+				return documentAssetService.updateAsset((DocumentAsset) asset, permission);
+			case MODEL_CONFIGURATION:
+				return modelConfigurationService.updateAsset((ModelConfiguration) asset,
+						permission);
+			case MODEL:
+				return modelService.updateAsset((Model) asset, permission);
+			case WORKFLOW:
+				return workflowService.updateAsset((Workflow) asset, permission);
+			default:
+				throw new IllegalArgumentException("Invalid asset type: " + type);
+		}
 	}
 }
