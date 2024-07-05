436b7ecebeea958432198350bf726a0592d992ff
Charles Coleman <bigglesandginger@gmail.com>
2024-07-05 15:44:47 -0400

    identify JPA/Hibernate terminology

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/project/ProjectAndAssetAggregate.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/project/ProjectAndAssetAggregate.java
@@ -4,6 +4,9 @@ import java.sql.Timestamp;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * In confusing JPA speech - this is a "Interface-based Projection"
+ */
 public interface ProjectAndAssetAggregate {
 	UUID getId();
 	Timestamp getCreatedOn();
