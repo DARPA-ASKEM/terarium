407b7724e280a79560282f7e7ba55038e7f96d11
Tom Szendrey <T.Szendrey@hotmail.com>
2024-07-09 14:03:54 -0400

    remove intervention policy group form. (#4080)

--- packages/client/hmi-client/src/components/workflow/ops/optimize-ciemss/tera-dynamic-intervention-policy-group.vue
+++ packages/client/hmi-client/src/components/workflow/ops/optimize-ciemss/tera-dynamic-intervention-policy-group.vue
@@ -1,20 +1,93 @@
 <template>
-	<p>
-		Set the <strong>{{ config.intervention?.type }}</strong>
-		<strong>{{ config.intervention?.appliedTo }}</strong> to
-		<strong>{{ dynamicInterventions[0].threshold }}</strong> days when it
-		<strong>{{
-			dynamicInterventions[0].isGreaterThan ? 'increase to above' : 'decrease to below'
-		}}</strong>
-		the threshold value <strong>{{ dynamicInterventions[0].value }}</strong> person.
-	</p>
+	<div class="policy-group">
+		<div class="form-header">
+			<tera-toggleable-edit
+				class="mr-auto"
+				:model-value="knobs.name"
+				@update:model-value="onEditName($event)"
+				tag="h5"
+			/>
+			<div>
+				<!-- TODO: We should be able to utilize dynamic in the future -->
+				<label for="active">Optimize</label>
+				<InputSwitch
+					v-model="knobs.isActive"
+					:disabled="true"
+					@change="emit('update-self', knobs)"
+				/>
+			</div>
+		</div>
+		<p>
+			Set the <strong>{{ config.intervention?.type }}</strong>
+			<strong>{{ config.intervention?.appliedTo }}</strong> to
+			<strong>{{ dynamicInterventions[0].threshold }}</strong> days when it
+			<strong>{{
+				dynamicInterventions[0].isGreaterThan ? 'increase to above' : 'decrease to below'
+			}}</strong>
+			the threshold value <strong>{{ dynamicInterventions[0].value }}</strong> person.
+		</p>
+	</div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue';
 import { DynamicIntervention } from '@/types/Types';
 import { InterventionPolicyGroup } from '@/components/workflow/ops/optimize-ciemss/optimize-ciemss-operation';
+import InputSwitch from 'primevue/inputswitch';
+import TeraToggleableEdit from '@/components/widgets/tera-toggleable-edit.vue';
 
-defineProps<{
+const props = defineProps<{
 	config: InterventionPolicyGroup;
 	dynamicInterventions: DynamicIntervention[];
 }>();
+
+const emit = defineEmits(['update-self']);
+
+const knobs = ref({
+	name: props.config.name ?? 'policy bounds',
+	isActive: props.config.isActive ?? false
+});
+
+const onEditName = (name: string) => {
+	knobs.value.name = name;
+	emit('update-self', knobs);
+};
 </script>
+<style>
+.form-header {
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	gap: var(--gap-4);
+	padding-bottom: 0.5rem;
+
+	& > *:first-child {
+		margin-right: auto;
+	}
+
+	& > * {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		gap: var(--gap-2);
+	}
+}
+
+.policy-group {
+	display: flex;
+	padding: 1rem 1rem 1rem 1.5rem;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+	gap: var(--gap-2);
+	border-radius: 0.375rem;
+	background: #fff;
+	border: 1px solid rgba(0, 0, 0, 0.08);
+	/* Shadow/medium */
+	box-shadow:
+		0 2px 4px -1px rgba(0, 0, 0, 0.06),
+		0 4px 6px -1px rgba(0, 0, 0, 0.08);
+}
+</style>
--- packages/client/hmi-client/src/components/workflow/ops/optimize-ciemss/tera-static-intervention-policy-group.vue
+++ packages/client/hmi-client/src/components/workflow/ops/optimize-ciemss/tera-static-intervention-policy-group.vue
@@ -1,85 +1,101 @@
 <template>
-	<template v-if="config.isActive">
-		<section class="input-row">
-			<p>
-				Find the
-				<Dropdown
-					class="toolbar-button"
-					v-model="knobs.optimizationType"
-					:options="OPTIMIZATION_TYPES"
-				/>
-				for the <strong>{{ config.intervention?.type }}</strong>
-				<strong>{{ config.intervention?.appliedTo }}</strong
-				>.
-			</p>
-		</section>
-		<div>
+	<div class="policy-group">
+		<div class="form-header">
+			<tera-toggleable-edit
+				class="mr-auto"
+				:model-value="knobs.name"
+				@update:model-value="onEditName($event)"
+				tag="h5"
+			/>
+			<div>
+				<label for="active">Optimize</label>
+				<InputSwitch v-model="knobs.isActive" @change="emit('update-self', knobs)" />
+			</div>
+		</div>
+		<template v-if="knobs.isActive">
 			<section class="input-row">
 				<p>
-					The objective is the
-					<template v-if="showNewValueOptions">
-						<span>value closet to the</span>
-						<Dropdown
-							class="toolbar-button ml-1 mr-1"
-							v-model="knobs.newValueOption"
-							:options="NEW_VALUE_OPTIONS"
-						/>
-					</template>
-					<span v-if="showNewValueOptions && showStartTimeOptions">and at the</span>
-					<template v-if="showStartTimeOptions">
-						<Dropdown
-							class="toolbar-button ml-1 mr-1"
-							v-model="knobs.startTimeOption"
-							:options="START_TIME_OPTIONS"
-						/>
-						<span>start time</span>
-					</template>
-					<span>.</span>
+					Find the
+					<Dropdown
+						class="toolbar-button"
+						v-model="knobs.optimizationType"
+						:options="OPTIMIZATION_TYPES"
+					/>
+					for the <strong>{{ knobs.intervention?.type }}</strong>
+					<strong>{{ knobs.intervention?.appliedTo }}</strong
+					>.
 				</p>
 			</section>
+			<div>
+				<section class="input-row">
+					<p>
+						The objective is the
+						<template v-if="showNewValueOptions">
+							<span>value closet to the</span>
+							<Dropdown
+								class="toolbar-button ml-1 mr-1"
+								v-model="knobs.newValueOption"
+								:options="NEW_VALUE_OPTIONS"
+							/>
+						</template>
+						<span v-if="showNewValueOptions && showStartTimeOptions">and at the</span>
+						<template v-if="showStartTimeOptions">
+							<Dropdown
+								class="toolbar-button ml-1 mr-1"
+								v-model="knobs.startTimeOption"
+								:options="START_TIME_OPTIONS"
+							/>
+							<span>start time</span>
+						</template>
+						<span>.</span>
+					</p>
+				</section>
 
-			<section v-if="showNewValueOptions">
-				<h6 class="pt-4, pb-3">New Value</h6>
-				<div class="input-row">
-					<div v-for="(objective, index) in newValueInputs" :key="index" class="label-and-input">
-						<div class="label-and-input">
-							<label :for="objective">{{ NEW_VALUE_OPTIONS[index] }}</label>
-							<tera-input type="number" v-model="knobs[objective]" />
+				<section v-if="showNewValueOptions">
+					<h6 class="pt-4, pb-3">New Value</h6>
+					<div class="input-row">
+						<div v-for="(objective, index) in newValueInputs" :key="index" class="label-and-input">
+							<div class="label-and-input">
+								<label :for="objective">{{ NEW_VALUE_OPTIONS[index] }}</label>
+								<tera-input type="number" v-model="knobs[objective]" />
+							</div>
 						</div>
 					</div>
-				</div>
-			</section>
+				</section>
 
-			<section v-if="showStartTimeOptions">
-				<h6 class="pt-4, pb-3">Start Time</h6>
-				<div class="input-row">
-					<div
-						v-for="(objective, index) in newStartTimeInputs"
-						:key="index"
-						class="label-and-input"
-					>
-						<div class="label-and-input">
-							<label :for="objective">{{ NEW_VALUE_OPTIONS[index] }}</label>
-							<tera-input type="number" v-model="knobs[objective]" />
+				<section v-if="showStartTimeOptions">
+					<h6 class="pt-4, pb-3">Start Time</h6>
+					<div class="input-row">
+						<div
+							v-for="(objective, index) in newStartTimeInputs"
+							:key="index"
+							class="label-and-input"
+						>
+							<div class="label-and-input">
+								<label :for="objective">{{ NEW_VALUE_OPTIONS[index] }}</label>
+								<tera-input type="number" v-model="knobs[objective]" />
+							</div>
 						</div>
 					</div>
-				</div>
-			</section>
-		</div>
-	</template>
-	<template v-else>
-		<p v-for="(staticIntervention, index) in staticInterventions" :key="index">
-			Set the <strong>{{ config.intervention?.type }}</strong>
-			<strong>{{ config.intervention?.appliedTo }}</strong> to a new value of
-			<strong>{{ staticIntervention.value }}</strong> day at start time
-			<strong>{{ staticIntervention.timestep }}</strong> day.
-		</p>
-	</template>
+				</section>
+			</div>
+		</template>
+		<template v-else>
+			<p v-for="(staticIntervention, index) in staticInterventions" :key="index">
+				Set the <strong>{{ config.intervention?.type }}</strong>
+				<strong>{{ config.intervention?.appliedTo }}</strong> to a new value of
+				<strong>{{ staticIntervention.value }}</strong> day at start time
+				<strong>{{ staticIntervention.timestep }}</strong> day.
+			</p>
+		</template>
+	</div>
 </template>
 
 <script setup lang="ts">
 import Dropdown from 'primevue/dropdown';
 import TeraInput from '@/components/widgets/tera-input.vue';
+import TeraToggleableEdit from '@/components/widgets/tera-toggleable-edit.vue';
+import InputSwitch from 'primevue/inputswitch';
 import { computed, ref } from 'vue';
 import { StaticIntervention } from '@/types/Types';
 import { InterventionPolicyGroup } from '@/components/workflow/ops/optimize-ciemss/optimize-ciemss-operation';
@@ -89,7 +105,12 @@ const props = defineProps<{
 	staticInterventions: StaticIntervention[];
 }>();
 
+const emit = defineEmits(['update-self']);
+
 const knobs = ref({
+	name: props.config.name ?? 'policy bounds',
+	isActive: props.config.isActive ?? false,
+	intervention: props.config.intervention,
 	optimizationType: props.config.optimizationType ?? 'new value',
 	newValueOption: props.config.newValueOption ?? 'initial guess',
 	startTimeOption: props.config.startTimeOption ?? 'earliest',
@@ -101,7 +122,10 @@ const knobs = ref({
 	initialGuess: props.config.initialGuess ?? 0
 });
 
-// const emit = defineEmits(['update-self']);
+const onEditName = (name: string) => {
+	knobs.value.name = name;
+	emit('update-self', knobs);
+};
 
 const OPTIMIZATION_TYPES = ['new value', 'start time', 'new value and start time'];
 const NEW_VALUE_OPTIONS = ['lower bound', 'upper bound', 'initial guess'];
@@ -150,5 +174,43 @@ const showNewValueOptions = computed(
 	display: flex;
 	flex-direction: column;
 	gap: var(--gap-2);
+}
+
+.form-header {
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	gap: var(--gap-4);
+	padding-bottom: 0.5rem;
+
+	& > *:first-child {
+		margin-right: auto;
+	}
+
+	& > * {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		gap: var(--gap-2);
+	}
+}
+
+.policy-group {
+	display: flex;
+	padding: 1rem 1rem 1rem 1.5rem;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+	gap: var(--gap-2);
+	border-radius: 0.375rem;
+	background: #fff;
+	border: 1px solid rgba(0, 0, 0, 0.08);
+	/* Shadow/medium */
+	box-shadow:
+		0 2px 4px -1px rgba(0, 0, 0, 0.06),
+		0 4px 6px -1px rgba(0, 0, 0, 0.08);
 }
 </style>
