204519191441d7df656ed352d8332792e61381ec
Derek Vince <dvince@uncharted.software>
2024-07-23 16:16:37 -0400

    Update packages/client/hmi-client/src/components/workflow/ops/model-config/tera-model-config-drilldown.vue
    lets make it weird
    
    Co-authored-by: Yohann Paris <github@yohannparis.com>

--- packages/client/hmi-client/src/components/workflow/ops/model-config/tera-model-config-drilldown.vue
+++ packages/client/hmi-client/src/components/workflow/ops/model-config/tera-model-config-drilldown.vue
@@ -517,8 +517,7 @@ const createConfiguration = async () => {
 		return;
 	}
 
-	knobs.value.transientModelConfig = cloneDeep(data);
-	state.transientModelConfig = knobs.value.transientModelConfig;
+	state.transientModelConfig = knobs.value.transientModelConfig = cloneDeep(data);
 	useToastService().success('', 'Created model configuration');
 	emit('append-output', {
 		type: ModelConfigOperation.outputs[0].type,
