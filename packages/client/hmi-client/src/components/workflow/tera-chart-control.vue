a7078fbd6d72a21aa0d3c6c873d05e500ababcae
Yohann Paris <github@yohannparis.com>
2024-07-23 13:36:27 -0400

    clean up

--- packages/client/hmi-client/src/components/workflow/tera-chart-control.vue
+++ packages/client/hmi-client/src/components/workflow/tera-chart-control.vue
@@ -1,23 +1,21 @@
 <template>
-	<div class="chart-control">
-		<div class="multiselect-container">
-			<MultiSelect
-				v-model="selectedVariable"
-				:options="variables"
-				placeholder="Select variables to display"
-				@update:model-value="updateSelectedVariable"
-				filter
-			>
-				<template v-slot:value>
-					<template v-for="(variable, index) in selectedVariable" :key="index">
-						<template v-if="index > 0">,&nbsp;</template>
-						<span> {{ variable }} </span>
-					</template>
+	<aside class="multiselect-container">
+		<MultiSelect
+			v-model="selectedVariable"
+			:options="variables"
+			placeholder="Select variables to display"
+			@update:model-value="updateSelectedVariable"
+			filter
+		>
+			<template v-slot:value>
+				<template v-for="(variable, index) in selectedVariable" :key="index">
+					<template v-if="index > 0">,&nbsp;</template>
+					<span> {{ variable }} </span>
 				</template>
-			</MultiSelect>
-			<Button v-if="showRemoveButton" title="Remove chart" icon="pi pi-trash" @click="$emit('remove')" rounded text />
-		</div>
-	</div>
+			</template>
+		</MultiSelect>
+		<Button v-if="showRemoveButton" title="Remove chart" icon="pi pi-trash" @click="$emit('remove')" rounded text />
+	</aside>
 </template>
 
 <script lang="ts" setup>
@@ -45,21 +43,13 @@ const updateSelectedVariable = () => {
 </script>
 
 <style scoped>
-.chart-control {
-	position: relative;
-	margin-top: var(--gap-2);
-	margin-bottom: var(--gap-2);
-}
-
-.multiselect-title {
-	font-size: smaller;
-	font-weight: var(--font-weight-semibold);
-}
 .multiselect-container {
 	display: flex;
 	flex-direction: row;
 	align-items: center;
 	gap: var(--gap-2);
 	justify-content: space-between;
+	position: relative;
+	margin: 0 var(--gap-2);
 }
 </style>
