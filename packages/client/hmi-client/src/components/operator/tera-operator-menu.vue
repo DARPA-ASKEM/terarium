1cfe33b8c9781c17272941e62a2f7f9fec4d29c1
Adrian <95376249+asylves1@users.noreply.github.com>
2024-07-31 15:25:29 -0400

    Enable menu button on hover over output port (#4329)

---
+++ packages/client/hmi-client/src/components/operator/tera-operator-menu.vue
@@ -0,0 +1,63 @@
+<template>
+	<Button
+		v-bind="$attrs"
+		type="button"
+		size="small"
+		@click="showMenu"
+		icon="pi pi-plus"
+		aria-haspopup="true"
+		aria-controls="overlay_menu"
+		severity="secondary"
+	/>
+	<Menu ref="menu" id="overlay_menu" :model="menuItems" :popup="true" />
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import Menu from 'primevue/menu';
+import Button from 'primevue/button';
+
+const isMenuShowing = ref<boolean>(false);
+
+const menu = ref();
+const menuItems = ref([
+	{
+		// label: "Options",
+		items: [
+			{
+				label: 'Configure Model',
+				command() {}
+			},
+			{
+				label: 'Stratify Model',
+				command() {}
+			},
+			{
+				label: 'Edit Model',
+				command() {}
+			}
+		]
+	}
+]);
+
+function showMenu(event) {
+	menu.value.show(event);
+	isMenuShowing.value = true;
+}
+</script>
+<style scoped>
+nav {
+	position: relative;
+}
+
+button {
+	outline: 1px solid var(--surface-border);
+	border-radius: var(--border-radius-medium);
+	box-shadow: var(--overlay-menu-shadow);
+	transition: box-shadow 80ms ease;
+
+	&:hover {
+		box-shadow: var(--overlay-menu-shadow-hover);
+		z-index: 2;
+	}
+}
+</style>
