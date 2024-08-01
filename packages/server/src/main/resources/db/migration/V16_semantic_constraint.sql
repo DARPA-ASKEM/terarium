2045b9c0d8d4df965c98e53e002a84d876abff5b
Daniel Chang <mwdchang@gmail.com>
2024-08-01 19:08:05 -0400

    update

--- packages/server/src/main/resources/db/migration/V16_semantic_constraint.sql
+++ packages/server/src/main/resources/db/migration/V16_semantic_constraint.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+ALTER TABLE model_configuration DROP COLUMN IF EXISTS calibration_run_id;
 ALTER TABLE semantic DROP CONSTRAINT semantic_type_check;
 ALTER TABLE semantic ADD CONSTRAINT semantic_type_check CHECK (type >= 0 AND type <= 3);
 
