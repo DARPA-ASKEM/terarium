73827e09d3e9076a925ec6f7f9e05073abd38ec9
Daniel Chang <mwdchang@gmail.com>
2024-08-14 08:00:38 -0400

    rework lint-staged to only work on staged files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,14 @@
+// module.exports = {
+//   '!(Types.ts).{ts,vue}': () => 'yarn run format',
+//   '*.java': () => 'npx prettier --write "**/*.java"'
+// }
+
 module.exports = {
-  '!(Types.ts).{ts,vue}': () => 'yarn run format',
-  '*.java': () => 'npx prettier --write "**/*.java"'
+  '!(Types.ts).{ts,vue}': [
+		'prettier --write',
+		'eslint --fix --cache'
+	],
+  '*.java': [
+		'prettier --write'
+	]
 }
