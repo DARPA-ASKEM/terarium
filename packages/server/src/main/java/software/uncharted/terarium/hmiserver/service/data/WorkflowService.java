5a13a1933e537af452a119e634fc114212f8d35b
Daniel Chang <mwdchang@gmail.com>
2024-08-16 14:42:54 -0400

    Do not mirror workflow (#4479)

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/data/WorkflowService.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/data/WorkflowService.java
@@ -22,41 +22,17 @@ import software.uncharted.terarium.hmiserver.service.s3.S3ClientService;
 import software.uncharted.terarium.hmiserver.utils.rebac.Schema;
 
 @Service
-public class WorkflowService extends TerariumAssetServiceWithSearch<Workflow, WorkflowRepository> {
+public class WorkflowService extends TerariumAssetServiceWithoutSearch<Workflow, WorkflowRepository> {
 
 	public WorkflowService(
 		final ObjectMapper objectMapper,
 		final Config config,
-		final ElasticsearchConfiguration elasticConfig,
-		final ElasticsearchService elasticService,
 		final ProjectService projectService,
 		final ProjectAssetService projectAssetService,
 		final S3ClientService s3ClientService,
 		final WorkflowRepository repository
 	) {
-		super(
-			objectMapper,
-			config,
-			elasticConfig,
-			elasticService,
-			projectService,
-			projectAssetService,
-			s3ClientService,
-			repository,
-			Workflow.class
-		);
-	}
-
-	@Override
-	@Observed(name = "function_profile")
-	protected String getAssetIndex() {
-		return elasticConfig.getWorkflowIndex();
-	}
-
-	@Override
-	@Observed(name = "function_profile")
-	public String getAssetAlias() {
-		return elasticConfig.getWorkflowAlias();
+		super(objectMapper, config, projectService, projectAssetService, repository, s3ClientService, Workflow.class);
 	}
 
 	@Override
