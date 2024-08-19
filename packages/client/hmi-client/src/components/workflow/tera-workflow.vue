095ee1c72d926df298563e428c5771c4af8f0d1d
Daniel Chang <mwdchang@gmail.com>
2024-08-18 23:15:27 -0400

    better save/update timing

--- packages/client/hmi-client/src/components/workflow/tera-workflow.vue
+++ packages/client/hmi-client/src/components/workflow/tera-workflow.vue
@@ -63,7 +63,9 @@
 					top: `${node.y}px`,
 					left: `${node.x}px`
 				}"
+				@dragstart="nodeDragging = true"
 				@dragging="(event) => updatePosition(node, event)"
+				@dragend="nodeDragging = false"
 			>
 				<tera-operator
 					ref="teraOperatorRefs"
@@ -256,6 +258,7 @@ let currentPortPosition: Position = { x: 0, y: 0 };
 let isMouseOverPort: boolean = false;
 let saveTimer: any = null;
 
+const nodeDragging = false;
 let workflowDirty: boolean = false;
 let startTime: number = 0;
 
@@ -880,10 +883,13 @@ const handleDrilldown = () => {
 };
 
 watch(
-	() => [props.assetId],
-	async () => {
-		// Save previous location
-		setLocalStorageTransform(wf.value.getId(), canvasTransform);
+	() => props.assetId,
+	async (newId, oldId) => {
+		if (newId !== oldId && oldId) {
+			// Save previous
+			if (workflowDirty) workflowService.updateWorkflow(wf.value.dump());
+			setLocalStorageTransform(wf.value.getId(), canvasTransform);
+		}
 
 		isRenamingWorkflow.value = false; // Closes rename input if opened in previous workflow
 		if (wf.value && workflowDirty) {
@@ -918,7 +924,7 @@ onMounted(() => {
 	document.addEventListener('mousemove', mouseUpdate);
 	window.addEventListener('beforeunload', unloadCheck);
 	saveTimer = setInterval(async () => {
-		if (workflowDirty && useProjects().hasEditPermission()) {
+		if (workflowDirty && useProjects().hasEditPermission() && nodeDragging === false) {
 			const updated = await workflowService.updateWorkflow(wf.value.dump());
 			wf.value.update(updated);
 			workflowDirty = false;
