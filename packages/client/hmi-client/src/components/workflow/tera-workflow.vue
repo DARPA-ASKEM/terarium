e6133fa4755bcba754960ad43accbb90f852e8f8
Daniel Chang <mwdchang@gmail.com>
2024-08-19 09:07:32 -0400

    lint

--- packages/client/hmi-client/src/components/workflow/tera-workflow.vue
+++ packages/client/hmi-client/src/components/workflow/tera-workflow.vue
@@ -258,7 +258,7 @@ let currentPortPosition: Position = { x: 0, y: 0 };
 let isMouseOverPort: boolean = false;
 let saveTimer: any = null;
 
-const nodeDragging = false;
+const nodeDragging: boolean = false;
 let workflowDirty: boolean = false;
 let startTime: number = 0;
 
