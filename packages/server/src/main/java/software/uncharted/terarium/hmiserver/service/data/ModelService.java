db7ac0a9d49e51ed599a6dcd16bcd2ad4e192dc8
Cole Blanchard <cblanchard@Cole-Blanchards-MacBook-Pro.local>
2024-08-19 15:12:37 -0400

    model service

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/data/ModelService.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/data/ModelService.java
@@ -140,7 +140,9 @@ public class ModelService extends TerariumAssetServiceWithSearch<Model, ModelRep
 				});
 		}
 
-		asset.setName(asset.getHeader().getName());
+		if (asset.getHeader().getName() != null) {
+			asset.setName(asset.getHeader().getName());
+		}
 		final Model created = super.createAsset(asset, projectId, hasWritePermission);
 
 		if (created.getPublicAsset() && !created.getTemporary()) {
@@ -173,7 +175,9 @@ public class ModelService extends TerariumAssetServiceWithSearch<Model, ModelRep
 		final UUID projectId,
 		final Schema.Permission hasWritePermission
 	) throws IOException, IllegalArgumentException {
-		asset.setName(asset.getHeader().getName());
+		if (asset.getHeader().getName() != null) {
+			asset.setName(asset.getHeader().getName());
+		}
 		final Optional<Model> updatedOptional = super.updateAsset(asset, projectId, hasWritePermission);
 		if (updatedOptional.isEmpty()) {
 			return Optional.empty();
