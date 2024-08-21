03303f4c4598126925778053289283cea6d90278
kbirk <birk.kevin@gmail.com>
2024-08-21 12:12:39 -0400

    Remove unused method.

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/repository/notification/NotificationGroupRepository.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/repository/notification/NotificationGroupRepository.java
@@ -37,30 +37,4 @@ public interface NotificationGroupRepository extends PSCrudRepository<Notificati
 		@Param("userId") @NotNull String userId,
 		@Param("createdOn") @NotNull Timestamp createdOn
 	);
-
-	@Query(
-		"""
-		SELECT ne
-			FROM NotificationEvent ne
-			WHERE ne.createdOn = (
-					SELECT MAX(ne2.createdOn)
-					FROM NotificationEvent ne2
-					WHERE ne2.notificationGroup.id = ne.notificationGroup.id
-			)
-			AND ne.notificationGroup.userId = :userId
-			 AND ne.notificationGroup.createdOn > :createdOn
-			AND EXISTS (
-					SELECT 1
-					FROM NotificationEvent ne3
-					WHERE ne3.notificationGroup.id = ne.notificationGroup.id
-					AND ne3.acknowledgedOn IS NULL
-			)
-			ORDER BY ne.notificationGroup.createdOn DESC
-
-		"""
-	)
-	List<NotificationGroup> findAllUnackedByUserReturnAllEventsLatestOnly(
-		@Param("userId") @NotNull String userId,
-		@Param("createdOn") @NotNull Timestamp createdOn
-	);
 }
