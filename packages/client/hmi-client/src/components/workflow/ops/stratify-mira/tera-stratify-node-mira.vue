cf8fc2f60f63a2f5533c204dacb7c1424e23c511
Shawn Yama <syama@uncharted.software>
2024-08-27 08:29:50 -0400

    Revert "show spinner earlier"
    This reverts commit 1913eef6c34f5c72783953fb9671dae7743d65df.

--- packages/client/hmi-client/src/components/workflow/ops/stratify-mira/tera-stratify-node-mira.vue
+++ packages/client/hmi-client/src/components/workflow/ops/stratify-mira/tera-stratify-node-mira.vue
@@ -1,25 +1,21 @@
 <template>
 	<section>
-		<tera-progress-spinner v-if="isStratifyInProgress" :font-size="2" is-centered>
-			Processing...
-		</tera-progress-spinner>
-		<tera-model-diagram v-else-if="outputPreview" :model="outputPreview" :feature-config="{ isPreview: true }" />
-		<tera-operator-placeholder v-else :node="node">
+		<tera-operator-placeholder v-if="!outputPreview" :node="node">
 			<template v-if="!node.inputs[0].value">Attach a model</template>
 		</tera-operator-placeholder>
+		<tera-model-diagram v-if="outputPreview" :model="outputPreview" :feature-config="{ isPreview: true }" />
 		<Button v-if="node.inputs[0].value" @click="emit('open-drilldown')" label="Edit" severity="secondary" outlined />
 	</section>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, computed } from 'vue';
+import { ref, watch } from 'vue';
 import { WorkflowNode } from '@/types/workflow';
 import TeraOperatorPlaceholder from '@/components/operator/tera-operator-placeholder.vue';
 import TeraModelDiagram from '@/components/model/petrinet/model-diagrams/tera-model-diagram.vue';
 import Button from 'primevue/button';
 import { getModel } from '@/services/model';
 import { Model } from '@/types/Types';
-import TeraProgressSpinner from '@/components/widgets/tera-progress-spinner.vue';
 import { StratifyOperationStateMira } from './stratify-mira-operation';
 
 const emit = defineEmits(['open-drilldown']);
@@ -28,8 +24,6 @@ const outputPreview = ref<Model | null>();
 const props = defineProps<{
 	node: WorkflowNode<StratifyOperationStateMira>;
 }>();
-
-const isStratifyInProgress = computed(() => props.node.state.isStratifyInProgress);
 
 watch(
 	() => props.node.active,
