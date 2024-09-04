45b240822e54fa2173b8975637a027ec50cae518
asylves1 <asylvester@uncharted.software>
2024-09-04 11:35:44 -0400

    add project name

--- packages/client/hmi-client/src/services/notificationEventHandlers.ts
+++ packages/client/hmi-client/src/services/notificationEventHandlers.ts
@@ -19,6 +19,7 @@ import {
 } from '@/types/common';
 import { snakeToCapitalSentence } from '@/utils/text';
 import { ProjectPages } from '@/types/Project';
+import { useProjects } from '@/composables/project';
 import { getDocumentAsset } from './document-assets';
 import { getWorkflow } from './workflow';
 
@@ -135,9 +136,13 @@ export const createNotificationEventHandlers = (notificationItems: Ref<Notificat
 
 	// Register handlers for each client event type
 	registerHandler<CloneProjectStatusUpdate>(ClientEventType.CloneProject, (event, created) => {
-		console.log('event', event);
 		created.assetId = event.data.data.projectId;
 		created.typeDisplayName = 'Cloned Project';
+		useProjects()
+			.get(created.assetId)
+			.then((project) => {
+				created.sourceName = project?.name ?? 'Source Project';
+			});
 	});
 
 	registerHandler<ExtractionStatusUpdate>(ClientEventType.ExtractionPdf, (event, created) => {
