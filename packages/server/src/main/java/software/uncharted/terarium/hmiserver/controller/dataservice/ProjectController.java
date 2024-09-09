a1bbb6b8b9fa48838e480c3183f1810d391b1033
Adrian <95376249+asylves1@users.noreply.github.com>
2024-09-09 15:01:45 -0400

    4514 feat refine the fork a project workflow (#4710)

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/ProjectController.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/controller/dataservice/ProjectController.java
@@ -533,7 +533,7 @@ public class ProjectController {
 	public ResponseEntity<Project> copyProject(@PathVariable("id") final UUID id) {
 		projectService.checkPermissionCanRead(currentUserService.get().getId(), id);
 		// time the progress takes to reach each subsequent half.
-		final Double HALFTIME_SECONDS = 2.0;
+		final Double HALFTIME_SECONDS = 1.0;
 
 		Future<Project> project;
 		Project clonedProject;
@@ -541,16 +541,16 @@ public class ProjectController {
 		final String userId = currentUserService.get().getId();
 		final String userName = userService.getById(userId).getName();
 
-		try {
-			final NotificationGroupInstance<Properties> notificationInterface = new NotificationGroupInstance<Properties>(
-				clientEventService,
-				notificationService,
-				ClientEventType.CLONE_PROJECT,
-				null,
-				new Properties(id),
-				HALFTIME_SECONDS
-			);
+		final NotificationGroupInstance<Properties> notificationInterface = new NotificationGroupInstance<Properties>(
+			clientEventService,
+			notificationService,
+			ClientEventType.CLONE_PROJECT,
+			null,
+			new Properties(id),
+			HALFTIME_SECONDS
+		);
 
+		try {
 			notificationInterface.sendMessage("Cloning the Project...");
 
 			project = executor.submit(() -> {
@@ -562,7 +562,6 @@ public class ProjectController {
 				log.info("Cloned...");
 				return cloneProject;
 			});
-			notificationInterface.sendFinalMessage("Cloning complete");
 			clonedProject = project.get();
 		} catch (ExecutionException e) {
 			log.error("Execution Exception exporting project", e);
@@ -595,6 +594,7 @@ public class ProjectController {
 				messages.get("rebac.relationship-already-exists")
 			);
 		}
+		notificationInterface.sendFinalMessage("Cloning complete");
 		return ResponseEntity.status(HttpStatus.CREATED).body(clonedProject);
 	}
 
