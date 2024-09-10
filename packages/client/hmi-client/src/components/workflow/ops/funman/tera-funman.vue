6e44a0773346a4dd9cdb0e0efa32b4c80632ed97
Brittany Kondo <bkondo@brittanys-mbp.home>
2024-09-09 20:25:11 -0400

    add accordion tabs

--- packages/client/hmi-client/src/components/workflow/ops/funman/tera-funman.vue
+++ packages/client/hmi-client/src/components/workflow/ops/funman/tera-funman.vue
@@ -6,102 +6,102 @@
 		@on-close-clicked="emit('close')"
 		@update-state="(state: any) => emit('update-state', state)"
 	>
-		<div :tabName="FunmanTabs.Wizard" class="ml-4 mr-2 mt-3">
+		<!---<template #sidebar>
+			<tera-slider-panel v-model:is-open="isSidebarOpen" header="Validate configuration settings" content-width="360px">
+				<template #content>
+
+				</template>
+			</tera-slider-panel>
+		</template>-->
+		<div :tabName="FunmanTabs.Wizard" class="px-3 wizard-section">
 			<tera-drilldown-section>
 				<template #header-controls-right>
-					<Button
-						:loading="showSpinner"
-						class="run-button"
-						label="Run"
-						icon="pi pi-play"
-						@click="runMakeQuery"
-						size="large"
-					/>
+					<Button :loading="showSpinner" class="run-button" label="Run" icon="pi pi-play" @click="runMakeQuery" />
 				</template>
 				<main>
-					<h5>
-						Set validation parameters
-						<i class="pi pi-info-circle" v-tooltip="validateParametersToolTip" />
-					</h5>
-					<p class="secondary-text mt-1">The validator will use these parameters to execute the sanity checks.</p>
-					<div class="section-row timespan">
-						<div class="w-full">
-							<label>Start time</label>
-							<tera-input-number v-model="knobs.currentTimespan.start" />
-						</div>
-						<div class="w-full">
-							<label>End time</label>
-							<tera-input-number v-model="knobs.currentTimespan.end" />
-						</div>
-						<div class="w-full">
-							<label>Number of steps</label>
-							<tera-input-number v-model="knobs.numberOfSteps" />
-						</div>
-					</div>
-					<tera-input-text :disabled="true" class="timespan-list mb-2" v-model="requestStepListString" />
-					<template v-if="showAdditionalOptions">
-						<div>
-							<label>Tolerance</label>
-							<div class="input-tolerance fadein animation-ease-in-out animation-duration-350">
-								<tera-input-number v-model="knobs.tolerance" />
-								<Slider v-model="knobs.tolerance" :min="0" :max="1" :step="0.01" class="w-full mr-2" />
-							</div>
-						</div>
-						<div class="section-row fadein animation-duration-600">
-							<!-- This will definitely require a proper tool tip. -->
-							<label class="w-auto mr-2">Select parameters of interest <i class="pi pi-info-circle" /></label>
-							<MultiSelect
-								ref="columnSelect"
-								:modelValue="variablesOfInterest"
-								:options="requestParameters.map((d: any) => d.name)"
-								:show-toggle-all="false"
-								class="w-auto"
-								@update:modelValue="onToggleVariableOfInterest"
-								:maxSelectedLabels="1"
-								placeholder="Select variables"
+					<Accordion multiple :active-index="[0, 1]">
+						<AccordionTab header="Model checks">
+							<!---<i class="pi pi-info-circle" v-tooltip="validateParametersToolTip" />-->
+							<p class="mt-1">Model configurations will be tested against these constraints.</p>
+							<tera-compartment-constraint :variables="modelStates" :mass="mass" />
+							<tera-constraint-group-form
+								v-for="(cfg, index) in node.state.constraintGroups"
+								:key="selectedOutputId + ':' + index"
+								:config="cfg"
+								:index="index"
+								:model-states="modelStates"
+								:model-parameters="modelParameters"
+								@delete-self="deleteConstraintGroupForm"
+								@update-self="updateConstraintGroupForm"
 							/>
-						</div>
-					</template>
-					<Button
-						text
-						icon="pi pi-eye"
-						label="Show additional options"
-						size="small"
-						v-if="!showAdditionalOptions"
-						@click="toggleAdditonalOptions"
-					/>
-					<Button
-						text
-						icon="pi pi-eye-slash"
-						label="Hide additional options"
-						size="small"
-						v-if="showAdditionalOptions"
-						@click="toggleAdditonalOptions"
-					/>
-
-					<div class="spacer">
-						<h5>Add sanity checks</h5>
-						<p class="secondary-text mt-1">Model configurations will be tested against these constraints.</p>
-					</div>
-					<tera-compartment-constraint :variables="modelStates" :mass="mass" />
-					<tera-constraint-group-form
-						v-for="(cfg, index) in node.state.constraintGroups"
-						:key="selectedOutputId + ':' + index"
-						:config="cfg"
-						:index="index"
-						:model-states="modelStates"
-						:model-parameters="modelParameters"
-						@delete-self="deleteConstraintGroupForm"
-						@update-self="updateConstraintGroupForm"
-					/>
-					<Button
-						class="add-constraint-spacer"
-						text
-						icon="pi pi-plus"
-						label="Add another constraint"
-						size="small"
-						@click="addConstraintForm"
-					/>
+							<Button
+								class="add-constraint-spacer"
+								text
+								icon="pi pi-plus"
+								label="Add new check"
+								size="small"
+								@click="addConstraintForm"
+							/>
+						</AccordionTab>
+						<AccordionTab header="Settings">
+							<!---<i class="pi pi-info-circle" v-tooltip="validateParametersToolTip" />-->
+							<p class="mt-1">The validator will use these parameters to execute the sanity checks.</p>
+							<div class="section-row timespan">
+								<div class="w-full">
+									<label>Start time</label>
+									<tera-input-number v-model="knobs.currentTimespan.start" />
+								</div>
+								<div class="w-full">
+									<label>End time</label>
+									<tera-input-number v-model="knobs.currentTimespan.end" />
+								</div>
+								<div class="w-full">
+									<label>Number of steps</label>
+									<tera-input-number v-model="knobs.numberOfSteps" />
+								</div>
+							</div>
+							<tera-input-text :disabled="true" class="timespan-list mb-2" v-model="requestStepListString" />
+							<template v-if="showAdditionalOptions">
+								<div>
+									<label>Tolerance</label>
+									<div class="input-tolerance fadein animation-ease-in-out animation-duration-350">
+										<tera-input-number v-model="knobs.tolerance" />
+										<Slider v-model="knobs.tolerance" :min="0" :max="1" :step="0.01" class="w-full mr-2" />
+									</div>
+								</div>
+								<div class="section-row fadein animation-duration-600">
+									<!-- This will definitely require a proper tool tip. -->
+									<label class="w-auto mr-2">Select parameters of interest <i class="pi pi-info-circle" /></label>
+									<MultiSelect
+										ref="columnSelect"
+										:modelValue="variablesOfInterest"
+										:options="requestParameters.map((d: any) => d.name)"
+										:show-toggle-all="false"
+										class="w-auto"
+										@update:modelValue="onToggleVariableOfInterest"
+										:maxSelectedLabels="1"
+										placeholder="Select variables"
+									/>
+								</div>
+							</template>
+							<Button
+								text
+								icon="pi pi-eye"
+								label="Show additional options"
+								size="small"
+								v-if="!showAdditionalOptions"
+								@click="toggleAdditonalOptions"
+							/>
+							<Button
+								text
+								icon="pi pi-eye-slash"
+								label="Hide additional options"
+								size="small"
+								v-if="showAdditionalOptions"
+								@click="toggleAdditonalOptions"
+							/>
+						</AccordionTab>
+					</Accordion>
 				</main>
 			</tera-drilldown-section>
 		</div>
@@ -150,11 +150,14 @@ import TeraInputText from '@/components/widgets/tera-input-text.vue';
 import TeraInputNumber from '@/components/widgets/tera-input-number.vue';
 import Slider from 'primevue/slider';
 import MultiSelect from 'primevue/multiselect';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
 
 import TeraDrilldown from '@/components/drilldown/tera-drilldown.vue';
 import TeraDrilldownPreview from '@/components/drilldown/tera-drilldown-preview.vue';
 import TeraDrilldownSection from '@/components/drilldown/tera-drilldown-section.vue';
 import TeraOperatorPlaceholder from '@/components/operator/tera-operator-placeholder.vue';
+// import TeraSliderPanel from '@/components/widgets/tera-slider-panel.vue';
 
 import TeraProgressSpinner from '@/components/widgets/tera-progress-spinner.vue';
 
@@ -180,11 +183,12 @@ enum FunmanTabs {
 	Notebook = 'Notebook'
 }
 const toast = useToastService();
-const validateParametersToolTip =
+/* const validateParametersToolTip =
 	'Validate the configuration of the model using functional model analysis (FUNMAN). \n \n The parameter space regions defined by the model configuration are evaluated to satisfactory or unsatisfactory depending on whether they generate model outputs that are within a given set of time-dependent constraints';
-
+*/
 const showSpinner = ref(false);
 const showAdditionalOptions = ref(false);
+// const isSidebarOpen = ref(true);
 
 interface BasicKnobs {
 	tolerance: number;
@@ -625,5 +629,9 @@ div.section-row.timespan > div > span {
 
 .run-button {
 	margin-right: auto;
+}
+
+.wizard-section {
+	background-color: var(--surface-100);
 }
 </style>
