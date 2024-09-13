433fe23262390d52401b00f00b33a20f69632d82
Daniel Chang <mwdchang@gmail.com>
2024-09-13 15:51:26 -0400

    remove unused tera-drilldown emit event (#4803)

--- packages/client/hmi-client/src/components/drilldown/tera-drilldown.vue
+++ packages/client/hmi-client/src/components/drilldown/tera-drilldown.vue
@@ -149,8 +149,7 @@ const props = defineProps<{
 	hideDropdown?: boolean;
 }>();
 
-// FIXME: update-output-port unused
-const emit = defineEmits(['on-close-clicked', 'update-state', 'update:selection', 'update-output-port']);
+const emit = defineEmits(['on-close-clicked', 'update-state', 'update:selection']);
 
 const slots = useSlots();
 
