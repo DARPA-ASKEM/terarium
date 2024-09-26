5b5c86934190d7a5cfe0faaff04143b1a203e946
Tom Szendrey <T.Szendrey@hotmail.com>
2024-09-26 10:15:38 -0400

    adding validateIntervention

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/simulationservice/interventions/Intervention.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/simulationservice/interventions/Intervention.java
@@ -3,10 +3,12 @@ package software.uncharted.terarium.hmiserver.models.simulationservice.intervent
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import software.uncharted.terarium.hmiserver.annotations.TSModel;
 
 @Data
 @TSModel
+@Slf4j
 public class Intervention {
 
 	private String name;
@@ -31,5 +33,27 @@ public class Intervention {
 			}
 		}
 		return intervention;
+	}
+
+	// Check for no negative timesteps
+	// Check for no duplicate time + appliedTo pairs in static interventions
+	public Boolean validateIntervention() {
+		for (int i = 0; i < this.staticInterventions.size(); i++) {
+			if (this.staticInterventions.get(i).getTimestep().intValue() < 0) {
+				log.warn("Timestep is less than 0.");
+				return false;
+			}
+			for (int j = 0; j < this.staticInterventions.size(); j++) {
+				final String appliedToOne = this.staticInterventions.get(i).getAppliedTo();
+				final Number timeOne = this.staticInterventions.get(i).getTimestep();
+				final String appliedToTwo = this.staticInterventions.get(j).getAppliedTo();
+				final Number timeTwo = this.staticInterventions.get(j).getTimestep();
+				if (i != j && appliedToOne == appliedToTwo && timeOne == timeTwo) {
+					log.warn("Duplicate appliedto and time pairs.");
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
