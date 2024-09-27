823cdfdc4cf264b350bfd0a3020b45ab1a0a3e3f
Jaehwan Ryu <jryu@uncharted.software>
2024-09-27 12:22:50 -0400

    Show default charts for variables, errors and parameters on calibrate run (#4931)

--- packages/client/hmi-client/src/components/workflow/ops/calibrate-ciemss/tera-calibrate-ciemss-drilldown.vue
+++ packages/client/hmi-client/src/components/workflow/ops/calibrate-ciemss/tera-calibrate-ciemss-drilldown.vue
@@ -1,3 +1,4 @@
+t d
 <template>
 	<tera-drilldown
 		:node="node"
@@ -423,6 +424,7 @@ import {
 	ModelConfiguration,
 	ChartAnnotation,
 	InterventionPolicy,
+	ModelParameter,
 	AssetType
 } from '@/types/Types';
 import { CiemssPresetTypes, DrilldownTabs, ChartSetting, ChartSettingType } from '@/types/common';
@@ -458,6 +460,7 @@ import TeraSaveSimulationModal from '@/components/project/tera-save-simulation-m
 import { useClientEvent } from '@/composables/useClientEvent';
 import { getInterventionPolicyById } from '@/services/intervention-policy';
 import TeraInterventionSummaryCard from '@/components/workflow/ops/simulate-ciemss/tera-intervention-summary-card.vue';
+import { getParameters } from '@/model-representation/service';
 import type { CalibrationOperationStateCiemss } from './calibrate-operation';
 import { renameFnGenerator, mergeResults, getErrorData } from './calibrate-utils';
 
@@ -525,6 +528,8 @@ const odeSolverOptionsTooltip: string = 'TODO';
 
 // Model variables checked in the model configuration will be options in the mapping dropdown
 const modelStateOptions = ref<any[] | undefined>();
+
+const modelParameters = ref<ModelParameter[]>([]);
 
 const isOutputSettingsPanelOpen = ref(true);
 const activeChartSettings = ref<ChartSetting | null>(null);
@@ -840,6 +845,30 @@ const updateLossChartSpec = (data: string | Record<string, any>[]) => {
 	);
 };
 
+const initDefaultChartSettings = (state: CalibrationOperationStateCiemss) => {
+	// Initialize default selected chart settings when chart settings are not set yet. Return if chart settings are already set.
+	if (Array.isArray(state.chartSettings)) return;
+	const defaultSelectedParam = modelParameters.value.filter((p) => !!p.distribution).map((p) => p.id);
+	const mappedModelVariables = mapping.value
+		.filter((c) => ['state', 'observable'].includes(modelPartTypesMap.value[c.modelVariable]))
+		.map((c) => c.modelVariable);
+	state.chartSettings = updateChartSettingsBySelectedVariables(
+		[],
+		ChartSettingType.VARIABLE_COMPARISON,
+		mappedModelVariables
+	);
+	state.chartSettings = updateChartSettingsBySelectedVariables(
+		state.chartSettings,
+		ChartSettingType.ERROR_DISTRIBUTION,
+		mappedModelVariables
+	);
+	state.chartSettings = updateChartSettingsBySelectedVariables(
+		state.chartSettings,
+		ChartSettingType.DISTRIBUTION_COMPARISON,
+		defaultSelectedParam
+	);
+};
+
 const runCalibrate = async () => {
 	if (!modelConfigId.value || !datasetId.value || !currentDatasetFileName.value) return;
 
@@ -885,16 +914,7 @@ const runCalibrate = async () => {
 		state.currentProgress = 0;
 		state.inProgressForecastId = '';
 		state.inProgressPreForecastId = '';
-
-		// show selected input settings in the charts & output panel
-		state.chartSettings = updateChartSettingsBySelectedVariables(
-			chartSettings.value,
-			ChartSettingType.VARIABLE_COMPARISON,
-			mapping.value
-				.filter((c) => ['state', 'observable'].includes(modelPartTypesMap.value[c.modelVariable]))
-				.map((c) => c.modelVariable)
-		);
-
+		initDefaultChartSettings(state);
 		emit('update-state', state);
 	}
 };
@@ -997,11 +1017,12 @@ async function getAutoMapping() {
 
 const initialize = async () => {
 	// Model configuration input
-	const { modelConfiguration, modelOptions, modelPartUnits, modelPartTypes } = await setupModelInput(
+	const { model, modelConfiguration, modelOptions, modelPartUnits, modelPartTypes } = await setupModelInput(
 		modelConfigId.value
 	);
 	modelConfig.value = modelConfiguration;
 	modelStateOptions.value = modelOptions;
+	modelParameters.value = model ? getParameters(model) : [];
 	modelVarUnits.value = modelPartUnits ?? {};
 	modelPartTypesMap.value = modelPartTypes ?? {};
 
