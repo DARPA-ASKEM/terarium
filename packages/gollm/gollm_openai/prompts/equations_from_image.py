15e658acde365bb491e213873ea39a973935e507
David Gauldie <dgauldie@uncharted.software>
2024-09-30 13:56:03 -0400

    move gollm scripts into terarium (#4968)

---
+++ packages/gollm/gollm_openai/prompts/equations_from_image.py
@@ -0,0 +1,8 @@
+EQUATIONS_FROM_IMAGE_PROMPT = """
+This image will contain one or more equations. For each equation, create a LaTeX representation of the equation.
+Multiple equations will probably be vertically separated, they may be enumerated by numbers or letters. Do not include these enumerations in the LaTeX equation.
+
+Do not respond in full sentences; only create a JSON object that satisfies the JSON schema specified in the response format.
+
+Answer:
+"""
