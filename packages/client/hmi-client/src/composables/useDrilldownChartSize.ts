ffc360576a7121fe243ce339455e205ef7227292
Jaehwan Ryu <jryu@uncharted.software>
2024-10-01 18:33:52 -0400

    Updated comments

--- packages/client/hmi-client/src/composables/useDrilldownChartSize.ts
+++ packages/client/hmi-client/src/composables/useDrilldownChartSize.ts
@@ -6,7 +6,8 @@ import { useResizeObserver } from './useResizeObserver';
 /**
  * A Vue composable that provides reactive chart size based on the container element's size.
  *
- * @param {Ref<HTMLElement>} containerElement - A reference to the container HTML element.
+ * @param {Ref<HTMLElement | null>} containerElement - A reference to the container HTML element.
+ * @param {number} [debounceDelay=100] - The debounce delay in milliseconds for resize events.
  * @returns {Ref<{ width: number, height: number }>} - A reactive reference to the chart size.
  */
 export function useDrilldownChartSize(containerElement: Ref<HTMLElement | null>, debounceDelay = 100) {
