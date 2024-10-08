0e6887e1b6668efa3448cd0112b84e2e3ae0c72b
Tom Szendrey <T.Szendrey@hotmail.com>
2024-10-08 10:15:50 -0400

    adding box shadow - thanks Yohann

--- packages/client/hmi-client/src/components/drilldown/tera-notebook-output.vue
+++ packages/client/hmi-client/src/components/drilldown/tera-notebook-output.vue
@@ -21,9 +21,14 @@ const emit = defineEmits(['close']);
 
 <style scoped>
 .container {
-	background-color: var(--surface-200);
+	background-color: var(--surface-100);
 	padding: var(--gap-small);
 	border-radius: var(--border-radius);
+	border: 1px solid var(--surface-border-light);
+	/* Shadow/medium */
+	box-shadow:
+		0 2px 4px -1px rgba(0, 0, 0, 0.06),
+		0 4px 6px -1px rgba(0, 0, 0, 0.08);
 }
 
 h6 {
