702336b14994002e976a35feeaa687ab76f8e766
dvince <dvince@uncharted.software>
2024-10-23 12:09:42 -0400

    reversion

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/TerariumAsset.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/TerariumAsset.java
@@ -121,8 +121,7 @@ public abstract class TerariumAsset extends TerariumEntity {
 			"temporary",
 			"publicAsset",
 			"fileNames",
-			"userId",
-			"modifiers"
+			"userId"
 		};
 		List<String> deleteFieldsList = new ArrayList<>(Arrays.asList(deleteFields));
 		if (keepFields != null) {
