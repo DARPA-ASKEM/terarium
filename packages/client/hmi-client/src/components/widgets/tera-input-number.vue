4ea38c27ebc42e26b12d4f8528b52893a1a0637b
mloppie <mloppie@uncharted.software>
2024-10-24 15:05:29 -0400

    show negative timesteps as invalid in the intervention policy drilldown (#5279)

--- packages/client/hmi-client/src/components/widgets/tera-input-number.vue
+++ packages/client/hmi-client/src/components/widgets/tera-input-number.vue
@@ -34,6 +34,7 @@ const props = defineProps<{
 	disabled?: boolean;
 	placeholder?: string;
 	autoWidth?: boolean;
+	invalidateNegative?: boolean;
 }>();
 const emit = defineEmits(['update:model-value', 'focusout']);
 const inputField = ref<HTMLInputElement | null>(null);
@@ -63,7 +64,7 @@ const updateValue = (event: Event) => {
 	const value = (event.target as HTMLInputElement).value;
 	maskedValue.value = value;
 	const numValue = toNumber(value);
-	error.value = isNaN(numValue) ? 'Invalid number' : '';
+	error.value = isNaN(numValue) || (props.invalidateNegative && numValue < 0) ? 'Invalid number' : '';
 };
 
 function displayValue() {
