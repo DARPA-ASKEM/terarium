8b1929aac08dd7f68da864167a88aca2cfdd5204
Tom Szendrey <T.Szendrey@hotmail.com>
2024-11-06 14:54:42 -0500

    Ensemble Calibrate - Intermediate Results (#5401)

--- packages/client/hmi-client/src/components/workflow/ops/calibrate-ensemble-ciemss/tera-calibrate-ensemble-node-ciemss.vue
+++ packages/client/hmi-client/src/components/workflow/ops/calibrate-ensemble-ciemss/tera-calibrate-ensemble-node-ciemss.vue
@@ -1,6 +1,12 @@
 <template>
 	<main>
 		<template v-if="!inProgressCalibrationId && !inProgressForecastId && runResults && csvAsset">
+			<vega-chart
+				v-if="!_.isEmpty(lossValues)"
+				:are-embed-actions-visible="false"
+				:visualization-spec="lossChartSpec"
+			/>
+
 			<tera-simulate-chart
 				v-for="(config, index) of props.node.state.chartConfigs"
 				:key="index"
@@ -22,7 +28,9 @@
 			:font-size="2"
 			is-centered
 			style="height: 100%"
-		/>
+		>
+			{{ node.state.currentProgress }}%
+		</tera-progress-spinner>
 
 		<Button v-if="areInputsFilled" label="Edit" @click="emit('open-drilldown')" severity="secondary" outlined />
 		<tera-operator-placeholder v-else :node="node">
@@ -32,7 +40,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, shallowRef, watch } from 'vue';
+import { computed, ref, shallowRef, watch, onMounted } from 'vue';
 import _ from 'lodash';
 import Button from 'primevue/button';
 import TeraOperatorPlaceholder from '@/components/operator/tera-operator-placeholder.vue';
@@ -47,14 +55,15 @@ import {
 import { setupCsvAsset } from '@/services/calibrate-workflow';
 import { chartActionsProxy, nodeMetadata, nodeOutputLabel } from '@/components/workflow/util';
 import { logger } from '@/utils/logger';
-
 import { Poller, PollerState } from '@/api/api';
 import type { WorkflowNode } from '@/types/workflow';
 import { WorkflowPortStatus } from '@/types/workflow';
-import type { CsvAsset, EnsembleSimulationCiemssRequest, Dataset } from '@/types/Types';
+import type { CsvAsset, EnsembleSimulationCiemssRequest, Dataset, Simulation } from '@/types/Types';
 import type { RunResults } from '@/types/SimulateConfig';
 import { getDataset } from '@/services/dataset';
+import VegaChart from '@/components/widgets/VegaChart.vue';
 import type { CalibrateEnsembleCiemssOperationState } from './calibrate-ensemble-ciemss-operation';
+import { updateLossChartSpec, getLossValuesFromSimulation } from './calibrate-ensemble-util';
 
 const props = defineProps<{
 	node: WorkflowNode<CalibrateEnsembleCiemssOperationState>;
@@ -67,6 +76,9 @@ const csvAsset = shallowRef<CsvAsset | undefined>(undefined);
 const areInputsFilled = computed(() => props.node.inputs[0].value && props.node.inputs[1].value);
 const inProgressCalibrationId = computed(() => props.node.state.inProgressCalibrationId);
 const inProgressForecastId = computed(() => props.node.state.inProgressForecastId);
+const lossValues = ref<{ [key: string]: number }[]>([]);
+const lossChartSpec = ref();
+const lossChartSize = { width: 180, height: 120 };
 
 const chartProxy = chartActionsProxy(props.node, (state: CalibrateEnsembleCiemssOperationState) => {
 	emit('update-state', state);
@@ -77,11 +89,31 @@ const pollResult = async (runId: string) => {
 	poller
 		.setInterval(3000)
 		.setThreshold(300)
-		.setPollAction(async () => pollAction(runId));
+		.setPollAction(async () => pollAction(runId))
+		.setProgressAction((data: Simulation) => {
+			if (data?.updates?.length) {
+				lossValues.value = data?.updates
+					.sort((a, b) => a.data.progress - b.data.progress)
+					.map((d, i) => ({
+						iter: i,
+						loss: d.data.loss
+					}));
+				lossChartSpec.value = updateLossChartSpec(lossValues.value, lossChartSize);
+			}
+			if (runId === props.node.state.inProgressCalibrationId && data.updates.length > 0) {
+				const checkpoint = _.last(data.updates);
+				if (checkpoint) {
+					const state = _.cloneDeep(props.node.state);
+					state.currentProgress = +((100 * checkpoint.data.progress) / state.extra.numIterations).toFixed(2);
+					emit('update-state', state);
+				}
+			}
+		});
 	const pollerResults = await poller.start();
 
 	if (pollerResults.state === PollerState.Cancelled) {
 		const state = _.cloneDeep(props.node.state);
+		state.currentProgress = 0;
 		state.inProgressForecastId = '';
 		state.inProgressCalibrationId = '';
 		emit('update-state', state);
@@ -96,6 +128,12 @@ const pollResult = async (runId: string) => {
 	}
 	return pollerResults;
 };
+
+// Init loss chart
+onMounted(async () => {
+	lossValues.value = await getLossValuesFromSimulation(props.node.state.calibrationId);
+	lossChartSpec.value = await updateLossChartSpec(lossValues.value, lossChartSize);
+});
 
 watch(
 	() => props.node.state.inProgressCalibrationId,
@@ -143,6 +181,7 @@ watch(
 			const state = _.cloneDeep(props.node.state);
 
 			state.chartConfigs = [[]];
+			state.currentProgress = 0;
 			state.inProgressForecastId = '';
 			state.forecastRunId = id;
 			emit('update-state', state);
