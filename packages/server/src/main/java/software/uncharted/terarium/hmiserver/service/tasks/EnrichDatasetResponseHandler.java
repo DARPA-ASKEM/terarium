82fa1daf506d929e7651549adb2e54c9ea900157
Yohann Paris <github@yohannparis.com>
2024-11-11 14:28:22 -0500

    Update EnrichDatasetResponseHandler.java

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/EnrichDatasetResponseHandler.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/EnrichDatasetResponseHandler.java
@@ -1,8 +1,10 @@
 package software.uncharted.terarium.hmiserver.service.tasks;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.StreamSupport;
@@ -11,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import software.uncharted.terarium.hmiserver.models.dataservice.dataset.Dataset;
+import software.uncharted.terarium.hmiserver.models.dataservice.dataset.DatasetColumn;
 import software.uncharted.terarium.hmiserver.models.task.TaskResponse;
 import software.uncharted.terarium.hmiserver.service.data.DatasetService;
 
@@ -41,7 +44,9 @@ public class EnrichDatasetResponseHandler extends TaskResponseHandler {
 
 	@Data
 	private static class Enrichment {
-		// TODO
+
+		JsonNode card;
+		JsonNode columns;
 	}
 
 	@Data
@@ -77,83 +82,24 @@ public class EnrichDatasetResponseHandler extends TaskResponseHandler {
 			// Update the dataset with the new card
 			((ObjectNode) dataset.getMetadata()).set("dataCard", response.response.card);
 
-			// Update the dataset with the new descriptions
-			//			for (final DescriptionsAndUnits state : response.response.states) {
-			//				dataset
-			//					.getInitials()
-			//					.stream()
-			//					.filter(initial -> initial.getTarget().equalsIgnoreCase(state.id))
-			//					.findFirst()
-			//					.ifPresent(initial -> {
-			//						initial.setDescription(state.description);
-			//						StreamSupport.stream(dataset.getModel().get("states").spliterator(), false)
-			//							.filter(stateNode -> stateNode.path("id").asText().equals(state.id))
-			//							.findFirst()
-			//							.ifPresent(stateNode -> {
-			//								((ObjectNode) stateNode).put("description", state.description);
-			//								if (state.units != null) {
-			//									((ObjectNode) stateNode).put("units", objectMapper.createObjectNode());
-			//									((ObjectNode) stateNode.get("units")).put("expression", state.units.expression);
-			//									((ObjectNode) stateNode.get("units")).put("expression_mathml", state.units.expressionMathml);
-			//								}
-			//							});
-			//					});
-			// 			}
+			// Update the dataset columns with the new descriptions
+			for (final JsonNode enrichedColumn : response.response.columns) {
+				final String name = enrichedColumn.get("name").asText();
+				final String description = enrichedColumn.get("description").asText();
+				final String unit = enrichedColumn.get("unit").asText();
+				final String concept = enrichedColumn.get("concept").asText();
 
-			//			for (final DescriptionsAndUnits parameter : response.response.parameters) {
-			//				dataset
-			//					.getParameters()
-			//					.stream()
-			//					.filter(param -> param.getId().equalsIgnoreCase(parameter.id))
-			//					.findFirst()
-			//					.ifPresent(param -> {
-			//						param.setDescription(parameter.description);
-			//						if (parameter.units != null) {
-			//							param.setUnits(
-			//								new ModelUnit()
-			//									.setExpression(parameter.units.expression)
-			//									.setExpressionMathml(parameter.units.expressionMathml)
-			//							);
-			//						}
-			//					});
-			//			}
+				dataset
+					.getColumns()
+					.stream()
+					.filter(column -> column.getName().equalsIgnoreCase(name))
+					.findFirst()
+					.ifPresent(column -> {
+						column.setDescription(description);
+					});
+			}
 
-			//			for (final DescriptionsAndUnits observable : response.response.observables) {
-			//				dataset
-			//					.getObservables()
-			//					.stream()
-			//					.filter(observe -> observe.getId().equalsIgnoreCase(observable.id))
-			//					.findFirst()
-			//					.ifPresent(observe -> {
-			//						observe.setDescription(observable.description);
-			//						if (observable.units != null) {
-			//							observe.setUnits(
-			//								new ModelUnit()
-			//									.setExpression(observable.units.expression)
-			//									.setExpressionMathml(observable.units.expressionMathml)
-			//							);
-			//						}
-			//					});
-			//			}
-
-			//			for (final Descriptions transition : response.response.transitions) {
-			//				dataset
-			//					.getRates()
-			//					.stream()
-			//					.filter(rate -> rate.getTarget().equalsIgnoreCase(transition.id))
-			//					.findFirst()
-			//					.ifPresent(trans -> {
-			//						trans.setDescription(transition.description);
-			//
-			//						StreamSupport.stream(dataset.getModel().get("transitions").spliterator(), false)
-			//							.filter(transitionNode -> transitionNode.path("id").asText().equals(transition.id))
-			//							.findFirst()
-			//							.ifPresent(transitionNode -> {
-			//								((ObjectNode) transitionNode).put("description", transition.description);
-			//							});
-			//					});
-			//			}
-
+			// Update the dataset
 			datasetService.updateAsset(dataset, dataset.getId(), ASSUME_WRITE_PERMISSION_ON_BEHALF_OF_USER);
 		} catch (final Exception e) {
 			log.error("Failed to enrich dataset", e);
