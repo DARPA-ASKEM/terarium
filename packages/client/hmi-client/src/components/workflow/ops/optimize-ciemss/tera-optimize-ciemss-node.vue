ab2815682f18d4baf0280a88c4ef749d6fda05b9
Jaehwan Ryu <jryu@uncharted.software>
2024-11-12 15:12:11 -0500

    Removed comments

--- packages/client/hmi-client/src/components/workflow/ops/optimize-ciemss/tera-optimize-ciemss-node.vue
+++ packages/client/hmi-client/src/components/workflow/ops/optimize-ciemss/tera-optimize-ciemss-node.vue
@@ -75,34 +75,6 @@ const model = ref<Model | null>(null);
 
 const modelVarUnits = ref<{ [key: string]: string }>({});
 
-// const preparedChartInputs = computed(() => {
-// 	const { preForecastRunId, postForecastRunId } = props.node.state;
-// 	if (!postForecastRunId || !preForecastRunId) return null;
-// 	const preResult = runResults.value[preForecastRunId];
-// 	const preResultSummary = runResultsSummary.value[preForecastRunId];
-// 	const postResult = runResults.value[postForecastRunId];
-// 	const postResultSummary = runResultsSummary.value[postForecastRunId];
-
-// 	if (!postResult || !postResultSummary || !preResultSummary || !preResult) return null;
-// 	const pyciemssMap = parsePyCiemssMap(postResult[0]);
-// 	if (_.isEmpty(pyciemssMap)) return null;
-
-// 	// Merge before/after for chart
-// 	const { result, resultSummary } = mergeResults(preResult, postResult, preResultSummary, postResultSummary);
-
-// 	const translationMap: Record<string, string> = {};
-// 	Object.keys(pyciemssMap).forEach((key) => {
-// 		translationMap[`${pyciemssMap[key]}_mean`] = `${key} after optimization`;
-// 		translationMap[`${pyciemssMap[key]}_mean:pre`] = `${key} before optimization`;
-// 	});
-
-// 	return {
-// 		result,
-// 		resultSummary,
-// 		translationMap,
-// 		pyciemssMap,
-// 	};
-// });
 const preparedChartInputs = usePreparedChartInputs(props, runResults, runResultsSummary);
 const { selectedVariableSettings } = useChartSettings(props, emit);
 
