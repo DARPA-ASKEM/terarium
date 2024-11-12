1e1c8188a9e23625ecb153d87b05d7758d064d75
bigglesandginger <bigglesandginger@users.noreply.github.com>
2024-11-12 14:45:33 +0000

    chore: update generated types

--- packages/client/hmi-client/src/types/Types.ts
+++ packages/client/hmi-client/src/types/Types.ts
@@ -800,8 +800,8 @@ export interface ModelUnit {
 
 export interface GroundedSemantic {
     id: string;
-    name?: string;
     grounding?: ModelGrounding;
+    name?: string;
     description?: string;
 }
 
