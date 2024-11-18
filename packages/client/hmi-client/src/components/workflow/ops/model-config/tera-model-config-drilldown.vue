a032ed703c762b29ae3ab2eb4a01f957743e8406
Jamie Waese <120480244+jamiewaese-uncharted@users.noreply.github.com>
2024-11-18 16:05:44 -0500

    style updates for Configure Model Notebook tab

--- packages/client/hmi-client/src/components/workflow/ops/model-config/tera-model-config-drilldown.vue
+++ packages/client/hmi-client/src/components/workflow/ops/model-config/tera-model-config-drilldown.vue
@@ -860,8 +860,8 @@ onUnmounted(() => {
 }
 
 .notebook-section {
-	background-color: var(--surface-disabled);
-	border-right: 1px solid var(--surface-border-dark);
+	background-color: var(--surface-200);
+	border-right: 1px solid var(--surface-border-light);
 	padding: var(--gap-4);
 }
 
@@ -958,6 +958,7 @@ button.start-edit {
 
 .executed-code {
 	white-space: pre-wrap;
+	padding: var(--gap-4);
 }
 :deep(.content-wrapper) {
 	& > section {
