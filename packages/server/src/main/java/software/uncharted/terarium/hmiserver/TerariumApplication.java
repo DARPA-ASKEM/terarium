033ddad832b34cf53351b4b4d4a7f47ce6fe9494
Yohann Paris <github@yohannparis.com>
2024-11-21 10:43:36 -0500

    Enable async in the app

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/TerariumApplication.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/TerariumApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
 @PropertySource("classpath:application.properties")
+@EnableAsync
 public class TerariumApplication {
 
 	public static void main(final String[] args) {
