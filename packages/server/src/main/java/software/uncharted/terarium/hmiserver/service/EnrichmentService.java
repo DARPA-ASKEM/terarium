5dcddd855960e1c96b0722a756827e2e979a08d8
Yohann Paris <github@yohannparis.com>
2024-11-21 11:19:14 -0500

    Update EnrichmentService.java

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/EnrichmentService.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/EnrichmentService.java
@@ -76,8 +76,7 @@ public class EnrichmentService {
 	 * @param permission the permission
 	 */
 	@Async
-	public void modelWithDocument(UUID projectId, UUID documentId, UUID modelId, Schema.Permission permission)
-		throws IOException, ExecutionException, InterruptedException, TimeoutException {
+	public void modelWithDocument(UUID projectId, UUID documentId, UUID modelId, Schema.Permission permission) {
 		// Create the notification group
 		final NotificationGroupInstance<Properties> notificationInterface = new NotificationGroupInstance<>(
 			clientEventService,
@@ -88,140 +87,144 @@ public class EnrichmentService {
 		);
 
 		executor.submit(() -> {
-			notificationInterface.sendMessage("Beginning model enrichment with using document extraction...");
-
-			// Get the Document
-			DocumentAsset document = documentService
-				.getAsset(documentId, permission)
-				.orElseThrow(() -> {
-					final String errorString = String.format("Document %s not found", documentId);
-					log.warn(errorString);
-					return new IOException(errorString);
-				});
-
-			// Make sure there is text in the document
-			if (document.getText() == null || document.getText().isBlank()) {
-				final String errorString = String.format("Document %s has no extracted text", documentId);
-				log.warn(errorString);
-				throw new IOException(errorString);
-			}
-
-			notificationInterface.sendMessage("Document text found.");
-
-			// Get the model
-			Model model = modelService
-				.getAsset(modelId, permission)
-				.orElseThrow(() -> {
-					final String errorString = String.format("Model %s not found", modelId);
-					log.warn(errorString);
-					return new IOException(errorString);
-				});
-
-			notificationInterface.sendMessage("Model found.");
-
-			// Stripping the metadata before it's sent since it can cause GoLLM to fail with massive inputs
-			model.setMetadata(new ModelMetadata());
-			notificationInterface.sendMessage("Model metadata stripped.");
-
-			// Create the tasks
-			final TaskRequest enrichAmrRequest = TaskUtilities.getEnrichAMRTaskRequest(
-				currentUserService.get().getId(),
-				document,
-				model,
-				projectId
-			);
-
-			final TaskRequest modelCardRequest = TaskUtilities.getModelCardTask(
-				currentUserService.get().getId(),
-				document,
-				model,
-				projectId
-			);
-
-			final TaskRequest taskRequest = new CompoundTask(enrichAmrRequest, modelCardRequest);
-
 			try {
-				taskService.runTask(TaskService.TaskMode.SYNC, taskRequest);
-			} catch (final Exception e) {
-				final String errorString = String.format("Task failed: %s", e);
-				log.warn(errorString);
-				throw new IOException(errorString);
-			}
+				notificationInterface.sendMessage("Beginning model enrichment with using document extraction...");
 
-			notificationInterface.sendMessage("Model enrichment and model card complete.");
+				// Get the Document
+				DocumentAsset document = documentService
+					.getAsset(documentId, permission)
+					.orElseThrow(() -> {
+						final String errorString = String.format("Document %s not found", documentId);
+						log.warn(errorString);
+						return new IOException(errorString);
+					});
 
-			// Get the enriched model
-			final Model enrichedModel = modelService
-				.getAsset(modelId, permission)
-				.orElseThrow(() -> {
-					final String errorString = String.format("Enriched Model %s not found", modelId);
+				// Make sure there is text in the document
+				if (document.getText() == null || document.getText().isBlank()) {
+					final String errorString = String.format("Document %s has no extracted text", documentId);
 					log.warn(errorString);
-					return new IOException(errorString);
+					throw new IOException(errorString);
+				}
+
+				notificationInterface.sendMessage("Document text found.");
+
+				// Get the model
+				Model model = modelService
+					.getAsset(modelId, permission)
+					.orElseThrow(() -> {
+						final String errorString = String.format("Model %s not found", modelId);
+						log.warn(errorString);
+						return new IOException(errorString);
+					});
+
+				notificationInterface.sendMessage("Model found.");
+
+				// Stripping the metadata before it's sent since it can cause GoLLM to fail with massive inputs
+				model.setMetadata(new ModelMetadata());
+				notificationInterface.sendMessage("Model metadata stripped.");
+
+				// Create the tasks
+				final TaskRequest enrichAmrRequest = TaskUtilities.getEnrichAMRTaskRequest(
+					currentUserService.get().getId(),
+					document,
+					model,
+					projectId
+				);
+
+				final TaskRequest modelCardRequest = TaskUtilities.getModelCardTask(
+					currentUserService.get().getId(),
+					document,
+					model,
+					projectId
+				);
+
+				final TaskRequest taskRequest = new CompoundTask(enrichAmrRequest, modelCardRequest);
+
+				try {
+					taskService.runTask(TaskService.TaskMode.SYNC, taskRequest);
+				} catch (final Exception e) {
+					final String errorString = String.format("Task failed: %s", e);
+					log.warn(errorString);
+					throw new IOException(errorString);
+				}
+
+				notificationInterface.sendMessage("Model enrichment and model card complete.");
+
+				// Get the enriched model
+				final Model enrichedModel = modelService
+					.getAsset(modelId, permission)
+					.orElseThrow(() -> {
+						final String errorString = String.format("Enriched Model %s not found", modelId);
+						log.warn(errorString);
+						return new IOException(errorString);
+					});
+
+				notificationInterface.sendMessage("Updating model grounding...");
+
+				// Update State Grounding
+				final List<State> states = enrichedModel
+					.getStates()
+					.stream()
+					.map(state -> state == null ? new State() : state)
+					.collect(Collectors.toList());
+				states.forEach(state -> {
+					TaskUtilities.performDKGSearchAndSetGrounding(dkgService, state);
 				});
+				enrichedModel.setStates(states);
+				notificationInterface.sendMessage("State grounding complete.");
 
-			notificationInterface.sendMessage("Updating model grounding...");
+				// Update Observable Grounding
+				if (enrichedModel.getObservables() != null && !enrichedModel.getObservables().isEmpty()) {
+					final List<Observable> observables = enrichedModel.getObservables();
+					observables.forEach(observable -> {
+						if (observable == null) {
+							observable = new Observable();
+						}
+						TaskUtilities.performDKGSearchAndSetGrounding(dkgService, observable);
+					});
+					enrichedModel.setObservables(observables);
+					notificationInterface.sendMessage("Observable grounding complete.");
+				}
 
-			// Update State Grounding
-			final List<State> states = enrichedModel
-				.getStates()
-				.stream()
-				.map(state -> state == null ? new State() : state)
-				.collect(Collectors.toList());
-			states.forEach(state -> {
-				TaskUtilities.performDKGSearchAndSetGrounding(dkgService, state);
-			});
-			enrichedModel.setStates(states);
-			notificationInterface.sendMessage("State grounding complete.");
+				// Update Parameter Grounding
+				if (enrichedModel.getParameters() != null && !enrichedModel.getParameters().isEmpty()) {
+					final List<ModelParameter> parameters = enrichedModel.getParameters();
+					parameters.forEach(parameter -> {
+						if (parameter == null) {
+							parameter = new ModelParameter();
+						}
+						TaskUtilities.performDKGSearchAndSetGrounding(dkgService, parameter);
+					});
+					enrichedModel.setParameters(parameters);
+					notificationInterface.sendMessage("Parameter grounding complete.");
+				}
 
-			// Update Observable Grounding
-			if (enrichedModel.getObservables() != null && !enrichedModel.getObservables().isEmpty()) {
-				final List<Observable> observables = enrichedModel.getObservables();
-				observables.forEach(observable -> {
-					if (observable == null) {
-						observable = new Observable();
-					}
-					TaskUtilities.performDKGSearchAndSetGrounding(dkgService, observable);
-				});
-				enrichedModel.setObservables(observables);
-				notificationInterface.sendMessage("Observable grounding complete.");
-			}
+				// Update Transition Grounding
+				if (enrichedModel.getTransitions() != null && !enrichedModel.getTransitions().isEmpty()) {
+					final List<Transition> transitions = enrichedModel.getTransitions();
+					transitions.forEach(transition -> {
+						if (transition == null) {
+							transition = new Transition();
+						}
+						TaskUtilities.performDKGSearchAndSetGrounding(dkgService, transition);
+					});
+					enrichedModel.setTransitions(transitions);
+					notificationInterface.sendMessage("Transition grounding complete.");
+				}
 
-			// Update Parameter Grounding
-			if (enrichedModel.getParameters() != null && !enrichedModel.getParameters().isEmpty()) {
-				final List<ModelParameter> parameters = enrichedModel.getParameters();
-				parameters.forEach(parameter -> {
-					if (parameter == null) {
-						parameter = new ModelParameter();
-					}
-					TaskUtilities.performDKGSearchAndSetGrounding(dkgService, parameter);
-				});
-				enrichedModel.setParameters(parameters);
-				notificationInterface.sendMessage("Parameter grounding complete.");
-			}
+				// Update the model
+				try {
+					modelService.updateAsset(enrichedModel, projectId, permission);
+				} catch (final IOException e) {
+					final String errorString = String.format("Failed to update model %s", modelId);
+					log.warn(errorString);
+					throw new IOException(errorString);
+				}
 
-			// Update Transition Grounding
-			if (enrichedModel.getTransitions() != null && !enrichedModel.getTransitions().isEmpty()) {
-				final List<Transition> transitions = enrichedModel.getTransitions();
-				transitions.forEach(transition -> {
-					if (transition == null) {
-						transition = new Transition();
-					}
-					TaskUtilities.performDKGSearchAndSetGrounding(dkgService, transition);
-				});
-				enrichedModel.setTransitions(transitions);
-				notificationInterface.sendMessage("Transition grounding complete.");
-			}
-
-			// Update the model
-			try {
-				modelService.updateAsset(enrichedModel, projectId, permission);
+				notificationInterface.sendFinalMessage("Model enriched and grounded");
 			} catch (final IOException e) {
-				final String errorString = String.format("Failed to update model %s", modelId);
-				log.warn(errorString);
-				throw new IOException(errorString);
+				log.error("Error enriching model with document", e);
 			}
-
-			notificationInterface.sendFinalMessage("Model enriched and grounded");
 		});
 	}
 }
