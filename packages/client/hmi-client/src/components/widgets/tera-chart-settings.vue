bb3685ca13b042b4678a0a784dbddff35e2fad79
Jaehwan Ryu <jryu@uncharted.software>
2024-11-20 19:51:20 -0500

    Minor tweak

--- packages/client/hmi-client/src/components/widgets/tera-chart-settings.vue
+++ packages/client/hmi-client/src/components/widgets/tera-chart-settings.vue
@@ -12,21 +12,43 @@
 			@configuration-change="$emit('selection-change', $event.selectedVariable, type)"
 		/>
 		<tera-chart-settings-item
-			v-for="s of settings.filter((s) => s.type === type)"
+			v-for="s of targetSettings"
 			:key="s.id"
 			:settings="s"
 			@open="$emit('open', s)"
 			@remove="$emit('remove', s.id)"
 		/>
+		<template v-if="type === ChartSettingType.VARIABLE_ENSEMBLE">
+			<tera-checkbox
+				label="Show individual models"
+				:model-value="Boolean(ensembleChartOptions.showIndividualModels)"
+				@update:model-value="toggleEensembleChartOption('showIndividualModels', $event)"
+			/>
+			<tera-checkbox
+				class="pl-5"
+				label="Relative to ensemble"
+				:model-value="Boolean(ensembleChartOptions.relativeToEnsemble)"
+				@update:model-value="toggleEensembleChartOption('relativeToEnsemble', $event)"
+			/>
+			<tera-checkbox
+				v-if="ensembleChartOptions.showIndividualModelsWithWeight !== undefined"
+				label="Show individual models with weights"
+				:model-value="ensembleChartOptions.showIndividualModelsWithWeight"
+				@update:model-value="toggleEensembleChartOption('showIndividualModelsWithWeight', $event)"
+			/>
+		</template>
 	</div>
 </template>
 
 <script setup lang="ts">
+import TeraCheckbox from '@/components/widgets/tera-checkbox.vue';
 import TeraChartSettingsItem from '@/components/widgets/tera-chart-settings-item.vue';
 import TeraChartControl from '@/components/workflow/tera-chart-control.vue';
-import { ChartSetting, ChartSettingType } from '@/types/common';
+import { ChartSetting, ChartSettingEnsembleVariable, ChartSettingType } from '@/types/common';
+import { computed } from 'vue';
+import { isChartSettingEnsembleVariable } from '@/services/chart-settings';
 
-defineProps<{
+const props = defineProps<{
 	title: string;
 	settings: ChartSetting[];
 	type: ChartSettingType;
@@ -35,7 +57,36 @@ defineProps<{
 	// Selected dropdown options
 	selectedOptions: string[];
 }>();
-defineEmits(['open', 'remove', 'selection-change']);
+const emits = defineEmits(['open', 'remove', 'selection-change', 'toggle-ensemble-chart-option']);
+
+// Settings of the same type that we want to interact with.
+const targetSettings = computed(() => props.settings.filter((s) => s.type === props.type));
+
+// ------------------- Ensemble chart options -------------------
+// Extract ensemble variable chart options from the chart settings for each variable and merge into single option.
+const ensembleChartOptions = computed(() => {
+	// initial options
+	const options: Partial<ChartSettingEnsembleVariable> = {
+		showIndividualModels: false,
+		relativeToEnsemble: false,
+		showIndividualModelsWithWeight: undefined // only applicable for the simulate ensemble otherwise undefined
+	};
+	// Merge options from all ensemble variables since each variable setting has its own options but all controlled by the single UI.
+	targetSettings.value.forEach((s) => {
+		if (!isChartSettingEnsembleVariable(s)) return;
+		options.showIndividualModels = options.showIndividualModels || s.showIndividualModels;
+		options.relativeToEnsemble = options.relativeToEnsemble || s.relativeToEnsemble;
+		options.showIndividualModelsWithWeight = options.showIndividualModelsWithWeight || s.showIndividualModelsWithWeight;
+	});
+	return options;
+});
+const toggleEensembleChartOption = (
+	option: 'showIndividualModels' | 'relativeToEnsemble' | 'showIndividualModelsWithWeight',
+	value: Boolean
+) => {
+	emits('toggle-ensemble-chart-option', props.type, option, value);
+};
+// ---------------------------------------------------------------
 </script>
 <style scoped>
 .chart-settings {
