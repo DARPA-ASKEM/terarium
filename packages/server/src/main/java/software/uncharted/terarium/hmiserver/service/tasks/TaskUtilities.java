d697976eb9ecea5a794f801c6c0bcb1eea949ce4
Derek Vince <dvince@uncharted.software>
2024-11-28 12:09:16 -0500

    5634 bulk embeddings generation (#5665)
    Co-authored-by: David Gauldie <dgauldie@uncharted.software>

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/TaskUtilities.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/TaskUtilities.java
@@ -2,10 +2,13 @@ package software.uncharted.terarium.hmiserver.service.tasks;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micrometer.observation.annotation.Observed;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import software.uncharted.terarium.hmiserver.models.dataservice.document.DocumentAsset;
 import software.uncharted.terarium.hmiserver.models.dataservice.model.Model;
@@ -102,27 +105,33 @@ public class TaskUtilities {
 		return req;
 	}
 
-	public static void performDKGSearchAndSetGrounding(DKGService dkgService, GroundedSemantic part) {
-		if (part == null || part.getId() == null || part.getId().isEmpty()) return;
-		try {
-			// Set up our search term. We want to use the description but if its empty or null fall back to the ID
-			String searchTerm = (part.getDescription() == null || part.getDescription().isEmpty())
-				? part.getId()
-				: part.getDescription();
+	@Observed(name = "function_profile")
+	public static void performDKGSearchAndSetGrounding(DKGService dkgService, List<? extends GroundedSemantic> parts) {
+		List<String> searchTerms = parts
+			.stream()
+			.filter(part -> part != null && part.getId() != null && !part.getId().isEmpty())
+			.map(TaskUtilities::getSearchTerm)
+			.collect(Collectors.toList());
 
-			List<DKG> curies = dkgService.knnSearchEpiDKG(0, 1, 1, searchTerm, null);
-			if (!curies.isEmpty()) {
-				DKG dkg = curies.get(0);
-				if (part.getGrounding() == null) part.setGrounding(new ModelGrounding());
-				if (part.getGrounding().getIdentifiers() == null) part.getGrounding().setIdentifiers(new HashMap<>());
-				String[] currieId = dkg.getCurie().split(":");
-				part.getGrounding().getIdentifiers().put(currieId[0], currieId[1]);
-			} else {
-				part.setGrounding(new ModelGrounding());
-			}
+		List<DKG> curies = new ArrayList<>();
+		try {
+			curies = dkgService.knnSearchEpiDKG(0, 100, 1, searchTerms, null);
 		} catch (Exception e) {
-			// If we can't find the DKG, just ignore it
-			log.warn("Unable to find DKG for semantic: {}", part.getId(), e);
+			log.warn("Unable to find DKG for semantics: {}", searchTerms, e);
+			return;
 		}
+
+		for (int i = 0; i < curies.size(); i++) {
+			DKG dkg = curies.get(i);
+			GroundedSemantic part = parts.get(i);
+			if (part.getGrounding() == null) part.setGrounding(new ModelGrounding());
+			if (part.getGrounding().getIdentifiers() == null) part.getGrounding().setIdentifiers(new HashMap<>());
+			String[] currieId = dkg.getCurie().split(":");
+			part.getGrounding().getIdentifiers().put(currieId[0], currieId[1]);
+		}
+	}
+
+	private static String getSearchTerm(GroundedSemantic part) {
+		return (part.getDescription() == null || part.getDescription().isEmpty()) ? part.getId() : part.getDescription();
 	}
 }
