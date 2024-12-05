488e78e266dc725bc210034169dbe0ad99f631f5
Yohann Paris <github@yohannparis.com>
2024-12-05 11:52:09 -0700

    Only display project overview

--- packages/client/hmi-client/src/page/tera-home.vue
+++ packages/client/hmi-client/src/page/tera-home.vue
@@ -139,7 +139,15 @@ import Dropdown from 'primevue/dropdown';
 import MultiSelect from 'primevue/multiselect';
 import SelectButton from 'primevue/selectbutton';
 import { useProjectMenu } from '@/composables/project-menu';
-import { ClientEventType, ProgressState, Project, ProjectAsset, ProjectSearchResult } from '@/types/Types';
+import {
+	AssetType,
+	ClientEventType,
+	ProgressState,
+	Project,
+	ProjectAsset,
+	ProjectSearchResult,
+	TerariumAssetEmbeddingType
+} from '@/types/Types';
 import { ProjectWithKnnData } from '@/types/Project';
 import { Vue3Lottie } from 'vue3-lottie';
 import EmptySeed from '@/assets/images/lottie-empty-seed.json';
@@ -231,7 +239,16 @@ const searchedAndFilterProjects = computed(() => {
 		.map((result: ProjectSearchResult) => {
 			const project = tabProjects.find(({ id }) => id === result.projectId);
 			if (!project) return null;
-			result.assets = result.assets?.sort((a, b) => b.score - a.score);
+
+			// Only display Project Overview as search results and sort the assets by descending score
+			result.assets = result.assets
+				?.filter((asset) => {
+					const isNotAProject = asset.assetType !== AssetType.Project;
+					const isProjectOverview =
+						asset.assetType === AssetType.Project && asset.embeddingType === TerariumAssetEmbeddingType.Overview;
+					return isNotAProject || isProjectOverview;
+				})
+				.sort((a, b) => b.score - a.score);
 			return { ...project, ...result } as ProjectWithKnnData;
 		})
 		.filter((project) => !!project); // Remove null values
