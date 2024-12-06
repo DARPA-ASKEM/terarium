e4ee9772e5b7e3066ca1efc44bdf4e57e7d811ad
Yohann Paris <github@yohannparis.com>
2024-12-06 06:14:47 -0700

    add controller

---
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/CompareModelsConceptsResponseHandler.java
@@ -0,0 +1,40 @@
+package software.uncharted.terarium.hmiserver.service.tasks;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.List;
+import java.util.UUID;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CompareModelsConceptsResponseHandler extends TaskResponseHandler {
+
+	public static final String NAME = "mira:compare_models";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Data
+	public static class Input {
+
+		List<String> amrs;
+	}
+
+	@Data
+	public static class Properties {
+
+		List<UUID> modelIds;
+		UUID workflowId;
+		UUID nodeId;
+	}
+
+	@Data
+	public static class Response {
+
+		JsonNode response;
+	}
+}
