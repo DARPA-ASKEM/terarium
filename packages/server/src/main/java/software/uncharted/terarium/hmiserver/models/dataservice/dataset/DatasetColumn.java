3a67e93d4687cbf47718c46233326cf699949f27
Yohann Paris <github@yohannparis.com>
2024-12-17 15:48:15 -0500

    Add messages and python/pandas type to ColumnType

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/dataset/DatasetColumn.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/dataset/DatasetColumn.java
@@ -137,4 +137,33 @@ public class DatasetColumn extends TerariumEntity {
 		@JsonAlias("time")
 		TIME
 	}
+
+	// Map Python data types to ColumnType
+	public static ColumnType mapDataType(String pythonDataType) {
+		if (pythonDataType == null) {
+			return ColumnType.UNKNOWN;
+		}
+
+		// Convert to lowercase for case-insensitive matching
+		String normalizedType = pythonDataType.toLowerCase();
+
+		return switch (normalizedType) {
+			// Integer types
+			case "int64", "int32", "int16", "int8" -> ColumnType.INTEGER;
+			// Float types
+			case "float64", "float32", "float16" -> ColumnType.FLOAT;
+			// Double types (often same as float in Pandas)
+			case "float", "double" -> ColumnType.DOUBLE;
+			// Boolean
+			case "bool" -> ColumnType.BOOLEAN;
+			// String types
+			case "object", "string" -> ColumnType.STRING;
+			// Date and Time types
+			case "datetime64", "datetime" -> ColumnType.DATETIME;
+			case "timedelta", "time" -> ColumnType.TIME;
+			case "date" -> ColumnType.DATE;
+			// Default case
+			default -> ColumnType.UNKNOWN;
+		};
+	}
 }
