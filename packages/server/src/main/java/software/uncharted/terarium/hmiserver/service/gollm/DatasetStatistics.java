77e9de70f78ec6d4dd269d46a8f733053dce44e0
Yohann Paris <github@yohannparis.com>
2024-12-18 11:26:36 -0500

    add the presignedURL

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/gollm/DatasetStatistics.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/gollm/DatasetStatistics.java
@@ -15,11 +15,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import software.uncharted.terarium.hmiserver.models.dataservice.PresignedURL;
 import software.uncharted.terarium.hmiserver.models.dataservice.dataset.Dataset;
 import software.uncharted.terarium.hmiserver.models.dataservice.dataset.DatasetColumn;
 import software.uncharted.terarium.hmiserver.models.task.TaskRequest;
 import software.uncharted.terarium.hmiserver.models.task.TaskRequest.TaskType;
 import software.uncharted.terarium.hmiserver.models.task.TaskResponse;
+import software.uncharted.terarium.hmiserver.service.data.DatasetService;
 import software.uncharted.terarium.hmiserver.service.tasks.TaskService;
 import software.uncharted.terarium.hmiserver.utils.Messages;
 
@@ -32,6 +34,7 @@ public class DatasetStatistics {
 	private final ObjectMapper objectMapper;
 	private final TaskService taskService;
 	final Messages messages;
+	private final DatasetService datasetService;
 
 	@Data
 	public static class DatasetStatisticsRequest {
@@ -53,7 +56,7 @@ public class DatasetStatistics {
 	/**
 	 * Add statistics to the columns of a Dataset
 	 * @param dataset - The Dataset
-	 * @throws IllegalArgumentException - If the Dataset does not exist or the statistics have already been calculated
+	 * @throws IllegalArgumentException - If the Dataset does not exist, the statistics have already been calculated, or the download URL is not found
 	 * @throws IOException - If there is an error reading the response from the Gollm service
 	 * @throws ExecutionException - If there is an error running the Task
 	 * @throws InterruptedException - If the Task is interrupted
@@ -62,14 +65,17 @@ public class DatasetStatistics {
 	public void add(final Dataset dataset)
 		throws IllegalArgumentException, IOException, ExecutionException, InterruptedException, TimeoutException {
 		// Check that the Statistics have already been calculated, as Dataset are immutable
-		final boolean isStatisticsExist = dataset.getColumns().stream().anyMatch(column -> column.getMetadata() != null);
+		final boolean isStatisticsExist = dataset.getColumns().stream().anyMatch(column -> column.getStats() != null);
 		if (isStatisticsExist) {
 			throw new IllegalArgumentException(messages.get("dataset.statistics.exist"));
 		}
 
 		// Create the request to the Gollm service
 		final DatasetStatisticsRequest request = new DatasetStatisticsRequest();
-		request.setDatasetUrl(dataset.getDatasetUrl());
+		final PresignedURL datasetUrl = datasetService
+			.getDownloadUrl(dataset.getId(), dataset.getFileNames().get(0))
+			.orElseThrow(() -> new IllegalArgumentException(messages.get("dataset.download.url.not.found")));
+		request.setDatasetUrl(datasetUrl.toString());
 
 		final TaskRequest taskRequest = new TaskRequest();
 		taskRequest.setType(TaskType.GOLLM);
