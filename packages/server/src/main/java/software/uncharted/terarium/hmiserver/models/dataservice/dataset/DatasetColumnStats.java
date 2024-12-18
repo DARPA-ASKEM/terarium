37fa82ea105c1b9c954315e3162f4907973cf4c6
Yohann Paris <github@yohannparis.com>
2024-12-18 16:15:51 -0500

    fix the issue with 🐍 to 🐫  case

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/dataset/DatasetColumnStats.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/dataset/DatasetColumnStats.java
@@ -1,5 +1,6 @@
 package software.uncharted.terarium.hmiserver.models.dataservice.dataset;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
@@ -17,24 +18,42 @@ public class DatasetColumnStats {
 	@Data
 	public static class NumericColumnStats {
 
+		@JsonProperty("data_type")
 		private String dataType;
-		private Double mean;
-		private Double median;
-		private Double min;
-		private Double max;
-		private Double stdDev;
-		private List<Double> quartiles;
+
+		private double mean;
+		private double median;
+		private double min;
+		private double max;
+
+		@JsonProperty("std_dev")
+		private double stdDev;
+
+		private double[] quartiles;
+
+		@JsonProperty("unique_values")
 		private int uniqueValues;
+
+		@JsonProperty("missing_values")
 		private int missingValues;
-		private List<Double> histogramBins;
+
+		@JsonProperty("histogram_bins")
+		private double[] histogramBins;
 	}
 
 	@Data
 	public static class NonNumericColumnStats {
 
+		@JsonProperty("data_type")
 		private String dataType;
+
+		@JsonProperty("unique_values")
 		private int uniqueValues;
-		private Map<String, Long> mostCommon;
+
+		@JsonProperty("most_common")
+		private Map<String, Integer> mostCommon;
+
+		@JsonProperty("missing_values")
 		private int missingValues;
 	}
 }
