b2d782da1ed40e36b56a8f5fea878f2f2de3c6d3
Adrian <asylvester@uncharted.software>
2025-01-07 16:57:33 -0500

    draft working version

--- packages/client/hmi-client/src/components/widgets/tera-common-modal-dialogs.vue
+++ packages/client/hmi-client/src/components/widgets/tera-common-modal-dialogs.vue
@@ -24,6 +24,22 @@
 			<Button label="Delete project" severity="danger" @click="removeProject" autofocus />
 		</template>
 	</Dialog>
+
+	<Dialog
+		modal
+		header="What do you want to call this project?"
+		style="max-width: 65ch"
+		v-model:visible="isCopyDialogVisible"
+	>
+		<section class="flex flex-column gap-2">
+			<label>Name</label>
+			<InputText id="projectName" v-model="projectName"></InputText>
+		</section>
+		<template #footer>
+			<Button label="Cancel" outlined class="p-button-secondary" @click="isCopyDialogVisible = false" />
+			<Button label="Save" @click="saveCopy" />
+		</template>
+	</Dialog>
 	<tera-share-project v-if="menuProject" v-model="isShareDialogVisible" :project="menuProject" />
 </template>
 
@@ -31,9 +47,11 @@
 // To easily display modal dialogs that will commonly appear throughout the app (therefore lives in App.vue)
 import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 import TeraShareProject from '@/components/widgets/share-project/tera-share-project.vue';
 import TeraProjectConfigurationModal from '@/components/project/tera-project-configuration-modal.vue';
 import { logger } from '@/utils/logger';
+import { ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import { RoutePath, useCurrentRoute } from '@/router/index';
 import { RouteName } from '@/router/routes';
@@ -45,7 +63,24 @@ const currentRoute = useCurrentRoute();
 
 // For now, we just use project-menu.ts to manage modals related to projects
 // For non-project related modals we may want to create new composable or abstract project-menu.ts into a modal manager
-const { isShareDialogVisible, isRemoveDialogVisible, isProjectConfigDialogVisible, menuProject } = useProjectMenu();
+const { isCopyDialogVisible, isShareDialogVisible, isRemoveDialogVisible, isProjectConfigDialogVisible, menuProject } =
+	useProjectMenu();
+
+const projectName = ref<string>('');
+const project = ref();
+
+const emit = defineEmits(['copied-project']);
+
+const saveCopy = async () => {
+	console.log('saveCopy');
+	isCopyDialogVisible.value = false;
+	router.push(RoutePath.Home);
+
+	const copiedProject = await useProjects().clone(project.value.id);
+	if (!copiedProject) return;
+	copiedProject.name = projectName.value;
+	emit('copied-project', copiedProject);
+};
 
 const removeProject = async () => {
 	if (!menuProject.value) return;
@@ -58,6 +93,17 @@ const removeProject = async () => {
 		if (currentRoute.value.name !== RouteName.Home) router.push(RoutePath.Home);
 	}
 };
+
+watch(
+	() => isCopyDialogVisible.value,
+	async () => {
+		if (isCopyDialogVisible.value) {
+			project.value = await useProjects().activeProject.value;
+			const name = project.value.name ?? null;
+			projectName.value = name ? `Copy of ${name}` : 'Enter new project name';
+		}
+	}
+);
 </script>
 
 <style scoped>
