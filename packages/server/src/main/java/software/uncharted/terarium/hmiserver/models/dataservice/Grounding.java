c93c518712df652bb061780e49f55a1fdc8cf064
Yohann Paris <github@yohannparis.com>
2025-01-24 15:38:51 -0500

    Update Grounding.java

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Grounding.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/Grounding.java
@@ -1,11 +1,11 @@
 package software.uncharted.terarium.hmiserver.models.dataservice;
 
-import com.google.api.client.util.Value;
 import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import java.io.Serial;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,6 +14,7 @@ import org.hibernate.annotations.Type;
 import software.uncharted.terarium.hmiserver.annotations.TSModel;
 import software.uncharted.terarium.hmiserver.annotations.TSOptional;
 import software.uncharted.terarium.hmiserver.models.TerariumEntity;
+import software.uncharted.terarium.hmiserver.models.mira.DKG;
 
 /** Represents a grounding document from TDS */
 @Data
@@ -29,21 +30,20 @@ public class Grounding extends TerariumEntity {
 	/** Ontological identifier per DKG */
 	@Type(JsonType.class)
 	@Column(columnDefinition = "json")
-	private Map<String, String> identifiers; // Map<curie, name>
+	private List<DKG> identifiers;
 
 	/** (Optional) Additional context that informs the grounding */
 	@TSOptional
 	@Type(JsonType.class)
 	@Column(columnDefinition = "json")
-	private Map<String, String> context; // Map<key, value>
+	private Map<String, String> context;
 
 	@Override
 	public Grounding clone() {
 		final Grounding clone = new Grounding();
 
 		if (this.identifiers != null && !this.identifiers.isEmpty()) {
-			clone.identifiers = new HashMap<>();
-			clone.identifiers.putAll(this.identifiers);
+			clone.identifiers = this.identifiers;
 		}
 		if (this.context != null && !this.context.isEmpty()) {
 			clone.context = new HashMap<>();
