dd842bfec6b46259807cd2fdcbe23816c087ae5a
Yohann Paris <github@yohannparis.com>
2025-01-28 16:11:31 -0500

    improve the v-model pattern

--- packages/client/hmi-client/src/components/widgets/tera-concept.vue
+++ packages/client/hmi-client/src/components/widgets/tera-concept.vue
@@ -1,60 +1,83 @@
 <template>
 	<main>
-		<label>Concept</label>
+		<p>Concept</p>
 
-		<template v-if="isPreview"> </template>
+		<section v-if="isPreview || true">
+			<p>{{ identifierName }}</p>
+			<ul>
+				<li v-for="(context, index) in contextNames" :key="index">
+					{{ context }}
+				</li>
+			</ul>
+		</section>
 
-		<template v-else>
-			<AutoComplete
-				size="small"
-				placeholder="Search concepts"
-				v-model="query"
-				:suggestions="results"
-				optionLabel="name"
-				@complete="async () => (results = await searchCuriesEntities(query))"
-				@item-select="$emit('update-item', { key: 'concept', value: $event.value.curie })"
-				@keyup.enter="applyValidConcept"
-				@blur="applyValidConcept"
-			/>
-		</template>
+		<section v-else>
+			<AutoComplete size="small" placeholder="Search concepts" optionLabel="name" />
+			<!--				v-model="query"-->
+			<!--				:suggestions="results"-->
+			<!--				@complete="async () => (results = await searchCuriesEntities(query))"-->
+			<!--				@item-select="$emit('update-item', { key: 'concept', value: $event.value.curie })"-->
+			<!--				@keyup.enter="applyValidConcept"-->
+			<!--				@blur="applyValidConcept"-->
+			<AutoComplete size="small" placeholder="Additional concepts" optionLabel="name" />
+			<!--				v-model="query"-->
+			<!--				:suggestions="results"-->
+			<!--				@complete="async () => (results = await searchCuriesEntities(query))"-->
+			<!--				@item-select="$emit('update-item', { key: 'concept', value: $event.value.curie })"-->
+			<!--				@keyup.enter="applyValidConcept"-->
+			<!--				@blur="applyValidConcept"-->
+		</section>
 	</main>
 </template>
 
 <script setup lang="ts">
-import { getCurieFromGroundingIdentifier, getNameOfCurieCached, searchCuriesEntities } from '@/services/concept';
+import { ref, watch } from 'vue';
 import AutoComplete from 'primevue/autocomplete';
-import { Grounding } from '@/types/Types';
-import { watch } from 'vue';
+import { type Grounding } from '@/types/Types';
+import { getCurieFromGroundingIdentifier, getNameOfCurieCached } from '@/services/concept';
 
-const props = defineProps<{
+const grounding = defineModel<Grounding>();
+
+defineProps<{
 	isPreview?: boolean;
-	grounding: Grounding;
 }>();
 
-const emit = defineEmits(['update']);
+// const emit = defineEmits(['update']);
 
 // Used if an option isn't selected from the Autocomplete suggestions but is typed in regularly
-function applyValidConcept() {
-	// Allows to empty the concept
-	if (query.value === '') {
-		emit('update', { key: 'concept', value: '' });
-	}
-	// If what was typed was one of the results then choose that result
-	else {
-		const concept = results.value.find((result) => result.name === query.value);
-		if (concept) {
-			emit('update', { key: 'concept', value: concept.curie });
-		}
-	}
-}
+// function applyValidConcept() {
+// 	// Allows to empty the concept
+// 	if (query.value === '') {
+// 		emit('update', { key: 'concept', value: '' });
+// 	}
+// 	// If what was typed was one of the results then choose that result
+// 	else {
+// 		const concept = results.value.find((result) => result.name === query.value);
+// 		if (concept) {
+// 			emit('update', { key: 'concept', value: concept.curie });
+// 		}
+// 	}
+// }
+
+const identifierName = ref<string>('');
+const contextNames = ref<string[]>([]);
 
 watch(
-	props.grounding,
-	async (identifiers) => {
-		if (identifiers) query.value = await getNameOfCurieCached(getCurieFromGroundingIdentifier(identifiers));
+	() => grounding.value,
+	(newGrounding) => {
+		if (newGrounding) {
+			getNameOfCurieCached(getCurieFromGroundingIdentifier(newGrounding.identifiers)).then((name) => {
+				identifierName.value = name;
+			});
+			contextNames.value = Object.entries(newGrounding.context ?? {}).map(([key, value]) => `${key}: ${value}`);
+		}
 	},
 	{ immediate: true }
 );
 </script>
 
-<style scoped></style>
+<style scoped>
+main > p {
+	color: var(--text-color-subdued);
+}
+</style>
