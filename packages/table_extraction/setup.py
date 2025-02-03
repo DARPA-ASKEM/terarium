6bdd15ed67ef568b1f4685c943539a4c97bbae2c
David Gauldie <dgauldie@uncharted.software>
2025-02-03 15:46:51 -0500

    pin table extraction to pytorch 2.5.1 (#6361)

--- packages/table_extraction/setup.py
+++ packages/table_extraction/setup.py
@@ -4,7 +4,9 @@ setup(
     name="table_extraction_task",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "torch==2.5.1"
+    ],
     entry_points={
         "console_scripts": [
             "table_extraction_task:extract_tables=tasks.extract_tables:main",
