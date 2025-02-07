83aec27dc589175842cfce68d4cd8fb1ea0b7d03
Tom Szendrey <T.Szendrey@hotmail.com>
2025-02-07 09:24:06 -0500

    Notebook now scrollable

--- packages/client/hmi-client/src/components/drilldown/tera-notebook-output.vue
+++ packages/client/hmi-client/src/components/drilldown/tera-notebook-output.vue
@@ -2,6 +2,7 @@
 	<tera-resizable-panel v-if="!isHidden" :start-height="100" :resize-from-top="true" class="container output-panel">
 		<h6 class="pt-1">
 			<span>{{ name }}</span>
+
 			<Button rounded text icon="pi pi-times" @click="isHidden = true" />
 		</h6>
 		<code class="code-section">{{ props.traceback }}</code>
@@ -41,6 +42,7 @@ const isHidden = ref<boolean>(false);
 }
 
 .output-panel {
+	overflow-y: auto;
 	background: var(--surface-50);
 }
 .output-panel-closed {
