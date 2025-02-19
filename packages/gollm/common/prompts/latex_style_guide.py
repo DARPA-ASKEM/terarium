907f21eb16dabde0bc114ff52f48d160c1d3034f
Nelson Liu <nliu@uncharted.software>
2025-02-18 19:46:21 -0500

    Handle named functions, operators

--- packages/gollm/common/prompts/latex_style_guide.py
+++ packages/gollm/common/prompts/latex_style_guide.py
@@ -10,7 +10,8 @@ LATEX_STYLE_GUIDE = """
     a) replace "b S(t) I(t)" with "b * S(t) * I(t)"
     b) replace "b \\ast S(t) \ast I(t)" with "b * S(t)"
     c) replace "b \\star S(t)" with "b * S(t)"
-    d) replace "(a b)S(t) with "a * b * S(t)"
+    d) replace "b \times S(t)" with "b * S(t)"
+    e) replace "(a b)S(t) with "a * b * S(t)"
 7) Avoid capital sigma and pi notations for summation and product
 8) Avoid non-ASCII characters when possible
 9) Avoid using homoglyphs
@@ -34,6 +35,16 @@ LATEX_STYLE_GUIDE = """
     a) for example, replace "x(t) (\\alpha y(t) + \\beta z(t))" with "\\alpha * x(t) * y(t) + \\beta * x(t) * z(t)"
     b) for example, replace "-(\\alpha + \\beta + \\gamma) x(t)" with "- \\alpha * x(t) - \\beta * x(t) - \\gamma * x(t)"
     c) for example, replace "(x(t) + y(t)) (z(t) + w(t))" with "x(t) * z(t) + x(t) * w(t) + y(t) * z(t) + y(t) * w(t)"
-17) Do not expand expressions of the form "(1 - a) * S(t)" where "a" is some constant parameter
-    a) for example, preserve "(1 - \\alpha) \\gamma I(t)" as "(1 - \\alpha) * \\gamma * I(t)"
+17) Do not expand expressions of the form "(1 - a * b)" where "a" and "b" are some constant parameters
+    a) these expressions represent branching ratios
+    b) for example, preserve "(1 - \\alpha) \\gamma I(t)" as "(1 - \\alpha) * \\gamma * I(t)"
+    c) for example, preserve "(1 - \\gamma_1 - \\gamma_2) I(t)" as "(1 - \\gamma_1 - \\gamma_2) * I(t)"
+18) Write named mathematical functions and operators using their corresponding LaTeX code
+    a) replace "sin(\\omega t)" with "\\sin{{(\\omega * t)}}"
+    b) replace "cos", "tan", "csc", "sec", "cot, sinh, cosh, tanh, csch, sech, coth" with "\\cos", "\\tan", "\\csc", "\\cot", "\\csc", "\\sinh", "\\cosh", "\\tanh", "\\csch", "\\coth", "\\csc" similarly
+    c) replace "ln(a x + b)" or "log(a x + b)" with "\\log {{(a * x + b)}}"
+    d) replace "log_2 (a x + b)" with "\\log_2 {{(a * x + b)}}"
+    e) replace "exp(-a t)", "e^(-a t)", or "\\mathrm{e}^(-a t)" with "\\exp{{(-a * t)}}"
+    f) replace "max(x(t))", "min(x(t))" with "\\max{{(x(t))}}", "\\min{{(x(t))}}"
+    g) for other named functions like "CustomFunc(a * x)", write "\operatorname{{CustomFunc}}{{(a * x)}}"
 """
