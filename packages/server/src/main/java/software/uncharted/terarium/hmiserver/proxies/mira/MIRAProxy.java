55684726196400e7a461fd794d069ee28c1a992d
Yohann Paris <github@yohannparis.com>
2025-02-19 09:51:18 -0500

    Remove unused endpoint

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/proxies/mira/MIRAProxy.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/proxies/mira/MIRAProxy.java
@@ -7,20 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import software.uncharted.terarium.hmiserver.models.dataservice.model.Model;
-import software.uncharted.terarium.hmiserver.models.mira.Curies;
 import software.uncharted.terarium.hmiserver.models.mira.DKG;
-import software.uncharted.terarium.hmiserver.models.mira.EntitySimilarityResult;
 
 @FeignClient(name = "mira-api", url = "${mira-api.url}", path = "/api")
 public interface MIRAProxy {
-	// This converts MIRANet (Petrinet) to AMR
-	@PostMapping("/to_petrinet")
-	ResponseEntity<Model> toPetrinet(@RequestBody Object obj) throws FeignException;
-
-	@PostMapping("/entity_similarity")
-	ResponseEntity<List<EntitySimilarityResult>> entitySimilarity(@RequestBody Curies obj) throws FeignException;
-
 	@PostMapping("/search")
 	ResponseEntity<List<DKG>> search(@RequestBody String q, @RequestParam Integer limit, @RequestParam Integer offset)
 		throws FeignException;
