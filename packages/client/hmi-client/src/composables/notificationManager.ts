a0a4637739f46710b2d6da7e79365935b0f8c141
mloppie <mloppie@uncharted.software>
2025-02-21 09:53:58 -0500

    remove too generic double name for array

--- packages/client/hmi-client/src/composables/notificationManager.ts
+++ packages/client/hmi-client/src/composables/notificationManager.ts
@@ -15,18 +15,18 @@ const isFinished = (item: NotificationItem) =>
 	[ProgressState.Complete, ProgressState.Failed, ProgressState.Cancelled, ProgressState.Error].includes(item.status);
 
 // Items stores the notifications for all projects
-const items = ref<NotificationItem[]>([]);
+const notificationItems = ref<NotificationItem[]>([]);
 
 export function useNotificationManager() {
-	const hasFinishedItems = computed(() => items.value.some((item: NotificationItem) => isFinished(item)));
+	const hasFinishedItems = computed(() => notificationItems.value.some((item: NotificationItem) => isFinished(item)));
 	const unacknowledgedFinishedItems = computed(() =>
-		items.value.filter((item: NotificationItem) => isFinished(item) && !item.acknowledged)
+		notificationItems.value.filter((item: NotificationItem) => isFinished(item) && !item.acknowledged)
 	);
 
 	async function init() {
 		// Make sure this init function gets called only once for the lifetime of the app
 		if (initialized) return;
-		const handlers = createNotificationEventHandlers(items);
+		const handlers = createNotificationEventHandlers(notificationItems);
 		// Supported client event types for the notification manager
 		const supportedEventTypes = handlers.getSupportedEventTypes();
 
@@ -38,18 +38,18 @@ export function useNotificationManager() {
 		// Initialize SSE event handlers for the subsequent events for the notification manager
 		supportedEventTypes.forEach((eventType) => subscribe(eventType, handlers.get(eventType)));
 		// Attach handlers for logging
-		supportedEventTypes.forEach((eventType) => subscribe(eventType, createNotificationEventLogger(items)));
+		supportedEventTypes.forEach((eventType) => subscribe(eventType, createNotificationEventLogger(notificationItems)));
 
 		initialized = true;
 	}
 
 	function clearFinishedItems() {
-		items.value.filter(isFinished).forEach((item) => acknowledgeNotification(item.notificationGroupId));
-		items.value = items.value.filter((item) => !isFinished(item));
+		notificationItems.value.filter(isFinished).forEach((item) => acknowledgeNotification(item.notificationGroupId));
+		notificationItems.value = notificationItems.value.filter((item) => !isFinished(item));
 	}
 
 	function acknowledgeFinishedItems() {
-		items.value.forEach((item) => {
+		notificationItems.value.forEach((item) => {
 			if (isFinished(item)) {
 				item.acknowledged = true;
 			}
@@ -58,7 +58,7 @@ export function useNotificationManager() {
 
 	return {
 		init,
-		notificationItems: items,
+		notificationItems,
 		clearFinishedItems,
 		acknowledgeFinishedItems,
 		hasFinishedItems,
