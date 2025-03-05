aca38dc6a24a6b74016c46d81aa1dfca96b7e459
Daniel Chang <mwdchang@gmail.com>
2025-03-05 13:45:59 -0500

    simplify task plumbing

---
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/service/tasks/SimplifyModelResponseHandler.java
@@ -0,0 +1,18 @@
+package software.uncharted.terarium.hmiserver.service.tasks;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SimplifyMOdelResponseHandler extends TaskResponseHandler {
+
+	public static final String NAME = "mira_task:simplify_model";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+}
