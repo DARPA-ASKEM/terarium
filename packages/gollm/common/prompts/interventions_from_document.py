8f285f80dfd89675b5c90bc98377e3736c6e4149
Cole Blanchard <cblanchard@Coles-MacBook-Pro.local>
2025-03-11 17:09:31 -0400

    from doc

--- packages/gollm/common/prompts/interventions_from_document.py
+++ packages/gollm/common/prompts/interventions_from_document.py
@@ -32,5 +32,4 @@ For each policy described in the paper, create an interventionPolicy object. To 
         ii. `type` should be either "state" or "parameter" depending on what the intervention is applied to.
         iii. For dynamic interventions, `parameter` should be the id of a state that the threshold is applied to.
         iv. `valueType` should be either "value" or "percentage" depending on the type of intervention.
-    d. Set the `id` value to a random UUID in a format like  "00000000-0000-0000-0000-000000000000", the characters in the uuid should only be hexidecimal characters '0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F. A, B, C, D, E, F'.
 """
