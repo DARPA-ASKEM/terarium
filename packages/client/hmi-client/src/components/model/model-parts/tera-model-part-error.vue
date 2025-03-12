453ba21b700af787c294ff9b073ea3d027c680d6
Yohann Paris <github@yohannparis.com>
2025-03-12 15:28:09 -0400

    Update tera-model-part-error.vue

--- packages/client/hmi-client/src/components/model/model-parts/tera-model-part-error.vue
+++ packages/client/hmi-client/src/components/model/model-parts/tera-model-part-error.vue
@@ -1,32 +1,30 @@
 <template>
-	<section v-if="hasErrors" class="error">
-		<Button size="small" severity="danger" @click.stop="filterByType('error')">Show errors</Button>
+	<Message v-if="hasErrors" severity="error">
 		<ul>
 			<li v-for="item in errors" :key="item.id" @click.stop="filterByItem(item.id)">
 				{{ item.content }}
 			</li>
 		</ul>
-	</section>
-	<section v-if="hasWarnings" class="warning">
-		<Button size="small" severity="warning" @click.stop="filterByType('warn')">Show warnings</Button>
+	</Message>
+	<Message v-if="hasWarnings" severity="warn">
 		<ul>
 			<li v-for="item in warnings" :key="item.id" @click.stop="filterByItem(item.id)">
 				{{ item.content }}
 			</li>
 		</ul>
-	</section>
+	</Message>
 </template>
 
 <script setup lang="ts">
 import { ModelError } from '@/model-representation/service';
 import { computed } from 'vue';
-import Button from 'primevue/button';
+import Message from 'primevue/message';
 
 const props = defineProps<{
 	items: ModelError[];
 }>();
 
-const emit = defineEmits(['filter-item', 'filter-type']);
+const emit = defineEmits(['filter-item']);
 
 const warnings = computed(() => props.items.filter((item) => item.severity === 'warn'));
 const errors = computed(() => props.items.filter((item) => item.severity === 'error'));
@@ -35,27 +33,9 @@ const hasWarnings = computed(() => warnings.value.length > 0);
 const hasErrors = computed(() => errors.value.length > 0);
 
 const filterByItem = (id: string) => emit('filter-item', id);
-const filterByType = (type: string) => emit('filter-type', type);
 </script>
 
 <style scoped>
-section {
-	border-radius: var(--border-radius);
-	font-size: var(--font-body-small);
-	font-weight: normal;
-	margin-bottom: var(--gap-2);
-	padding: var(--gap-2);
-	padding-left: var(--gap-3);
-
-	& > .p-button {
-		float: right;
-	}
-
-	& + & {
-		margin-top: var(--gap-2);
-	}
-}
-
 ul {
 	list-style-type: disc;
 	padding: 0;
@@ -76,13 +56,5 @@ ul {
 			content: ': ';
 		}
 	}
-}
-
-.warning {
-	background-color: var(--surface-warning);
-}
-
-.error {
-	background-color: var(--surface-error);
 }
 </style>
