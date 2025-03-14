6ab1e88f76413876cb221d3ea93a15f8ed08ef2f
Jaehwan Ryu <jryu@uncharted.software>
2025-03-14 10:22:36 -0400

    Added file

---
+++ packages/client/hmi-client/src/components/widgets/tera-pdf-viewer.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="pdf-viewer-container">
+		<VuePDF :pdf="pdf" text-layer annotation-layer />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { VuePDF, usePDF } from '@tato30/vue-pdf';
+
+const props = defineProps<{
+	pdfLink?: string;
+	title: string;
+	filePromise?: Promise<ArrayBuffer | null>;
+}>();
+
+const { pdf } = usePDF(computed(() => props.pdfLink));
+</script>
+
+<style>
+.pdf-viewer-container {
+	position: relative;
+	width: 100%;
+	height: 100%;
+}
+.viewer-container {
+	overflow: auto;
+	position: absolute;
+	width: 100%;
+	height: 100%;
+}
+</style>
