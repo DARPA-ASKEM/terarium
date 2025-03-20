105686f30dc8b21bac2dec73ab101a4096c91618
Daniel Chang <mwdchang@gmail.com>
2025-03-20 12:11:22 -0400

    java extraction data model

---
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/extraction/ExtractionItem.java
@@ -0,0 +1,32 @@
+package software.uncharted.terarium.hmiserver.models.extraction;
+
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import java.util.List;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import software.uncharted.terarium.hmiserver.annotations.TSModel;
+
+@Slf4j
+@Entity
+@TSModel
+public class ExtractionItem {
+
+	// Identifying fields:
+	private String id;
+	private String type;
+	private String subType;
+	private String extractedBy;
+
+	// Provenance fields
+	private Long page;
+	private Double pageWidth;
+	private Double pageHeight;
+	private BBox bbox;
+	private List<Long> charspan;
+
+	// Data fiels
+	private String rawText;
+	private String text;
+}
