eb1f9a8c3c7187ea220292db5d5d052fda1bfe57
Cole Blanchard <cblanchard@Coles-MacBook-Pro.local>
2025-03-24 15:56:41 -0400

    enrichments

--- packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/enrichment/Enrichment.java
+++ packages/server/src/main/java/software/uncharted/terarium/hmiserver/models/dataservice/enrichment/Enrichment.java
@@ -3,10 +3,8 @@ package software.uncharted.terarium.hmiserver.models.dataservice.enrichment;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.util.UUID;
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 import software.uncharted.terarium.hmiserver.annotations.TSModel;
 
-@Slf4j
 @Data
 @TSModel
 public class Enrichment {
