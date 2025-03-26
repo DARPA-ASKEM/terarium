2d4d98a01b6379ecaa5176312bd9997ef4a74b49
Cole Blanchard <cblanchard@Coles-MacBook-Pro.local>
2025-03-26 09:57:14 -0400

    types

--- packages/client/hmi-client/src/types/Types.ts
+++ packages/client/hmi-client/src/types/Types.ts
@@ -911,13 +911,13 @@ export interface ProjectGroupPermission {
 }
 
 export interface IProjectUserPermissionDisplayModel {
-    user: User;
     username: string;
     email: string;
-    id: string;
+    givenName: string;
     permissionLevel: Permission;
     familyName: string;
-    givenName: string;
+    user: User;
+    id: string;
 }
 
 export interface ProvenanceNode {
