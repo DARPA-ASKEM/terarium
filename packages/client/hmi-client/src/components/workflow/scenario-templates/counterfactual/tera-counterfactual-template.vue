f417e0f5af427bc09b292b4d4f03c2db745a1867
Tom Szendrey <T.Szendrey@hotmail.com>
2025-03-31 10:45:48 -0400

    add types to computed + getting model data + add intervention

--- packages/client/hmi-client/src/components/workflow/scenario-templates/counterfactual/tera-counterfactual-template.vue
+++ packages/client/hmi-client/src/components/workflow/scenario-templates/counterfactual/tera-counterfactual-template.vue
@@ -20,6 +20,8 @@
 			<label>Select a model configuration</label>
 			<Dropdown
 				:model-value="props.scenario.getModelConfigId()"
+				:loading="isFetchingModelData"
+				:disabled="!isModelSelected"
 				:options="allModelOptions"
 				option-label="assetName"
 				option-value="assetId"
@@ -31,6 +33,7 @@
 			<label>Select a dataset</label>
 			<Dropdown
 				:model-value="props.scenario.getDatasetId()"
+				:disabled="!isModelSelected"
 				:options="allDatasetOptions"
 				option-label="assetName"
 				option-value="assetId"
@@ -38,17 +41,46 @@
 				@update:model-value="props.scenario.setDatasetId($event)"
 				class="mb-3"
 			/>
+
+			<label>Select an intervention</label>
+			<Dropdown
+				:disabled="!isModelSelected"
+				:model-value="props.scenario.getInterventionPolicyId()"
+				:options="allInterventionPolicyOptions"
+				option-label="name"
+				option-value="id"
+				placeholder="Select an intervention for this model"
+				@update:model-value="props.scenario.setInterventionPolicyId($event)"
+				class="mb-3"
+				:loading="isFetchingModelData"
+			/>
 		</template>
-		<template #outputs> </template>
+		<template #outputs>
+			<!-- <label :class="{ 'disabled-label': _.isEmpty(modelStateAndObsOptions) || isFetchingModelData }"
+				>Select an output metric</label
+			>
+			<MultiSelect
+				:disabled="_.isEmpty(modelStateAndObsOptions) || isFetchingModelData"
+				:model-value="selectedOutputSettings"
+				placeholder="Select output metrics"
+				:options="modelStateAndObsOptions"
+				@update:model-value="scenario.setOptimizeOutputSettings($event)"
+				:loading="isFetchingModelData"
+				filter
+			/>
+			<img :src="horizon" alt="Horizon scanning chart" class="" /> -->
+		</template>
 	</tera-scenario-template>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { AssetType, InterventionPolicy, Model, ModelConfiguration } from '@/types/Types';
+import { computed, ref, watch } from 'vue';
+import { AssetType, InterventionPolicy, Model, ModelConfiguration, ProjectAsset } from '@/types/Types';
 import { useProjects } from '@/composables/project';
 import Dropdown from 'primevue/dropdown';
 import _ from 'lodash';
+import { getModelConfigurationById } from '@/services/model-configurations';
+import { getInterventionPoliciesForModel, getModel, getModelConfigurationsForModel } from '@/services/model';
 import { ScenarioHeader } from '../base-scenario';
 import { CounterfactualScenario } from './counterfactual-scenario';
 import TeraScenarioTemplate from '../tera-scenario-template.vue';
@@ -56,6 +88,7 @@ import TeraScenarioTemplate from '../tera-scenario-template.vue';
 const props = defineProps<{
 	scenario: CounterfactualScenario;
 }>();
+const emit = defineEmits(['save-workflow']);
 
 const header: ScenarioHeader = Object.freeze({
 	title: 'Necessary or sufficient analysis template',
@@ -68,11 +101,11 @@ const header: ScenarioHeader = Object.freeze({
 	]
 });
 
-const allModelOptions = computed(() => useProjects().getActiveProjectAssets(AssetType.Model));
-const allDatasetOptions = computed(() => useProjects().getActiveProjectAssets(AssetType.Dataset));
-const selectedModelId = computed(() => props.scenario.getModelId());
-const selectedModelConfigurationId = computed(() => props.scenario.getModelConfigId());
-const isModelSelected = computed(() => !_.isEmpty(selectedModelId.value));
+const allModelOptions = computed<ProjectAsset[]>(() => useProjects().getActiveProjectAssets(AssetType.Model));
+const allDatasetOptions = computed<ProjectAsset[]>(() => useProjects().getActiveProjectAssets(AssetType.Dataset));
+const selectedModelId = computed<string>(() => props.scenario.getModelId());
+const selectedModelConfigurationId = computed<string>(() => props.scenario.getModelConfigId());
+const isModelSelected = computed<boolean>(() => !_.isEmpty(selectedModelId.value));
 const model = ref<Model | null>(null);
 const modelConfiguration = ref<ModelConfiguration>();
 const allModelConfigOptions = ref<ModelConfiguration[]>([]);
@@ -80,5 +113,40 @@ const allInterventionPolicyOptions = ref<InterventionPolicy[]>([]);
 const isFetchingModelData = ref<boolean>(false);
 const modelStateAndObsOptions = ref<string[]>([]);
 
-const emit = defineEmits(['save-workflow']);
+watch(
+	() => selectedModelId,
+	async () => {
+		if (selectedModelId.value) {
+			isFetchingModelData.value = true;
+			model.value = await getModel(selectedModelId.value);
+			allModelConfigOptions.value = await getModelConfigurationsForModel(selectedModelId.value);
+			allInterventionPolicyOptions.value = await getInterventionPoliciesForModel(selectedModelId.value);
+
+			// Set model state and obs:
+			if (model.value) {
+				// States:
+				const modelStates = model.value.model.states;
+				modelStates.forEach((state) => modelStateAndObsOptions.value.push(state.id));
+				// Obs:
+				const modelObs = model.value.semantics?.ode.observables;
+				if (modelObs) {
+					modelObs.forEach((obs) => modelStateAndObsOptions.value.push(obs.id));
+				}
+			}
+		}
+
+		isFetchingModelData.value = false;
+	},
+	{ deep: true }
+);
+
+watch(
+	() => selectedModelConfigurationId,
+	async () => {
+		if (selectedModelConfigurationId.value) {
+			modelConfiguration.value = await getModelConfigurationById(selectedModelConfigurationId.value);
+		}
+	},
+	{ deep: true }
+);
 </script>
